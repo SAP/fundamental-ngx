be4be7819074426ff1e6dd392dbb8c10174ab2c1
Mike O'Donnell <m.o-donnell@sap.com>
2018-06-05 11:49:47 -0600

    missed button-group and dropdown

--- src/dropdown/dropdown.module.ts
+++ src/dropdown/dropdown.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UtilsModule } from '../utils/utils.module';
-import {
-    DropdownComponent,
-    DropdownItemComponent,
-    DropdownGroupComponent,
-    DropdownControlDirective,
-    DropdownControlNoBorderDirective,
-    DropdownGroupHeaderDirective
-} from './dropdown.component';
+
+import { DropdownComponent } from './dropdown.component';
+import { DropdownItemComponent } from './dropdown-item.component';
+import { DropdownGroupComponent } from './dropdown-group.component';
+import { DropdownControlDirective } from './dropdown-control.directive';
+import { DropdownControlNoBorderDirective } from './dropdown-control-no-border.directive';
+import { DropdownGroupHeaderDirective } from './dropdown-header.directive';
 
 @NgModule({
     declarations: [
