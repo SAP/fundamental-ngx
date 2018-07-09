756c872ef3eea92df0a52fa978da14ed7dd61018
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2018-07-09 17:16:16 -0600

    docs: fix indentation

--- docs/modules/documentation/containers/side-navigation/side-navigation-docs.component.ts
+++ docs/modules/documentation/containers/side-navigation/side-navigation-docs.component.ts
@@ -6,158 +6,158 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavigationDocsComponent implements OnInit {
     oneLevelSideNavHtml = `<fd-side-nav>
-    <fd-side-nav-list>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-        </fd-side-nav-item>
-    </fd-side-nav-list>
+  <fd-side-nav-list>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+    </fd-side-nav-item>
+  </fd-side-nav-list>
 </fd-side-nav>`;
     titlesSideNavHtml = `<fd-side-nav>
-    <fd-side-nav-group>
-        <fd-side-nav-title>
-        Group Title
-        </fd-side-nav-title>
-        <fd-side-nav-list>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-        </fd-side-nav-list>
-    </fd-side-nav-group>
-    <fd-side-nav-group>
-        <fd-side-nav-title>
-        Group Title
-        </fd-side-nav-title>
-        <fd-side-nav-list>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-            <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-            </fd-side-nav-item>
-        </fd-side-nav-list>
-    </fd-side-nav-group>
+  <fd-side-nav-group>
+    <fd-side-nav-title>
+      Group Title
+    </fd-side-nav-title>
+    <fd-side-nav-list>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+    </fd-side-nav-list>
+  </fd-side-nav-group>
+  <fd-side-nav-group>
+    <fd-side-nav-title>
+      Group Title
+    </fd-side-nav-title>
+    <fd-side-nav-list>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+    </fd-side-nav-list>
+  </fd-side-nav-group>
 </fd-side-nav>`;
     multiLevelsSideNavHtml = `<fd-side-nav>
-    <fd-side-nav-group>
-        <fd-side-nav-title>
-        Group Name
-        </fd-side-nav-title>
-        <fd-side-nav-list>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
-        </fd-side-nav-item>
-        </fd-side-nav-list>
-    </fd-side-nav-group>
-    <fd-side-nav-group>
-        <fd-side-nav-title>
-        Group Name
-        </fd-side-nav-title>
-        <fd-side-nav-list>
-        <fd-side-nav-item>
-            <fd-side-nav-link [hasSublist]="true">
-            Link Item
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [hasSublist]="true">
-            Link Item
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [hasSublist]="true">
-            Link Item
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        </fd-side-nav-list>
-    </fd-side-nav-group>
+  <fd-side-nav-group>
+    <fd-side-nav-title>
+      Group Name
+    </fd-side-nav-title>
+    <fd-side-nav-list>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [url]="'#'">Link Item</fd-side-nav-link>
+      </fd-side-nav-item>
+    </fd-side-nav-list>
+  </fd-side-nav-group>
+  <fd-side-nav-group>
+    <fd-side-nav-title>
+      Group Name
+    </fd-side-nav-title>
+    <fd-side-nav-list>
+      <fd-side-nav-item>
+        <fd-side-nav-link [hasSublist]="true">
+          Link Item
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+        </fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [hasSublist]="true">
+          Link Item
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+        </fd-side-nav-link>
+      </fd-side-nav-item>
+      <fd-side-nav-item>
+        <fd-side-nav-link [hasSublist]="true">
+          Link Item
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+          <fd-side-nav-subitem [url]="'#'">Link Item</fd-side-nav-subitem>
+        </fd-side-nav-link>
+      </fd-side-nav-item>
+    </fd-side-nav-list>
+  </fd-side-nav-group>
 </fd-side-nav>`;
     iconsSideNavHtml = `<fd-side-nav>
-    <fd-side-nav-list>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">
-                <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-                Link Icon
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">
-                <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-                Link Icon
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">
-                <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-                Link Icon
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">
-                <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-                Link Icon
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-    </fd-side-nav-list>
+  <fd-side-nav-list>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+        Link Icon
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+        Link Icon
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+        Link Icon
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+        Link Icon
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+  </fd-side-nav-list>
 </fd-side-nav>`;
     collapsedSideNavHtml = `<fd-side-nav [collapsed]="true">
-    <fd-side-nav-list>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">
-                <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-                <fd-side-nav-link [url]="'#'">
-                    <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-                </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">
-                <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-        <fd-side-nav-item>
-            <fd-side-nav-link [url]="'#'">
-                <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
-            </fd-side-nav-link>
-        </fd-side-nav-item>
-    </fd-side-nav-list>
+  <fd-side-nav-list>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+    <fd-side-nav-item>
+      <fd-side-nav-link [url]="'#'">
+        <fd-side-nav-icon [glyph]="'home'" [size]="'l'"></fd-side-nav-icon>
+      </fd-side-nav-link>
+    </fd-side-nav-item>
+  </fd-side-nav-list>
 </fd-side-nav>`;
 
     constructor() {}
