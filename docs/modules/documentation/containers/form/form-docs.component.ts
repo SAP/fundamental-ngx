e5ff9d828d1cbc3ada6d3a742a735170d0551836
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-07-09 14:39:20 -0400

    Minor changes for the id-s of the form-items

--- docs/modules/documentation/containers/form/form-docs.component.ts
+++ docs/modules/documentation/containers/form/form-docs.component.ts
@@ -77,25 +77,25 @@ export class FormDocsComponent implements OnInit {
 </fd-form-item>
 
 <fd-form-item>
-    <fd-form-label [forValue]="'input-53'">
+    <fd-form-label [forValue]="'input-54'">
         Warning Input
     </fd-form-label>
-    <input fd-form-control [state]="'warning'" type="text" id="input-53" placeholder="Field placeholder text">
+    <input fd-form-control [state]="'warning'" type="text" id="input-54" placeholder="Field placeholder text">
     <fd-form-message [type]="'warning'">Pellentesque metus lacus commodo eget justo ut rutrum varius nunc</fd-form-message>
 </fd-form-item>
 
 <fd-form-item>
-    <fd-form-label [forValue]="'input-53'">
+    <fd-form-label [forValue]="'input-55'">
         Disables Input
     </fd-form-label>
-    <input fd-form-control type="text" id="input-53" placeholder="Field placeholder text" disabled>
+    <input fd-form-control type="text" id="input-55" placeholder="Field placeholder text" disabled>
 </fd-form-item>
 
 <fd-form-item>
-    <fd-form-label [forValue]="'input-53'">
+    <fd-form-label [forValue]="'input-56'">
         Readonly Input
     </fd-form-label>
-    <input fd-form-control type="text" id="input-53" placeholder="Field placeholder text" readonly>
+    <input fd-form-control type="text" id="input-56" placeholder="Field placeholder text" readonly>
 </fd-form-item>`;
 
     selectFormHtml = `<div fd-form-set>
@@ -113,10 +113,10 @@ export class FormDocsComponent implements OnInit {
 
 <div fd-form-set>
     <fd-form-item>
-        <fd-form-label [forValue]="'select-1'">
+        <fd-form-label [forValue]="'select-2'">
             Disabled Select
         </fd-form-label>
-        <select fd-form-control id="select-1" name="" disabled>
+        <select fd-form-control id="select-2" name="" disabled>
             <option value="1">Duis malesuada odio volutpat elementum</option>
             <option value="2">Suspendisse ante ligula</option>
             <option value="3">Sed bibendum sapien at posuere interdum</option>
@@ -146,16 +146,16 @@ export class FormDocsComponent implements OnInit {
     <fd-form-legend>Radio buttons</fd-form-legend>
     <fd-form-group>
         <fd-form-item [isCheck]="true">
-            <input fd-form-control type="radio" id="radio-1" name="radio-name-2" value="" checked disabled>
-            <fd-form-label [forValue]="'radio-1'">Option One</fd-form-label>
+            <input fd-form-control type="radio" id="radio-4" name="radio-name-2" value="" checked disabled>
+            <fd-form-label [forValue]="'radio-4'">Option One</fd-form-label>
         </fd-form-item>
         <fd-form-item [isCheck]="true">
-            <input fd-form-control type="radio" id="radio-2" name="radio-name-2" value="" disabled>
-            <fd-form-label [forValue]="'radio-2'">Option Two</fd-form-label>
+            <input fd-form-control type="radio" id="radio-5" name="radio-name-2" value="" disabled>
+            <fd-form-label [forValue]="'radio-5'">Option Two</fd-form-label>
         </fd-form-item>
         <fd-form-item [isCheck]="true">
-            <input fd-form-control type="radio" id="radio-3" name="radio-name-2" value="" disabled>
-            <fd-form-label [forValue]="'radio-3'">Option Three</fd-form-label>
+            <input fd-form-control type="radio" id="radio-6" name="radio-name-2" value="" disabled>
+            <fd-form-label [forValue]="'radio-6'">Option Three</fd-form-label>
         </fd-form-item>
     </fd-form-group>
 </fieldset>
@@ -164,20 +164,20 @@ export class FormDocsComponent implements OnInit {
     <fd-form-legend>Inline Radio buttons</fd-form-legend>
     <fd-form-group>
         <fd-form-item [isInline]="true" [isCheck]="true">
-            <fd-form-label [forValue]="'radio-4'">
-                <input fd-form-control type="radio" id="radio-4" name="radio-name-3" value="" checked> 
+            <fd-form-label [forValue]="'radio-7'">
+                <input fd-form-control type="radio" id="radio-7" name="radio-name-3" value="" checked> 
                 Option One
             </fd-form-label>
         </fd-form-item>
         <fd-form-item [isInline]="true" [isCheck]="true">
-            <fd-form-label [forValue]="'radio-5'">
-                <input fd-form-control type="radio" id="radio-5" name="radio-name-3" value=""> 
+            <fd-form-label [forValue]="'radio-8'">
+                <input fd-form-control type="radio" id="radio-8" name="radio-name-3" value=""> 
                 Option Two
             </fd-form-label>
         </fd-form-item>
         <fd-form-item [isInline]="true" [isCheck]="true">
-            <fd-form-label [forValue]="'radio-6'">
-                <input fd-form-control type="radio" id="radio-6" name="radio-name-3" value=""> 
+            <fd-form-label [forValue]="'radio-9'">
+                <input fd-form-control type="radio" id="radio-9" name="radio-name-3" value=""> 
                 Option Three
             </fd-form-label>
         </fd-form-item>
@@ -203,16 +203,16 @@ export class FormDocsComponent implements OnInit {
 <fieldset fd-form-set>
     <fd-form-legend>Checkboxes disabled</fd-form-legend>
     <fd-form-item [isCheck]="true">
-        <input fd-form-control type="checkbox" id="checkbox-1" name="checkbox-name-1" disabled checked>
-        <fd-form-label [forValue]="'checkbox-1'">Option One</fd-form-label>
+        <input fd-form-control type="checkbox" id="checkbox-4" name="checkbox-name-2" disabled checked>
+        <fd-form-label [forValue]="'checkbox-4'">Option One</fd-form-label>
     </fd-form-item>
     <fd-form-item [isCheck]="true">
-        <input fd-form-control type="checkbox" id="checkbox-2" name="checkbox-name-1" disabled>
-        <fd-form-label [forValue]="'checkbox-2'">Option Two</fd-form-label>
+        <input fd-form-control type="checkbox" id="checkbox-5" name="checkbox-name-2" disabled>
+        <fd-form-label [forValue]="'checkbox-5'">Option Two</fd-form-label>
     </fd-form-item>
     <fd-form-item [isCheck]="true">
-        <input fd-form-control type="checkbox" id="checkbox-3" name="checkbox-name-1" disabled>
-        <fd-form-label [forValue]="'checkbox-3'">Option Three</fd-form-label>
+        <input fd-form-control type="checkbox" id="checkbox-6" name="checkbox-name-2" disabled>
+        <fd-form-label [forValue]="'checkbox-6'">Option Three</fd-form-label>
     </fd-form-item>
 </fieldset>
 
@@ -220,20 +220,20 @@ export class FormDocsComponent implements OnInit {
     <fd-form-legend>Checkboxes inline</fd-form-legend>
     <fd-form-group>
         <fd-form-item [isInline]="true" [isCheck]="true">
-            <fd-form-label [forValue]="'checkbox-1'">
-                <input fd-form-control type="checkbox" id="checkbox-1" name="checkbox-name-1" checked> 
+            <fd-form-label [forValue]="'checkbox-7'">
+                <input fd-form-control type="checkbox" id="checkbox-7" name="checkbox-name-3" checked> 
                 Option One
             </fd-form-label>
         </fd-form-item>
         <fd-form-item [isInline]="true" [isCheck]="true">
-            <fd-form-label [forValue]="'checkbox-2'">
-                <input fd-form-control type="checkbox" id="checkbox-2" name="checkbox-name-1"> 
+            <fd-form-label [forValue]="'checkbox-8'">
+                <input fd-form-control type="checkbox" id="checkbox-8" name="checkbox-name-3"> 
                 Option Two
             </fd-form-label>
         </fd-form-item>
         <fd-form-item [isInline]="true" [isCheck]="true">
-            <fd-form-label [forValue]="'checkbox-3'">
-                <input fd-form-control type="checkbox" id="checkbox-3" name="checkbox-name-1"> 
+            <fd-form-label [forValue]="'checkbox-9'">
+                <input fd-form-control type="checkbox" id="checkbox-9" name="checkbox-name-3"> 
                 Option Three
             </fd-form-label>
         </fd-form-item>
