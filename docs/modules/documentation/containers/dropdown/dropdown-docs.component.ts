6c9be1c35eee9ab1ca33f879d3e3cda816bbb7c5
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2018-07-31 11:11:26 -0600

    use multiline strings for example code

--- docs/modules/documentation/containers/dropdown/dropdown-docs.component.ts
+++ docs/modules/documentation/containers/dropdown/dropdown-docs.component.ts
@@ -5,241 +5,236 @@ import { Component } from '@angular/core';
     templateUrl: './dropdown-docs.component.html'
 })
 export class DropdownDocsComponent {
-    textDropdownHtml =
-        '<fd-popover [isDropdown]="true">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '      <fd-menu-group>\n' +
-        '        <fd-menu-title>\n' +
-        '          Group header\n' +
-        '        </fd-menu-title>\n' +
-        '        <fd-menu-list>\n' +
-        '          <fd-menu-item [url]="\'#\'">\n' +
-        '            Option 4\n' +
-        '          </fd-menu-item>\n' +
-        '          <fd-menu-item [url]="\'#\'">\n' +
-        '            Option 5\n' +
-        '          </fd-menu-item>\n' +
-        '          <fd-menu-item [url]="\'#\'">\n' +
-        '            Option 6\n' +
-        '          </fd-menu-item>\n' +
-        '        </fd-menu-list>\n' +
-        '      </fd-menu-group>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>';
+    textDropdownHtml = `<fd-popover [isDropdown]="true">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+      <fd-menu-group>
+        <fd-menu-title>
+          Group header
+        </fd-menu-title>
+        <fd-menu-list>
+          <fd-menu-item [url]="'#'">
+            Option 4
+          </fd-menu-item>
+          <fd-menu-item [url]="'#'">
+            Option 5
+          </fd-menu-item>
+          <fd-menu-item [url]="'#'">
+            Option 6
+          </fd-menu-item>
+        </fd-menu-list>
+      </fd-menu-group>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>`;
 
-    disabledDropdownHtml =
-        '<fd-popover [isDropdown]="true" [disabled]="true">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>';
+    disabledDropdownHtml = `<fd-popover [isDropdown]="true" [disabled]="true">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>`;
 
-    iconDropdownHtml =
-        '<fd-popover [isDropdown]="true" [glyph]="\'filter\'">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>';
+    iconDropdownHtml = `<fd-popover [isDropdown]="true" [glyph]="'filter'">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>`;
 
-    dropdownSizeHtml =
-        '<fd-popover [isDropdown]="true" [size]="\'xs\'">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>\n' +
-        '\n' +
-        '<fd-popover [isDropdown]="true" [size]="\'s\'">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>\n' +
-        '\n' +
-        '<fd-popover [isDropdown]="true" [size]="\'compact\'">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>\n' +
-        '\n' +
-        '<fd-popover [isDropdown]="true">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>\n' +
-        '\n' +
-        '<fd-popover [isDropdown]="true" [size]="\'l\'">\n' +
-        '  Dropdown\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>';
+    dropdownSizeHtml = `<fd-popover [isDropdown]="true" [size]="'xs'">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>
 
-    contextualMenuDropdownHtml =
-        '<fd-popover>\n' +
-        '  <fd-popover-control>\n' +
-        '    <button fd-button [type]="\'secondary\'" [glyph]="\'vertical-grip\'"></button>\n' +
-        '  </fd-popover-control>\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>\n' +
-        '\n' +
-        '<fd-popover>\n' +
-        '  <fd-popover-control>\n' +
-        '    <button fd-button>More</button>\n' +
-        '  </fd-popover-control>\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>\n' +
-        '\n' +
-        '<fd-popover>\n' +
-        '  <fd-popover-control>\n' +
-        '    <button fd-button [type]="\'secondary\'">More</button>\n' +
-        '  </fd-popover-control>\n' +
-        '  <fd-popover-body>\n' +
-        '    <fd-menu>\n' +
-        '      <fd-menu-list>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 1\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 2\n' +
-        '        </fd-menu-item>\n' +
-        '        <fd-menu-item [url]="\'#\'">\n' +
-        '          Option 3\n' +
-        '        </fd-menu-item>\n' +
-        '      </fd-menu-list>\n' +
-        '    </fd-menu>\n' +
-        '  </fd-popover-body>\n' +
-        '</fd-popover>';
+<fd-popover [isDropdown]="true" [size]="'s'">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>
+
+<fd-popover [isDropdown]="true" [size]="'compact'">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>
+
+<fd-popover [isDropdown]="true">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>
+
+<fd-popover [isDropdown]="true" [size]="'l'">
+  Dropdown
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>`;
+
+    contextualMenuDropdownHtml = `<fd-popover>
+  <fd-popover-control>
+    <button fd-button [type]="'secondary'" [glyph]="'vertical-grip'"></button>
+  </fd-popover-control>
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>
+
+<fd-popover>
+  <fd-popover-control>
+    <button fd-button>More</button>
+  </fd-popover-control>
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>
+
+<fd-popover>
+  <fd-popover-control>
+    <button fd-button [type]="'secondary'">More</button>
+  </fd-popover-control>
+  <fd-popover-body>
+    <fd-menu>
+      <fd-menu-list>
+        <fd-menu-item [url]="'#'">
+          Option 1
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 2
+        </fd-menu-item>
+        <fd-menu-item [url]="'#'">
+          Option 3
+        </fd-menu-item>
+      </fd-menu-list>
+    </fd-menu>
+  </fd-popover-body>
+</fd-popover>`;
 
     constructor() {}
 }
