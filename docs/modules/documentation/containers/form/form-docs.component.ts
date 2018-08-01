430d2bef3e64ecf205182430f5cc94a0d37e28dd
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-08-01 14:22:42 -0400

    Issue#133 fixes the binding syntax inconsistency in documentation

--- docs/modules/documentation/containers/form/form-docs.component.ts
+++ docs/modules/documentation/containers/form/form-docs.component.ts
@@ -7,32 +7,32 @@ import { Component } from '@angular/core';
 export class FormDocsComponent {
     inputsFormHtml = `<div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="input-1">Default Input</fd-form-label>
+    <fd-form-label [forValue]="'input-1'">Default Input</fd-form-label>
     <input fd-form-control [type]="'text'" id="input-1" placeholder="Field placeholder text">
   </div>
 </div>
 <div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="input-2" [isRequired]="true">Required Input*</fd-form-label>
+    <fd-form-label [forValue]="'input-2'" [isRequired]="true">Required Input*</fd-form-label>
     <input fd-form-control [type]="'text'" id="input-2" placeholder="Field placeholder text">
   </div>
 </div>
 <div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="input-3" [isRequired]="true">Password*</fd-form-label>
+    <fd-form-label [forValue]="'input-3'" [isRequired]="true">Password*</fd-form-label>
     <input fd-form-control [type]="'password'" id="input-3" placeholder="Field placeholder text">
   </div>
 </div>
 <div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="textarea-1">Text area</fd-form-label>
+    <fd-form-label [forValue]="'textarea-1'">Text area</fd-form-label>
     <textarea fd-form-control id="textarea-1">Pellentesque metus lacus commodo eget justo ut rutrum varius nunc.</textarea>
   </div>
 </div>`;
 
     inputsHelpFormHtml = `<div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="input-41">
+    <fd-form-label [forValue]="'input-41'">
       Input with inline help
       <span class="fd-inline-help fd-has-float-right">
         <span class="fd-inline-help__content fd-inline-help__content--bottom-right">
@@ -45,7 +45,7 @@ export class FormDocsComponent {
 </div>
 <div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="input-42">
+    <fd-form-label [forValue]="'input-42'">
       Input with help message
     </fd-form-label>
     <input fd-form-control [type]="'text'" id="input-42">
@@ -54,40 +54,40 @@ export class FormDocsComponent {
 </div>`;
 
     inputStatesFormHtml = `<div fd-form-item>
-  <fd-form-label forValue="input-51">
+  <fd-form-label [forValue]="'input-51'">
     Normal Input
   </fd-form-label>
   <input fd-form-control [type]="'text'" id="input-51" placeholder="Field placeholder text">
   <fd-form-message>Pellentesque metus lacus commodo eget justo ut rutrum varius nunc</fd-form-message>
 </div>
 <div fd-form-item>
-  <fd-form-label forValue="input-52">
+  <fd-form-label [forValue]="'input-52'">
     Valid Input
   </fd-form-label>
   <input fd-form-control [state]="'valid'" [type]="'text'" id="input-52">
 </div>
 <div fd-form-item>
-  <fd-form-label forValue="input-53">
+  <fd-form-label [forValue]="'input-53'">
     Invalid Input
   </fd-form-label>
   <input fd-form-control [state]="'invalid'" [type]="'text'" id="input-53" placeholder="Field placeholder text">
   <fd-form-message [type]="'error'">Pellentesque metus lacus commodo eget justo ut rutrum varius nunc</fd-form-message>
 </div>
 <div fd-form-item>
-  <fd-form-label forValue="input-54">
+  <fd-form-label [forValue]="'input-54'">
     Warning Input
   </fd-form-label>
   <input fd-form-control [state]="'warning'" [type]="'text'" id="input-54" placeholder="Field placeholder text">
   <fd-form-message [type]="'warning'">Pellentesque metus lacus commodo eget justo ut rutrum varius nunc</fd-form-message>
 </div>
 <div fd-form-item>
-  <fd-form-label forValue="input-55">
+  <fd-form-label [forValue]="'input-55'">
     Disables Input
   </fd-form-label>
   <input fd-form-control [type]="'text'" id="input-55" placeholder="Field placeholder text" disabled>
 </div>
 <div fd-form-item>
-  <fd-form-label forValue="input-56">
+  <fd-form-label [forValue]="'input-56'">
     Readonly Input
   </fd-form-label>
   <input fd-form-control [type]="'text'" id="input-56" placeholder="Field placeholder text" readonly>
@@ -95,7 +95,7 @@ export class FormDocsComponent {
 
     selectFormHtml = `<div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="select-1">
+    <fd-form-label [forValue]="'select-1'">
       Default Select
     </fd-form-label>
     <select fd-form-control id="select-1" name="">
@@ -107,7 +107,7 @@ export class FormDocsComponent {
 </div>
 <div fd-form-set>
   <div fd-form-item>
-    <fd-form-label forValue="select-2">
+    <fd-form-label [forValue]="'select-2'">
       Disabled Select
     </fd-form-label>
     <select fd-form-control id="select-2" name="" disabled>
@@ -123,15 +123,15 @@ export class FormDocsComponent {
   <fd-form-group>
     <div fd-form-item [isCheck]="true">
       <input fd-form-control [type]="'radio'" id="radio-1" name="radio-name-1" value="" checked>
-      <fd-form-label forValue="radio-1">Option One</fd-form-label>
+      <fd-form-label [forValue]="'radio-1'">Option One</fd-form-label>
     </div>
     <div fd-form-item [isCheck]="true">
       <input fd-form-control [type]="'radio'" id="radio-2" name="radio-name-1" value="">
-      <fd-form-label forValue="radio-2">Option Two</fd-form-label>
+      <fd-form-label [forValue]="'radio-2'">Option Two</fd-form-label>
     </div>
     <div fd-form-item [isCheck]="true">
       <input fd-form-control [type]="'radio'" id="radio-3" name="radio-name-1" value="">
-      <fd-form-label forValue="radio-3">Option Three</fd-form-label>
+      <fd-form-label [forValue]="'radio-3'">Option Three</fd-form-label>
     </div>
   </fd-form-group>
 </fieldset>
@@ -141,15 +141,15 @@ export class FormDocsComponent {
   <fd-form-group>
     <div fd-form-item [isCheck]="true">
       <input fd-form-control [type]="'radio'" id="radio-4" name="radio-name-2" value="" checked disabled>
-      <fd-form-label forValue="radio-4">Option One</fd-form-label>
+      <fd-form-label [forValue]="'radio-4'">Option One</fd-form-label>
     </div>
     <div fd-form-item [isCheck]="true">
       <input fd-form-control [type]="'radio'" id="radio-5" name="radio-name-2" value="" disabled>
-      <fd-form-label forValue="radio-5">Option Two</fd-form-label>
+      <fd-form-label [forValue]="'radio-5'">Option Two</fd-form-label>
     </div>
     <div fd-form-item [isCheck]="true">
       <input fd-form-control [type]="'radio'" id="radio-6" name="radio-name-2" value="" disabled>
-      <fd-form-label forValue="radio-6">Option Three</fd-form-label>
+      <fd-form-label [forValue]="'radio-6'">Option Three</fd-form-label>
     </div>
   </fd-form-group>
 </fieldset>
@@ -158,17 +158,17 @@ export class FormDocsComponent {
   <fd-form-legend>Inline Radio buttons</fd-form-legend>
   <fd-form-group>
     <div fd-form-item [isInline]="true" [isCheck]="true">
-      <fd-form-label forValue="radio-7">
+      <fd-form-label [forValue]="'radio-7'">
         <input fd-form-control [type]="'radio'" id="radio-7" name="radio-name-3" value="" checked> Option One
       </fd-form-label>
     </div>
     <div fd-form-item [isInline]="true" [isCheck]="true">
-      <fd-form-label forValue="radio-8">
+      <fd-form-label [forValue]="'radio-8'">
         <input fd-form-control [type]="'radio'" id="radio-8" name="radio-name-3" value=""> Option Two
       </fd-form-label>
     </div>
     <div fd-form-item [isInline]="true" [isCheck]="true">
-      <fd-form-label forValue="radio-9">
+      <fd-form-label [forValue]="'radio-9'">
         <input fd-form-control [type]="'radio'" id="radio-9" name="radio-name-3" value=""> Option Three
       </fd-form-label>
     </div>
@@ -179,15 +179,15 @@ export class FormDocsComponent {
   <fd-form-legend>Checkboxes</fd-form-legend>
   <div fd-form-item [isCheck]="true">
     <input fd-form-control [type]="'checkbox'" id="checkbox-1" name="checkbox-name-1" checked>
-    <fd-form-label forValue="checkbox-1">Option One</fd-form-label>
+    <fd-form-label [forValue]="'checkbox-1'">Option One</fd-form-label>
   </div>
   <div fd-form-item [isCheck]="true">
     <input fd-form-control [type]="'checkbox'" id="checkbox-2" name="checkbox-name-1">
-    <fd-form-label forValue="checkbox-2">Option Two</fd-form-label>
+    <fd-form-label [forValue]="'checkbox-2'">Option Two</fd-form-label>
   </div>
   <div fd-form-item [isCheck]="true">
     <input fd-form-control [type]="'checkbox'" id="checkbox-3" name="checkbox-name-1">
-    <fd-form-label forValue="checkbox-3">Option Three</fd-form-label>
+    <fd-form-label [forValue]="'checkbox-3'">Option Three</fd-form-label>
   </div>
 </fieldset>
 
@@ -195,21 +195,21 @@ export class FormDocsComponent {
   <fd-form-legend>Checkboxes disabled</fd-form-legend>
   <div fd-form-item [isCheck]="true">
     <input fd-form-control [type]="'checkbox'" id="checkbox-4" name="checkbox-name-2" disabled>
-    <fd-form-label forValue="checkbox-4">Option One</fd-form-label>
+    <fd-form-label [forValue]="'checkbox-4'">Option One</fd-form-label>
   </div>
   <div fd-form-item [isCheck]="true">
     <input fd-form-control [type]="'checkbox'" id="checkbox-5" name="checkbox-name-2" checked disabled>
-    <fd-form-label forValue="checkbox-5">Option Two</fd-form-label>
+    <fd-form-label [forValue]="'checkbox-5'">Option Two</fd-form-label>
   </div>
   <div fd-form-item [isCheck]="true">
     <input fd-form-control [type]="'checkbox'" id="checkbox-6" name="checkbox-name-2" disabled>
-    <fd-form-label forValue="checkbox-6">Option Three</fd-form-label>
+    <fd-form-label [forValue]="'checkbox-6'">Option Three</fd-form-label>
   </div>
 </fieldset>
 
 <div fd-form-set>
     <div fd-form-item>
-        <fd-form-label forValue="input-21" [isRequired]="true">Required Input*</fd-form-label>
+        <fd-form-label [forValue]="'input-21'" [isRequired]="true">Required Input*</fd-form-label>
         <input fd-form-control [type]="'text'" id="input-21" placeholder="Field placeholder text">
     </div>
 </div>
@@ -218,17 +218,17 @@ export class FormDocsComponent {
   <fd-form-legend>Checkboxes inline</fd-form-legend>
   <fd-form-group>
     <div fd-form-item [isInline]="true" [isCheck]="true">
-      <fd-form-label forValue="checkbox-7">
+      <fd-form-label [forValue]="'checkbox-7'">
         <input fd-form-control [type]="'checkbox'" id="checkbox-7" name="checkbox-name-3" checked> Option One
       </fd-form-label>
     </div>
     <div fd-form-item [isInline]="true" [isCheck]="true">
-      <fd-form-label forValue="checkbox-8">
+      <fd-form-label [forValue]="'checkbox-8'">
         <input fd-form-control [type]="'checkbox'" id="checkbox-8" name="checkbox-name-3"> Option Two
       </fd-form-label>
     </div>
     <div fd-form-item [isInline]="true" [isCheck]="true">
-      <fd-form-label forValue="checkbox-9">
+      <fd-form-label [forValue]="'checkbox-9'">
         <input fd-form-control [type]="'checkbox'" id="checkbox-9" name="checkbox-name-3"> Option Three
       </fd-form-label>
     </div>
--- docs/modules/documentation/containers/modal/modal-docs.component.ts
+++ docs/modules/documentation/containers/modal/modal-docs.component.ts
@@ -50,75 +50,54 @@ export class ModalDocsComponent implements OnInit {
 
     confirmationReason: string;
 
-    informationalModalHtml =
-        '<ng-template #informationalModal>\n' +
-        '  <fd-modal>\n' +
-        '    <fd-modal-header>\n' +
-        '      Modal Header/Title\n' +
-        '    </fd-modal-header>\n' +
-        '    <fd-modal-body>\n' +
-        '      Modal Body\n' +
-        '    </fd-modal-body>\n' +
-        '  </fd-modal>\n' +
-        '</ng-template>\n' +
-        '<button fd-button (click)="openInfoModal(informationalModal)">Launch Demo</button>';
+    informationalModalHtml =`<ng-template #informationalModal>
+    <fd-modal>
+      <fd-modal-header>
+        Modal Header/Title
+      </fd-modal-header>
+      <fd-modal-body>
+        Modal Body
+      </fd-modal-body>
+    </fd-modal>
+</ng-template>
+<button fd-button (click)="openInfoModal(informationalModal)">Launch Demo</button>`;
 
-    confirmationModalHtml =
-        '<ng-template #confirmationModal let-c="close">\n' +
-        '  <fd-modal>\n' +
-        '    <fd-modal-header>\n' +
-        '      Modal Header/Title\n' +
-        '    </fd-modal-header>\n' +
-        '    <fd-modal-body>\n' +
-        '      Modal Body\n' +
-        '    </fd-modal-body>\n' +
-        '    <fd-modal-footer>\n' +
-        '      <button fd-button (click)="c(\'No\')" fdType="secondary">No</button>\n' +
-        '      <button fd-button (click)="c(\'Yes\')" fdType="main">Yes</button>\n' +
-        '    </fd-modal-footer>\n' +
-        '  </fd-modal>\n' +
-        '</ng-template>\n' +
-        '<button fd-button (click)="openConfirmationModal(confirmationModal)">Launch Demo</button>\n' +
-        '<span>{{confirmationReason}}</span>';
+    confirmationModalHtml =`<ng-template #confirmationModal let-c="close">
+    <fd-modal>
+      <fd-modal-header>
+        Modal Header/Title
+      </fd-modal-header>
+      <fd-modal-body>
+        Modal Body
+      </fd-modal-body>
+      <fd-modal-footer>
+        <button fd-button (click)="c('No')" [fdType]="'secondary'">No</button>
+        <button fd-button (click)="c('Yes')" [fdType]="'main'">Yes</button>
+      </fd-modal-footer>
+    </fd-modal>
+</ng-template>
+<button fd-button (click)="openConfirmationModal(confirmationModal)">Launch Demo</button>`;
 
-    formModalHtml =
-        '<ng-template #formModal>\n' +
-        '  <fd-modal>\n' +
-        '    <fd-modal-header>\n' +
-        '      Modal Header/Title\n' +
-        '    </fd-modal-header>\n' +
-        '    <fd-modal-body>\n' +
-        '      <div class="fd-form__group">\n' +
-        '        <div class="fd-form__item">\n' +
-        '          <label class="fd-form__label is-required" for="input-2">Email*</label>\n' +
-        '          <input class="fd-form__control" type="text" id="input-2">\n' +
-        '        </div>\n' +
-        '      </div>\n' +
-        '    </fd-modal-body>\n' +
-        '    <fd-modal-footer>\n' +
-        '      <button fd-button fdType="secondary">Cancel</button>\n' +
-        '      <button fd-button fdType="main">Invite</button>\n' +
-        '    </fd-modal-footer>\n' +
-        '  </fd-modal>\n' +
-        '</ng-template>';
+    formModalHtml =``;
 
-    informationalModalJs = 'openModal(modalType) {\n' + '    this.modalService.open(modalType);\n' + '}';
+    informationalModalJs = `openInfoModal(modalType) {
+    this.modalService.open(modalType);
+}`;
 
-    confirmationModalJs =
-        'openConfirmationModal(modalType) {\n' +
-        '    this.modalService.open(modalType).result.then(\n' +
-        '        result => {\n' +
-        '            if (result === "Yes") {\n' +
-        '                this.confirmationReason = \'Modal closed with "Yes" button\';\n' +
-        '            } else if (result === "No") {\n' +
-        '                this.confirmationReason = \'Modal closed with "No" button\';\n' +
-        '            }\n' +
-        '        },\n' +
-        '        () => {\n' +
-        '            this.confirmationReason = \'Modal dismissed with the "X" button\';\n' +
-        '        }\n' +
-        '    );\n' +
-        '}';
+    confirmationModalJs =`openConfirmationModal(modalType) {
+    this.modalService.open(modalType).result.then(
+        result => {
+            if (result === 'Yes') {
+                this.confirmationReason = 'Modal closed with "Yes" button';
+            } else if (result === 'No') {
+                this.confirmationReason = 'Modal closed with "No" button';
+            }
+        },
+        () => {
+            this.confirmationReason = 'Modal dismissed with the "X" button';
+        }
+    );
+}`;
 
     constructor(private schemaFactory: SchemaFactoryService, private modalService: ModalService) {
         this.schema = this.schemaFactory.getComponent('modal');
