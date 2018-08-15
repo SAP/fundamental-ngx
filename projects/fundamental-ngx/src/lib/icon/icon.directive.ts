5921578f464a881f22d13d2ff47f704d15a2ebd2
Hammad Syed <hammad.syed@sap.com>
2018-08-15 12:45:28 -0700

    Update Icon component to support Glyph groups
    Update Icon Component add support to take new parameter for Glyph group other then SAP-Icon, following the same convention of "<glyph-group>-icon--".
    Add Ariba font-Icon SCSS to documentation style.

--- projects/fundamental-ngx/src/lib/icon/icon.directive.ts
+++ projects/fundamental-ngx/src/lib/icon/icon.directive.ts
@@ -2,10 +2,10 @@ import { Directive, Input, ElementRef, Inject } from '@angular/core';
 import { AbstractFdNgxClass } from '../utils/abstract-fd-ngx-class';
 
 /** The base class for the icon component */
-const BASE_ICON_CLASS = 'sap-icon';
+//const BASE_ICON_CLASS = 'sap-icon';
 
 /** Prefix for icon prop classes */
-const PREFIX_ICON_CLASS = BASE_ICON_CLASS + '--';
+//const PREFIX_ICON_CLASS = BASE_ICON_CLASS + '--';
 
 @Directive({
     selector: 'fd-icon',
@@ -16,15 +16,20 @@ const PREFIX_ICON_CLASS = BASE_ICON_CLASS + '--';
 export class IconDirective extends AbstractFdNgxClass {
     @Input() glyph;
 
+    @Input() glyphGroup: 'sap' | 'ariba' = 'sap';
+
     @Input() size: string = '';
 
     _setProperties() {
+
+        let prefixClass = this.glyphGroup + '-icon--';
+
         if (this.glyph) {
-            this._addClassToElement(PREFIX_ICON_CLASS + this.glyph);
+            this._addClassToElement(prefixClass + this.glyph);
         }
 
         if (this.size) {
-            this._addClassToElement(PREFIX_ICON_CLASS + this.size);
+            this._addClassToElement(prefixClass + this.size);
         }
     }
 
