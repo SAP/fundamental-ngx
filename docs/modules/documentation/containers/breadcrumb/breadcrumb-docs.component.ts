96423a70ae221b52c4cc147596e737817dede7a8
droshev <mladen.droshev@sap.com>
2018-09-30 22:40:27 -0400

    #248 add 'tabindex' to elements that expect to get focus

--- docs/modules/documentation/containers/breadcrumb/breadcrumb-docs.component.ts
+++ docs/modules/documentation/containers/breadcrumb/breadcrumb-docs.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class BreadcrumbDocsComponent {
     breadcrumbHtml =
         '<fd-breadcrumb>\n' +
-        '  <fd-breadcrumb-item [url]="\'#\'">Link Text</fd-breadcrumb-item>\n' +
+        '  <fd-breadcrumb-item [routerLink]="\'#\'">Link Text</fd-breadcrumb-item>\n' +
         '  <fd-breadcrumb-item [routerLink]="\'#\'" [queryParams]="\'#\'">Link Text</fd-breadcrumb-item>\n' +
         '  <fd-breadcrumb-item>Link Text</fd-breadcrumb-item>\n' +
         '</fd-breadcrumb>\n';
