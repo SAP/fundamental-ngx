e18fb90d4dabd2927c724c33a9cb595bca5f6ea4
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2018-10-22 17:42:53 -0600

    fix bug with side nav link too

--- projects/fundamental-ngx/src/lib/side-navigation/side-navigation-link.component.ts
+++ projects/fundamental-ngx/src/lib/side-navigation/side-navigation-link.component.ts
@@ -29,13 +29,17 @@ export class SideNavigationLinkComponent implements OnChanges, AfterViewInit {
          */
         if (!this.hasSublist) {
             if (this.linkEl && this.linkEl.nativeElement) {
-                if (this.linkEl.nativeElement.children && this.linkEl.nativeElement.children.length) {
+                if (this.linkEl.nativeElement.children && this.linkEl.nativeElement.children.length > 0) {
                     const children = this.linkEl.nativeElement.children;
                     for (let i = 0; i < children.length; i++) {
+                        // if the item contains a child anchor element, apply 'fd-side-nav__link' class to that anchor
                         if (children[i].tagName === 'A') {
                             children[i].classList.add('fd-side-nav__link');
                         }
                     }
+                } else if (this.linkEl.nativeElement.children && this.linkEl.nativeElement.children.length === 0) {
+                    // if the item does not contain a child element, apply 'fd-side-nav__link' class to the fd-side-nav-link component
+                    this.linkEl.nativeElement.classList.add('fd-side-nav__link');
                 }
             }
         }
