17fdda968604a19d5121cf7887749d2a3b5a4004
O'Donnell <mikerodonnell89@users.noreply.github.com>
2018-12-14 13:24:42 -0700

    search input

--- library/src/lib/search-input/search-input.component.spec.ts
+++ library/src/lib/search-input/search-input.component.spec.ts
@@ -1,25 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SearchInputComponent } from './search-input.component';
+import { FormsModule } from '@angular/forms';
+import { MenuModule } from '../menu/menu.module';
+import { PopoverModule } from '../popover/popover.module';
+
+import { FdSearchPipe, SearchInputComponent } from './search-input.component';
 
 describe('SearchInputComponent', () => {
-  let component: SearchInputComponent;
-  let fixture: ComponentFixture<SearchInputComponent>;
+    let component: SearchInputComponent;
+    let fixture: ComponentFixture<SearchInputComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchInputComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [FdSearchPipe, SearchInputComponent],
+            imports: [FormsModule, MenuModule, PopoverModule]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchInputComponent);
+        component = fixture.componentInstance;
+        component.searchTerms = [
+            {text: 'Apple', callback: () => {}}
+        ];
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
