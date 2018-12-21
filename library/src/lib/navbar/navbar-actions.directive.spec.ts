3002e8c94f243c191fb5bfd437bc21d7e30c69bd
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2018-12-21 11:08:38 -0700

    fix import statements and remove navbar (#365)

--- library/src/lib/navbar/navbar-actions.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NavbarActionsDirective } from './navbar-actions.directive';
-
-describe('NavbarActionsDirective', () => {
-    it('should create an instance', () => {
-        const directive = new NavbarActionsDirective();
-        expect(directive).toBeTruthy();
-    });
-});
--- library/src/lib/navbar/navbar.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'fd-navbar',
-    templateUrl: './navbar.component.html'
-})
-export class NavbarComponent {}
