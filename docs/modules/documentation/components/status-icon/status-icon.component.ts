e70cbf2cd85eb379b2203ee8fa4ab5489493d0f5
Matt Lajoie <mlajoie03@gmail.com>
2019-01-13 17:27:59 -0500

    #406 implemented status icon

---
+++ docs/modules/documentation/components/status-icon/status-icon.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'status-icon',
+    templateUrl: './status-icon.component.html',
+    styleUrls: ['./status-icon.component.scss']
+})
+export class StatusIconComponent {
+
+    @Input() status: string = 'SAFE';
+    @Input() iconSize: string = 's';
+
+    private states = {
+        SAFE: {glyph: 'thumb-up', color: '#7ca10c', tooltip: 'Safe to use'},
+        ACTIVE: {glyph: 'edit', color: '#2b78c5', tooltip: 'Under development'},
+        UNSAFE: {glyph: 'lab', color: '#e38b16', tooltip: 'Work-in-progress'}
+    };
+
+    constructor() {
+    }
+}
