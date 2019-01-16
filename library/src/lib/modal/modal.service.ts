36845d8051ead5ce54d82be0ab98b20a7d0dd9fe
O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-01-16 10:24:02 -0700

    expose instance object on modal reference object

--- library/src/lib/modal/modal.service.ts
+++ library/src/lib/modal/modal.service.ts
@@ -24,6 +24,7 @@ export class ModalService {
         } else if (typeof modalType === 'function') { // component as content
             const componentRef = this.componentFactoryResolver.resolveComponentFactory(modalType).create(this.injector);
             this.modalRef = (componentRef.instance as any).modal;
+            this.modalRef.instance = componentRef.instance;
             this.appRef.attachView(componentRef.hostView);
             const domElem = (componentRef.hostView as EmbeddedViewRef < any > )
                 .rootNodes[0] as HTMLElement;
