207dd9b93c62a774de67756fd7cbe3ca01675083
O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-01-23 17:22:52 -0700

    docs for column sorting, rename fdTableSortable to fdColumnSortable

--- library/src/lib/table/column-sortable.directive.ts
+++ library/src/lib/table/column-sortable.directive.ts
@@ -1,14 +1,14 @@
 import { Directive, Input } from '@angular/core';
 
 @Directive({
-    selector: '[fdTableSortable]',
+    selector: '[fdColumnSortable]',
     host: {
         class: 'fd-table__sort-column',
         '[class.fd-table__sort-column--dsc]': 'sortDir === "desc" || sortDir === "dsc"',
         '[class.fd-table__sort-column--asc]': 'sortDir === "asc"'
     }
 })
-export class TableSortableDirective {
+export class ColumnSortableDirective {
 
     @Input() sortDir: SortDirections;
 
--- library/src/lib/table/table.module.ts
+++ library/src/lib/table/table.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TableComponent } from './table.component';
 import { TableHeaderComponent } from './table-header.component';
 import { TableBodyComponent } from './table-body.component';
-import { TableSortableDirective } from './table-sortable.directive';
+import { ColumnSortableDirective } from './column-sortable.directive';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [TableComponent, TableHeaderComponent, TableBodyComponent, TableSortableDirective],
-    exports: [TableComponent, TableHeaderComponent, TableBodyComponent, TableSortableDirective]
+    declarations: [TableComponent, TableHeaderComponent, TableBodyComponent, ColumnSortableDirective],
+    exports: [TableComponent, TableHeaderComponent, TableBodyComponent, ColumnSortableDirective]
 })
 export class TableModule {}
