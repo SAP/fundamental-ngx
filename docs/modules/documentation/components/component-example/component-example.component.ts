097ce776afa204c9c26e2a950970ce79f5710dc0
deno <mladen.droshev@sap.com>
2019-02-12 10:40:33 -0500

    #510 enable RTL toggle in the documentation

--- docs/modules/documentation/components/component-example/component-example.component.ts
+++ docs/modules/documentation/components/component-example/component-example.component.ts
@@ -9,7 +9,7 @@ declare let hljs: any;
         <div class="fd-tile docs-component docs-component__ fd-has-background-color-background-1" id="{{id}}">
             <div class="fd-tile__content">
                 <div class="component-example__features">
-                    <rtl-toggle *ngIf="showRTL"[label]="id2"></rtl-toggle>
+                    <rtl-toggle [label]="id2"></rtl-toggle>
                     <background-toggle [label]="id"></background-toggle>
                 </div>
                 <div class="fd-doc-component" id="{{id2}}">
@@ -35,14 +35,11 @@ export class ComponentExampleComponent implements OnInit {
     @Input()
     name: string;
 
-    showRTL: boolean = false;
-
     constructor(private element: ElementRef, private activatedRoute: ActivatedRoute) {}
 
     ngOnInit() {
         this.id = '' + Date.now() + '_wrapper_' + this.name;
         this.id2 = '' + Date.now() + this.name;
-        this.showRTL = this.activatedRoute.snapshot.queryParams['rtl'];
     }
 
     id: string;
