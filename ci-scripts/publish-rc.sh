96b0ee6a9b51b8c9fcce8b4ddd1e34242e6cb744
Greg Smith <greg.smith03@sap.com>
2019-02-19 12:01:46 -0600

    Updates for build process

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -5,7 +5,11 @@ set -e
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
 
+cd library
+
 npm run std-version -- --prerelease rc --no-verify
+
+cd ..
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
 
@@ -14,3 +18,5 @@ npm run build-deploy-library
 cd dist/fundamental-ngx
 
 npm publish --tag prerelease
+
+cd ../..
--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -8,10 +8,14 @@ git config --global user.name "fundamental-bot"
 # delete temp branch
 git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" ":$TRAVIS_BRANCH" > /dev/null 2>&1;
 
+cd library
+
 std_ver=$(npm run std-version)
 release_tag=$(echo "$std_ver" | grep "tagging release" | awk '{print $4}')
 
 echo "$std_ver"
+
+cd ..
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master > /dev/null 2>&1;
 
@@ -23,5 +27,7 @@ cd dist/fundamental-ngx
 
 npm publish
 
-#npm run build-doc
-#npm run deploy -- --repo "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG"
+cd ../..
+
+npm run build-docs
+npm run deploy-docs -- --repo "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG"
