edd1aeefdd501866d74dd4b944a37a951c038164
Mathieu Lajoie <mlajoie03@gmail.com>
2019-02-21 12:06:38 -0500

    #555 added toggle tests (#556)

--- library/src/lib/toggle/toggle.component.spec.ts
+++ library/src/lib/toggle/toggle.component.spec.ts
@@ -29,4 +29,53 @@ describe('ToggleComponent', () => {
         expect(component).toBeTruthy();
     });
 
+    it('should accept custom id', () => {
+        const id = 'custom-id';
+        component.id = id;
+        component.ngOnInit();
+        expect(component.id).toBe(id);
+    });
+
+    it('should generate id', () => {
+        component.ngOnInit();
+        expect(component.id).toBeTruthy();
+    });
+
+    it('should toggle on click', () => {
+        spyOn(component, 'onChange');
+        spyOn(component.onToggle, 'emit');
+
+        component.ngOnInit();
+        component.checked = false;
+        fixture.detectChanges();
+
+        input.click();
+        fixture.detectChanges();
+
+        expect(component.onChange).toHaveBeenCalledWith(true);
+        expect(component.onToggle.emit).toHaveBeenCalledWith(true);
+
+        input.click();
+        fixture.detectChanges();
+
+        expect(component.onChange).toHaveBeenCalledWith(false);
+        expect(component.onToggle.emit).toHaveBeenCalledWith(false);
+    });
+
+    it('should have a default size', () => {
+        component.size = 'test';
+        component.ngOnInit();
+        expect(component.size).toBeNull();
+    });
+
+    it('should display size', () => {
+        const size = 'l';
+        component.size = size;
+        component.ngOnInit();
+        fixture.detectChanges();
+
+        const toggle = fixture.nativeElement.querySelector('.fd-toggle');
+        expect(toggle.classList).toContain('fd-toggle--' + size)
+    });
+
 });
