0fe799c2a04a87f3afbcb2d92744035bb753ea63
deno <mladen.droshev@sap.com>
2019-02-26 09:39:17 -0500

    #579 disable all failing tslint errors for now; enable pre-commit tslint checks

--- library/src/lib/breadcrumb/breadcrumb-item.directive.ts
+++ library/src/lib/breadcrumb/breadcrumb-item.directive.ts
@@ -1,6 +1,8 @@
 import { Directive } from '@angular/core';
 
 @Directive({
+    // TODO to be discussed
+    // tslint:disable-next-line:directive-selector
     selector: 'fd-breadcrumb-item',
     host: {
         class: 'fd-breadcrumb__item'
--- library/src/lib/list/list-item.component.ts
+++ library/src/lib/list/list-item.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
+    // TODO to be discussed
+    // tslint:disable-next-line:component-selector
     selector: '[fd-list-item]',
     host: {
         class: 'fd-list-group__item'
     },
-    template: `<ng-content></ng-content>`
+    template: `
+        <ng-content></ng-content>
+    `
 })
 export class ListItemComponent {}
