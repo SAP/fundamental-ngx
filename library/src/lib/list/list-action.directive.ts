4f22e35787f6f98aba46bbe4ce80280e50b432f6
deno <mladen.droshev@sap.com>
2019-02-26 09:56:34 -0500

    #579 disable all failing tslint errors for now; enable pre-commit tslint checks (#584)

--- library/src/lib/list/list-action.directive.ts
+++ library/src/lib/list/list-action.directive.ts
@@ -1,6 +1,8 @@
 import { Directive } from '@angular/core';
 
 @Directive({
+    // TODO to be discussed
+    // tslint:disable-next-line:directive-selector
     selector: 'fd-list-action',
     host: {
         class: 'fd-list-group__action'
