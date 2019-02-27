24d5d54398ea714d65d76c6d5400d59bbf9fd992
Matt Lajoie <mlajoie03@gmail.com>
2019-02-27 10:19:36 -0500

    added HTMLElement type

--- library/src/lib/scroll-spy/scroll-spy.directive.ts
+++ library/src/lib/scroll-spy/scroll-spy.directive.ts
@@ -15,21 +15,21 @@ export class ScrollSpyDirective {
     public targetPercent: number = 0;
 
     @Output()
-    public spyChange: EventEmitter<any> = new EventEmitter<any>();
+    public spyChange: EventEmitter<HTMLElement> = new EventEmitter<HTMLElement>();
 
-    private currentActive: any;
+    private currentActive: HTMLElement;
 
     constructor(private elRef: ElementRef) {}
 
     @HostListener('scroll', ['$event'])
     onScroll(event: any) {
-        let spiedTag: any;
+        let spiedTag: HTMLElement;
         const children = this.elRef.nativeElement.children;
         const targetScrollTop = event.target.scrollTop;
         const targetOffsetTop = event.target.offsetTop;
 
         for (let i = 0; i < children.length; i++) {
-            const element = children[i];
+            const element: HTMLElement = children[i];
             if (this.trackedTags.some(tag => tag.toLocaleUpperCase() === element.tagName.toLocaleUpperCase())) {
                 if ((element.offsetTop - targetOffsetTop) <= targetScrollTop + event.target.offsetHeight * this.targetPercent) {
                     spiedTag = element;
