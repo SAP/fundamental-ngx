c0c01ff36aeb0b243c6675a99f059639aee4fbb3
Matt Lajoie <mlajoie03@gmail.com>
2019-03-11 19:02:15 -0400

    fix attempt #1

--- docs/app/components/app/app.component.ts
+++ docs/app/components/app/app.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-
-import { Observable } from 'rxjs';
-import { filter, pluck } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-    url$: Observable<string>;
-
-    goToGithub() {
-        window.location.href = 'https://github.com/SAP/fundamental-ngx';
-    }
-
-    constructor(private router: Router) {}
-
-    ngOnInit() {
-        this.url$ = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).pipe(pluck('url'));
-    }
-}
+export class AppComponent {}
