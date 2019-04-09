a7cc70d8607166e892ebc5d7854353ca8227c9a6
O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-04-09 08:36:57 -0600

    rename components to directives

--- library/src/lib/table/table-header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    // TODO to be discussed
-    // tslint:disable-next-line:component-selector
-    selector: '[fd-table-header]',
-    templateUrl: './table-header.component.html'
-})
-export class TableHeaderComponent {}
