9cde7bee9e9f37cb49631b3c8e9032127e5b4b38
Mathieu Lajoie <mlajoie03@gmail.com>
2019-04-10 12:29:24 -0400

    documented file-input (#711)

--- library/src/lib/file-input/directives/file-select.directive.ts
+++ library/src/lib/file-input/directives/file-select.directive.ts
@@ -1,22 +1,29 @@
 import { Directive, Input, Output, EventEmitter } from '@angular/core';
 import { HostListener, HostBinding } from '@angular/core';
 
+/**
+ * Directive tool to facilitate interacting with a native file input element.
+ */
 @Directive({
     selector: '[fdFileSelect]',
 })
 export class FileSelectDirective {
 
+    /** @Input Whether the input should accept multiple file selections. */
     @Input()
     private multiple: boolean = true;
 
+    /** @Output Event emitted when files are selected. */
     @Output()
     onFileSelect: EventEmitter<File[]> = new EventEmitter<File[]>();
 
+    /** @hidden */
     @HostBinding('attr.multiple')
     get multipleBinding(): string {
         return this.multiple ? '' : undefined;
     }
 
+    /** @hidden */
     @HostListener('change', ['$event'])
     onChange(event: Event): void {
         if (event.target instanceof HTMLInputElement) {
--- library/src/lib/file-input/file-input.component.ts
+++ library/src/lib/file-input/file-input.component.ts
@@ -1,6 +1,11 @@
 import { Component, ElementRef, EventEmitter, forwardRef, Input, Output, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+/**
+ * Tool to facilitate the input of files from the user.
+ * It supports drag and drop, multiple input, max file size and more.
+ * The drag events make it very easy to create and style elements like a dropzone.
+ */
 @Component({
     selector: 'fd-file-input',
     templateUrl: './file-input.component.html',
@@ -16,57 +21,74 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 })
 export class FileInputComponent implements ControlValueAccessor {
 
+    /** @hidden */
     @ViewChild('input')
     inputRef: ElementRef;
 
+    /** @Input Whether the file input is disabled. */
     @Input()
     disabled: boolean = false;
 
+    /** @Input Whether the file input should accept multiple files. */
     @Input()
     multiple: boolean = true;
 
+    /** @Input Accepted file extensions. Format: `'.png,.jpg'`. */
     @Input()
     accept: string;
 
+    /** @Input Whether the file input accepts drag and dropped files. */
     @Input()
     dragndrop: boolean = true;
 
+    /** @Input Max file size in bytes that the input will accept. */
     @Input()
     maxFileSize: number;
 
+    /** @Output Event fired when files are selected. Passed object is the array of files selected. */
     @Output()
     onSelect: EventEmitter<File[]> = new EventEmitter<File[]>();
 
+    /** @Output Event fired when some invalid files are selected. Passed object is the array of invalid files. */
     @Output()
     onInvalidFiles: EventEmitter<File[]> = new EventEmitter<File[]>();
 
+    /** @Output Event fired when the dragged file enters the component boundaries. */
     @Output()
     onDragEnter: EventEmitter<null> = new EventEmitter<null>();
 
+    /** @Output Event fired when the dragged file exits the component boundaries. */
     @Output()
     onDragLeave: EventEmitter<null> = new EventEmitter<null>();
 
+    /** @hidden */
     onChange: Function = () => {};
+
+    /** @hidden */
     onTouched: Function = () => {};
 
+    /** @hidden */
     registerOnChange(fn: any): void {
         this.onChange = fn;
     }
 
+    /** @hidden */
     registerOnTouched(fn: any): void {
         this.onTouched = fn;
     }
 
+    /** @hidden */
     setDisabledState(isDisabled: boolean): void {
         this.disabled = isDisabled;
     }
 
+    /** @hidden */
     writeValue(files: File[]): void {
         // not needed - should be handled by user.
     }
 
+    /** @hidden */
     selectHandler(event: File[]) {
-
         if (this.maxFileSize) {
             const valid_files: File[] = [];
             const invalid_files: File[] = [];
@@ -90,10 +112,16 @@ export class FileInputComponent implements ControlValueAccessor {
         }
     }
 
+    /**
+     * Opens the file selector.
+     */
     public open(): void {
         this.inputRef.nativeElement.click();
     }
 
+    /**
+     * Clears the files from the input.
+     */
     public clear(): void {
         this.inputRef.nativeElement.value = '';
         this.onChange([]);
