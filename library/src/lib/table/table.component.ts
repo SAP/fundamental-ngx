4d2150195de13be887c40ec3968001518d5a17fb
O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-04-10 08:11:13 -0600

    remove all table css

--- library/src/lib/table/table.component.ts
+++ library/src/lib/table/table.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-table',
-    styleUrls: ['./table.component.scss'],
     templateUrl: './table.component.html'
 })
 export class TableComponent {}
