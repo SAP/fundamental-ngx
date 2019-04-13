2bdd25111b3f8b6e1d9f790b6cf425586da45e64
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-04-12 20:35:38 -0600

    docs: add badge and label typedocs (#724)
    * add badge and label typedocs
    
    * fixed doc

--- library/src/lib/badge-label/badge.component.ts
+++ library/src/lib/badge-label/badge.component.ts
@@ -1,15 +1,22 @@
 import { Input, Component, Inject, ElementRef } from '@angular/core';
 import { AbstractFdNgxClass } from '../utils/abstract-fd-ngx-class';
 
+/**
+ * Badge component, used to indicate status.
+ * Colors, generally in combination with text, are used to easily highlight the state of an object.
+ */
 @Component({
     selector: 'fd-badge',
     templateUrl: './badge-label.component.html'
 })
 export class BadgeComponent extends AbstractFdNgxClass {
+    /** @Input Color coded status for the badge. Options are 'success', 'warning', and 'error'. Leave empty for default badge. */
     @Input() status;
 
+    /** @Input Modifier for the badge. Options are 'pill' and 'filled'. Leave empty for normal. */
     @Input() modifier;
 
+    /** @hidden */
     _setProperties() {
         this._addClassToElement('fd-badge');
         if (this.status) {
@@ -20,6 +27,7 @@ export class BadgeComponent extends AbstractFdNgxClass {
         }
     }
 
+    /** @hidden */
     constructor(@Inject(ElementRef) elementRef: ElementRef) {
         super(elementRef);
     }
--- library/src/lib/badge-label/label.component.ts
+++ library/src/lib/badge-label/label.component.ts
@@ -1,18 +1,28 @@
 import { Component, ElementRef, Inject, Input } from '@angular/core';
 import { AbstractFdNgxClass } from '../utils/abstract-fd-ngx-class';
 
+/**
+ * Badge component, used to indicate status.
+ * Colors, generally in combination with text, are used to easily highlight the state of an object.
+ */
 @Component({
     selector: 'fd-label',
     templateUrl: './badge-label.component.html'
 })
 export class LabelComponent extends AbstractFdNgxClass {
+    /** @Input Color coded status for the label. Options are 'success', 'warning', and 'error'. Leave empty for default label. */
     @Input() status: string = '';
-    @Input() isStatusLabel: boolean = false;
-    @Input() statusIcon: string = '';
-    @Input() indicator: string = '';
-    @Input() icon: string = '';
-    @Input() semantic: string = '';
 
+    /** @Input When set to 'true', the type of the label is 'Status Indicator Label'. Leave empty for default type. */
+    @Input() isStatusLabel: boolean = false;
+
+    /** @Input Built-in status icon. Options include 'available', 'away', 'busy', and 'offline'. */
+    @Input() statusIcon: string = '';
+
+    /** @Input The icon used with the status indicator. See the icon page for the list of icons. */
+    @Input() icon: string = '';
+
+    /** @hidden */
     _setProperties() {
         if (this.isStatusLabel) {
             this._addClassToElement('fd-status-label');
@@ -33,6 +43,7 @@ export class LabelComponent extends AbstractFdNgxClass {
         }
     }
 
+    /** @hidden */
     constructor(@Inject(ElementRef) elementRef: ElementRef) {
         super(elementRef);
     }
