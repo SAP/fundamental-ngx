116b1d7fdceaaa3b42146d5053d05ad1932b7b9b
Matt Lajoie <mlajoie03@gmail.com>
2019-04-22 11:09:02 -0400

    small fix

--- docs/modules/documentation/containers/button/examples/button-examples.component.ts
+++ docs/modules/documentation/containers/button/examples/button-examples.component.ts
@@ -2,30 +2,55 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-button-options-example',
-    templateUrl: './button-options-example.component.html'
+    templateUrl: './button-options-example.component.html',
+    styles: [`        
+        .fd-button {
+            margin-right: 12px;
+        }
+    `]
 })
 export class ButtonOptionsExampleComponent {}
 
 @Component({
     selector: 'fd-button-types-example',
-    templateUrl: './button-types-example.component.html'
+    templateUrl: './button-types-example.component.html',
+    styles: [`        
+        .fd-button {
+            margin-right: 12px;
+        }
+    `]
 })
 export class ButtonTypesExampleComponent {}
 
 @Component({
     selector: 'fd-button-sizes-example',
-    templateUrl: './button-sizes-example.component.html'
+    templateUrl: './button-sizes-example.component.html',
+    styles: [`        
+        .fd-button {
+            margin-right: 12px;
+        }
+    `]
 })
 export class ButtonSizesExampleComponent {}
 
 @Component({
     selector: 'fd-button-icons-example',
-    templateUrl: './button-icons-example.component.html'
+    templateUrl: './button-icons-example.component.html',
+    styles: [`        
+        .fd-button {
+            margin-right: 12px;
+        }
+    `]
 })
 export class ButtonIconsExampleComponent {}
 
 @Component({
     selector: 'fd-button-state-example',
-    templateUrl: './button-state-example.component.html'
+    templateUrl: './button-state-example.component.html',
+    styles: [`        
+        .fd-button {
+            margin-right: 12px;
+        }
+    `]
 })
 export class ButtonStateExampleComponent {}
