8c8793e53bd68af268378a6c124314e5efa8b0dc
Inna Atanasova <i.atanasova@sap.com>
2019-04-23 11:15:45 -0400

    #697: adds menu typedocs

--- library/src/lib/menu/menu-group.component.ts
+++ library/src/lib/menu/menu-group.component.ts
@@ -1,10 +1,14 @@
 import { Component, HostBinding } from '@angular/core';
 
+/**
+ * The component that represents a menu group.
+ */
 @Component({
     selector: 'fd-menu-group',
     templateUrl: './menu-group.component.html',
     styles: [':host {display: block;}']
 })
 export class MenuGroupComponent {
+    /** @hidden */
     @HostBinding('class.fd-menu__group') true;
 }
