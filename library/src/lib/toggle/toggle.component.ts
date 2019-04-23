f9caccb3126c4345d18e9996121dbe704789d74e
Inna Atanasova <i.atanasova@sap.com>
2019-04-23 14:16:29 -0400

    #696: adds toggle typedocs

--- library/src/lib/toggle/toggle.component.ts
+++ library/src/lib/toggle/toggle.component.ts
@@ -2,6 +2,9 @@ import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@ang
 import { HashService } from '../utils/hash.service';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+/**
+ * The Toggle component is used to activate or deactivate an element. It uses a visual metaphor to inform the user of the state of the toggle. 
+ */
 @Component({
     selector: 'fd-toggle',
     templateUrl: './toggle.component.html',
@@ -19,28 +22,38 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 })
 export class ToggleComponent implements OnInit, ControlValueAccessor {
 
+    /** @Input The size of the toggle. Can be one of the four *xs*, *s*, *l*, *error* or default. */
     @Input()
     size: string;
 
+    /** @Input Whether the toggle is disabled. */
     @Input()
     disabled: boolean = false;
 
+    /** @Input Id for the toggle component. If omitted, a unique one is generated. */
     @Input()
     id: string;
 
+    /** @Input Whether the toggle is checked. */
     @Input()
     checked: boolean = false;
 
+    /** @Output Event fired when the toggle is checked. */
     @Output()
     checkedChange = new EventEmitter<boolean>();
 
-    onChange: any = () => {};
-    onTouched: any = () => {};
+    /** @hidden */
+    onChange: any = () => { };
 
+    /** @hidden */
+    onTouched: any = () => { };
+
+    /** Get the isChecked property of the toggle. */
     get isChecked() {
         return this.checked;
     }
 
+    /** Set the isChecked property of the toggle. */
     set isChecked(value) {
         this.checked = value;
         this.onChange(value);
@@ -48,20 +61,32 @@ export class ToggleComponent implements OnInit, ControlValueAccessor {
         this.checkedChange.emit(value);
     }
 
+    /**
+     * @param value Sets the value of the *checked* property of the toggle.
+     */
     writeValue(value: any) {
         this.checked = value;
     }
 
+    /**
+     * @param fn User defined function that handles the *onChange* event of the toggle.
+     */
     registerOnChange(fn) {
         this.onChange = fn;
     }
+
+    /**
+     * @param fn User defined function that handles the *onTouch* event of the toggle.
+     */
     registerOnTouched(fn) {
         this.onTouched = fn;
     }
 
+    /** @hidden */
     constructor(private hasher: HashService) {
     }
 
+    /** @hidden */
     ngOnInit() {
         if (!this.id) {
             this.id = this.hasher.hash();
