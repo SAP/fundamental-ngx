14c3736a3aee52b23db9dc594fe4446c2b5d93ae
Mathieu Lajoie <mlajoie03@gmail.com>
2019-04-29 19:35:31 -0400

    infinite-scroll docs

--- library/src/lib/infinite-scroll/infinite-scroll.directive.ts
+++ library/src/lib/infinite-scroll/infinite-scroll.directive.ts
@@ -1,21 +1,28 @@
 import { Directive, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { fromEvent, Observable } from 'rxjs';
 
+/**
+ * Tool directive used to achieve the infinite scroll mechanism.
+ */
 @Directive({
     selector: '[fdInfiniteScroll]'
 })
 export class InfiniteScrollDirective implements OnInit {
 
-    @Output()
-    onScrollAction = new EventEmitter<any>();
-
+    /** @Input Scroll percentage at which the onScrollAction event is fired. */
     @Input()
     scrollPercent: number = 75;
 
+    /** @Output Event emitted when the scrollPercent threshold is met. */
+    @Output()
+    onScrollAction = new EventEmitter<any>();
+
     private scrollEvent: Observable<any>;
 
+    /** @hidden */
     constructor(private element: ElementRef) {}
 
+    /** @hidden */
     ngOnInit(): void {
         this.scrollEvent = fromEvent(this.element.nativeElement, 'scroll');
 
