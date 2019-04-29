363a21e1ad2236d9faee810fe8372fd077978c6b
Mathieu Lajoie <mlajoie03@gmail.com>
2019-04-29 19:30:00 -0400

    token docs

--- library/src/lib/token/token.component.ts
+++ library/src/lib/token/token.component.ts
@@ -7,34 +7,41 @@ import { HashService } from '../utils/hash.service';
     styleUrls: ['./token.component.scss'],
     host: {
         'class': 'fd-token',
-        'role': 'button'
+        'role': 'button',
+        '[id]': 'id'
     }
 })
 export class TokenComponent implements OnInit {
 
+    /** @hidden */
     @ViewChild('contentContainer')
     contentContainer: ElementRef;
 
-    @Output()
-    onCloseClick: EventEmitter<string> = new EventEmitter<string>();
-
+    /** @Input Id of the element. Returned back through the onCloseClick event. */
     @Input()
     id: string;
 
+    /** @Output Emitted when the *x* icon is clicked. Specifically, any pseudo-element. */
+    @Output()
+    onCloseClick: EventEmitter<string> = new EventEmitter<string>();
+
+    /** @hidden */
+    constructor(private elRef: ElementRef, private hash: HashService) {}
+
+    /** @hidden */
+    ngOnInit(): void {
+        if (!this.id) {
+            this.id = this.hash.hash();
+        }
+    }
+
+    /** @hidden */
     @HostListener('click', ['$event'])
     clickHandler(event): void {
         if (this.contentContainer) {
             if (this.elRef.nativeElement.contains(event.target) && !this.contentContainer.nativeElement.contains(event.target)) {
                 this.onCloseClick.emit(this.id);
             }
-        }
-    }
-
-    constructor(private elRef: ElementRef, private hash: HashService) {}
-
-    ngOnInit(): void {
-        if (!this.id) {
-            this.id = this.hash.hash();
         }
     }
 
