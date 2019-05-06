0beb619c82207dee36398d492affe7d4f9a132df
Mathieu Lajoie <mlajoie03@gmail.com>
2019-05-06 17:21:12 -0400

    feat: replaced useless form components with directives (#777)
    * reorganized files
    
    * small button css fix
    
    * restructured forms + docs
    
    * fixed tests
    
    * fixed bad documentation
    
    * small fix

---
+++ library/src/lib/form/form-legend/form-legend.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, HostBinding } from '@angular/core';
+
+/**
+ * Applies css to a legend html element.
+ *
+ * <legend fd-form-legend>Legend</legend>
+ */
+@Directive({
+    // TODO to be discussed
+    // tslint:disable-next-line:directive-selector
+    selector: '[fd-form-legend]',
+})
+export class FormLegendDirective {
+
+    /** @hidden */
+    @HostBinding('class.fd-form__legend')
+    fdFormLegendClass: boolean = true;
+}
