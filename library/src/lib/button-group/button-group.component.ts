78b6a050fdc28f5504137acb5e48ac880ff10843
Mathieu Lajoie <mlajoie03@gmail.com>
2019-05-08 15:31:49 -0400

    docs: added button group docs (#782)
    * added button group docs
    
    * fixed tests

--- library/src/lib/button-group/button-group.component.ts
+++ library/src/lib/button-group/button-group.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
+/**
+ * Container for grouped buttons.
+ *
+ * ```html
+ * <fd-button-group>
+ *     <button fd-button-grouped>Button</button>
+ * </fd-button-group>
+ * ```
+ */
 @Component({
     selector: 'fd-button-group',
     templateUrl: './button-group.component.html',
     host: {
-        'role': 'group',
-        'aria-label': 'Group label',
-        class: 'fd-button-group'
+        'role': 'group'
     }
 })
-export class ButtonGroupComponent {}
+export class ButtonGroupComponent {
+
+    /** @hidden */
+    @HostBinding('class.fd-button-group')
+    fdButtonGroupClass: boolean = true;
+}
