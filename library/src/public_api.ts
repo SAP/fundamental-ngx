daf5e653e673246e00dbc4da1955b44c9f974575
Mathieu Lajoie <mlajoie03@gmail.com>
2019-05-08 16:36:43 -0400

    removed useless components and fixed dependencies (#781)

--- library/src/public_api.ts
+++ library/src/public_api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of fundamental-ngx
  */
 
-export * from './lib/fundamental-ngx.service';
-export * from './lib/fundamental-ngx.component';
 export * from './lib/fundamental-ngx.module';
 
 export * from './lib/action-bar/action-bar.module';
