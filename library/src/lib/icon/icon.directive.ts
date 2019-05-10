13e23540f5cac717705479844b53cecdf59c2d68
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2019-05-10 15:22:02 -0400

    #687: adds typedocs for icon component (#794)

--- library/src/lib/icon/icon.directive.ts
+++ library/src/lib/icon/icon.directive.ts
@@ -1,12 +1,25 @@
 import { Directive, Input, ElementRef, Inject } from '@angular/core';
 import { AbstractFdNgxClass } from '../utils/abstract-fd-ngx-class';
 
-/** The base class for the icon component */
+/** 
+ * @hidden
+ * The base class for the icon component 
+ */
 const BASE_ICON_CLASS = 'sap-icon';
 
-/** Prefix for icon prop classes */
+/** 
+ * @hidden
+ * Prefix for icon prop classes 
+ */
 const PREFIX_ICON_CLASS = BASE_ICON_CLASS + '--';
 
+/**
+ * The directive that represents an icon. 
+ *
+ * ```html
+ * <fd-icon [glyph]="cart-approval" [size]="'l'"></fd-icon>
+ * ```
+ */
 @Directive({
     // TODO to be discussed
     // tslint:disable-next-line:directive-selector
@@ -16,10 +29,18 @@ const PREFIX_ICON_CLASS = BASE_ICON_CLASS + '--';
     }
 })
 export class IconDirective extends AbstractFdNgxClass {
+    
+    /** The glyph name */
     @Input() glyph;
 
+    /** 
+     * The size of the icon
+     * The predefined values for the input size are *xs*, *s*, *l*, and *xl*.
+     * *size* can accept any other string, for example *xxs*, which will be translated into class *sap-icon--xxs*.
+     */
     @Input() size: string = '';
 
+    /** @hidden */
     _setProperties() {
         if (this.glyph) {
             this._addClassToElement(PREFIX_ICON_CLASS + this.glyph);
@@ -30,7 +51,8 @@ export class IconDirective extends AbstractFdNgxClass {
         }
     }
 
-    constructor(@Inject(ElementRef) elementRef: ElementRef) {
+    /** @hidden */
+    constructor(private elementRef: ElementRef) {
         super(elementRef);
     }
 }
