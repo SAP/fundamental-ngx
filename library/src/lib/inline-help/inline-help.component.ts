15eab9a757765b9606f5478635c8c7517e60fae2
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2019-05-10 15:22:51 -0400

    #687: adds documentation typedoc comments for inline-help component (#798)

--- library/src/lib/inline-help/inline-help.component.ts
+++ library/src/lib/inline-help/inline-help.component.ts
@@ -1,5 +1,14 @@
 import { Component, Input } from '@angular/core';
-
+/**
+ * The component that represents an inline-help. 
+ * Inline help is used to display help text in a popover, often inline with headers, body text and form labels.
+ *
+ * ```html
+ * <fd-inline-help [position]="'bottom-left'">
+ *      Lorem ipsum dolor sit amet, consectetur adipiscing.
+ * </fd-inline-help>
+ * ```
+ */
 @Component({
     selector: 'fd-inline-help',
     templateUrl: './inline-help.component.html',
@@ -9,6 +18,11 @@ import { Component, Input } from '@angular/core';
     }
 })
 export class InlineHelpComponent {
+    /** 
+     * The position of the inline help component.
+     * Options include *bottom-right*, *bottom-left*, *bottom-center*, *right*, and *left*.
+     * The default position is *bottom right* 
+     */
     @Input()
     position: string;
 }
