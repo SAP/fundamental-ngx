dbd1c043029310907922efb1a0f9dde0d25122db
Inna Atanasova <i.atanasova@sap.com>
2019-05-10 11:49:46 -0400

    #687: adds documentation typedoc comments for image component

--- library/src/lib/image/image.component.ts
+++ library/src/lib/image/image.component.ts
@@ -1,15 +1,35 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * The component that represents an image. 
+ *
+ * ```html
+ * <fd-image style="margin-right: 10px;" [size]="'l'" [circle]="true" [photo]="'https://placeimg.com/400/400/nature'"></fd-image>
+ * ```
+ */
 @Component({
     selector: 'fd-image',
     templateUrl: './image.component.html'
 })
 export class ImageComponent {
+    /** 
+     * The size of the image. 
+     * The predefined values for the size are *s*, *m*, and *l*.
+     */
     @Input() size: string = 'm';
 
+    /** 
+     * Whether to render a circle style for the image. 
+     */
     @Input() circle: boolean = false;
 
+    /** 
+     * The image label. 
+     */
     @Input() label: string = 'Image label';
 
+    /** 
+     * The image url. 
+     */
     @Input() photo: string;
 }
