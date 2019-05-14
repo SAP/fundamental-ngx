9a8bec3e4cd2c07fb54f3486f87f4dd61598beee
Inna Atanasova <i.atanasova@sap.com>
2019-05-14 11:56:39 -0400

    #687: adds typedocs for pagination component

--- library/src/lib/pagination/pagination.component.ts
+++ library/src/lib/pagination/pagination.component.ts
@@ -2,6 +2,16 @@ import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from
 import { PaginationService } from './pagination.service';
 import { Pagination } from './pagination.model';
 
+/**
+ * The component that is used to provide navigation between paged information.
+ * ```html
+ * <fd-pagination 
+ *          [totalItems]="50" 
+ *          [itemsPerPage]="10" 
+ *          [currentPage]="3">
+ * </fd-pagination>
+ * ```
+ */
 @Component({
     selector: 'fd-pagination',
     templateUrl: './pagination.component.html',
@@ -16,15 +26,26 @@ import { Pagination } from './pagination.model';
     `]
 })
 export class PaginationComponent implements OnChanges {
+    /** Represents the total number of items. */
     @Input()
     totalItems: number;
+    
+    /** Represents the current page number. */
     @Input()
     currentPage: number;
+    
+    /** Represents the number of items per page. */
     @Input()
     itemsPerPage: number;
 
+    /** Whether to display the total number of items. */
     @Input()
     displayTotalItems: boolean = true;
+    
+    /** 
+     * The text appended to the total number of items. 
+     * The default text is set to 'items' 
+     */
     @Input()
     displayText: string = 'items';
 
@@ -36,13 +57,17 @@ export class PaginationComponent implements OnChanges {
     @Input()
     nextLabel: string = 'Next';
 
+    /** Event fired when the page is changed. */
     @Output()
     pageChangeStart = new EventEmitter<number>();
 
+    /** @hidden */
     pages: number[];
 
+    /** @hidden */
     constructor(private paginationService: PaginationService) {}
 
+    /** @hidden */
     ngOnChanges(changes: SimpleChanges) {
         if (changes && changes.currentPage) {
             this.currentPage = changes.currentPage.currentValue;
@@ -56,10 +81,18 @@ export class PaginationComponent implements OnChanges {
         }
     }
 
+    /**
+     * Checks if the current page is the last page.
+     */
     isLastPage(): boolean {
         return this.currentPage === this.paginationService.getTotalPages(this.getPaginationObject());
     }
 
+    /**
+     * Navigates to a specific page when the user presses 'Space' or 'Enter' key.
+     * @param page The number of the page.
+     * @param $event The keyboard event.
+     */
     onKeypressHandler(page: number, $event: KeyboardEvent) {
         if ($event.code === 'Space' || $event.code === 'Enter') {
             $event.preventDefault();
@@ -67,6 +100,11 @@ export class PaginationComponent implements OnChanges {
         }
     }
 
+    /**
+     * Navigates to a specific page.
+     * @param page The number of the page to navigate to.
+     * @param $event The mouse event (optional).
+     */
     goToPage(page: number, $event?: MouseEvent) {
         if ($event) {
             $event.preventDefault();
@@ -78,6 +116,10 @@ export class PaginationComponent implements OnChanges {
         this.pageChangeStart.emit(page);
     }
 
+    /**
+     * Retrieves an object that represents 
+     * the total number of items, the current page, and the number of items per page.
+     */
     getPaginationObject() {
         const retVal = {
             totalItems: this.totalItems,
