d0607cac4694691cb3fc8744843504968f41ea4f
Inna Atanasova <i.atanasova@sap.com>
2019-05-14 10:48:18 -0400

    #687: adds typedocs for loading spinner component

--- library/src/lib/loading-spinner/loading-spinner.component.ts
+++ library/src/lib/loading-spinner/loading-spinner.component.ts
@@ -1,11 +1,19 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * The component that represents a loading spinner. 
+ *
+ * ```html
+ * <fd-loading-spinner [loading]="true"></fd-loading-spinner>
+ * ```
+ */
 @Component({
     selector: 'fd-loading-spinner',
     templateUrl: './loading-spinner.component.html',
     styleUrls: ['./loading-spinner.component.scss']
 })
 export class LoadingSpinnerComponent {
+    /** Whether to display the loading indicator animation. */
     @Input()
     loading: boolean = false;
 
