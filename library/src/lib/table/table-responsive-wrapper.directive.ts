a5e157756d93c9c8f5eb4ab9d85fade1e5317dce
jmarkowski <jmarkowski@divante.co>
2019-06-07 20:35:15 +0200

    fix: add possibility to include scrollable table + example in docs (#869)
    * Enhancement - add possibility to include scrollable table + example in docs
    
    * Replace fd-table--fixed class by adding overflow

---
+++ library/src/lib/table/table-responsive-wrapper.directive.ts
@@ -0,0 +1,19 @@
+import { Directive } from '@angular/core';
+/**
+ * The directive that represents a table.
+ * A table is a set of tabular data. Line items can support data, images and actions.
+ * ```html
+ *     <div fd-table-responsive-wrapper>
+ *         <table fd-table>
+ *         </table>
+ *     </div>
+ * ```
+ */
+@Directive({
+    // tslint:disable-next-line:directive-selector
+    selector: '[fd-table-responsive-wrapper]',
+    host: {
+        style: 'overflow-x: auto'
+    }
+})
+export class TableResponsiveWrapperDirective {}
--- library/src/lib/table/table.module.ts
+++ library/src/lib/table/table.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TableDirective } from './table.directive';
 import { ColumnSortableDirective } from './column-sortable.directive';
+import { TableResponsiveWrapperDirective } from './table-responsive-wrapper.directive';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [TableDirective, ColumnSortableDirective],
-    exports: [TableDirective, ColumnSortableDirective]
+    declarations: [TableDirective, TableResponsiveWrapperDirective, ColumnSortableDirective],
+    exports: [TableDirective, TableResponsiveWrapperDirective, ColumnSortableDirective]
 })
 export class TableModule {}
