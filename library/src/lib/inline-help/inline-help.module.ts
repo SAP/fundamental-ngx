048b3846a8c2c2d52587f229a8a6b2428b878f38
Matt Lajoie <mlajoie03@gmail.com>
2019-07-05 13:50:40 -0400

    Removed barrel imports and references to index.ts files

--- library/src/lib/inline-help/inline-help.module.ts
+++ library/src/lib/inline-help/inline-help.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InlineHelpComponent } from './inline-help.component';
-import { PopoverModule } from '../popover';
+import { PopoverModule } from '../popover/public_api';
 
 @NgModule({
     imports: [CommonModule, PopoverModule],
