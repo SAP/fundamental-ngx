084dc40a80fa47ef48e095aca60ccb18f221a1b6
Matt Lajoie <mlajoie03@gmail.com>
2019-07-16 09:47:55 -0400

    improved schema

--- library/schematics/ng-add/setup-project.ts
+++ library/schematics/ng-add/setup-project.ts
@@ -1,3 +1,9 @@
+import { chain, noop, Rule, Tree } from '@angular-devkit/schematics';
+
 const browserAnimationsModuleName = 'BrowserAnimationsModule';
 const noopAnimationsModuleName = 'NoopAnimationsModule';
+
+function setup(options: any): Rule {
+    return;
+}
 
