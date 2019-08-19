379e419627f001b578de7ebb37d4cfeb739953fa
deno <mladen.droshev@sap.com>
2019-08-18 22:51:53 -0400

    #1140 fix the localization issue on day view after changing i18n

--- docs/app/documentation/component-docs/date-picker/examples/date-picker-i18n-example.component.ts
+++ docs/app/documentation/component-docs/date-picker/examples/date-picker-i18n-example.component.ts
@@ -9,29 +9,59 @@ const localized_values = {
         months: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Déc'],
         fullMonths: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet',
             'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+    },
+    'de': {
+        weekdays: ['Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag', 'Montag'],
+        months: [' Jän', 'Feb', 'März', 'Apr', 'Mai', 'Juni', 'Juli', 'Aug', 'Sept', 'Okt', 'Nov', 'Dez'],
+        fullMonths: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
+    },
+    'bg': {
+        weekdays: ['неделя', 'понеделник', 'вторник', 'сряда', 'четвъртък', 'петък', 'събота'],
+        months: [' яну', 'фев', 'март', 'апр', 'май', 'юни', 'юли', 'авг', 'септ', 'окт', 'ное', 'дек'],
+        fullMonths: ['януари', 'февруари', 'март', 'април', 'май', 'юни', 'юли', 'август', 'септември', 'октомври', 'ноември', 'декември']
     }
 };
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LanguageService {
+
+    selectedLanguage: string = 'bg';
+
+    setLanguage(newLanguage: string) {
+        this.selectedLanguage = newLanguage;
+    }
+
+    getLanguage() { return this.selectedLanguage; }
+}
 
 @Injectable()
 export class CustomCalendarI18n extends CalendarI18n {
 
     // You could also define a custom service and inject it here
-    language: string = 'fr';
+    //language: string = 'fr';
+    languageService: LanguageService;
+
+    constructor(languageService: LanguageService) {
+        super();
+        this.languageService = languageService;
+    }
 
     getDayAriaLabel(date: Date): string {
-        return date.getDate() + ' ' + localized_values[this.language].fullMonths[date.getMonth()] + ' ' + date.getFullYear();
+        return date.getDate() + ' ' + localized_values[this.languageService.getLanguage()].fullMonths[date.getMonth()] + ' ' + date.getFullYear();
     }
 
     getAllFullMonthNames(): string[] {
-        return localized_values[this.language].fullMonths;
+        return localized_values[this.languageService.getLanguage()].fullMonths;
     }
 
     getAllShortMonthNames(): string[] {
-        return localized_values[this.language].months;
+        return localized_values[this.languageService.getLanguage()].months;
     }
 
     getAllShortWeekdays(): string[] {
-        return localized_values[this.language].weekdays;
+        return localized_values[this.languageService.getLanguage()].weekdays;
     }
 }
 
@@ -55,7 +85,15 @@ export class CustomI18nLabels extends CalendarI18nLabels {
 @Component({
     selector: 'fd-datepicker-i18n-example',
     template: `
-        <fd-date-picker [(ngModel)]="date"></fd-date-picker>`,
+        <label fd-form-label for="language">Select language:</label>
+        <fd-button-group id="language" style="margin-bottom:20px">
+            <button fd-button-grouped [size]="'xs'" (click)="setFrench()" [state]="selected === 1 ? 'selected' : ''">French</button>
+            <button fd-button-grouped [size]="'xs'" (click)="setGerman()" [state]="selected === 2 ? 'selected' : ''">German</button>
+            <button fd-button-grouped [size]="'xs'" (click)="setBulgarian()" [state]="selected === 3 ? 'selected' : ''">Bulgarian</button>
+        </fd-button-group>
+        <br>
+        <fd-date-picker [(ngModel)]="date" [startingDayOfWeek]="2"></fd-date-picker>
+        `,
 
     // Note that this can be provided in the root of your application.
     providers: [
@@ -70,7 +108,26 @@ export class CustomI18nLabels extends CalendarI18nLabels {
     ]
 })
 export class DatePickerI18nExampleComponent {
+    languageService: LanguageService;
+    selected: number = 3;
+
+    constructor(languageService: LanguageService) {
+        this.languageService = languageService;
+    }
 
     date = FdDate.getToday();
+
+    setGerman() {
+        this.selected = 2;
+        this.languageService.setLanguage('de');
+    }
+    setFrench() {
+        this.selected = 1;
+        this.languageService.setLanguage('fr');
+    }
+    setBulgarian() {
+        this.selected = 3;
+        this.languageService.setLanguage('bg');
+    }
 
 }
