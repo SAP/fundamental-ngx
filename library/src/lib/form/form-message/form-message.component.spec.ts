f7ece8fd2c279fc4d9d4019d198fc1afbf84fa4c
junyang <jun.yang05@sap.com>
2019-08-20 09:14:24 -0400

    test on form set and form form message

--- library/src/lib/form/form-message/form-message.component.spec.ts
+++ library/src/lib/form/form-message/form-message.component.spec.ts
@@ -21,4 +21,32 @@ describe('FormMessageComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should append the proper class name --text', () => {
+        const type = 'text';
+        component.type = type;
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelector('span').classList).toContain('fd-form__message--' + type);
+    });
+
+    it('should append the proper class name --help', () => {
+        const type = 'help';
+        component.type = type;
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelector('span').classList).toContain('fd-form__message--' + type);
+    });
+
+    it('should append the proper class name --error', () => {
+        const type = 'error';
+        component.type = type;
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelector('span').classList).toContain('fd-form__message--' + type);
+    });
+
+    it('should append the proper class name --warning', () => {
+        const type = 'warning';
+        component.type = type;
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelector('span').classList).toContain('fd-form__message--' + type);
+    });
 });
--- library/src/lib/form/form-set/form-set.directive.spec.ts
+++ library/src/lib/form/form-set/form-set.directive.spec.ts
@@ -1,8 +1,43 @@
 import { FormSetDirective } from './form-set.directive';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormModule } from '../form.module';
+
+
+@Component({
+    template: `
+            <div #directiveElement fd-form-set>Form Parent Test Text</div>       
+            `
+})
+
+class TestComponent {
+    @ViewChild('directiveElement')
+    ref: ElementRef;
+}
 
 describe('FormSetDirective', () => {
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestComponent],
+            imports: [FormModule]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
     it('should create an instance', () => {
         const directive = new FormSetDirective();
         expect(directive).toBeTruthy();
+    });
+
+    it('should assign class', () => {
+        expect(component.ref.nativeElement.className).toBe('fd-form__set');
     });
 });
