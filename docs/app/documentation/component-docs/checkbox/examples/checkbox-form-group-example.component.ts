39085e7e9d11cc45b903557514e66250d4e9b3ab
SaiShan <sai-shan.gagnon@sap.com>
2019-08-28 14:56:30 -0400

    added disabled checkboxes in reactive form

--- docs/app/documentation/component-docs/checkbox/examples/checkbox-form-group-example.component.ts
+++ docs/app/documentation/component-docs/checkbox/examples/checkbox-form-group-example.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'fd-checkbox-form-group',
@@ -10,5 +10,11 @@ export class CheckboxFormGroupExampleComponent {
         firstOption: new FormControl(true),
         secondOption: new FormControl(false),
         thirdOption: new FormControl(false)
+    });
+
+    disabledCustomForm = new FormGroup({
+        disabledFirstOption: new FormControl({value: false, disabled: true}, Validators.required),
+        disabledSecondOption: new FormControl({value: false, disabled: true}, Validators.required),
+        disabledThirdOption: new FormControl({value: true, disabled: true }, Validators.required),
     });
 }
