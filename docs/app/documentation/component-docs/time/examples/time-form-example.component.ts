06246de1b6847950d898090e6737073110aa236b
droshev <mladen.droshev@sap.com>
2019-08-31 09:04:46 -0400

    #1179 improve time reactive form examples

--- docs/app/documentation/component-docs/time/examples/time-form-example.component.ts
+++ docs/app/documentation/component-docs/time/examples/time-form-example.component.ts
@@ -3,10 +3,17 @@ import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'fd-time-form-example',
+    styles: [`
+        .flex-form{
+            display: flex;
+            justify-content: space-between;
+        }
+    `],
     templateUrl: './time-form-example.component.html'
 })
 export class TimeFormExampleComponent {
     customForm = new FormGroup({
-        time: new FormControl('')
+        time: new FormControl(''),
+        disabledTime: new FormControl({ value: '', disabled: true })
     });
 };
