51dcd408092235d1230c3a1a6301877e3011a5b3
droshev <mladen.droshev@sap.com>
2019-08-31 12:08:39 -0400

    #1179 improve radio button reactive form examples

--- docs/app/documentation/component-docs/radio/examples/radio-form-group-example.component.ts
+++ docs/app/documentation/component-docs/radio/examples/radio-form-group-example.component.ts
@@ -3,10 +3,17 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'fd-radio-form-group',
-    templateUrl: './radio-form-group-example.component.html'
+    templateUrl: './radio-form-group-example.component.html',
+    styles: [`
+        .flex-form{
+            display:flex;
+            justify-content:space-between;
+        }
+    `]
 })
 export class RadioFormGroupExampleComponent {
     customForm = new FormGroup({
-        radioInput: new FormControl('')
+        radioInput: new FormControl({ value: 'option1', disabled: false }),
+        disabledRadio: new FormControl({ value: 'option13', disabled: true })
     });
 }
