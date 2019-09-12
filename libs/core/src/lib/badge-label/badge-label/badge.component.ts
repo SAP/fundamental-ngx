e69fdf51f4d09d2fb19f83f590b1cdfe1237c35e
jmarkowski <jmarkowski@divante.co>
2019-09-12 11:14:32 +0200

    Add labels, change them to components, change action bar naming (#1251)
    * Add labels, change them to components, change action bar naming
    
    * Change directives to component naming on spec

--- libs/core/src/lib/badge-label/badge-label/badge.component.ts
+++ libs/core/src/lib/badge-label/badge-label/badge.component.ts
@@ -1,15 +1,18 @@
-import { Input, Directive, ElementRef, HostBinding } from '@angular/core';
-import { AbstractFdNgxClass } from '../utils/abstract-fd-ngx-class';
+import { Component, ElementRef, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { AbstractFdNgxClass } from '../../utils/abstract-fd-ngx-class';
 
 /**
- * Badge directive, used to indicate status.
+ * Badge component, used to indicate status.
  * Colors, generally in combination with text, are used to easily highlight the state of an object.
  */
-@Directive({
-    // tslint:disable-next-line:directive-selector
-    selector: '[fd-badge]'
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: '[fd-badge]',
+    template: `<ng-content></ng-content>`,
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./badge-label.component.scss']
 })
-export class BadgeDirective extends AbstractFdNgxClass {
+export class BadgeComponent extends AbstractFdNgxClass {
     /** Color coded status for the badge. Options are 'success', 'warning', and 'error'. Leave empty for default badge. */
     @Input() status;
 
---
+++ libs/core/src/lib/badge-label/label/label.component.spec.ts
@@ -0,0 +1,53 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { LabelComponent } from './label.component';
+
+@Component({
+    template: `
+        <span fd-label>Fd-badge test</span>
+    `
+})
+
+export class TestComponent { }
+
+describe('LabelComponent', () => {
+    let fixture: ComponentFixture<TestComponent>,
+        debugElement: DebugElement,
+        element: HTMLElement;
+
+    let component, componentInstance;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LabelComponent, TestComponent]
+        });
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+        element = debugElement.nativeElement;
+
+        fixture.detectChanges();
+        component = debugElement.query(By.directive(LabelComponent));
+        componentInstance = component.injector.get(LabelComponent);
+
+
+        spyOn(componentInstance, '_setProperties').and.callThrough();
+        spyOn(componentInstance, '_addClassToElement');
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should add label class', () => {
+        componentInstance.status = 'someStatus';
+        componentInstance.ngOnInit();
+        expect(componentInstance._setProperties).toHaveBeenCalled();
+        expect(componentInstance._addClassToElement).toHaveBeenCalledWith('fd-label');
+        expect(componentInstance._addClassToElement).toHaveBeenCalledWith('fd-label--someStatus');
+    });
+});
