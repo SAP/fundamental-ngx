ccc28675015cecd17420d4bc73d701843b795502
Frantisek Kolar <fkolar@users.noreply.github.com>
2019-09-20 19:43:24 +0200

    chore: Updated npm publish functionality (#1284)
    * chore(build): Updated npm publish functionality
    
     - standard-version run now on root package.json
     - this updated version is then used to update both core,
        platform platform.json files
     - Added placeholder variables into package.json files to remove hardcoded
       dependencies
    - npm publish now support both version core, platform
    
    chore(build): Sync version files
    
    chore(build): Remove platform test script as there are none at the moment
    
    chore(build): Version replace is extracted to script so it can be reused
    
    chore(build): Add ng pack also for platform
    
    * chore(build): Add description to the package.json

---
+++ ci-scripts/sync-version.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -u -e
+
+
+#
+# Update Package version in the library package.json from root package.json
+#
+
+NEW_VERSION=$(node -p "require('./package.json').version")
+echo "Updating packages.json under dist/libs with version ${NEW_VERSION}"
+
+ANGULAR_VERSION=$(node -p "require('./package.json').dependencies['@angular/core']")
+RXJS_VERSION=$(node -p "require('./package.json').dependencies['rxjs']")
+
+cd ./dist/
+
+grep -rl 'VERSION_PLACEHOLDER' . | xargs  perl -p -i -e "s/VERSION_PLACEHOLDER/${NEW_VERSION}/g"
+grep -rl 'ANGULAR_VER_PLACEHOLDER' . | xargs  perl -p -i -e "s/ANGULAR_VER_PLACEHOLDER/${ANGULAR_VERSION}/g"
+grep -rl 'RXJS_VER_PLACEHOLDER' . | xargs  perl -p -i -e "s/RXJS_VER_PLACEHOLDER/${RXJS_VERSION}/g"
+
