8ab9efa95aa275daaabb8cd349351a7fe6ba5f62
Frantisek Kolar <fkolar@users.noreply.github.com>
2019-09-22 20:48:49 +0200

    chore(build): Debugging travis (#1290)
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Debugging travis
    
    * chore(build): Travis build fix

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,98 +1,63 @@
 #!/usr/bin/env bash
 
 set -u -e
-args=("$@")
-
-if [ $# -eq 0 ]
-  then
-    printf "No arguments supplied\n"
-    exit 1
-fi
-
-
-if [[ ${args[0]}  != "rc"  &&  ${args[0]}  != "master" ]]; then
-
-    echo "Missing mandatory argument: . "
-    echo " - Usage: ./publish.sh  [deploy type]  "
-    echo "      type: rc | master"
-    exit 1
-fi
-
 
 git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
 
-
-
 PACKAGES=(core platform)
 CURRENT_BRANCH=master
 
-
-
-if [ ${args[0]} == "master" ]; then
+if [[ $TRAVIS_BUILD_STAGE_NAME =~ "Release" ]]; then
    echo "################ Running Master deploy tasks ################"
    CURRENT_BRANCH=master
 
   # delete temp branch
   git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" ":$TRAVIS_BRANCH" > /dev/null 2>&1;
-
-  echo "Running standard version"
   std_ver=$(npm run std-version)
   release_tag=$(echo "$std_ver" | grep "tagging release" | awk '{print $4}')
-
   echo "New release version: $std_ver"
 
 
-else
+
+
+elif [[ $TRAVIS_BUILD_STAGE_NAME =~ "Pre-release" ]]; then
    echo "################ Running RC deploy tasks ################"
 
    CURRENT_BRANCH=${TRAVIS_BRANCH}
    npm run std-version -- --prerelease rc --no-verify
 
+
+
+else
+   echo "Missing required stage name"
+   exit 1
 fi
 
 
-echo "Running GIT PUSH: $CURRENT_BRANCH"
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" $CURRENT_BRANCH > /dev/null 2>&1;
-
-
-
-echo "Building libraries and applications"
 npm run build-deploy-library
 
-npm -v
 
-#
-# Update Package version in the library package.json from root package.json
-#
-echo "current directory"
-pwd
-./ci-scripts/sync-version.sh
-
-echo "current directory"
-pwd
 
 cd dist/libs
-pwd
+NPM_BIN="$(which npm)"
 
 for P in ${PACKAGES[@]};
 do
     echo publish "@fundamental-ngx/${P}"
     cd ${P}
-    pwd
-    npm publish
+    $NPM_BIN  publish
     cd ..
 done
 
 cd ../../
 
 
-if [ ${args[0]} == "master" ]; then
-    echo "Run after publish to make sure GitHub finishes updating from the push"
-    npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch master
+if [[ $TRAVIS_BUILD_STAGE_NAME =~ "Release" ]]; then
 
+    npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch master
     npm run build-docs
     npm run deploy-docs -- --repo "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG"
-
 fi
 
