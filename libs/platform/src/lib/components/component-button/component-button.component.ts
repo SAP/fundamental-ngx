6756f37ca05c26746cc83acf878ff6164e2ff770
sushmakudum <sushma.kudum@sap.com>
2019-09-25 17:18:11 +0530

    fix:StaticinjectableError fix

---
+++ libs/platform/src/lib/components/component-button/component-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fdp-component-button',
+  templateUrl: './component-button.component.html',
+  styleUrls: ['./component-button.component.scss']
+})
+export class ComponentButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
