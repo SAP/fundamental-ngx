b6cf8fbb9a45cb791dadc7ac6c88e7c45a1adb3c
Kavya <kavya.bhat@sap.com>
2019-09-27 17:16:15 +0530

    feat(platform): #1264 added support for attributes
    such as:
    disabled,
    tooltip customization,
    keyboard accessibility,
    var colors to scss,
    
    fixed compiler error issue

--- libs/platform/src/lib/components/menu/menu-comp/menu-item.component.ts
+++ libs/platform/src/lib/components/menu/menu-comp/menu-item.component.ts
@@ -1,60 +1,63 @@
 import {
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-  Output,
-  EventEmitter
+    Component,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output,
+    EventEmitter,
+    AfterViewInit,
+    ViewEncapsulation
 } from '@angular/core';
-import {
-  MenuItem,
-  MenuGroup
-} from './menu.component';
+import { MenuItem, MenuGroup } from './menu.component';
 import { Highlightable } from '@angular/cdk/a11y';
 
 @Component({
-  selector: 'fdp-menu-item',
-  templateUrl: './menu-item.component.html',
-  styleUrls: ['./menu-item.component.scss']
+    selector: 'fdp-menu-item',
+    templateUrl: './menu-item.component.html',
+    styleUrls: ['./menu-item.component.scss'],
+    encapsulation: ViewEncapsulation.None
+    // providers: [MenuKeyboardService]
 })
-export class MenuItemComponent implements OnInit, OnDestroy, Highlightable {
+export class MenuItemComponent implements OnInit, OnDestroy, /*DefaultMenuItem, */ AfterViewInit, Highlightable {
+    @Input() public label: string;
+    @Input() public index: string;
+    @Input() public icon: string;
+    @Input() public selectable: boolean;
+    @Input() public selected: boolean;
+    @Input() public secondaryIcon: string;
 
-  @Input() public label: string;
-  @Input() public index: string;
-  @Input() public icon: string;
-  @Input() public selectable: boolean;
-  @Input() public selected: boolean;
-  @Input() public secondaryIcon: string;
+    @Input() public item: MenuItem;
+    @Input() public group: MenuGroup;
 
-  @Input() public item: MenuItem;
-  @Input() public group: MenuGroup;
+    @Input() public separated: boolean;
+    @Input() public disabled: boolean;
+    @Input() public customLabel: string;
 
-  @Input() public separated: boolean;
+    public _isActive = false;
 
-  public _isActive = false;
+    @Output() itemClick: EventEmitter<void> = new EventEmitter();
 
-  @Output() itemClick: EventEmitter<void> = new EventEmitter();
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {
+        // this._isActive = this.selected ? true : false;
+    }
 
-  ngOnInit() {
+    ngOnDestroy() {}
 
-  }
+    ngAfterViewInit() {
+        // this.menuKeyboardService.focusEscapeAfterList = () => {};
+    }
 
-  ngOnDestroy() {
+    setActiveStyles() {
+        this._isActive = true;
+    }
 
-  }
+    setInactiveStyles() {
+        this._isActive = false;
+    }
 
-  setActiveStyles() {
-    this._isActive = true;
-  }
-
-  setInactiveStyles() {
-    this._isActive = false;
-  }
-
-  onItemClick() {
-    this.itemClick.emit();
-  }
-
+    onItemClick() {
+        this.itemClick.emit();
+    }
 }
---
+++ libs/platform/src/lib/components/menu/public_api.ts
@@ -0,0 +1,3 @@
+export * from './menu.module';
+export * from './menu-comp/menu-item.component';
+export * from './menu-comp/menu.component';
