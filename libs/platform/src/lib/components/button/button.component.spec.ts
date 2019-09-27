3696265d4218cac8a982898a580dbbbc255303a3
sushmakudum <sushma.kudum@sap.com>
2019-09-27 15:49:13 +0530

    fix: fdtype changed to type and mapped to fdtype of button directive

--- libs/platform/src/lib/components/button/button.component.spec.ts
+++ libs/platform/src/lib/components/button/button.component.spec.ts
@@ -1,25 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonComponent } from './button.component';
+import { ButtonModule } from '@fundamental-ngx/core';
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+    let component: ButtonComponent;
+    let fixture: ComponentFixture<ButtonComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ButtonModule],
+            declarations: [ButtonComponent]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ButtonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have a content disabled button', () => {
+        spyOn(component, 'setDisabledState');
+        component.setDisabledState(true);
+        expect(component.setDisabledState).toHaveBeenCalled();
+    });
+
+    it('size should set to small', () => {
+        component.size = 'small';
+        spyOn(component, '_setProperties').and.callThrough();
+        spyOn(component, '_addClassToElement');
+        component._setProperties();
+        expect(component._addClassToElement).toHaveBeenCalledWith('fd-button--compact');
+    });
+
+    it('button onclick should be called', () => {
+        spyOn(component, 'onBtnClick');
+        component.onBtnClick(event);
+        expect(component.onBtnClick).toHaveBeenCalled();
+    });
 });
