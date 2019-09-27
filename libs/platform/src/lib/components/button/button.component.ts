286bbc8b21fb3bf04f02d5cbd4ea58470981883a
sushmakudum <sushma.kudum@sap.com>
2019-09-27 15:49:13 +0530

    fix: fdtype changed to type and mapped to fdtype of button directive

--- libs/platform/src/lib/components/button/button.component.ts
+++ libs/platform/src/lib/components/button/button.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, ElementRef } from '@angular/core';
+import { Component, Input, ElementRef, Output, EventEmitter, OnInit } from '@angular/core';
 
-export type ButtonType = 'standard' | 'positive' | 'medium' | 'negative' | 'toolbar' | 'main' | '';
+export type ButtonType = 'standard' | 'positive' | 'medium' | 'negative' | 'toolbar' | 'main';
 export type ButtonOptions = 'light' | 'emphasized' | '';
+export type sizeType = 'small' | '';
 
 @Component({
     selector: 'fdp-button',
@@ -10,21 +11,54 @@ export type ButtonOptions = 'light' | 'emphasized' | '';
 })
 export class ButtonComponent {
     private _elementRef: ElementRef;
-    /** Whether to apply compact mode to the button. */
-    @Input() compact: boolean;
 
-    /** The icon to include in the button. See the icon page for the list of icons. */
+    /** Whether to apply size of the button. */
+    @Input() size: sizeType;
+
+    /** The icon to include in the button */
     @Input() glyph: string;
+
+    /** Whether the date picker is disabled. */
+    @Input()
+    disabled: boolean;
 
     /** The type of the button. Types include 'standard', 'positive', 'medium', and 'negative'.
      * Leave empty for default (Action button).'*/
-    @Input() fdType: ButtonType;
+    @Input() type: ButtonType;
 
     /** Button options.  Options include 'emphasized' and 'light'. Leave empty for default.' */
     @Input() options: ButtonOptions | ButtonOptions[];
 
+    /** Event sent when button is clicked */
+    @Output()
+    buttonClicked = new EventEmitter();
+
+    /**
+     *  Handles button click
+     */
+    public onBtnClick($event: any) {
+        this.buttonClicked.emit();
+    }
+
     /** @hidden */
     constructor(private elementRef: ElementRef) {
         this._elementRef = elementRef;
+    }
+
+    /** @hidden */
+    setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    /** @hidden */
+    _addClassToElement(className: string) {
+        (this._elementRef.nativeElement as HTMLElement).classList.add(...className.split(' '));
+    }
+    // /** @hidden */
+    _setProperties() {
+        this._addClassToElement('fd-button');
+        if (this.size === 'small') {
+            this._addClassToElement('fd-button--compact');
+        }
     }
 }
