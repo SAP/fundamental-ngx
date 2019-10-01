08b90fcc924f888b5f96b76c6f594e971610614c
Inna Atanasova <i.atanasova@sap.com>
2019-10-01 16:44:48 -0400

    fix: self-contained adoption issues

--- apps/docs/src/app/documentation/component-docs/popover/examples/popover-placement/popover-placement-example.component.ts
+++ apps/docs/src/app/documentation/component-docs/popover/examples/popover-placement/popover-placement-example.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-popover-placement-example',
-    templateUrl: './popover-placement-example.component.html'
+    templateUrl: './popover-placement-example.component.html',
+    styleUrls: ['./popover-placement-example.scss'],
 })
 export class PopoverPlacementExampleComponent {
 
     menu1 = [
-        {text: 'Option 1', url: '#'},
-        {text: 'Option 2', url: '#'},
-        {text: 'Option 3', url: '#'}
+        { text: 'Option 1', url: '#' },
+        { text: 'Option 2', url: '#' },
+        { text: 'Option 3', url: '#' }
     ];
 
 }
