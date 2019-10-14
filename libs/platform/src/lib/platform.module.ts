5e26b37fab08152362ef00701c814a323cc02615
manu-kr <53512176+manu-kr@users.noreply.github.com>
2019-10-14 14:08:49 +0530

    feat(platform): Code changes to convert action bar as a component
    * feat(platform):code changes to enhance the action bar as a component
    
    * feat(platform):code changes to enhance the action bar as a component

--- libs/platform/src/lib/platform.module.ts
+++ libs/platform/src/lib/platform.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
 import { PlatformButtonModule } from './components/button/button.module';
-
+import { ActionBarModule } from './components/action-bar/action-bar.module';
 @NgModule({
-    imports: [
-        CommonModule,
-        FundamentalNgxCoreModule
-    ],
-    exports: [PlatformButtonModule]
+    imports: [CommonModule, FundamentalNgxCoreModule],
+    exports: [PlatformButtonModule, ActionBarModule]
 })
-export class FundamentalNgxPlatformModule { }
+export class FundamentalNgxPlatformModule {}
