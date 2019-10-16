447ac7644f42704f04bf0782b372ee7f0185d735
Kavya <kavya.bhat@sap.com>
2019-10-16 18:04:12 +0530

    style(platform): removed an incorrect color value from fundamental styles
    added MenuModule back into platform.module.ts after it got removed while merging conflicts

--- libs/platform/src/lib/platform.module.ts
+++ libs/platform/src/lib/platform.module.ts
@@ -4,9 +4,10 @@ import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
 import { PlatformButtonModule } from './components/button/button.module';
 import { PlatformSearchInputModule } from './components/search-input/search-input.module';
 import { ActionBarModule } from './components/action-bar/action-bar.module';
+import { MenuModule } from './components/menu/menu.module';
 
 @NgModule({
     imports: [CommonModule, FundamentalNgxCoreModule],
-    exports: [PlatformButtonModule, PlatformSearchInputModule, ActionBarModule]
+    exports: [PlatformButtonModule, PlatformSearchInputModule, ActionBarModule, MenuModule]
 })
 export class FundamentalNgxPlatformModule {}
