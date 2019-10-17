a5fabc6b8f744b308f766000abf013553f1049f6
i505460 <lokanathan.k@sap.com>
2019-10-17 09:40:50 +0530

    refactor(core): need pipe module for platform code
    refactor(core): Exposeing the Pipe module to the platform code for the code reuse issue number: 1268

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,4 +1,4 @@
-import { angularCliVersion } from '@nrwl/schematics/src/lib-versions';
+import {angularCliVersion} from '@nrwl/schematics/src/lib-versions';
 
 export * from './lib/fundamental-ngx-core.module';
 export * from './lib/action-bar/public_api';
