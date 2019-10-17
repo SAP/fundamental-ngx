c917f516c9a0d53085248e20cb7f3786ad3f9e7b
Stefano Scalzo <stefano.scalzo@hotmail.com>
2019-10-17 16:24:47 -0400

    deleted files

--- apps/docs/src/app/documentation/core-helpers/code-example/code-example-stack/app_app_component.ts
+++
@@ -1,9 +0,0 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//     selector: '${this.parameters.html_tag}',
-//     templateUrl: './${this.parameters.app_component_html}'
-// })
-// export class $ {this.parameters.app_component} {
-//     title = 'my-fd-ngx-dream-app';
-// }
