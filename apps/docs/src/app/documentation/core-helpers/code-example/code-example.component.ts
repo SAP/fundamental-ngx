a5b4871284bd544999b9735a64254815e76f399f
Stefano Scalzo <stefano.scalzo@hotmail.com>
2019-10-17 16:08:26 -0400

    changed angular file to a seperate file

--- apps/docs/src/app/documentation/core-helpers/code-example/code-example.component.ts
+++ apps/docs/src/app/documentation/core-helpers/code-example/code-example.component.ts
@@ -16,6 +16,7 @@ import { AlertService } from '@fundamental-ngx/core';
 import hljs from 'highlight.js/lib';
 import sdk from '@stackblitz/sdk';
 import * as polyfills from '!raw-loader!./code-example-stack/polyfills.ts';
+import * as angularJson from '!raw-loader!./code-example-stack/helper.js';
 // import * as maints from '!raw-loader!./code-example-stack/main.ts';
 
 
@@ -70,127 +71,7 @@ export class CodeExampleComponent implements OnInit, AfterViewInit {
         .catch(err => console.error(err));`
 
 
-    angular_json = `
-        {
-          "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
-          "version": 1,
-          "newProjectRoot": "projects",
-          "projects": {
-            "my-fd-ngx-dream": {
-              "projectType": "application",
-              "schematics": {
-                "@schematics/angular:component": {
-                  "style": "scss"
-                }
-              },
-              "root": "",
-              "sourceRoot": "src",
-              "prefix": "",
-              "architect": {
-                "build": {
-                  "builder": "@angular-devkit/build-angular:browser",
-                  "options": {
-                    "outputPath": "dist/my-fd-ngx-dream",
-                    "index": "src/index.html",
-                    "main": "src/main.ts",
-                    "polyfills": "src/polyfills.ts",
-                    "tsConfig": "tsconfig.app.json",
-                    "aot": false,
-                    "assets": [
-                      "src/favicon.ico",
-                      "src/assets"
-                    ],
-                    "styles": [
-                      "src/styles.scss"
-                    ],
-                    "scripts": []
-                  },
-                  "configurations": {
-                    "production": {
-                      "optimization": true,
-                      "outputHashing": "all",
-                      "sourceMap": false,
-                      "extractCss": true,
-                      "namedChunks": false,
-                      "aot": true,
-                      "extractLicenses": true,
-                      "vendorChunk": false,
-                      "buildOptimizer": true,
-                      "budgets": [
-                        {
-                          "type": "initial",
-                          "maximumWarning": "2mb",
-                          "maximumError": "5mb"
-                        }
-                      ]
-                    }
-                  }
-                },
-                "serve": {
-                  "builder": "@angular-devkit/build-angular:dev-server",
-                  "options": {
-                    "browserTarget": "my-fd-ngx-dream:build"
-                  },
-                  "configurations": {
-                    "production": {
-                      "browserTarget": "my-fd-ngx-dream:build:production"
-                    }
-                  }
-                },
-                "extract-i18n": {
-                  "builder": "@angular-devkit/build-angular:extract-i18n",
-                  "options": {
-                    "browserTarget": "my-fd-ngx-dream:build"
-                  }
-                },
-                "test": {
-                  "builder": "@angular-devkit/build-angular:karma",
-                  "options": {
-                    "main": "src/test.ts",
-                    "polyfills": "src/polyfills.ts",
-                    "tsConfig": "tsconfig.spec.json",
-                    "karmaConfig": "karma.conf.js",
-                    "assets": [
-                      "src/favicon.ico",
-                      "src/assets"
-                    ],
-                    "styles": [
-                      "src/styles.scss"
-                    ],
-                    "scripts": []
-                  }
-                },
-                "lint": {
-                  "builder": "@angular-devkit/build-angular:tslint",
-                  "options": {
-                    "tsConfig": [
-                      "tsconfig.app.json",
-                      "tsconfig.spec.json",
-                      "e2e/tsconfig.json"
-                    ],
-                    "exclude": [
-                      "**/node_modules/**"
-                    ]
-                  }
-                },
-                "e2e": {
-                  "builder": "@angular-devkit/build-angular:protractor",
-                  "options": {
-                    "protractorConfig": "e2e/protractor.conf.js",
-                    "devServerTarget": "my-fd-ngx-dream:serve"
-                  },
-                  "configurations": {
-                    "production": {
-                      "devServerTarget": "my-fd-ngx-dream:serve:production"
-                    }
-                  }
-                }
-              }
-            }
-          },
-          "defaultProject": "my-fd-ngx-dream"
-        }
-        `;
+    angular_json = angularJson;
 
     project = {
         files: {},
