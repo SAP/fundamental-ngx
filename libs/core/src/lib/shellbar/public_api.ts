a043b8bf396b46b8167a8444222b102bfc953bd2
jmarkowski <jedrzej.markowski@sap.com>
2019-10-21 11:07:52 +0200

    fix: Add links and glyphs to product menu (#1421)
    * Add links and glyphs to product menu
    
    * Change typings, add it to use menu
    
    * trigger build

--- libs/core/src/lib/shellbar/public_api.ts
+++ libs/core/src/lib/shellbar/public_api.ts
@@ -2,6 +2,7 @@ export * from './shellbar.module';
 export * from './model/shellbar-menu-item';
 export * from './model/shellbar-product';
 export * from './model/shellbar-user';
+export * from './model/shellbar-user-menu';
 export * from './product-menu/product-menu.component';
 export * from './shellbar-actions/shellbar-actions.component';
 export * from './shellbar.component';
--- libs/core/src/lib/shellbar/user-menu/shellbar-user-menu.component.ts
+++ libs/core/src/lib/shellbar/user-menu/shellbar-user-menu.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PopoverComponent } from '../../popover/popover.component';
 import { ShellbarMenuItem } from '../model/shellbar-menu-item';
 import { ShellbarUser } from '../model/shellbar-user';
+import { ShellbarUserMenu } from '../model/shellbar-user-menu';
 
 /**
  * This Component extends popover component and passes all of the options and events from outside to popover component
@@ -19,7 +20,7 @@ export class ShellbarUserMenuComponent extends PopoverComponent {
 
     /** The user menu data. */
     @Input()
-    userMenu: ShellbarMenuItem[];
+    userMenu: ShellbarUserMenu[];
 
     /** Event emitted on item click */
     @Output()
@@ -28,8 +29,10 @@ export class ShellbarUserMenuComponent extends PopoverComponent {
     /**
      * @hidden
      */
-    itemClick(item: any, event: any): void {
+    itemClick(item: ShellbarUserMenu, event: any): void {
         this.itemClicked.emit();
-        item.callback(event);
+        if (item.callback) {
+            item.callback(event);
+        }
     }
 }
