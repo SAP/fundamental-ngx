0e15929a68dbfa8b5d80a997e30fe424eb44dbb9
i505460 <lokanathan.k@sap.com>
2019-11-04 13:17:10 +0530

    feat: (platform and core)adding new component select under platform
    feat: (platform and core)adding new component select under platform #…

--- libs/platform/src/lib/components/select-platform/select-platform.component.ts
+++ libs/platform/src/lib/components/select-platform/select-platform.component.ts
@@ -4,33 +4,34 @@ import { Component, OnInit, Input, TemplateRef } from '@angular/core';
     selector: 'fdp-select',
     templateUrl: './select-platform.component.html',
     styleUrls: ['./select-platform.component.scss']
-   })
+})
 export class SelectPlatformComponent implements OnInit {
+
+    /** variable used to inject the value from platform to core compomnent to select the type of button. */
     @Input()
     selectType: string = '';
 
+    /** this variable is to input the icon in the select component. */
     @Input()
     gylph: string = '';
 
+    /** variable to input any type of object. */
     @Input()
     list: any[] = [];
 
+    /** variable to allow the compact sizing of the select component. */
     @Input()
     compact: boolean = false;
 
+    /** variable to allow the accessing of the selcted value. */
     @Input()
     value: any;
 
+    /** placeholder value for the selcect component. */
     @Input()
     placeholder: string = '';
 
-    /**
-    * The template with which to display the individual listed items.
-    * Use it by passing an ng-template with implicit content. See examples for more info.
-    */
-    @Input()
-    itemTemplate: TemplateRef<any>;
-
+    /** overriding methord for the allowing to handle complex object. */
     @Input()
     displayFn: Function = this.defaultDisplay;
 
