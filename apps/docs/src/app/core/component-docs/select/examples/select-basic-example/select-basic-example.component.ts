1f38f3abf0f5fec707c22279a266a65e8aa448ef
jmarkowski <jedrzej.markowski@sap.com>
2019-11-20 08:52:09 +0100

    Add unselectMissingOption to select component (#1605)

--- apps/docs/src/app/core/component-docs/select/examples/select-basic-example/select-basic-example.component.ts
+++ apps/docs/src/app/core/component-docs/select/examples/select-basic-example/select-basic-example.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-select-basic-example',
     templateUrl: './select-basic-example.component.html',
     styleUrls: ['select-basic-example.component.scss']
 })
-export class SelectBasicExampleComponent implements OnInit {
+export class SelectBasicExampleComponent {
 
     selectOneValue: string;
 
     selectTwoValue: string;
 
     selectThreeValue: string;
-
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
 
 }
