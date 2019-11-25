b955522e53c81966d113e4d0a510c14662a7ad80
stefanoScalzo <50607147+stefanoScalzo@users.noreply.github.com>
2019-11-25 09:47:32 -0500

    docs: (core) add form-array (#1625)
    * add form array
    
    * add fd-button
    
    * resolved comment

--- apps/docs/src/app/core/component-docs/input/examples/input-form-group-example.component.ts
+++ apps/docs/src/app/core/component-docs/input/examples/input-form-group-example.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
-    selector: 'fd-input-form-group-example',
-    templateUrl: './input-form-group-example.component.html',
-    styleUrls: ['input-form-group-example.component.scss']
+  selector: 'fd-input-form-group-example',
+  templateUrl: './input-form-group-example.component.html',
+  styleUrls: ['input-form-group-example.component.scss']
 })
-export class InputFormGroupExampleComponent {
-    customForm = new FormGroup({
-        inputControl: new FormControl('', Validators.required),
-        disabledInputControl: new FormControl({ value: 'initial value', disabled: true }, Validators.required)
-    });
+export class InputFormGroupExampleComponent implements OnInit {
+
+  myForm: FormGroup;
+  arr: FormArray;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      inputControl: new FormControl('', Validators.required),
+      disabledInputControl: new FormControl({ value: 'initial value', disabled: true }, Validators.required),
+      arr: this.fb.array([this.createItem()])
+    })
+  }
+
+  createItem() {
+    return this.fb.group({
+      primaryInput: [''],
+      secondaryInput: ['']
+    })
+  }
+
+  addItem() {
+    this.arr = this.myForm.get('arr') as FormArray;
+    this.arr.push(this.createItem());
+  }
+
+  onSubmit() {
+  }
 }
