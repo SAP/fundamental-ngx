e02dd3f0f5e1c8f5b2fab9fb76f5ff13346e1e94
jmarkowski <jedrzej.markowski@sap.com>
2019-11-26 13:44:15 +0100

    Change way how the optional methods passed to default-notification are used (#1634)

--- libs/core/src/lib/notification/notification-utils/default-notification/default-notification.component.ts
+++ libs/core/src/lib/notification/notification-utils/default-notification/default-notification.component.ts
@@ -14,4 +14,22 @@ export class DefaultNotificationComponent {
     /** */
     type?: NotificationType;
 
+    handleCloseButtonClick(): void {
+        if (this.defaultConfigurationNotification.closeButtonCallback) {
+            this.defaultConfigurationNotification.closeButtonCallback();
+        }
+    }
+
+    handleApproveButtonClick(): void {
+        if (this.defaultConfigurationNotification.approveCallback) {
+            this.defaultConfigurationNotification.approveCallback();
+        }
+    }
+
+    handleCancelButtonClick(): void {
+        if (this.defaultConfigurationNotification.cancelCallback) {
+            this.defaultConfigurationNotification.cancelCallback();
+        }
+    }
+
 }
