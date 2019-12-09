ca94c860c412539a0c70e6a3f7a561f40664fc0c
Lokanathan-k <53534379+Lokanathan-k@users.noreply.github.com>
2019-12-09 15:00:57 +0530

    feat(platform): select component  for platform to enhance the core (#1694)
    * feat(platform): adding new wrapper select component  for platform
    
    feat(platform): adding new wrapper select component  for platform
    
    * feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    * feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    * feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    *  feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    * feat(platform): adding new wrapper select component  for platform
    
    feat(platform): adding new wrapper select component  for platform
    
    feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform
    
    *  feat(platform): adding new wrapper select component for platform
    
    feat(platform): adding new wrapper select component for platform

--- libs/platform/src/lib/platform.module.ts
+++ libs/platform/src/lib/platform.module.ts
@@ -5,9 +5,10 @@ import { PlatformButtonModule } from './components/button/button.module';
 import { PlatformSearchInputModule } from './components/search-input/search-input.module';
 import { ActionBarModule } from './components/action-bar/action-bar.module';
 import { FdpMenuModule } from './components/menu/menu.module';
+import { PlatformSelectModule } from './components/select/select.module';
 
 @NgModule({
     imports: [CommonModule, FundamentalNgxCoreModule],
-    exports: [PlatformButtonModule, PlatformSearchInputModule, ActionBarModule, FdpMenuModule]
+    exports: [PlatformButtonModule, PlatformSearchInputModule, ActionBarModule, FdpMenuModule, PlatformSelectModule]
 })
 export class FundamentalNgxPlatformModule {}
