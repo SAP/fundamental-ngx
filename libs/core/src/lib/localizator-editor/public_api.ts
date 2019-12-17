9094c46baa8c06e5fc2102e7895d20703e9e9f69
Jedrzej <jedrzej.markowski@sap.com>
2019-12-17 17:39:19 +0100

    Add exports to public_api, add @angular/cdk to schematics

--- libs/core/src/lib/localizator-editor/public_api.ts
+++ libs/core/src/lib/localizator-editor/public_api.ts
@@ -2,3 +2,4 @@ export * from './localization-editor.module';
 export * from './localization-editor-item/localization-editor-item.component';
 export * from './localization-editor-main/localization-editor-main.component';
 export * from './localization-editor.directives';
+export * from './localization-editor.component';
