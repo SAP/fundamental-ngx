b76890bf8e8c4b6175112334d00e691253fc18bd
gorecki gracjan <gorecki.gracjan@hotmail.com>
2020-01-02 14:28:26 +0100

    removed unused import

--- libs/core/src/lib/radio/radio.module.ts
+++ libs/core/src/lib/radio/radio.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RadioGroupComponent } from './radio-group/radio-group.component';
 import { RadioButtonComponent } from './radio-button/radio-button.component';
 import { FormsModule } from '@angular/forms';
 
