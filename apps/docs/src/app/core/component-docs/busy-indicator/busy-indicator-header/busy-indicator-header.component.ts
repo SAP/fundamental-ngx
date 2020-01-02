ef2c186b0cc12aa2febd3622f0f3826e1d2fafb4
Stefano Scalzo <stefano.scalzo@hotmail.com>
2020-01-02 11:41:00 -0500

    removed oninit

--- apps/docs/src/app/core/component-docs/busy-indicator/busy-indicator-header/busy-indicator-header.component.ts
+++ apps/docs/src/app/core/component-docs/busy-indicator/busy-indicator-header/busy-indicator-header.component.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-busy-indicator-header',
     templateUrl: './busy-indicator-header.component.html',
 })
-export class BusyIndicatorHeaderComponent implements OnInit {
-
-    constructor() { }
-
-    ngOnInit() {
-    }
+export class BusyIndicatorHeaderComponent {
 
 }
