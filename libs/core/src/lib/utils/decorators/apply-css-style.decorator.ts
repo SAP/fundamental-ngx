56b091491111a983faae79d2839914124ac2edb8
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2020-01-09 22:38:42 +0100

    fix: null check in decorator added (#1784)
    * upstream synced with fork
    
    * added null check for elementRef
    
    * removed changes from new_component and readme
    
    * changes removed from readme.md

--- libs/core/src/lib/utils/decorators/apply-css-style.decorator.ts
+++ libs/core/src/lib/utils/decorators/apply-css-style.decorator.ts
@@ -11,15 +11,20 @@ import { Hash, ELEMENT_REF_EXCEPTION } from '../public_api';
  */
 export function applyCssStyle(target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
     const originalMethod = descriptor.value;
-    descriptor.value = function (): Hash<number | string> {
-        if (!this.elementRef) { throw ELEMENT_REF_EXCEPTION; }
+    descriptor.value = function(): Hash<number | string> {
+        if (!this.elementRef) {
+            throw ELEMENT_REF_EXCEPTION;
+        }
 
         const _styles: Hash<number | string> = originalMethod.apply(this);
+        const elementRef = this.elementRef();
 
-        Object.keys(_styles).forEach(key => {
-            (this.elementRef().nativeElement as HTMLElement).style[key] = _styles[key];
-        });
+        if (elementRef) {
+            Object.keys(_styles).forEach(key => {
+                (elementRef.nativeElement as HTMLElement).style[key] = _styles[key];
+            });
+        }
 
         return _styles;
-    }
+    };
 }
