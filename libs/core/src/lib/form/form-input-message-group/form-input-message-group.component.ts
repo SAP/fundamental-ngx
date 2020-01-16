1e87c7b708f4815137d00c1d38443235e9bb8f48
Stefano Scalzo <stefano.scalzo@hotmail.com>
2020-01-16 15:16:19 -0500

    add text area

--- libs/core/src/lib/form/form-input-message-group/form-input-message-group.component.ts
+++ libs/core/src/lib/form/form-input-message-group/form-input-message-group.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, Input, ViewChild, Output, EventEmitter, ContentChild, AfterContentInit, ElementRef, ViewEncapsulation } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 import { MessageStates } from '../form-message/form-message.component';
-import { PopoverComponent } from '../../popover/public_api';
-import { FormMessageComponent } from '../form-message/form-message.component';
-import { FormControlDirective } from '../form-control/form-control.directive';
 
 @Component({
   selector: 'fd-form-input-message-group',
@@ -11,7 +8,7 @@ import { FormControlDirective } from '../form-control/form-control.directive';
   encapsulation: ViewEncapsulation.None
 
 })
-export class FormInputMessageGroupComponent implements OnInit, AfterContentInit {
+export class FormInputMessageGroupComponent {
 
   @Input() state: string;
   @Input() compact: boolean = false;
@@ -21,48 +18,8 @@ export class FormInputMessageGroupComponent implements OnInit, AfterContentInit 
   @Input()
   open: boolean = false;
 
-  /** @hidden */
-  @ViewChild(PopoverComponent, { static: false })
-  popoverRef: PopoverComponent;
-
-  @ContentChild(FormMessageComponent, { static: false }) message: FormMessageComponent;
-  @ContentChild(FormControlDirective, { read: ElementRef, static: false }) input: FormControlDirective;
-
   @Output()
   readonly openChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-
-  /** @hidden */
-  openChangeHandle(open: boolean): void {
-    this.open = open;
-    this.openChange.emit(this.open);
-    // this.onTouched();
-    // if (this.open) {
-    //     this.focusTrap.activate();
-    // } else {
-    //     this.focusTrap.deactivate();
-    // }
-  }
-
   constructor() { }
-
-  ngOnInit() {
-    if (this.state === 'valid') {
-      this.type = 'success';
-    }
-    else if (this.state === 'invalid') {
-      this.type = 'error';
-    }
-    else if (this.state === 'warning') {
-      this.type = 'warning';
-    }
-    else if (this.state === 'information') {
-      this.type = 'information';
-    }
-  }
-
-  ngAfterContentInit() {
-    console.log(this.input)
-  }
-
 }
