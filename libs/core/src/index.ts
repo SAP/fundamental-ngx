35227fa9fb073165a1a7db651cbf9610c34f55ad
Gracjan Gorecki <gorecki.gracjan@hotmail.com>
2020-01-22 11:27:15 +0100

    added missing checkbox exports

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -9,6 +9,7 @@ export * from './lib/busy-indicator/public_api';
 export * from './lib/button/public_api';
 export * from './lib/button-group/public_api';
 export * from './lib/calendar/public_api';
+export * from './lib/checkbox/public_api';
 export * from './lib/combobox/public_api';
 export * from './lib/date-picker/public_api';
 export * from './lib/datetime-picker/public_api';
