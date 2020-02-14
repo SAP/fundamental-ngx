5d255904165b0b38993c603f4cac2764063f1a65
Stefano Scalzo <stefano.scalzo@hotmail.com>
2020-02-14 13:22:56 -0500

    add compact

--- apps/docs/src/app/core/component-docs/table/examples/table-checkboxes-example.component.ts
+++ apps/docs/src/app/core/component-docs/table/examples/table-checkboxes-example.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class TableCheckboxesExampleComponent {
 
     selectedRows = [];
-
+    selectMasterModel = false;
     allSelected = false;
 
     tableRows = [
