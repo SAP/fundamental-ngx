aa88ecc3218f97df6ce1532a2fc4a43044ae5e42
gorecki gracjan <gorecki.gracjan@hotmail.com>
2020-02-17 10:59:30 +0100

    rtl service moved to utils

--- libs/core/src/lib/utils/services/rtl.service.ts
+++ libs/core/src/lib/utils/services/rtl.service.ts
@@ -1,8 +1,15 @@
 import { BehaviorSubject } from 'rxjs';
-import { Optional } from '@angular/core';
+import { Optional, Injectable } from '@angular/core';
 
 const DefaultRtlLanguages = ['ar', 'arc', 'dv', 'fa', 'ha', 'he', 'khw', 'ks', 'ku', 'ps', 'ur', 'yi'];
 
+@Injectable()
+/** 
+ * Service taking care of RTL trough behavior subject
+ * language list is used to determine if rtl should be enabled at start
+ * user can overwrite default languages by using injection token 
+ * name of variable used in constructor: injectedRtlLanguages
+*/
 export class RtlService {
     rtl: BehaviorSubject<boolean>;
     constructor(@Optional() injectedRtlLanguages: string[]) {
