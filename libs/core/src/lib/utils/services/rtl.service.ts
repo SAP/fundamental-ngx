6225a6a4c5eea383f59b2db887ea77a1f6f6a291
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2020-02-18 14:19:04 +0100

    fix: added support for rtl (#1993)
    * added support for rtl
    
    * service error fix
    
    * comment updated
    
    * Update nested-list-popover.component.ts

--- libs/core/src/lib/utils/services/rtl.service.ts
+++ libs/core/src/lib/utils/services/rtl.service.ts
@@ -1,19 +1,19 @@
 import { BehaviorSubject } from 'rxjs';
-import { Optional, Injectable } from '@angular/core';
+import { Optional, Injectable, Inject, InjectionToken } from '@angular/core';
 
 const DefaultRtlLanguages = ['ar', 'arc', 'dv', 'fa', 'ha', 'he', 'khw', 'ks', 'ku', 'ps', 'ur', 'yi'];
+export const RtlLanguageToken = new InjectionToken<string[]>('RtlLanguageToken')
 
 @Injectable()
 /** 
  * Service taking care of RTL trough behavior subject
  * language list is used to determine if rtl should be enabled at start
- * user can overwrite default languages by using injection token 
- * name of variable used in constructor: injectedRtlLanguages
+ * user can overwrite default languages by using injection token RtlLanguageToken
 */
 export class RtlService {
     rtl: BehaviorSubject<boolean>;
-    constructor(@Optional() injectedRtlLanguages: string[]) {
-        injectedRtlLanguages = injectedRtlLanguages ? injectedRtlLanguages : DefaultRtlLanguages
+    constructor(@Optional() @Inject(RtlLanguageToken) injectedRtlLanguages: string[]) {
+        injectedRtlLanguages = injectedRtlLanguages || DefaultRtlLanguages
 
         const filtered = injectedRtlLanguages.filter(language => navigator.language.includes(language));
 
