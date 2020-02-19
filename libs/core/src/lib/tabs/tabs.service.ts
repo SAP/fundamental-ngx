63d6bbfef3bb98f60f2fa6553df33ca351934ff9
jmarkowski <jedrzej.markowski@sap.com>
2020-02-19 13:03:42 +0100

    fix: (tabs) Add keyboard support for RTl (#1988)

--- libs/core/src/lib/tabs/tabs.service.ts
+++ libs/core/src/lib/tabs/tabs.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
 import { Subject } from 'rxjs';
+import { RtlService } from '../utils/services/rtl.service';
 /**
  * Service that is responsible for providing keyboard actions support
  * */
@@ -12,22 +13,28 @@ export class TabsService {
     /** Event is thrown always, when some property is changed */
     readonly tabPanelPropertyChanged: Subject<void> = new Subject<void>();
 
+    constructor(
+        @Optional() private _rtlService: RtlService
+    ) {}
+
     /** @hidden */
     tabHeaderKeyHandler(index: number, event: any, elements: HTMLElement[]): void {
+        const rtlDirection: boolean = this._rtlService && this._rtlService.rtl.getValue();
+
         switch (event.key) {
             case ('ArrowLeft'): {
-                if (index - 1 >= 0) {
-                    this._getTabLinkFromIndex(index - 1, elements).focus();
+                if (!rtlDirection) {
+                    this._focusPrevious(index, elements);
                 } else {
-                    this._getTabLinkFromIndex(elements.length - 1, elements).focus();
+                    this._focusNext(index, elements);
                 }
                 break;
             }
             case ('ArrowRight'): {
-                if (index + 1 < elements.length) {
-                    this._getTabLinkFromIndex(index + 1, elements).focus();
+                if (!rtlDirection) {
+                    this._focusNext(index, elements);
                 } else {
-                    this._getTabLinkFromIndex(0, elements).focus();
+                    this._focusPrevious(index, elements);
                 }
                 break;
             }
@@ -41,6 +48,25 @@ export class TabsService {
             }
         }
     }
+
+    /** @hidden */
+    private _focusNext(index: number, elements: HTMLElement[]): void {
+        if (index + 1 < elements.length) {
+            this._getTabLinkFromIndex(index + 1, elements).focus();
+        } else {
+            this._getTabLinkFromIndex(0, elements).focus();
+        }
+    }
+
+    /** @hidden */
+    private _focusPrevious(index: number, elements: HTMLElement[]): void {
+        if (index - 1 >= 0) {
+            this._getTabLinkFromIndex(index - 1, elements).focus();
+        } else {
+            this._getTabLinkFromIndex(elements.length - 1, elements).focus();
+        }
+    }
+
 
     /** @hidden */
     private _getTabLinkFromIndex(index: number, elements: HTMLElement[]): HTMLElement {
