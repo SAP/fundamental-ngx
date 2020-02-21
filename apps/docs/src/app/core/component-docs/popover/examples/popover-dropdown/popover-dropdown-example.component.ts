188e5cb1d9915f6a6cc3e6e40c6315dae08ad3ab
gorecki gracjan <gorecki.gracjan@hotmail.com>
2020-02-21 11:13:51 +0100

    working on fix

--- apps/docs/src/app/core/component-docs/popover/examples/popover-dropdown/popover-dropdown-example.component.ts
+++ apps/docs/src/app/core/component-docs/popover/examples/popover-dropdown/popover-dropdown-example.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { RtlService } from '@fundamental-ngx/core';
+import { BehaviorSubject } from 'rxjs';
 
+const bottomStart = 'bottom-start';
+const bottomEnd = 'bottom-end';
 @Component({
   selector: 'fd-popover-dropdown-example',
   templateUrl: './popover-dropdown.component.html',
@@ -7,9 +11,15 @@ import { Component } from '@angular/core';
 })
 export class PopoverDropdownExampleComponent {
 
-    menu = [
-        'Option 1',
-        'Option 2',
-        'Option 3',
-    ];
+  dropDownPlacement$: BehaviorSubject<string> = new BehaviorSubject(bottomStart);
+
+  menu = [
+    'Option 1',
+    'Option 2',
+    'Option 3',
+  ];
+
+  constructor(private rtlService: RtlService) {
+    rtlService.rtl.subscribe(isRtl => this.dropDownPlacement$.next(isRtl ? bottomEnd : bottomStart))
+  }
 }
