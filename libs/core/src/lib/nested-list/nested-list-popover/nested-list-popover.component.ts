88f9d4ec16c10f5d8de7facd49927ddffeddfad5
salarenko <salarenko@users.noreply.github.com>
2020-02-24 17:05:37 +0100

    fix: Add RTL support for input messages (#2047)
    * fix(core) Add RTL support for input messages | improve nested list RTL code
    
    * Fix import paths

--- libs/core/src/lib/nested-list/nested-list-popover/nested-list-popover.component.ts
+++ libs/core/src/lib/nested-list/nested-list-popover/nested-list-popover.component.ts
@@ -1,12 +1,12 @@
 import { Component, ContentChild, HostBinding, ViewChild, ViewEncapsulation, Optional } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { NestedItemDirective } from '../nested-item/nested-item.directive';
 import { NestedLinkDirective } from '../nested-link/nested-link.directive';
 import { NestedListKeyboardService } from '../nested-list-keyboard.service';
 import { PopoverComponent } from '../../popover/popover.component';
 import { RtlService } from '../../utils/public_api';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'fd-nested-list-popover',
@@ -16,7 +16,7 @@ import { RtlService } from '../../utils/public_api';
 })
 export class NestedListPopoverComponent {
     /** @hidden */
-    placement$: BehaviorSubject<string> = new BehaviorSubject('right-start');
+    placement$: Observable<string>;
 
     /** @hidden */
     @ViewChild(PopoverComponent, { static: false })
@@ -30,28 +30,6 @@ export class NestedListPopoverComponent {
     @ContentChild(NestedLinkDirective, { static: false })
     linkDirective: NestedLinkDirective;
 
-    /** @hidden */
-    constructor(
-        private keyboardNestService: NestedListKeyboardService,
-        @Optional() private rtlService: RtlService
-    ) {
-        this.keyboardNestService.refresh$.subscribe(() => {
-            /** Update popover position, on list of hidden items change */
-            if (this.popoverComponent) {
-                this.popoverComponent.updatePopover();
-            }
-        });
-
-        if (rtlService) {
-            rtlService.rtl
-                .subscribe(
-                    rtl => {
-                        this.placement$.next(rtl ? 'left-start' : 'right-start');
-                    }
-                );
-        }
-    }
-
     /**
      * @hidden
      * Reference to parent item, to propagate open and close change from popover.
@@ -62,6 +40,15 @@ export class NestedListPopoverComponent {
      * @hidden
      */
     open: boolean = false;
+
+    /** @hidden */
+    constructor(
+        private _keyboardNestService: NestedListKeyboardService,
+        @Optional() private _rtlService: RtlService
+    ) {
+        this._listenOnKeyboardRefresh();
+        this._createRtlObservable();
+    }
 
     /**
      * Method called, when open state is changed, from popover component (escape key, outside click).
@@ -75,5 +62,22 @@ export class NestedListPopoverComponent {
                 this.parentItemElement.triggerClose();
             }
         }
+    }
+
+    /** @hidden */
+    private _listenOnKeyboardRefresh(): void {
+        this._keyboardNestService.refresh$.subscribe(() => {
+            /** Update popover position, on list of hidden items change */
+            if (this.popoverComponent) {
+                this.popoverComponent.updatePopover();
+            }
+        });
+    }
+
+    /** @hidden */
+    private _createRtlObservable(): void {
+        this.placement$ = this._rtlService
+            ? this._rtlService.rtl.pipe(map(isRtl => isRtl ? 'left-start' : 'right-start'))
+            : of('right-start')
     }
 }
