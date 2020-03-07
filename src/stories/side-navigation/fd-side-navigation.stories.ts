22c94f79b40c367411c88fc10bd60838884552aa
deno <mladen.droshev@sap.com>
2020-03-07 18:27:24 -0500

    test: update few storybooks

--- src/stories/side-navigation/fd-side-navigation.stories.ts
+++ src/stories/side-navigation/fd-side-navigation.stories.ts
@@ -20,58 +20,79 @@ export default {
 
 export const SideNavigation = () => ({
     template:
-        `<fd-side-nav [condensed]="condensedVar">
-        <div fd-side-nav-main>
-            <ul fd-nested-list [compact]="compactVar">
-                <li fd-nested-list-item>
-                    <a fd-nested-list-link>
-                        <span fd-nested-list-icon [glyph]="glyphVar"></span>
-                        <span fd-nested-list-title>{{textValue1}}</span>
-                    </a>
-                </li>
-                <li fd-nested-list-item>
-                    <a fd-nested-list-link>
-                        <span fd-nested-list-title>{{textValue2}}</span>
-                    </a>
-                </li>
-                <li fd-nested-list-item>
-                    <a fd-nested-list-link>
-                        <span fd-nested-list-title>{{textValue3}}</span>
-                    </a>
-                    <ul fd-nested-list>
-                        <li fd-nested-list-item>
-                            <a fd-nested-list-link>
-                                <span fd-nested-list-title>{{textValue4}}</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    
-        <div fd-side-nav-utility>
-            <ul fd-nested-list [compact]="utilityCompactVar" [textOnly]="utilityTextOnlyVar">
-                <li fd-nested-list-item><a fd-nested-list-link>
-                <span fd-nested-list-title>{{textValue5}}</span>
-                </a></li>
-                <li fd-nested-list-item><a fd-nested-list-link>
-                    <span fd-nested-list-title>{{textValue6}}</span>
-                </a></li>
-            </ul>
-        </div>
-    </fd-side-nav>
+        `<fd-side-nav 
+                [condensed]="condensedVar">
+            <div fd-side-nav-main>
+                <ul fd-nested-list 
+                        [compact]="mainCompactVar" 
+                        [textOnly]="mainTextOnlyVar">
+                    <li fd-nested-list-item>
+                        <a fd-nested-list-link>
+                            <span fd-nested-list-icon [glyph]="'menu'"></span>
+                            <span fd-nested-list-title>{{textValue1}}</span>
+                        </a>
+                    </li>
+                    <li fd-nested-list-item>
+                        <a fd-nested-list-link>
+                            <span fd-nested-list-icon [glyph]="'menu'"></span>
+                            <span fd-nested-list-title>{{textValue2}}</span>
+                        </a>
+                    </li>
+                    <li fd-nested-list-item>
+                        <a fd-nested-list-link>
+                            <span fd-nested-list-icon [glyph]="'menu'"></span>
+                            <span fd-nested-list-title>{{textValue3}}</span>
+                        </a>
+                        <ul *ngIf="multiLevels" fd-nested-list [textOnly]="mainSubTextOnlyVar">
+                            <li fd-nested-list-item>
+                                <a fd-nested-list-link>
+                                    <span fd-nested-list-icon [glyph]="'menu'"></span>
+                                    <span fd-nested-list-title>{{textValue4}}</span>
+                                </a>
+                                <a fd-nested-list-link>
+                                    <span fd-nested-list-icon [glyph]="'menu'"></span>
+                                    <span fd-nested-list-title>{{textValue5}}</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        
+            <div fd-side-nav-utility>
+                <ul fd-nested-list 
+                        [compact]="utilityCompactVar" 
+                        [textOnly]="utilityTextOnlyVar">
+                    <li fd-nested-list-item>
+                        <a fd-nested-list-link>
+                            <span fd-nested-list-icon [glyph]="'menu'"></span>  
+                            <span fd-nested-list-title>{{textValue6}}</span>
+                        </a>
+                    </li>
+                    <li fd-nested-list-item>
+                        <a fd-nested-list-link>
+                            <span fd-nested-list-icon [glyph]="'menu'"></span>
+                            <span fd-nested-list-title>{{textValue7}}</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </fd-side-nav>
     `,
     props: {
-        comapctVar: boolean('Compact', false),
+        mainCompactVar: boolean('Main Secion in Compact Mode', false),
+        mainTextOnlyVar: boolean('Main Section Text Only', false),
+        mainSubTextOnlyVar: boolean('Main Sub Section Text Only', false),
         condensedVar: boolean('Condensed', false),
-        utilityTextOnlyVar: boolean('Utility text only', false),
-        utilityCompactVar: boolean('Utility compact', false),
-        glyphVar: text('Glyph', 'home'),
-        textValue1: text('Text Value 1', 'Default 1'),
-        textValue2: text('Text Value 2', 'Default 2'),
-        textValue3: text('Text Value 3', 'Default 3'),
-        textValue4: text('Text Value 4', 'Default 4'),
-        textValue5: text('Text Value 5', 'Default 5'),
-        textValue6: text('Text Value 6', 'Default 6'),
+        multiLevels: boolean('Multi Levels', true),
+        utilityTextOnlyVar: boolean('Utility Section text only', false),
+        utilityCompactVar: boolean('Utility Section in Compact Mode', false),
+        textValue1: text('Text Value 1', 'Item 1'),
+        textValue2: text('Text Value 2', 'Item 2'),
+        textValue3: text('Text Value 3', 'Item 3'),
+        textValue4: text('Text Value 4', 'Item 4'),
+        textValue5: text('Text Value 5', 'Item 5'),
+        textValue6: text('Text Value 6', 'Item 6'),
+        textValue8: text('Text Value 7', 'Item 7'),
     }
 });
