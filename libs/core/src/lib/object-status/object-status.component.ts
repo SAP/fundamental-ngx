aa323b9d48092feebf55f5479d5b8ae7fe491044
InnaAtanasova <i.atanasova@sap.com>
2020-03-16 20:58:18 -0400

    use setters and getters for Input props

--- libs/core/src/lib/object-status/object-status.component.ts
+++ libs/core/src/lib/object-status/object-status.component.ts
@@ -1,77 +1,141 @@
-import { Component, OnInit, Input, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, OnInit, ViewEncapsulation } from '@angular/core';
+import { applyCssClass, CssClassBuilder } from '../utils/public_api';
 
 type ObjectStatus = 'negative' | 'critical' | 'positive' | 'informative';
 
 @Component({
-  selector: 'fd-object-status',
-  templateUrl: './object-status.component.html',
-  styleUrls: ['./object-status.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    // tslint:disable-next-line:component-selector
+    selector: '[fd-object-status]',
+    template: `<ng-content></ng-content>`,
+    styleUrls: ['./object-status.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ObjectStatusComponent implements OnInit {
+export class ObjectStatusComponent implements OnInit, OnChanges, CssClassBuilder {
 
-    /** Select type defines the border type of the select button. */
+    /** User's custom classes */
+    private _class: string = '';
     @Input()
-    status: ObjectStatus;
+    set class(userClass: string) {
+        this._class = userClass;
+        this.buildComponentCssClass();
+    }
 
+    /** 
+     * The status represented by the Object Status.
+     * Can be one of the following: 'negative' | 'critical' | 'positive' | 'informative'
+     * For default Object Status omit this property
+     */
+    private _status: ObjectStatus;
     @Input()
-    glyph: String; 
+    set status(status: ObjectStatus) {
+        this._status = status;
+        this.buildComponentCssClass();
+    }
 
+    get status(): ObjectStatus {
+        return this._status;
+    }
+
+    /** 
+     * Glyph (icon) of the Object Status.
+     */
+    private _glyph: string;
     @Input()
-    message: String;
+    set glyph(glyph: string) {
+        this._glyph = glyph;
+        this.buildComponentCssClass();
+    }
 
+    get glyph(): string {
+        return this._glyph;
+    }
+
+    /** 
+     * A number representing the indication color. 
+     * Option includes numbers from 1 to 8
+     */
+    private _indicationColor: number = null;
     @Input()
-    color: String;
+    set indicationColor(indicationColor: number) {
+        this._indicationColor = indicationColor;
+        this.buildComponentCssClass();
+    }
 
+    get indicationColor(): number {
+        return this._indicationColor;
+    }
+
+    /** Whether the Object Status is clickable. */
+    private _clickable: boolean = false;
     @Input()
-    inverted: boolean;
+    set clickable(clickable: boolean) {
+        this._clickable = clickable;
+        this.buildComponentCssClass();
+    }
 
+    get clickable(): boolean {
+        return this._clickable;
+    }
+
+    /** Whether the Object Status is inverted. */
+    private _inverted: boolean = false;
     @Input()
-    clickable: boolean;
+    set inverted(inverted: boolean) {
+        this._inverted = inverted;
+        this.buildComponentCssClass();
+    }
 
+    get inverted(): boolean {
+        return this._inverted;
+    }
+
+    /** Whether the Object Status is in Large Design. */
+    private _large: boolean = false;
     @Input()
-    large: boolean;
+    set large(large: boolean) {
+        this._large = large;
+        this.buildComponentCssClass();
+    }
 
-    cssname: string = '';
+    get large(): boolean {
+        return this._large;
+    }
+
+    @applyCssClass
+    /** CssClassBuilder interface implementation
+     * function must return single string
+     * function is responsible for order which css classes are applied
+     */
+    buildComponentCssClass(): string {
+        return [
+            'fd-object-status',
+            this._inverted ? 'fd-object-status--inverted' : '',
+            this._large ? 'fd-object-status--large' : '',
+            this._status ? `fd-object-status--${this._status}` : '',
+            this._glyph ? `sap-icon--${this._glyph}` : '',
+            this._indicationColor ? `fd-object-status--indication-${this._indicationColor}` : '',
+            this._clickable ? 'fd-object-status--link' : '',
+            this._class
+        ].filter(x => x !== '').join(' ');
+    }
 
     /** @hidden */
-    constructor() {
+    constructor(private _elementRef: ElementRef) { }
+
+    /** @hidden */
+    ngOnInit(): void {
+        this.buildComponentCssClass();
     }
-    ngOnInit() {
+
+    /** @hidden */
+    elementRef(): ElementRef<any> {
+        return this._elementRef;
     }
 
-    get buildCustomCss(): string {
-
-        if (this.status === 'negative') {
-            this.cssname = this.cssname + ' fd-object-status--negative';
-        }
-        if (this.status === 'critical') {
-            this.cssname = this.cssname + ' fd-object-status--critical';
-        }
-        if (this.status === 'positive') {
-            this.cssname = this.cssname + ' fd-object-status--positive';
-        }
-        if (this.status === 'informative') {
-            this.cssname = this.cssname + ' fd-object-status--informative';
-        }
-        if (this.glyph) {
-            this.cssname = this.cssname + ' sap-icon--' + this.glyph;
-        }
-        if (this.color) {
-            this.cssname = this.cssname + ' fd-object-status--indication-' + this.color;
-        }
-        if (this.inverted) {
-            this.cssname = this.cssname + ' fd-object-status--inverted';
-        }
-        if (this.clickable) {
-            this.cssname = this.cssname + ' fd-object-status--link';
-        }
-        if (this.large) {
-            this.cssname = this.cssname + ' fd-object-status--large';
-        }
-
-        return this.cssname;
+    /** @hidden */
+    ngOnChanges(): void {
+        this.buildComponentCssClass();
     }
 
 }
