c5f3bd3c21a6d158ba48108147c22a3bf923a857
InnaAtanasova <i.atanasova@sap.com>
2020-03-16 20:58:18 -0400

    use setters and getters for Input props

--- apps/docs/src/app/core/component-docs/object-status/examples/object-status-examples.component.ts
+++ apps/docs/src/app/core/component-docs/object-status/examples/object-status-examples.component.ts
@@ -2,31 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-object-status-default-example',
-    templateUrl: './object-status-default-example.component.html'
+    templateUrl: './object-status-default-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusDefaultExampleComponent { }
 
 @Component({
     selector: 'fd-object-status-text-example',
-    templateUrl: './object-status-text-example.component.html'
+    templateUrl: './object-status-text-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusTextExampleComponent { }
 
 @Component({
     selector: 'fd-object-status-generic-text-example',
-    templateUrl: './object-status-generic-text-example.component.html'
+    templateUrl: './object-status-generic-text-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusGenericExampleComponent { }
 
 @Component({
     selector: 'fd-object-status-numeric-icon-example',
-    templateUrl: './object-status-icon-text-example.component.html'
+    templateUrl: './object-status-icon-text-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusTextIconExampleComponent { }
 
 @Component({
     selector: 'fd-object-status-clickable-and-icon-example',
-    templateUrl: './object-status-clickable-and-icon-example.component.html'
+    templateUrl: './object-status-clickable-and-icon-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusClickableAndIconExampleComponent { 
     showAlert() {
@@ -36,25 +41,29 @@ export class ObjectStatusClickableAndIconExampleComponent {
 
 @Component({
     selector: 'fd-object-status-inverted-example',
-    templateUrl: './object-status-inverted-example.component.html'
+    templateUrl: './object-status-inverted-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusInvertedTextExampleComponent { }
 
 @Component({
     selector: 'fd-object-status-inverted-generic-text-example',
-    templateUrl: './object-status-inverted-generic-text-example.component.html'
+    templateUrl: './object-status-inverted-generic-text-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusInvertedGenericTextExampleComponent { }
 
 @Component({
     selector: 'fd-object-status-inverted-icon-and-text-example',
-    templateUrl: './object-status-inverted-icon-and-text-example.component.html'
+    templateUrl: './object-status-inverted-icon-and-text-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusInvertedGenericIconAndTextExampleComponent { }
 
 @Component({
     selector: 'fd-object-status-object-status-large-example',
-    templateUrl: './object-status-large-example.component.html'
+    templateUrl: './object-status-large-example.component.html',
+    styleUrls: ['./object-status-examples.component.scss']
 })
 export class ObjectStatusLargeExampleComponent { 
 
