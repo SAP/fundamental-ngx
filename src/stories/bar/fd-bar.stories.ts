0c2b06607f66c97c449f5cd7401df762341b0c9c
Stefano Scalzo <stefano.scalzo@hotmail.com>
2020-03-18 17:13:53 -0400

    fix errors in stories

--- src/stories/bar/fd-bar.stories.ts
+++ src/stories/bar/fd-bar.stories.ts
@@ -4,6 +4,8 @@ import { withA11y } from '@storybook/addon-a11y';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { BarComponent, BarModule } from 'libs/core/src/lib/bar/public_api';
+import { ButtonModule } from 'libs/core/src/lib/button/public_api';
+import { ImageModule } from 'libs/core/src/lib/image/public_api';
 
 export default {
     title: 'Fd bar',
@@ -13,7 +15,7 @@ export default {
         withKnobs,
         withA11y,
         moduleMetadata({
-            imports: [BarModule, ReactiveFormsModule],
+            imports: [BarModule, ButtonModule, ImageModule, ReactiveFormsModule],
             declarations: []
         })
     ]
@@ -26,30 +28,69 @@ const barDesign = {
     floatingFooter: 'floating-footer',
 }
 
+const sizes = {
+    default: '',
+    small: 's',
+    medium_large: 'm_l',
+    extra_large: 'xl'
+}
+
 export const Bar = () => ({
     template: `
-    <div style="padding: 30px">
-        <fd-bar [barDesign]="barDesign" [cosy]="cosy">
-            <div fd-bar-left>
+    <div style="padding:20px;">
+        <div fd-bar 
+            [barDesign]="barDesignVar"
+            [cosy]="cosy"
+            [inHomePage]="inHomePage"
+            [inPage]="inPage"
+            [size]="size">
+            <div fd-bar-left *ngIf="showLeft">
                 <fd-bar-element>
-                Left Section
+                    <button fd-button 
+                        [glyph]="'basket'">btn
+                    </button>
+                </fd-bar-element>
+                <fd-bar-element>
+                    Left Section
                 </fd-bar-element>
             </div>
-            <div fd-bar-middle>
+            <div fd-bar-middle *ngIf="showMiddle">
+                <fd-bar-element [fullWidth]="fullwidthVar">
+                    <button fd-button 
+                        [glyph]="'basket'">btn
+                    </button>
+                </fd-bar-element>
                 <fd-bar-element>
                 Middle Section
                 </fd-bar-element>
+                <fd-bar-element>
+                    <button fd-button 
+                        [glyph]="'basket'">btn
+                    </button>
+                </fd-bar-element>
             </div>
-            <div fd-bar-right>
+            <div fd-bar-right *ngIf="showRight">
                 <fd-bar-element>
                 Right Section
                 </fd-bar-element>
+                <fd-bar-element>
+                    <button fd-button 
+                        [glyph]="'basket'">btn
+                    </button>
+                </fd-bar-element>
             </div>
-        </fd-bar>
+        </div>
     </div>
   `,
     props: {
-        barDesign: select('barDesign', barDesign, 'header'),
-        cosy: boolean('cosy', false)
+        cosy: boolean('cosy', false),
+        showLeft: boolean('Show Left Section', true),
+        showRight: boolean('Show Right Section', true),
+        showMiddle: boolean('Show Middle Section', true),
+        barDesignVar: select('Bar Design', barDesign, 'header'),
+        inHomePage: boolean('In Home Page', true),
+        inPage: boolean('In Page', true),
+        size: select('Size', sizes, 's'),
+        fullwidthVar: boolean('Full width element', true),
     }
 });
