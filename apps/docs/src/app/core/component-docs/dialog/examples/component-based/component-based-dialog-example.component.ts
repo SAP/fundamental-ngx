bb1dc88d8d6878dd33fe9d48ab86c46058c6e603
Vanessa-Cusmich <54723167+Vanessa-Cusmich@users.noreply.github.com>
2020-03-31 07:11:34 -0400

    Update component-based-dialog-example.component.ts

--- apps/docs/src/app/core/component-docs/dialog/examples/component-based/component-based-dialog-example.component.ts
+++ apps/docs/src/app/core/component-docs/dialog/examples/component-based/component-based-dialog-example.component.ts
@@ -23,9 +23,9 @@ export class ComponentBasedDialogExampleComponent {
                     `You can grow your own pineapple by planting the top of the pineapple in soil`,
                     `Pulling leaves from a pineapple is not an indication of ripeness as many people think`,
                     `James Dole is considered the “King of Pineapples“`,
-                    `A pineapple cannot ripen more after it is picked`,
-                    `An unripe pineapple not only tastes awful, it can be poisonous`,
-                    `One of the ways you tell if a pineapple is ripe is by smelling it`,
+                    `A pineapple cannot continue to ripen after it has been picked`,
+                    `An unripe pineapple not only tastes awful, but can also be poisonous`,
+                    `One of the ways you can tell if a pineapple is ripe is by smelling it`,
                     `In Hawaii, the word for pineapple is “Hala kahiki“`
                 ],
             },
