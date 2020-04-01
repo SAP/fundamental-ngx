b0b5f79e0e37dbf5c25176f6d76633fe52955e07
Gracjan Gorecki <gorecki.gracjan@hotmail.com>
2020-04-01 11:47:31 +0200

    feat: added support for multiple directive's classes in cssclassbuilder

--- libs/core/src/lib/utils/decorators/apply-css-class.decorator.ts
+++ libs/core/src/lib/utils/decorators/apply-css-class.decorator.ts
@@ -1,5 +1,21 @@
 import { ELEMENT_REF_EXCEPTION } from '../public_api';
 
+function uuidv4(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        // tslint:disable-next-line: no-bitwise
+        const r = (Math.random() * 16) | 0,
+            // tslint:disable-next-line: no-bitwise
+            v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+}
+
+function buildClassFromMap(nativeElement: { _classMap: any }): string {
+    return Object.keys(nativeElement._classMap)
+        .map((key) => nativeElement._classMap[key])
+        .reduce((acc, value) => (acc += value), '');
+}
+
 /**
  * Method decorator to apply css class to a component through native element
  * decorator will store original method in variable and wrap it with custom one
@@ -11,7 +27,7 @@ import { ELEMENT_REF_EXCEPTION } from '../public_api';
  */
 export function applyCssClass(target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
     const originalMethod = descriptor.value;
-    descriptor.value = function(): string {
+    descriptor.value = function (): string {
         if (!this.elementRef) {
             throw ELEMENT_REF_EXCEPTION;
         }
@@ -20,7 +36,17 @@ export function applyCssClass(target: any, propertyKey: string, descriptor: Prop
         const elementRef = this.elementRef.apply(this);
 
         if (elementRef) {
-            (elementRef.nativeElement as HTMLElement).className = `${_class} ${this.class ? this.class : ''}`;
+            if (!elementRef.nativeElement._classMap) {
+                elementRef.nativeElement._classMap = {};
+            }
+
+            if (!this._uuidv4) {
+                this._uuidv4 = uuidv4();
+            }
+
+            elementRef.nativeElement._classMap[this._uuidv4] = `${_class} ${this.class ? this.class : ''}`;
+
+            (elementRef.nativeElement as HTMLElement).className = buildClassFromMap(elementRef.nativeElement);
         }
 
         return _class;
