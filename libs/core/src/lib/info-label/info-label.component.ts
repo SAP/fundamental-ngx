aab7405f44ad9e6c93a6001f66666aea90a69f78
InnaAtanasova <i.atanasova@sap.com>
2020-04-03 16:11:39 -0400

    remove left overs from rebase

--- libs/core/src/lib/info-label/info-label.component.ts
+++ libs/core/src/lib/info-label/info-label.component.ts
@@ -12,71 +12,34 @@ type LabelType = 'numeric' | 'only-icon' | 'icon';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InfoLabelComponent implements OnInit, OnChanges, CssClassBuilder {
+    /** user's custom classes */
+    @Input()
+    class: string = '';
   
     /** 
      * The LabelType represented by the info label .
      * Can be one of the following: 'numeric' | 'only-icon' | 'icon'
      * For default info label omit this property
      */
-    private _labelType: LabelType;
-    
     @Input()
-    set labelType(value: LabelType) {
-        this._labelType = value;
-        this.buildComponentCssClass();
-    }
-
-    get labelType(): LabelType {
-        return this._labelType;
-    }
+    labelType: LabelType;
 
     /** glyph define the icon of info label */
-    private _glyph: string; 
-
     @Input()
-    set glyph(value: string) {
-        this._glyph = value;
-        this.buildComponentCssClass();
-    }
-
-    get glyph(): string {
-        return this._glyph;
-    }
+    glyph: string; 
 
     /**define the colour of the info label starting form 1 to 10 */
-    private _color: string;
-    
     @Input()
-    set color(value: string) {
-        this._color = value;
-        this.buildComponentCssClass();
-    }
-
-    get color(): string {
-        return this._color;
-    }
-
-
-    private _class: string = '';
-
-    @Input()
-    set class(value: string) {
-        this._class = value;
-        this.buildComponentCssClass();
-    }
-
-    get class(): string {
-        return this._class;
-    }
+    color: string;
 
     @applyCssClass
     buildComponentCssClass(): string {
         return [
             'fd-info-label',
-            this._labelType ? `fd-info-label--${this._labelType}` : '',
-            this._glyph ? `sap-icon--${this._glyph}` : '',
-            this._color ? `fd-info-label--accent-color-${this._color}` : '',
-            this._class
+            this.labelType ? `fd-info-label--${this.labelType}` : '',
+            this.glyph ? `sap-icon--${this.glyph}` : '',
+            this.color ? `fd-info-label--accent-color-${this.color}` : '',
+            this.class
         ].filter(x => x !== '').join(' ');
     }
 
