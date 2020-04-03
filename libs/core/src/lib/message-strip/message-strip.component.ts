82a7d822a108cc7545bac0514bf81676553cb33e
InnaAtanasova <i.atanasova@sap.com>
2020-04-03 14:30:40 -0400

    add types to the doc

--- libs/core/src/lib/message-strip/message-strip.component.ts
+++ libs/core/src/lib/message-strip/message-strip.component.ts
@@ -24,126 +24,53 @@ let messageStripUniqueId: number = 0;
 export class MessageStripComponent implements OnInit, OnChanges, CssClassBuilder {
 
     /** user's custom classes */
-    private _class: string = '';
     @Input()
-    set class(userClass: string) {
-        this._class = userClass;
-        this.buildComponentCssClass();
-    }
+    class: string = '';
 
     /** Whether the message strip is dismissible. */
-    private _dismissible: boolean = true;
     @Input()
-    set dismissible(dismissible: boolean) {
-        this._dismissible = dismissible;
-        this.buildComponentCssClass();
-    }
-
-    get dismissible(): boolean {
-        return this._dismissible;
-    }
+    dismissible: boolean = true;
 
     /** The default message strip does not have an icon. 
      * The other types (warning, success, information and error) have icons by default. 
-     * To remove the icon set the property to true. */
-    private _noIcon: boolean = false;
+     * To remove the icon set the property to true. 
+     */
     @Input()
-    set noIcon(noIcon: boolean) {
-        this._noIcon = noIcon;
-        this.buildComponentCssClass();
-    }
+    noIcon: boolean = false;
 
-    /** The type of the message strip. Can be one of *warning*, *success*, *information*, *error* or null. */
-    private _type: string;
+    /** The type of the message strip. 
+     * Can be one of *warning*, *success*, *information*, *error* or null. 
+     */
     @Input()
-    set type(type: string) {
-        this._type = type;
-        this.buildComponentCssClass();
-    }
+    type: string;
 
     /** Id for the message-strip component. If omitted, a unique one is generated. */
-    private _id: string = 'fd-message-strip-' + messageStripUniqueId++;
     @Input()
-    set id(id: string) {
-        this._id = id;
-        this.buildComponentCssClass();
-    }
-
-    get id(): string {
-        return this._id;
-    }
+    id: string = 'fd-message-strip-' + messageStripUniqueId++;
 
     /** Id of the element that labels the message-strip. */
-    private _ariaLabelledBy: string = null;
     @Input()
-    set ariaLabelledBy(ariaLabelledBy: string) {
-        this._ariaLabelledBy = ariaLabelledBy;
-        this.buildComponentCssClass();
-    }
-
-    get ariaLabelledBy(): string {
-        return this._ariaLabelledBy;
-    }
+    ariaLabelledBy: string = null;
 
     /** Aria label for the message-strip component element. */
-    private _ariaLabel: string = null;
     @Input()
-    set ariaLabel(ariaLabel: string) {
-        this._ariaLabel = ariaLabel;
-        this.buildComponentCssClass();
-    }
-
-    get ariaLabel(): string {
-        return this._ariaLabel;
-    }
+    ariaLabel: string = null;
 
     /** Aria label for the dismiss button. */
-    private _dismissLabel: string = 'Dismiss';
     @Input()
-    set dismissLabel(dismissLabel: string) {
-        this._dismissLabel = dismissLabel;
-        this.buildComponentCssClass();
-    }
-
-    get dismissLabel(): string {
-        return this._dismissLabel;
-    }
+    dismissLabel: string = 'Dismiss';
 
     /** Width of the message-strip. */
-    private _width: string;
     @Input()
-    set width(width: string) {
-        this._width = width;
-        this.buildComponentCssClass();
-    }
-
-    get width(): string {
-        return this._width;
-    }
+    width: string;
 
     /** Minimum width of the message-strip. */
-    private _minWidth: string;
     @Input()
-    set minWidth(minWidth: string) {
-        this._minWidth = minWidth;
-        this.buildComponentCssClass();
-    }
-
-    get minWidth(): string {
-        return this._minWidth;
-    }
+    minWidth: string;
 
     /** Alternative way of passing in a message to the message-strip. */
-    private _message: string;
     @Input()
-    set message(message: string) {
-        this._message = message;
-        this.buildComponentCssClass();
-    }
-
-    get message(): string {
-        return this._message;
-    }
+    message: string;
 
     /** Event fired when the message-strip is dismissed. */
     @Output()
@@ -184,10 +111,10 @@ export class MessageStripComponent implements OnInit, OnChanges, CssClassBuilder
     buildComponentCssClass(): string {
         return [
             'fd-message-strip',
-            this._type ? `fd-message-strip--${this._type}` : '',
-            this._dismissible ? 'fd-message-strip--dismissible' : '',
-            this._noIcon ? 'fd-message-strip--no-icon' : '',
-            this._class
+            this.type ? `fd-message-strip--${this.type}` : '',
+            this.dismissible ? 'fd-message-strip--dismissible' : '',
+            this.noIcon ? 'fd-message-strip--no-icon' : '',
+            this.class
         ].filter(x => x !== '').join(' ');
     }
 
