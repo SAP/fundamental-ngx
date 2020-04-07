114190b467528763c77145fe5ebff7d0653f4f61
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-04-07 14:47:29 -0600

    fix: update action bar to new fundamental-styles (#2218)
    * update action bar
    
    * rebuild

--- apps/docs/src/app/core/component-docs/action-bar/action-bar-docs.component.ts
+++ apps/docs/src/app/core/component-docs/action-bar/action-bar-docs.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 import * as backButtonExample from '!raw-loader!./examples/action-bar-back-example.component.html';
+import * as backButtonExampleTs from '!raw-loader!./examples/action-bar-back-example.component.ts';
 import * as titleTruncationExample from '!raw-loader!./examples/action-bar-long-string-title-truncation-example.component.html';
+import * as titleTruncationExampleTs from '!raw-loader!./examples/action-bar-long-string-title-truncation-example.component.ts';
 import * as contextualMenuExample from '!raw-loader!./examples/action-bar-contextual-menu-example.component.html';
 import * as mobileExample from '!raw-loader!./examples/action-bar-mobile-example.component.html';
+import * as mobileExampleTs from '!raw-loader!./examples/action-bar-mobile-example.component.ts';
 import * as noBackButtonExample from '!raw-loader!./examples/action-bar-no-back-example.component.html';
 import { ExampleFile } from '../../../documentation/core-helpers/code-example/example-file';
 
@@ -18,6 +21,12 @@ export class ActionBarDocsComponent {
             language: 'html',
             code: backButtonExample,
             fileName: 'action-bar-back',
+        },
+        {
+            language: 'typescript',
+            code: backButtonExampleTs,
+            fileName: 'action-bar-back-button-example',
+            component: 'ActionBarBackButtonExampleComponent'
         }
     ];
 
@@ -26,6 +35,12 @@ export class ActionBarDocsComponent {
             language: 'html',
             code: titleTruncationExample,
             fileName: 'action-bar-long-string-title-truncation',
+        },
+        {
+            language: 'typescript',
+            code: titleTruncationExampleTs,
+            fileName: 'action-bar-long-string-title-truncation-example',
+            component: 'ActionBarLongStringTitleTruncationExampleComponent'
         }
     ];
 
@@ -50,6 +65,12 @@ export class ActionBarDocsComponent {
             language: 'html',
             code: mobileExample,
             fileName: 'action-bar-mobile',
+        },
+        {
+            language: 'typescript',
+            code: mobileExampleTs,
+            fileName: 'action-bar-mobile-button-example',
+            component: 'ActionBarMobileButtonExampleComponent'
         }
     ];
 }
--- apps/docs/src/app/core/component-docs/action-bar/examples/action-bar-examples.component.ts
+++ apps/docs/src/app/core/component-docs/action-bar/examples/action-bar-examples.component.ts
@@ -1,28 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'fd-action-bar-back-example',
-    templateUrl: './action-bar-back-example.component.html'
-})
-export class ActionBarBackExampleComponent {}
-
-@Component({
-    selector: 'fd-action-bar-long-string-title-truncation-example',
-    templateUrl: './action-bar-long-string-title-truncation-example.component.html'
-})
-export class ActionBarLongStringTitleTruncationExampleComponent { }
-
-@Component({
     selector: 'fd-action-bar-contextual-menu-example',
     templateUrl: './action-bar-contextual-menu-example.component.html'
 })
 export class ActionBarContextualMenuExampleComponent { }
-
-@Component({
-    selector: 'fd-action-bar-mobile-example',
-    templateUrl: './action-bar-mobile-example.component.html'
-})
-export class ActionBarMobileExampleComponent { }
 
 @Component({
     selector: 'fd-action-bar-no-back-example',
