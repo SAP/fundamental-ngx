f69553af3de925f4ab3db47d3b0198a2793168d9
Michal Salamon <msalamon@divante.com>
2020-04-08 13:41:58 +0200

    feat(core) Create Menu documentation

--- apps/docs/src/app/core/component-docs/menu/examples/menu-examples.component.ts
+++ apps/docs/src/app/core/component-docs/menu/examples/menu-examples.component.ts
@@ -7,13 +7,21 @@ import { Component } from '@angular/core';
 export class MenuExampleComponent {}
 
 @Component({
-    selector: 'fd-menu-group-example',
-    templateUrl: './menu-group-example.component.html'
-})
-export class MenuGroupExampleComponent {}
-
-@Component({
     selector: 'fd-menu-separator-example',
     templateUrl: './menu-separator-example.component.html'
 })
 export class MenuSeparatorExampleComponent {}
+
+@Component({
+    selector: 'fd-menu-mobile-example',
+    templateUrl: './menu-mobile-example.component.html'
+})
+export class MenuMobileExampleComponent {
+    isOpen: boolean = false;
+}
+
+@Component({
+    selector: 'fd-menu-with-submenu-example',
+    templateUrl: './menu-with-submenu-example.component.html'
+})
+export class MenuWithSubmenuExampleComponent {}
--- libs/core/src/lib/menu/menu.component.ts
+++ libs/core/src/lib/menu/menu.component.ts
@@ -2,15 +2,24 @@ import {
     AfterViewInit,
     ChangeDetectionStrategy,
     Component,
+    ElementRef,
     Input,
+    OnDestroy,
+    Optional,
     TemplateRef,
     ViewChild,
     ViewEncapsulation
 } from '@angular/core';
 import { DialogRef } from '../dialog/dialog-utils/dialog-ref.class';
 import { DialogService } from '../dialog/dialog-service/dialog.service';
+import { PopoverComponent } from '../popover/popover.component';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
-interface DialogContent { title: string, template: TemplateRef<any> }
+interface DialogContent {
+    title: string,
+    template: TemplateRef<any>
+}
 
 /**
  * The component that represents a menu.
@@ -22,14 +31,21 @@ interface DialogContent { title: string, template: TemplateRef<any> }
     styleUrls: ['menu.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MenuComponent implements AfterViewInit {
+export class MenuComponent implements AfterViewInit, OnDestroy {
 
     /** Display menu in compact mode */
     @Input()
     compact: boolean = false;
 
     /** Display menu in mobile mode */
-    @Input()
+    @Input('mobile') set setMobileMode(value: boolean) {
+        this.mobile = value;
+        if (this.isUsingPopoverAndMobileMode) {
+            this._setPopoverToMobileMode();
+        }
+    }
+
+    /** @hidden */
     mobile: boolean = false;
 
     /** Dialog title used for menu in mobile mode */
@@ -54,12 +70,22 @@ export class MenuComponent implements AfterViewInit {
     /** @hidden */
     private _menuTemplates: DialogContent[] = [];
 
-    constructor(private _dialogService: DialogService) {}
+    /** @hidden */
+    private _subscription = new Subscription();
+
+    constructor(private _dialogService: DialogService,
+                private _elementRef: ElementRef,
+                @Optional() private _popoverComponent: PopoverComponent) {}
 
     /** @hidden */
     ngAfterViewInit() {
         this._menuTemplates.push({title: this.mobileMenuTitle, template: this.menuTemplate});
-        this.openDialog();
+        this._listenOnPopoverOpen();
+    }
+
+    /** @hidden */
+    ngOnDestroy() {
+        this._subscription.unsubscribe();
     }
 
     /** @hidden */
@@ -72,16 +98,27 @@ export class MenuComponent implements AfterViewInit {
         return this._menuTemplates.length > 1;
     }
 
+    /** @hidden */
+    get isUsingPopoverAndMobileMode(): boolean {
+        return this._popoverComponent && this.mobile;
+    }
+
     /** Open the Dialog to display Menu in mobile mode */
     openDialog(): void {
-        if (this.mobile) {
-            this._dialogService.open(
-                this.dialogTemplate,
-                {
-                    mobile: true,
-                    verticalPadding: false
-                }
-            )
+        this._dialogService.open(
+            this.dialogTemplate,
+            {
+                mobile: true,
+                verticalPadding: false
+            }
+        )
+    }
+
+    /** Close mobile Menu mode Dialog */
+    closeDialog(dialogRef: DialogRef) {
+        dialogRef.close();
+        if (this.isUsingPopoverAndMobileMode) {
+            this._popoverComponent.close();
         }
     }
 
@@ -93,5 +130,22 @@ export class MenuComponent implements AfterViewInit {
     /** @hidden */
     loadView(view: DialogContent): void {
         this._menuTemplates.push(view);
+    }
+
+    /** @hidden If Menu is in mobile mode and it is used inside Popover,
+     * automatically open Dialog when popover has been opened */
+    private _listenOnPopoverOpen(): void {
+        if (this._popoverComponent) {
+            this._subscription.add(
+                this._popoverComponent.isOpenChange.pipe(
+                    filter(isOpen => this.mobile && isOpen)
+                ).subscribe(_ => this.openDialog())
+            )
+        }
+    }
+
+    /** @hidden Setup Popover to make it work with Menu in mobile mode */
+    private _setPopoverToMobileMode(): void {
+        this._popoverComponent.closeOnOutsideClick = false;
     }
 }
