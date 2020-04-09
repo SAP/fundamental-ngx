d0c1bc49708ba20b65520e5d25f6cbd16645e436
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2020-04-09 21:28:29 +0200

    test: ngonchanges removed when unnecessary (#2295)

--- libs/core/src/lib/object-status/object-status.component.spec.ts
+++ libs/core/src/lib/object-status/object-status.component.spec.ts
@@ -4,81 +4,73 @@ import { Component, ViewChild } from '@angular/core';
 import { ObjectStatusComponent } from './object-status.component';
 
 @Component({
-  selector: 'fd-test-object-status',
-  template: `
-        <span fd-object-status>Test Object Status</span>
-    `
+    selector: 'fd-test-object-status',
+    template: ` <span fd-object-status>Test Object Status</span> `,
 })
 class TestObjectStatusComponent {
-  @ViewChild(ObjectStatusComponent, { static: true })
-  objectStatusComponent: ObjectStatusComponent;
+    @ViewChild(ObjectStatusComponent, { static: true })
+    objectStatusComponent: ObjectStatusComponent;
 }
 
 describe('ObjectStatusComponent', () => {
-  let component: ObjectStatusComponent;
-  let fixture: ComponentFixture<TestObjectStatusComponent>;
+    let component: ObjectStatusComponent;
+    let fixture: ComponentFixture<TestObjectStatusComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ObjectStatusComponent, TestObjectStatusComponent]
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ObjectStatusComponent, TestObjectStatusComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestObjectStatusComponent);
-    component = fixture.componentInstance.objectStatusComponent;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestObjectStatusComponent);
+        component = fixture.componentInstance.objectStatusComponent;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('Should Add Status', () => {
-    component.ngOnInit();
-    component.status = 'positive';
-    component.ngOnChanges();
-    fixture.detectChanges();
-    expect(component.elementRef().nativeElement.classList.contains('fd-object-status--positive')).toBe(true);
-  });
+    it('Should Add Status', () => {
+        component.status = 'positive';
+        component.buildComponentCssClass();
+        fixture.detectChanges();
+        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--positive')).toBe(true);
+    });
 
-  it('Should Add Glyph', () => {
-    component.ngOnInit();
-    component.glyph = 'status-negative';
-    component.ngOnChanges();
-    fixture.detectChanges();
-    expect(component.elementRef().nativeElement.classList.contains('sap-icon--status-negative')).toBe(true);
-  });
+    it('Should Add Glyph', () => {
+        component.glyph = 'status-negative';
+        component.buildComponentCssClass();
+        fixture.detectChanges();
+        expect(component.elementRef().nativeElement.classList.contains('sap-icon--status-negative')).toBe(true);
+    });
 
-  it('Should Add Indication Color', () => {
-    component.ngOnInit();
-    component.indicationColor = 2;
-    component.ngOnChanges();
-    fixture.detectChanges();
-    expect(component.elementRef().nativeElement.classList.contains('fd-object-status--indication-2')).toBe(true);
-  });
+    it('Should Add Indication Color', () => {
+        component.indicationColor = 2;
+        component.buildComponentCssClass();
+        fixture.detectChanges();
+        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--indication-2')).toBe(true);
+    });
 
-  it('Should Add Clickable Class', () => {
-    component.ngOnInit();
-    component.clickable = true;
-    component.ngOnChanges();
-    fixture.detectChanges();
-    expect(component.elementRef().nativeElement.classList.contains('fd-object-status--link')).toBe(true);
-  });
+    it('Should Add Clickable Class', () => {
+        component.clickable = true;
+        component.buildComponentCssClass();
+        fixture.detectChanges();
+        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--link')).toBe(true);
+    });
 
-  it('Should Add Inverted Class', () => {
-    component.ngOnInit();
-    component.inverted = true;
-    component.ngOnChanges();
-    fixture.detectChanges();
-    expect(component.elementRef().nativeElement.classList.contains('fd-object-status--inverted')).toBe(true);
-  });
+    it('Should Add Inverted Class', () => {
+        component.inverted = true;
+        component.buildComponentCssClass();
+        fixture.detectChanges();
+        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--inverted')).toBe(true);
+    });
 
-  it('Should Apply Large Design', () => {
-    component.ngOnInit();
-    component.large = true;
-    component.ngOnChanges();
-    fixture.detectChanges();
-    expect(component.elementRef().nativeElement.classList.contains('fd-object-status--large')).toBe(true);
-  });
+    it('Should Apply Large Design', () => {
+        component.large = true;
+        component.buildComponentCssClass();
+        fixture.detectChanges();
+        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--large')).toBe(true);
+    });
 });
