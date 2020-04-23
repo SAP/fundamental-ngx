b2f95d598028a1c49995f7d51ff43bcb156895e4
Stefano Scalzo <stefano.scalzo@hotmail.com>
2020-04-23 15:48:23 -0400

    test:add textarea to storybook

---
+++ src/stories/textarea/fd-textarea-stories.ts
@@ -0,0 +1,44 @@
+import { moduleMetadata } from '@storybook/angular';
+import { withKnobs, boolean, select, text } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FormControlDirective, FormModule } from 'libs/core/src/lib/form/public_api';
+
+export default {
+    title: 'Fd text-area',
+    component: FormControlDirective,
+    moduleMetadata: moduleMetadata,
+    decorators: [
+        withKnobs,
+        withA11y,
+        moduleMetadata({
+            imports: [FormModule, ReactiveFormsModule],
+            declarations: []
+        })
+    ]
+};
+
+export const TextArea = () => ({
+    template:
+        `
+        <div fd-form-item>
+        <label fd-form-label for="input-1">Default Text Area</label>
+        <textarea fd-form-control type="text" id="input-1" placeholder="Field placeholder text"
+        [compact]="compactVar"
+        [state]="stateVar"
+        >
+        </textarea>
+        </div>
+  `,
+    props: {
+        stateVar: select('State', {
+            information: 'information',
+            success: 'success',
+            error: 'error',
+            warning: 'warning',
+            none: '',
+        }, ''),
+        compactVar: boolean('Compact', false),
+    }
+});
