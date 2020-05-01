38a2046ef6e209d38dac194193ab4a4d10cc9c42
Jedrzej <jedrzej.markowski@sap.com>
2020-05-01 14:19:01 +0200

    Add tests/comments

--- libs/core/src/lib/multi-input/multi-input-mobile/multi-input-mobile-configuration.ts
+++ libs/core/src/lib/multi-input/multi-input-mobile/multi-input-mobile-configuration.ts
@@ -1,9 +1,20 @@
-import { DefaultDialogObject } from '../../dialog/default-dialog/default-dialog-object';
+import { InjectionToken } from '@angular/core';
+
+export const MULTI_INPUT_MOBILE_CONFIG = new InjectionToken<string[]>('MultiInputMobileConfiguration');
+
 
 export class MultiInputMobileConfiguration {
 
-    /** TODO */
-    defaultObject?: DefaultDialogObject;
+    /** Dialog Title */
+    title?: string;
 
+    /** Approve Button Label. If empty, button will not appear */
+    approveButton?: string;
+
+    /** Cancel Button Label. If empty, button will not appear */
+    cancelButton?: string;
+
+    /** Defines if the close button should appear */
+    closeButton?: boolean;
 
 }
