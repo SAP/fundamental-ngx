5776487cab1d1e660a2615c1d78148d794c30e6d
deno <mladen.droshev@sap.com>
2020-05-14 07:55:40 -0400

    docs: move theme selector in the shellbar

--- apps/docs/src/app/documentation/core-helpers/sections-toolbar/sections-toolbar.component.ts
+++ apps/docs/src/app/documentation/core-helpers/sections-toolbar/sections-toolbar.component.ts
@@ -10,21 +10,7 @@ export class SectionsToolbarComponent implements OnInit {
     @Input() sections: SectionInterface[];
 
     search: string = '';
-    themes = [{
-        id: 'sap_fiori_3',
-        name: 'Fiori 3'
-    }, {
-        id: 'sap_fiori_3_dark',
-        name: 'Fiori 3 Dark'
-    }, {
-        id: 'sap_fiori_3_hcb',
-        name: 'High Contrast Black'
-    }, {
-        id: 'sap_fiori_3_hcw',
-        name: 'High Contrast White'
-    }];
 
-    selectedTheme: string;
     smallScreen: boolean = window.innerWidth < 992;
 
     @Input()
@@ -35,7 +21,6 @@ export class SectionsToolbarComponent implements OnInit {
 
     ngOnInit(): void {
         this.onActivate();
-        this.selectedTheme = this.themes[0].id;
     }
 
     onKeypressHandler(event: KeyboardEvent) {
@@ -69,8 +54,4 @@ export class SectionsToolbarComponent implements OnInit {
         this.windowSize();
     }
 
-    selectTheme() {
-        const cssLink: any = document.getElementById('themes');
-        cssLink.href = 'assets/' + this.selectedTheme + '.css';
-    }
 }
--- apps/docs/src/app/documentation/core-helpers/toolbar/toolbar.component.ts
+++ apps/docs/src/app/documentation/core-helpers/toolbar/toolbar.component.ts
@@ -1,15 +1,28 @@
-import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Inject, Output } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Inject,
+    Output,
+    OnInit,
+    ViewEncapsulation
+} from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Router } from '@angular/router';
 import { Libraries } from '../../utilities/libraries';
-import { ShellbarMenuItem } from '@fundamental-ngx/core';
+import { ShellbarMenuItem, ShellbarUser, ShellbarUserMenu, MenuKeyboardService } from '@fundamental-ngx/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'fd-docs-toolbar',
     templateUrl: './toolbar.component.html',
-    styleUrls: ['./toolbar.component.scss']
+    styleUrls: ['./toolbar.component.scss'],
+    providers: [MenuKeyboardService]
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
+    cssUrl: string;
+
     items: ShellbarMenuItem[] = [
         {
             name: 'Core Docs',
@@ -25,6 +38,31 @@ export class ToolbarComponent {
         }
     ];
 
+    isOpen: boolean = false;
+
+    action = {
+        glyph: 'palette'
+    };
+
+    themes = [
+        {
+            id: 'sap_fiori_3',
+            name: 'Fiori 3'
+        },
+        {
+            id: 'sap_fiori_3_dark',
+            name: 'Fiori 3 Dark'
+        },
+        {
+            id: 'sap_fiori_3_hcb',
+            name: 'High Contrast Black'
+        },
+        {
+            id: 'sap_fiori_3_hcw',
+            name: 'High Contrast White'
+        }
+    ];
+
     @Output()
     btnClicked: EventEmitter<undefined> = new EventEmitter<undefined>();
 
@@ -32,7 +70,25 @@ export class ToolbarComponent {
 
     public library: string;
 
-    constructor(private routerService: Router, @Inject('CURRENT_LIB') private currentLib: Libraries) {
+    ngOnInit(): void {
+        this.cssUrl = 'assets/sap_fiori_3.css';
+    }
+
+    constructor(
+        private routerService: Router,
+        @Inject('CURRENT_LIB') private currentLib: Libraries,
+        private menuKeyboardService: MenuKeyboardService,
+        private sanitizer: DomSanitizer
+    ) {
         this.library = routerService.routerState.snapshot.url.includes('core') ? 'Core' : 'Platform';
+    }
+
+    selectTheme(selectedTheme: string) {
+        this.isOpen = false;
+        this.cssUrl = 'assets/' + selectedTheme + '.css';
+    }
+
+    getCSSUrl() {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(this.cssUrl);
     }
 }
