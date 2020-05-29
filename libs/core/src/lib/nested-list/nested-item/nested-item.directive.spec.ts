346c419906bc92c3d07e524f8c8b17caf73751cb
Jedrzej <jedrzej.markowski@sap.com>
2020-05-29 11:55:58 +0200

    remove focused test

--- libs/core/src/lib/nested-list/nested-item/nested-item.directive.spec.ts
+++ libs/core/src/lib/nested-list/nested-item/nested-item.directive.spec.ts
@@ -82,7 +82,7 @@ class TestNestedContainerComponent {
     linkDirective: NestedLinkDirective;
 }
 
-fdescribe('NestedItemDirective', () => {
+describe('NestedItemDirective', () => {
     let component: TestNestedContainerComponent;
     let nestedItemPopoverDirective: NestedItemDirective;
     let nestedItemListDirective: NestedItemDirective;
