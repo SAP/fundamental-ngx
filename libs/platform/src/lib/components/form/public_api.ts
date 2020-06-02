de981b0737d5724476c36d63a9c51df35dbffdca
Lokanathan-k <53534379+Lokanathan-k@users.noreply.github.com>
2020-06-02 09:35:20 +0530

    fix: Platform wrapper component for info Label  with document changes (#2486)
    * Code Commit for the Info label platform component
    
    Code Commit for the Info label platform component
    
    Few document change s for the info lable component
    
    Few document change s for the info lable component
    
    Update info-label.component.ts
    
    updating the test case for the color change
    
    updating the test case for the color change
    
    remove the constructor
    
    remove the constructor
    
    Updating the docuement loading changes
    
    Updating the docuement loading changes
    
    * adding lazy loadin to the code
    
    adding lazy loadin to the code
    
    * document changes for the info label Lazy loading
    
    document changes for the info label Lazy loading
    
    * Update info-label.component.spec.ts
    
    * updating the documentation and accessability attributes
    
    updating the documentation and accessability attributes
    
    * adding aria attributes for accessability
    
    adding aria attributes for accessability
    
    * Update platform-info-label-header.component.html
    
    * Update platform-documentation.component.ts
    
    Reordering the URL  code alpahabeticallly

--- libs/platform/src/lib/components/form/public_api.ts
+++ libs/platform/src/lib/components/form/public_api.ts
@@ -6,5 +6,7 @@ export * from './input/fdp-input.module';
 export * from './input/input.component';
 export * from './combo-box/fdp-combo-box.module';
 export * from './combo-box/combo-box.component';
+export * from './input-message-group-with-template/input-message-group-with-template.component';
 export * from './radio-group/radio-group.module';
 export * from './radio-group/radio-group.component';
+export * from './radio-group/radio/radio.component';
