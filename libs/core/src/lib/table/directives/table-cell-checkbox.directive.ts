dc28023b86ebb9b0b7838c76c410e2fb912fd44e
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-06-03 17:21:53 -0600

    table cell checkbox does not need those hostbindings

--- libs/core/src/lib/table/directives/table-cell-checkbox.directive.ts
+++ libs/core/src/lib/table/directives/table-cell-checkbox.directive.ts
@@ -7,24 +7,4 @@ export class TableCellCheckboxDirective {
     /** @hidden */
     @HostBinding('class.fd-table__cell--checkbox')
     fdTableCellClass: boolean = true;
-
-    /** Whether or not to show the table cell's horizontal borders */
-    @HostBinding('class.fd-table__cell--no-horizontal-border')
-    @Input()
-    noBorderX: boolean = false;
-
-    /** Whether or not to show the table cell's vertical borders */
-    @HostBinding('class.fd-table__cell--no-vertical-border')
-    @Input()
-    noBorderY: boolean = false;
-
-    /** Whether or not the table cell is activable */
-    @HostBinding('class.fd-table__cell--activable')
-    @Input()
-    activable: boolean = false;
-
-    /** Whether or not the table cell is hoverable */
-    @HostBinding('class.fd-table__cell--hoverable')
-    @Input()
-    hoverable: boolean = false;
 }
