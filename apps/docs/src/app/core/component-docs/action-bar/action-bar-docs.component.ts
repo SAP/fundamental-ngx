4c756b04a5d80a006de8a1116847e742eb34f25f
Jedrzej <jedrzej.markowski@sap.com>
2020-06-08 13:10:17 +0200

    chore: Add fixes to stackblitz examples

--- apps/docs/src/app/core/component-docs/action-bar/action-bar-docs.component.ts
+++ apps/docs/src/app/core/component-docs/action-bar/action-bar-docs.component.ts
@@ -20,7 +20,7 @@ export class ActionBarDocsComponent {
         {
             language: 'html',
             code: backButtonExample,
-            fileName: 'action-bar-back'
+            fileName: 'action-bar-back-button-example'
         },
         {
             language: 'typescript',
@@ -34,7 +34,7 @@ export class ActionBarDocsComponent {
         {
             language: 'html',
             code: titleTruncationExample,
-            fileName: 'action-bar-long-string-title-truncation'
+            fileName: 'action-bar-long-string-title-truncation-example'
         },
         {
             language: 'typescript',
@@ -64,7 +64,7 @@ export class ActionBarDocsComponent {
         {
             language: 'html',
             code: mobileExample,
-            fileName: 'action-bar-mobile'
+            fileName: 'action-bar-mobile-button-example'
         },
         {
             language: 'typescript',
