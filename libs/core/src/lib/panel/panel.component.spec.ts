7214f4e5c339c8f47667c234bb2ae27cdf619dc4
InnaAtanasova <i.atanasova@sap.com>
2020-06-18 20:43:22 -0400

    feat: simplify Panel's implementation

--- libs/core/src/lib/panel/panel.component.spec.ts
+++ libs/core/src/lib/panel/panel.component.spec.ts
@@ -2,20 +2,15 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { PanelModule } from './panel.module';
-import { PanelService } from './panel.service';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
     template: `
-        <div #panelRef fd-panel [compact]="isCompact" [fixed]="isFixed">
-            <div fd-panel-header>
-                <div fd-panel-expand [compact]="isCompact"></div>
-                <h5 fd-panel-title>Panel Header</h5>
-            </div>
+        <fd-panel #panelRef [compact]="isCompact" [fixed]="isFixed">
+            <h5 fd-panel-title>Panel Header</h5>
             <div fd-panel-content>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut laoreet lorem.
             </div>
-        </div>
+        </fd-panel>
     `
 })
 class TestComponent {
@@ -31,25 +26,17 @@ describe('PanelComponent', () => {
     let fixture: ComponentFixture<TestComponent>;
     let panelContent: ElementRef;
     let button: ElementRef;
-    let panelServiceSpy: jasmine.SpyObj<PanelService>;
 
     beforeEach(async(() => {
-        const panelSpy = jasmine.createSpyObj('PanelService', ['updateExpanded']);
-
         TestBed.configureTestingModule({
             declarations: [TestComponent],
-            providers: [{ provide: PanelService, useValue: panelSpy }],
             imports: [PanelModule]
         }).compileComponents();
-
-        panelServiceSpy = TestBed.get(PanelService);
-        panelServiceSpy.expanded$ = new BehaviorSubject({isExpanded: false, isExpandTriggerClick: false});
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(TestComponent);
         component = fixture.debugElement.componentInstance;
-        button = fixture.debugElement.query(By.css('button'));
         fixture.detectChanges();
     });
 
@@ -82,9 +69,11 @@ describe('PanelComponent', () => {
     });
 
     it('should expand the content when the button is clicked', () => {
+        button = fixture.debugElement.query(By.css('button'));
         button.nativeElement.click();
         fixture.detectChanges();
         panelContent = fixture.debugElement.query(By.css('.fd-panel__content'));
+        expect(button.nativeElement.classList).toContain('is-expanded');
         expect(panelContent.nativeElement.innerHTML).toContain('Lorem ipsum dolor sit amet');
     });
 });
--- libs/core/src/lib/panel/public_api.ts
+++ libs/core/src/lib/panel/public_api.ts
@@ -1,6 +1,4 @@
 export * from './panel.module';
 export * from './panel-content/panel-content.directive';
-export * from './panel-expand/panel-expand.component';
-export * from './panel-header/panel-header.component';
 export * from './panel-title/panel-title.directive';
 export * from './panel.component';
