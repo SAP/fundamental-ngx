6ee66fdc951269354cfc489d2cf40292c09d6bc7
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2020-06-30 23:23:48 +0200

    docs: removed deprecated component (#2768)
    BREAKING CHANGES:
    - badge label
    - status indicator

--- apps/docs/src/app/core/documentation/core-documentation.component.ts
+++ apps/docs/src/app/core/documentation/core-documentation.component.ts
@@ -18,7 +18,6 @@ export class CoreDocumentationComponent extends DocumentationBaseComponent {
         this.components = [
             { url: 'core/action-bar', name: 'Action Bar' },
             { url: 'core/alert', name: 'Alert' },
-            { url: 'core/badgeLabel', name: 'Status Indicator' },
             { url: 'core/bar', name: 'Bar' },
             { url: 'core/breadcrumb', name: 'Breadcrumb' },
             { url: 'core/busyIndicator', name: 'Busy Indicator' },
