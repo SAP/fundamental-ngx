a99ef7ca808b4c67a9532db057cc6f418f0beb64
Michal Salamon <msalamon@divante.com>
2020-07-01 11:44:14 +0200

    feat(core) Update documentation

--- apps/docs/src/app/core/component-docs/step-input/examples/step-input-configuration-example/step-input-configuration-example.component.ts
+++ apps/docs/src/app/core/component-docs/step-input/examples/step-input-configuration-example/step-input-configuration-example.component.ts
@@ -4,14 +4,45 @@ import { Component } from '@angular/core';
     selector: 'fd-step-input-configuration-example',
     template: `
         <div style="display: flex; flex-wrap: wrap; justify-content: space-between;">
+
             <div class="step-input-example">
-                <fd-step-input [(value)]="value1"></fd-step-input>
+                <label fd-form-label for="min-fraction-input">4 minimal fraction digits</label>
+                <fd-step-input id="min-fraction-input" [minFractionDigits]="4" [(value)]="value1"></fd-step-input>
                 <small>Value: {{ value1 }}</small>
             </div>
 
             <div class="step-input-example">
-                <fd-step-input [min]="-10" [max]="10" [(value)]="value2"></fd-step-input>
+                <label fd-form-label for="integer-input">Integers only</label>
+                <fd-step-input id="integer-input" [maxFractionDigits]="0" [(value)]="value2"></fd-step-input>
                 <small>Value: {{ value2 }}</small>
+            </div>
+
+            <div class="step-input-example">
+                <label fd-form-label for="no-grouping-input">No grouping</label>
+                <fd-step-input id="no-grouping-input" [useGrouping]="group" [(value)]="value3"></fd-step-input>
+                <small>Value: {{ value3 }}</small>
+            </div>
+
+            <div class="step-input-example">
+                <label fd-form-label for="icons-input">Custom icons</label>
+                <fd-step-input id="icons-input"
+                               incrementButtonIcon="arrow-top"
+                               decrementButtonIcon="arrow-bottom"
+                               [(value)]="value4">
+                </fd-step-input>
+                <small>Value: {{ value4 }}</small>
+            </div>
+
+            <div class="step-input-example">
+                <label fd-form-label for="min-max-input">Min max limitation <-10,10></label>
+                <fd-step-input id="min-max-input" [min]="-10" [max]="10" [(value)]="value5"></fd-step-input>
+                <small>Value: {{ value5 }}</small>
+            </div>
+
+            <div class="step-input-example">
+                <label fd-form-label for="min-max-input">Step = 0.5</label>
+                <fd-step-input id="min-max-input" [step]="0.5" [(value)]="value6"></fd-step-input>
+                <small>Value: {{ value6 }}</small>
             </div>
         </div>
     `
@@ -19,4 +50,13 @@ import { Component } from '@angular/core';
 export class StepInputConfigurationExampleComponent {
     value1: number = 0;
     value2: number = 0;
+    value3: number = 10000.55;
+    value4: number = 0;
+    value5: number = 0;
+    value6: number = 0;
+    group;
+
+    constructor() {
+        setInterval(() => this.group = !this.group, 1500);
+    }
 }
--- apps/docs/src/app/core/component-docs/step-input/examples/step-input-label-example/step-input-label-example.component.ts
+++ apps/docs/src/app/core/component-docs/step-input/examples/step-input-label-example/step-input-label-example.component.ts
@@ -3,20 +3,20 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'fd-step-input-label-example',
     template: `
-        <div style="display: flex; flex-wrap: wrap; justify-content: space-evenly;">
+        <div style="display: flex; flex-wrap: wrap; justify-content: space-between;">
             <div class="step-input-example">
                 <label fd-form-label for="kg-input">Kilograms</label>
-                <fd-step-input id="kg-input" mode="currency" unit="kg"></fd-step-input>
+                <fd-step-input id="kg-input" unit="kg"></fd-step-input>
             </div>
 
             <div class="step-input-example">
                 <label fd-form-label for="mm-input">Millimeters</label>
-                <fd-step-input id="mm-input" mode="currency" unit="mm"></fd-step-input>
+                <fd-step-input id="mm-input" unit="mm"></fd-step-input>
             </div>
 
             <div class="step-input-example">
                 <label fd-form-label for="mol-input">Moles</label>
-                <fd-step-input id="mol-input" mode="currency" unit="mol"></fd-step-input>
+                <fd-step-input id="mol-input" unit="mol"></fd-step-input>
             </div>
         </div>
     `
