b8dccdb9b7d3f84884fffdbebf02e5dd6e06f870
jmarkowski <jedrzej.markowski@sap.com>
2020-07-06 14:16:44 +0200

    Revert "feat(select): Complete refactoring based on material common behavior (#2744)" (#2811)
    This reverts commit 7deffbd6bcf34bdea61897722f88af40ac5c9fe3.

--- apps/docs/src/app/documentation/core-helpers/playground/playground.component.ts
+++ apps/docs/src/app/documentation/core-helpers/playground/playground.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Schema } from '../../../schema/models/schema.model';
 
 @Component({
---
+++ apps/docs/src/app/platform/component-docs/platform-select/platform-select-header/platform-select-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-menu-header',
+    templateUrl: './platform-select-header.component.html',
+    styleUrls: ['./platform-select-header.component.scss']
+})
+export class PlatformSelectHeaderComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
