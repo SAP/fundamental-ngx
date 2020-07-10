41a94bf9394a56591213b4fa10ebedc2cf369b69
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-07-10 15:10:32 -0600

    table icon tests

--- libs/core/src/lib/table/directives/table-icon.directive.spec.ts
+++ libs/core/src/lib/table/directives/table-icon.directive.spec.ts
@@ -1,10 +1,49 @@
 import { TableIconDirective } from './table-icon.directive';
-import { ElementRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TableModule } from '@fundamental-ngx/core';
+
+@Component({
+    template: `
+        <span fd-table-icon></span>
+    `
+})
+class TestComponent {
+    @ViewChild(TableIconDirective)
+    icon: TableIconDirective;
+}
 
 describe('TableIconDirective', () => {
-  it('should create an instance', () => {
-    const elRef: ElementRef = new ElementRef<any>(`<div></div>`);
-    const directive = new TableIconDirective(elRef);
-    expect(directive).toBeTruthy();
-  });
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestComponent],
+            imports: [TableModule]
+        }).compileComponents();
+    }));
+
+    beforeEach(async() => {
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        await fixture.whenStable();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should assign classes', () => {
+
+        expect(component.icon.elementRef().nativeElement.classList.length).toBe(1);
+
+        component.icon.glyph = 'glyph';
+        component.icon.navigation = true;
+
+        fixture.detectChanges();
+
+        expect(component.icon.elementRef().nativeElement.classList.length).toBe(3);
+    });
 });
