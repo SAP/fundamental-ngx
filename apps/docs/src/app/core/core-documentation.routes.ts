3a355d6beb99f4350e7e23bcd24b3b082f3932e5
Jedrzej <jedrzej.markowski@sap.com>
2020-07-16 12:11:00 +0200

    fix: add form message examples, update code

--- apps/docs/src/app/core/core-documentation.routes.ts
+++ apps/docs/src/app/core/core-documentation.routes.ts
@@ -93,6 +93,11 @@ export const ROUTES: Routes = [
                     import('./component-docs/file-input/file-input-docs.module').then((m) => m.FileInputDocsModule)
             },
             {
+                path: 'form-message',
+                loadChildren: () =>
+                    import('./component-docs/form-message/form-message-docs.module').then((m) => m.FormMessageDocsModule)
+            },
+            {
                 path: 'global-config',
                 loadChildren: () =>
                     import('./component-docs/global-config/global-config-docs.module').then((m) => m.GlobalConfigDocsModule)
