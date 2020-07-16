f860924ae1c263c0b89b0ff757bd22061a6adc82
Dmitriy Tkachenko <dmitriy.tkachenko@valor-software.com>
2020-07-16 15:30:09 +0300

    fix(platform) update clean up code

--- libs/platform/src/lib/components/panel/panel.component.ts
+++ libs/platform/src/lib/components/panel/panel.component.ts
@@ -30,34 +30,42 @@ export class PlatformPanelComponent extends BaseComponent {
     /**
      * sets Panel title.
      */
-    @Input() title: string;
+    @Input()
+    title: string;
     /**
      * Whether the Panel Content is expanded
      */
-    @Input() expanded: boolean = true;
+    @Input()
+    expanded: boolean = true;
     /**
      * Whether the Panel is expandable
      */
-    @Input() expandable: boolean = true;
+    @Input()
+    expandable: boolean = true;
     /**
      * ARIA label for button when the Panel is collapsed
      */
-    @Input() expandLabel: string = 'Expand Panel';
+    @Input()
+    expandLabel: string = 'Expand Panel';
     /**
      * ARIA label for button when the Panel is expanded
      */
-    @Input() collapseLabel: string = 'Collapse Panel';
+    @Input()
+    collapseLabel: string = 'Collapse Panel';
 
     /** Output event triggered when the Expand button is clicked */
-    @Output() panelExpandChange: EventEmitter<PanelExpandChangeEvent> = new EventEmitter();
+    @Output()
+    panelExpandChange: EventEmitter<PanelExpandChangeEvent> = new EventEmitter();
 
     /** @hidden */
     @ContentChild(PlatformPanelActionsComponent) panelActions: PlatformPanelActionsComponent;
     /** @hidden */
     @ContentChild(PlatformPanelContentComponent) panelContent: PlatformPanelContentComponent;
 
-    /** @hidden */
-    get _expandAriaLabel(): string {
+    /**
+     *  Button label based on the current state
+     */
+    get expandAriaLabel(): string {
         return this.expanded ? this.collapseLabel : this.expandLabel;
     }
 
@@ -65,8 +73,10 @@ export class PlatformPanelComponent extends BaseComponent {
         super(_cd);
     }
 
-    /** @hidden */
-    _onExpandedChange(expanded: boolean) {
+    /**
+     *  Handles expanded/collapsed event
+     */
+    onExpandedChange(expanded: boolean) {
         const event = new PanelExpandChangeEvent(this, expanded);
         this.panelExpandChange.emit(event);
     }
