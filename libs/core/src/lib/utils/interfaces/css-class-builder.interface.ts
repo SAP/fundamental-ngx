73f355750beeb677abe1025f9401d14bcfa65531
Michal Salamon <msalamon@divante.com>
2020-07-30 11:07:56 +0200

    fix(core) Update typing

--- libs/core/src/lib/utils/interfaces/css-class-builder.interface.ts
+++ libs/core/src/lib/utils/interfaces/css-class-builder.interface.ts
@@ -1,7 +1,6 @@
 import { HasElementRef } from '../public_api';
 
 export interface CssClassBuilder extends HasElementRef {
-    classList?: string;
     class: string;
-    buildComponentCssClass(): string | string[];
+    buildComponentCssClass(): string[];
 }
