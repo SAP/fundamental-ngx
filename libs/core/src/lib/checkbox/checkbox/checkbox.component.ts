a6c86ee79564f7715d961c4b7c765b1acdbee460
Deepak Kumar <deepak.kumar14@sap.com>
2020-08-04 17:18:17 +0530

    fix:stopping click event

--- libs/core/src/lib/checkbox/checkbox/checkbox.component.ts
+++ libs/core/src/lib/checkbox/checkbox/checkbox.component.ts
@@ -178,6 +178,7 @@ export class CheckboxComponent implements ControlValueAccessor {
     /** @hidden Updates checkbox Indeterminate state on mouse click on IE11 */
     public checkByClick(event: MouseEvent): void {
         this._nextValueEvent(true, event);
+        this.muteKey(event);
     }
 
     /** @hidden Updates checkbox Indeterminate state on spacebar key on IE11 */
