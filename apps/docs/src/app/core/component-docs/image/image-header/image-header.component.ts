d4c2c388b526c6f193268503c5b526f88beaa660
kate <kozlowska.fed@gmail.com>
2020-08-10 15:23:25 +0200

    remove image

--- apps/docs/src/app/core/component-docs/image/image-header/image-header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-image-header',
-    templateUrl: './image-header.component.html',
-    styleUrls: ['./image-header.component.scss']
-})
-export class ImageHeaderComponent {}
