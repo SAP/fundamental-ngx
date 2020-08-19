979e85e60a90f45819c01ff9f1e5b9edd9bf8b63
sKudum <53487468+sKudum@users.noreply.github.com>
2020-08-19 19:46:39 +0530

    encapsulation from button documentation removed (#3041)

--- apps/docs/src/app/platform/component-docs/platform-button/platform-button-examples/platform-button-examples.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-button/platform-button-examples/platform-button-examples.component.ts
@@ -3,38 +3,33 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'fdp-button-types-example',
     templateUrl: './platform-button-types-example.component.html',
-    styleUrls: ['./platform-button-examples.scss'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['./platform-button-examples.scss']
 })
-export class PlatformButtonTypesExampleComponent {}
+export class PlatformButtonTypesExampleComponent { }
 
 @Component({
     selector: 'fdp-button-sizes-example',
     templateUrl: './platform-button-sizes-example.component.html',
-    styleUrls: ['./platform-button-examples.scss'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['./platform-button-examples.scss']
 })
-export class PlatformButtonSizesExampleComponent {}
+export class PlatformButtonSizesExampleComponent { }
 
 @Component({
     selector: 'fdp-button-icons-example',
     templateUrl: './platform-button-icons-example.component.html',
-    styleUrls: ['./platform-button-examples.scss'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['./platform-button-examples.scss']
 })
-export class PlatformButtonIconsExampleComponent {}
+export class PlatformButtonIconsExampleComponent { }
 
 @Component({
     selector: 'fdp-button-state-example',
     templateUrl: './platform-button-state-example.component.html',
-    styleUrls: ['./platform-button-examples.scss'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['./platform-button-examples.scss']
 })
-export class PlatformButtonStateExampleComponent {}
+export class PlatformButtonStateExampleComponent { }
 @Component({
     selector: 'fdp-button-truncate-example',
     templateUrl: './platform-button-truncate-example.component.html',
-    styleUrls: ['./platform-button-examples.scss'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['./platform-button-examples.scss']
 })
-export class PlatformButtonTruncateExampleComponent {}
+export class PlatformButtonTruncateExampleComponent { }
