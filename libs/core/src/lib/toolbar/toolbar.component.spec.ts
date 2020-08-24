b0e8cad5c7a7d86acc869b579869b95e844ac207
Art Olshansky <olshansky0@gmail.com>
2020-08-24 12:51:30 +0300

    fix: (core) toolbar enhancements (#3019)
    * fix: (core) toolbar enhancements
    
    * test: (core) toolbar
    
    * fix: (core) hiding elements
    
    * little refactor
    
    * add title to button with icon
    
    * fix: got rid of elements fliker
    
    Co-authored-by: salarenko <salarenko@users.noreply.github.com>

--- libs/core/src/lib/toolbar/toolbar.component.spec.ts
+++ libs/core/src/lib/toolbar/toolbar.component.spec.ts
@@ -1,13 +1,147 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViewChild, Component, ViewChildren, QueryList } from '@angular/core';
 
-import { ToolbarComponent } from './toolbar.component';
 import { whenStable } from '../utils/tests/when-stable';
+import { ToolbarComponent } from './toolbar.component';
 import { ToolbarItemDirective } from './toolbar-item.directive';
 import { ToolbarSpacerComponent } from './toolbar-spacer.component';
 import { ToolbarSeparatorComponent } from './toolbar-separator.component';
+import { ToolbarOverflowPriorityDirective } from './toolbar-overflow-priority.directive';
+import { ToolbarOverflowGroupDirective } from './toolbar-overflow-group.directive';
 
+describe('ToolbarComponent', () => {
+    let toolbar: ToolbarComponent;
+    let component: ToolbarTestComponent;
+    let fixture: ComponentFixture<ToolbarTestComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ToolbarComponent,
+                ToolbarTestComponent,
+                ToolbarItemDirective,
+                ToolbarOverflowPriorityDirective,
+                ToolbarOverflowGroupDirective,
+                ToolbarSpacerComponent,
+                ToolbarSeparatorComponent
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(async () => {
+        fixture = TestBed.createComponent(ToolbarTestComponent);
+
+        await whenStable(fixture);
+
+        component = fixture.componentInstance;
+        toolbar = component.toolbar;
+        toolbar.toolbarItems = component.childrens;
+
+        await whenStable(fixture);
+    });
+
+    it('should create', async () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should overflow', async () => {
+        expect(toolbar['_overflowElements'].length).toBeGreaterThan(0);
+    });
+});
+
+describe('ToolbarComponent - Prioritization', () => {
+    let toolbar: ToolbarComponent;
+    let component: ToolbarOverflowPriorityTestComponent;
+    let fixture: ComponentFixture<ToolbarOverflowPriorityTestComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ToolbarComponent,
+                ToolbarOverflowPriorityTestComponent,
+                ToolbarItemDirective,
+                ToolbarOverflowPriorityDirective,
+                ToolbarOverflowGroupDirective,
+                ToolbarSpacerComponent,
+                ToolbarSeparatorComponent
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(async () => {
+        fixture = TestBed.createComponent(ToolbarOverflowPriorityTestComponent);
+
+        await whenStable(fixture);
+
+        component = fixture.componentInstance;
+        toolbar = component.toolbar;
+        toolbar.toolbarItems = component.childrens;
+
+        await whenStable(fixture);
+    });
+
+    it('should hide element to overflow by priority', async () => {
+        const normalElements = ['Button First', 'Never', 'High'];
+        const overflowElements = ['Always', 'Low', 'Button Last'];
+        const disappearElements = ['Disappear'];
+
+        expect(toolbar['_overflowElements'].length).toBeGreaterThan(0);
+        expect(toolbar['_normalElements'].length).toBeGreaterThan(0);
+        expect(toolbar['_disappearElements'].length).toBeGreaterThan(0);
+
+        expect(toolbar['_overflowElements'].map(el => el.elementRef.nativeElement.textContent)).toEqual(overflowElements);
+        expect(toolbar['_normalElements'].map(el => el.elementRef.nativeElement.textContent)).toEqual(normalElements);
+        expect(toolbar['_disappearElements'].map(el => el.elementRef.nativeElement.textContent)).toEqual(disappearElements);
+    });
+});
+
+describe('ToolbarComponent - Prioritization and Grouping', () => {
+    let toolbar: ToolbarComponent;
+    let component: ToolbarOverflowGroupingTestComponent;
+    let fixture: ComponentFixture<ToolbarOverflowGroupingTestComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ToolbarComponent,
+                ToolbarOverflowGroupingTestComponent,
+                ToolbarItemDirective,
+                ToolbarOverflowPriorityDirective,
+                ToolbarOverflowGroupDirective,
+                ToolbarSpacerComponent,
+                ToolbarSeparatorComponent
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(async () => {
+        fixture = TestBed.createComponent(ToolbarOverflowGroupingTestComponent);
+
+        await whenStable(fixture);
+
+        component = fixture.componentInstance;
+        toolbar = component.toolbar;
+        toolbar.toolbarItems = component.childrens;
+
+        await whenStable(fixture);
+    });
+
+    it('should hide elements to overflow by group and priority', async () => {
+        const normalElements = ['Button', 'Never', 'Gr 1 / Low', 'Gr 1 / High'];
+        const overflowElements = ['Always', 'Gr 2 / Low', 'Gr 2 / Low'];
+        const disappearElements = ['Gr 2 / Disappear'];
+
+        expect(toolbar['_overflowElements'].length).toBeGreaterThan(0);
+        expect(toolbar['_normalElements'].length).toBeGreaterThan(0);
+        expect(toolbar['_disappearElements'].length).toBeGreaterThan(0);
+
+        expect(toolbar['_overflowElements'].map(el => el.elementRef.nativeElement.textContent)).toEqual(overflowElements);
+        expect(toolbar['_normalElements'].map(el => el.elementRef.nativeElement.textContent)).toEqual(normalElements);
+        expect(toolbar['_disappearElements'].map(el => el.elementRef.nativeElement.textContent)).toEqual(disappearElements);
+    });
+});
+
+/* Basic toolbar */
 @Component({
     template: `
         <div [style.width]="width">
@@ -53,53 +187,49 @@ class ToolbarTestComponent {
     width = '300px';
 }
 
-describe('ToolbarComponent', () => {
-    let toolbar: ToolbarComponent;
-    let hostComponent: ToolbarTestComponent;
-    let fixture: ComponentFixture<ToolbarTestComponent>;
+/* Toolbar with prioritization */
+@Component({
+    template: `
+        <div [style.width]="width">
+            <fd-toolbar #toolbar [shouldOverflow]="true">
+                <button fd-toolbar-item fd-button [compact]="true">Button First</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="always">Always</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="never">Never</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="low">Low</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="high">High</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="disappear">Disappear</button>
+                <button fd-toolbar-item fd-button [compact]="true">Button Last</button>
+            </fd-toolbar>
+        </div>
+    `
+})
+class ToolbarOverflowPriorityTestComponent {
+    @ViewChild('toolbar') toolbar: ToolbarComponent;
+    @ViewChildren(ToolbarItemDirective) childrens: QueryList<ToolbarItemDirective>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                ToolbarComponent,
-                ToolbarTestComponent,
-                ToolbarItemDirective,
-                ToolbarSpacerComponent,
-                ToolbarSeparatorComponent
-            ]
-        }).compileComponents();
-    }));
+    width = '300px';
+}
 
-    beforeEach(async () => {
-        fixture = TestBed.createComponent(ToolbarTestComponent);
+/* Toolbar with prioritization and grouping */
+@Component({
+    template: `
+        <div [style.width]="width">
+            <fd-toolbar #toolbar [shouldOverflow]="true">
+                <button fd-toolbar-item fd-button [compact]="true">Button</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="always">Always</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="never">Never</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="low" fdOverflowGroup="1">Gr 1 / Low</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="low" fdOverflowGroup="2">Gr 2 / Low</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="disappear" fdOverflowGroup="2">Gr 2 / Disappear</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="low" fdOverflowGroup="2" >Gr 2 / Low</button>
+                <button fd-toolbar-item fd-button [compact]="true" fdOverflowPriority="high" fdOverflowGroup="1">Gr 1 / High</button>
+            </fd-toolbar>
+        </div>
+    `
+})
+class ToolbarOverflowGroupingTestComponent {
+    @ViewChild('toolbar') toolbar: ToolbarComponent;
+    @ViewChildren(ToolbarItemDirective) childrens: QueryList<ToolbarItemDirective>;
 
-        await whenStable(fixture);
-
-        hostComponent = fixture.componentInstance;
-        toolbar = hostComponent.toolbar;
-        toolbar.toolbarItems = hostComponent.childrens;
-
-        await whenStable(fixture);
-    });
-
-    it('should create', async () => {
-        expect(hostComponent).toBeTruthy();
-    });
-
-    it('should overflow', async () => {
-        toolbar.ngOnInit();
-
-        await whenStable(fixture);
-
-        expect(toolbar['_overflowElements'].length).toBeGreaterThan(0);
-
-        hostComponent.width = '99999px';
-
-        await whenStable(fixture);
-
-        // toolbar['_onResize']().subscribe(async () => {
-        //     await whenStable(fixture);
-        //     expect(toolbar['_overflowElements'].length).toEqual(0);
-        // });
-    });
-});
+    width = '400px';
+}
