e8590e29e202f0e5b128f2916243425d3b22bd66
I505460 <lokanathan.k@sap.com>
2020-08-25 14:13:06 +0530

    code refatoring for the form components in core
    code refatoring for the form components in core

---
+++ libs/core/src/lib/form/form-input-message-group/form-input-message-group.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { FormInputMessageGroupComponent } from './form-input-message-group.component';
+import { NgModule } from '@angular/core';
+import { PopoverModule } from '../../popover/public_api';
+
+@NgModule({
+    imports: [CommonModule, PopoverModule],
+    exports: [FormInputMessageGroupComponent],
+    declarations: [FormInputMessageGroupComponent]
+})
+export class FormInputMessageGroupModule {}
