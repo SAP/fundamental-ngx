f555a320c36630976855e1cc5a6cd5839ecfe95a
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-09-02 16:50:18 -0600

    start tests

--- libs/core/src/lib/product-switch/product-switch-body/product-switch-body.component.spec.ts
+++ libs/core/src/lib/product-switch/product-switch-body/product-switch-body.component.spec.ts
@@ -1,28 +1,105 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductSwitchBodyComponent } from './product-switch-body.component';
-import { ButtonModule, PopoverModule } from '@fundamental-ngx/core';
+import { ButtonModule, PopoverModule, ProductSwitchItem } from '@fundamental-ngx/core';
 import { DragAndDropModule } from '../../utils/drag-and-drop/drag-and-drop.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { createKeyboardEvent } from '../../../../../platform/src/lib/testing/event-objects';
+import { ENTER, LEFT_ARROW, RIGHT_ARROW } from '@angular/cdk/keycodes';
 
-describe('ProductSwitchBodyComponent', () => {
-    let component: ProductSwitchBodyComponent;
-    let fixture: ComponentFixture<ProductSwitchBodyComponent>;
+@Component({
+    selector: 'fd-test-component',
+    template: '<fd-product-switch-body [products]="list"> </fd-product-switch-body>'
+})
+export class TestComponent {
+    list: ProductSwitchItem[] = [
+        {
+            title: 'Home',
+            subtitle: 'Central Home',
+            icon: 'home',
+            stickToPosition: true,
+            disabledDragAndDrop: true
+        },
+        {
+            title: 'Analytics Cloud',
+            subtitle: 'Analytics Cloud',
+            icon: 'business-objects-experience',
+            selected: true
+        },
+        {
+            title: 'Catalog',
+            subtitle: 'Ariba',
+            icon: 'contacts'
+        },
+        {
+            title: 'Guided Buying',
+            icon: 'credit-card'
+        },
+        {
+            title: 'Strategic Procurement',
+            icon: 'cart-3'
+        }
+    ];
+}
+
+fdescribe('ProductSwitchBodyComponent', () => {
+    let fixture: ComponentFixture<TestComponent>, debugElement: DebugElement, element: HTMLElement;
+
+    let component, componentInstance: ProductSwitchBodyComponent;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [PopoverModule, ButtonModule, DragAndDropModule, DragDropModule],
-            declarations: [ProductSwitchBodyComponent]
-        }).compileComponents();
+            declarations: [ProductSwitchBodyComponent, TestComponent]
+        });
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(ProductSwitchBodyComponent);
-        component = fixture.componentInstance;
+        fixture = TestBed.createComponent(TestComponent);
+        debugElement = fixture.debugElement;
+        element = debugElement.nativeElement;
         fixture.detectChanges();
+        component = debugElement.query(By.directive(ProductSwitchBodyComponent));
+        componentInstance = component.injector.get(ProductSwitchBodyComponent);
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should handle keydown enter', () => {
+        const el = fixture.debugElement.query(By.css('li'));
+        spyOn(el.nativeElement, 'click');
+        el.nativeElement.focus();
+        const keyboardEvent = createKeyboardEvent('keydown', ENTER, 'Enter', el.nativeElement);
+        spyOn(keyboardEvent, 'preventDefault');
+        el.nativeElement.dispatchEvent(keyboardEvent);
+
+        expect(keyboardEvent.preventDefault).toHaveBeenCalled();
+        expect(el.nativeElement.click).toHaveBeenCalled();
+    });
+
+    it('should handle no list keydown arrow right', () => {
+        spyOn(componentInstance, 'isListMode').and.returnValue(false);
+        const el = fixture.debugElement.query(By.css('li'));
+        const nextEl = el.nativeElement.nextElementSibling;
+        el.nativeElement.focus();
+        const keyboardEvent = createKeyboardEvent('keydown', RIGHT_ARROW, 'ArrowRight', el.nativeElement);
+        el.nativeElement.dispatchEvent(keyboardEvent);
+
+        expect(document.activeElement).toBe(nextEl);
+    });
+
+    it('should handle no list keydown arrow left', () => {
+        spyOn(componentInstance, 'isListMode').and.returnValue(false);
+        const el = fixture.debugElement.query(By.css('li'));
+        const nextEl = el.nativeElement.nextElementSibling;
+        nextEl.focus();
+        const keyboardEvent = createKeyboardEvent('keydown', LEFT_ARROW, 'ArrowLeft', nextEl);
+        nextEl.dispatchEvent(keyboardEvent);
+
+        expect(document.activeElement).toBe(el.nativeElement);
     });
 });
