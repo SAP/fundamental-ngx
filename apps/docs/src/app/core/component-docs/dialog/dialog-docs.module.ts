54fe6ddef80dc07873af11387c3de2b98fd8aae0
Michal Salamon <msalamon@divante.com>
2020-09-07 15:26:28 +0200

    chore(core) Import FormGroupModule | Imprve documentation

--- apps/docs/src/app/core/component-docs/dialog/dialog-docs.module.ts
+++ apps/docs/src/app/core/component-docs/dialog/dialog-docs.module.ts
@@ -1,55 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DialogModule, DialogService, FormModule, InputGroupModule, ListModule } from '@fundamental-ngx/core';
 import { ApiComponent } from '../../../documentation/core-helpers/api/api.component';
 import { API_FILES } from '../../api-files';
 import { DialogDocsHeaderComponent } from './dialog-docs-header/dialog-docs-header.component';
 import { DialogDocsComponent } from './dialog-docs.component';
-import { DialogModule, DialogService, FormModule, ListModule } from '@fundamental-ngx/core';
-import { DialogExampleComponent } from './examples/component-based/dialog-example.component';
-import { ComponentBasedDialogExampleComponent } from './examples/component-based/component-based-dialog-example.component';
-import { TemplateBasedDialogExampleComponent } from './examples/template-based/template-based-dialog-example.component';
-import { DialogConfigurationExample } from './examples/dialog-configuration/dialog-configuration-example.component';
-import { DialogPositionExampleComponent } from './examples/dialog-position/dialog-position-example.component';
-import { DialogMobileExampleComponent } from './examples/dialog-mobile/dialog-mobile-example.component';
-import { DialogStackedExampleComponent } from './examples/stacked-dialogs/dialog-stacked-example.component';
-import { FirstDialogExampleComponent } from './examples/stacked-dialogs/first-dialog-example.component';
-import { SecondDialogExampleComponent } from './examples/stacked-dialogs/second-dialog-example.component';
-import { DialogBackdropContainerExampleComponent } from './examples/dialog-backdrop-container/dialog-backdrop-container-example.component';
-import { DialogComplexExampleComponent } from './examples/dialog-complex/dialog-complex-example.component';
-import { DialogStateExample } from './examples/dialog-state/dialog-state-example.component';
-import { DialogObjectExampleComponent } from './examples/dialog-object-example/dialog-object-example.component';
 import { SharedDocumentationPageModule } from '../../../documentation/shared-documentation-page.module';
+import {
+    examples,
+    DialogExampleComponent,
+    DialogStackedExampleComponent,
+    FirstDialogExampleComponent,
+    SecondDialogExampleComponent
+} from './examples';
 
 const routes: Routes = [
     {
         path: '',
         component: DialogDocsHeaderComponent,
         children: [
-            { path: '', component: DialogDocsComponent },
-            { path: 'api', component: ApiComponent, data: { content: API_FILES.dialog } }
+            {path: '', component: DialogDocsComponent},
+            {path: 'api', component: ApiComponent, data: {content: API_FILES.dialog}}
         ]
     }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes), SharedDocumentationPageModule, FormModule, DialogModule, ListModule],
+    imports: [
+        RouterModule.forChild(routes),
+        SharedDocumentationPageModule,
+        FormModule,
+        ListModule,
+        DialogModule,
+        InputGroupModule
+    ],
     exports: [RouterModule],
     declarations: [
-        DialogStateExample,
+        examples,
         DialogDocsComponent,
-        DialogExampleComponent,
-        DialogDocsHeaderComponent,
-        DialogConfigurationExample,
-        FirstDialogExampleComponent,
-        SecondDialogExampleComponent,
-        DialogMobileExampleComponent,
-        DialogComplexExampleComponent,
-        DialogStackedExampleComponent,
-        DialogPositionExampleComponent,
-        TemplateBasedDialogExampleComponent,
-        ComponentBasedDialogExampleComponent,
-        DialogBackdropContainerExampleComponent,
-        DialogObjectExampleComponent
+        DialogDocsHeaderComponent
     ],
     entryComponents: [
         DialogExampleComponent,
@@ -59,4 +48,4 @@ const routes: Routes = [
     ],
     providers: [DialogService]
 })
-export class DialogDocsModule {}
+export class DialogDocsModule { }
