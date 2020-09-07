41f1aca974fff7555441e31043040df6642062aa
Michal Salamon <msalamon@divante.com>
2020-09-07 13:47:30 +0200

    fix(core) Add missing module | improve docs | fix focus styling

---
+++ apps/docs/src/app/core/component-docs/busy-indicator/examples/index.ts
@@ -0,0 +1,13 @@
+import { BusyIndicatorBasicExampleComponent } from './busy-indicator-basic-example.component';
+import { BusyIndicatorSizeExampleComponent } from './busy-indicator-size-example.component';
+import { BusyIndicatorWrapperExampleComponent } from './busy-indicator-wrapper-example.component';
+
+export * from './busy-indicator-basic-example.component';
+export * from './busy-indicator-size-example.component';
+export * from './busy-indicator-wrapper-example.component';
+
+export const examples = [
+    BusyIndicatorSizeExampleComponent,
+    BusyIndicatorBasicExampleComponent,
+    BusyIndicatorWrapperExampleComponent
+];
