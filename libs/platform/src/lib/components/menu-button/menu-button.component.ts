fe93c1ad12ff795ca5a3ba3bd8763105ef8b2655
Deepak Kumar <deepak.kumar14@sap.com>
2020-09-09 13:22:04 +0530

    fix:defect hunting issue fixed

--- libs/platform/src/lib/components/menu-button/menu-button.component.ts
+++ libs/platform/src/lib/components/menu-button/menu-button.component.ts
@@ -1,12 +1,4 @@
-import {
-    Component,
-    Input,
-    Output,
-    EventEmitter,
-    ChangeDetectorRef,
-    ChangeDetectionStrategy,
-    ViewEncapsulation
-} from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { ButtonType } from '@fundamental-ngx/core';
 import { BaseComponent } from '../base';
 
@@ -14,8 +6,7 @@ import { BaseComponent } from '../base';
     selector: 'fdp-menu-button',
     templateUrl: './menu-button.component.html',
     styleUrls: ['./menu-button.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    encapsulation: ViewEncapsulation.None
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuButtonComponent extends BaseComponent {
     /** text for tooltip */
