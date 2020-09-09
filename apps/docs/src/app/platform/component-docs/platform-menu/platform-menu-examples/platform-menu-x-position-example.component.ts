d87ad5ca349278eb1fa42bf90f1c82747a9f6d16
Kevin Okamoto <kevin.okamoto@sap.com>
2020-09-09 14:02:29 -0700

    Fix of menu component stackblitz examples

--- apps/docs/src/app/platform/component-docs/platform-menu/platform-menu-examples/platform-menu-x-position-example.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-menu/platform-menu-examples/platform-menu-x-position-example.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'fdp-platform-menu-x-position-example',
     templateUrl: './platform-menu-x-position-example.component.html',
-    styleUrls: ['./platform-menu-basic-example.component.scss'],
+    styleUrls: ['./platform-menu-x-position-example.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class PlatformMenuXPositionExampleComponent implements OnInit {
     public item = '';
 
-    ngOnInit(): void {}
+    ngOnInit(): void { }
 
     onItemSelect(item: string): void {
         this.item = item;
