361749f74054f638338871ab6f56ca887b18285d
Jedrzej <jedrzej.markowski@sap.com>
2020-09-10 10:09:47 +0200

    remove changes

--- libs/core/src/lib/checkbox/checkbox/checkbox.component.ts
+++ libs/core/src/lib/checkbox/checkbox/checkbox.component.ts
@@ -172,6 +172,7 @@ export class CheckboxComponent implements ControlValueAccessor {
     /** @hidden Updates checkbox Indeterminate state on mouse click on IE11 */
     public checkByClick(event: MouseEvent): void {
         this._nextValueEvent(true, event);
+        this.muteKey(event);
     }
 
     /** @hidden Updates checkbox Indeterminate state on spacebar key on IE11 */
