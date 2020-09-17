9c863dbcb1eb712f48d1cf25603183f8e7eeee3a
droshev <mladen.droshev@sap.com>
2020-09-17 18:07:22 -0400

    chore: deprecate icon size

--- libs/core/src/lib/icon/icon.component.ts
+++ libs/core/src/lib/icon/icon.component.ts
@@ -36,7 +36,8 @@ export class IconComponent extends AbstractFdNgxClass {
      * */
     @Input() glyph;
 
-    /**
+    /** @deprecated
+     * Icon size is deprecated. The size can be set by font-size. It will be removed after version 0.23
      * The size of the icon
      * The predefined values for the input size are *xs*, *s*, *l*, and *xl*.
      * *size* can accept any other string, for example *xxs*, which will be translated into class *sap-icon--xxs*.
@@ -47,10 +48,6 @@ export class IconComponent extends AbstractFdNgxClass {
     _setProperties(): void {
         if (this.glyph) {
             this._addClassToElement(PREFIX_ICON_CLASS + this.glyph);
-        }
-
-        if (this.size) {
-            this._addClassToElement(PREFIX_ICON_CLASS + this.size);
         }
     }
 
