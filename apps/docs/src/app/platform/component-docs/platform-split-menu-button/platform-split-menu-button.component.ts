ff0e21f1d2eb2e057e104a3dc0326e1b35794e86
Deepak Kumar <57661487+DeepakSap14@users.noreply.github.com>
2020-09-18 21:47:15 +0530

    fix: (platform) split menu button fix for label change on action selection from menu (#3230)
    * fix: defect funting issues
    
    * fix:position menu before
    
    * include comments in html and formatted
    
    * added truncation, changed examples

--- apps/docs/src/app/platform/component-docs/platform-split-menu-button/platform-split-menu-button.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-split-menu-button/platform-split-menu-button.component.ts
@@ -1,41 +1,57 @@
 import { Component } from '@angular/core';
-import * as splitMenuButtonExampleHtml from '!raw-loader!./platform-split-menu-button-examples/platform-split-menu-button-examples.component.html';
-import * as splitMenuButtonExampleCode from '!raw-loader!./platform-split-menu-button-examples/platform-split-menu-button-examples.component.ts';
-import * as splitMenuButtonOptionsHtml from '!raw-loader!./platform-split-menu-button-examples/platform-split-menu-button-options.component.html';
-import * as splitMenuButtonOptionsCode from '!raw-loader!./platform-split-menu-button-examples/platform-split-menu-button-options.component.ts';
+import * as splitMenuButtonTypesHtml from '!raw-loader!./platform-split-menu-button-examples/platform-split-button-types-example.component.html';
+import * as splitMenuButtonTypesCode from '!raw-loader!./platform-split-menu-button-examples/platform-split-button-types-example.component.ts';
+import * as splitMenuButtonIconsHtml from '!raw-loader!./platform-split-menu-button-examples/platform-split-button-icons-example.component.html';
+import * as splitMenuButtonIconsCode from '!raw-loader!./platform-split-menu-button-examples/platform-split-button-icons-example.component.ts';
+import * as splitMenuButtonBehaviorHtml from '!raw-loader!./platform-split-menu-button-examples/platform-split-button-behaviors-example.component.html';
+import * as splitMenuButtonBehaviorCode from '!raw-loader!./platform-split-menu-button-examples/platform-split-button-behaviors-example.component.ts';
 
 import { ExampleFile } from '../../../documentation/core-helpers/code-example/example-file';
 
 @Component({
     selector: 'app-split-menu-button',
-    templateUrl: './platform-split-menu-button.component.html',
+    templateUrl: './platform-split-menu-button.component.html'
 })
 export class PlatformDocsSplitMenuButtonComponent {
-    splitMenuButtonExample: ExampleFile[] = [
+    splitMenuButtonBehavior: ExampleFile[] = [
         {
             language: 'html',
-            code: splitMenuButtonExampleHtml,
-            fileName: 'platform-split-menu-button-example',
+            code: splitMenuButtonBehaviorHtml,
+            fileName: 'platform-split-button-behaviors-example'
         },
         {
             language: 'typescript',
-            code: splitMenuButtonExampleCode,
-            fileName: 'platform-split-menu-button-example',
-            component: 'PlatformSplitMenuButtonExampleComponent',
-        },
+            code: splitMenuButtonBehaviorCode,
+            fileName: 'platform-split-button-behaviors-example',
+            component: 'PlatformDocsSplitMenuButtonBehaviorComponent'
+        }
     ];
 
-    splitMenuButtonOptions: ExampleFile[] = [
+    splitMenuButtonTypes: ExampleFile[] = [
         {
             language: 'html',
-            code: splitMenuButtonOptionsHtml,
-            fileName: 'platform-split-menu-button-options',
+            code: splitMenuButtonTypesHtml,
+            fileName: 'platform-split-button-types-example'
         },
         {
             language: 'typescript',
-            code: splitMenuButtonOptionsCode,
-            fileName: 'platform-split-menu-button-options',
-            component: 'PlatformSplitMenuButtonExampleComponent',
+            code: splitMenuButtonTypesCode,
+            fileName: 'platform-split-button-types-example',
+            component: 'PlatformDocsSplitMenuButtonTypesComponent'
+        }
+    ];
+
+    splitMenuButtonIcons: ExampleFile[] = [
+        {
+            language: 'html',
+            code: splitMenuButtonIconsHtml,
+            fileName: 'platform-split-button-icons-example'
         },
+        {
+            language: 'typescript',
+            code: splitMenuButtonIconsCode,
+            fileName: 'platform-split-button-icons-example',
+            component: 'PlatformDocsSplitMenuButtonIconsComponent'
+        }
     ];
 }
