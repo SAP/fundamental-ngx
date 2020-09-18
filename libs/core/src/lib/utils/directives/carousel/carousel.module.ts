5a690b3500e666b61fe4ab53d64c59f353611458
Jedrzej <jedrzej.markowski@sap.com>
2020-09-18 12:40:38 +0200

    fix: Remove hammer config

--- libs/core/src/lib/utils/directives/carousel/carousel.module.ts
+++ libs/core/src/lib/utils/directives/carousel/carousel.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselItemDirective } from './carousel-item.directive';
 import { CarouselDirective } from './carousel.directive';
-import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-import { HammerConfig } from './hammer.config';
 
 @NgModule({
     imports: [CommonModule],
     exports: [CarouselItemDirective, CarouselDirective],
-    declarations: [CarouselItemDirective, CarouselDirective],
-    providers: [ {
-        provide: HAMMER_GESTURE_CONFIG,
-        useClass: HammerConfig
-    }]
+    declarations: [CarouselItemDirective, CarouselDirective]
 })
 export class CarouselModule {}
