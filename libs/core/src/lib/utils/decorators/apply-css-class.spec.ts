f452eb66133478d201850cf5298d0929e4dd3d25
Dmitriy Tkachenko <dmitriy.tkachenko@valor-software.com>
2020-09-21 12:11:47 +0300

    fix: (core) applyCssClass decorator (#3328)
    * fix(core): apply class names decorator
    
    * fix typo
    
    * typo

--- libs/core/src/lib/utils/decorators/apply-css-class.spec.ts
+++ libs/core/src/lib/utils/decorators/apply-css-class.spec.ts
@@ -1,10 +1,11 @@
-import { Component, Directive, OnChanges, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, Directive, OnChanges, OnInit, ElementRef, ViewChild, ContentChild, Renderer2 } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
 
 import { ButtonComponent } from '../../button/public_api';
 import { CssClassBuilder, applyCssClass } from '../../utils/public_api';
 
-const testDirectiveClass = 'fd-test-directive';
+const TEST_DIRECTIVE_CLASS = 'fd-test-directive';
 
 @Directive({
     // tslint:disable-next-line: directive-selector
@@ -25,7 +26,7 @@ export class TestDirective implements OnInit, OnChanges, CssClassBuilder {
 
     @applyCssClass
     buildComponentCssClass(): string[] {
-        return [ testDirectiveClass ];
+        return [TEST_DIRECTIVE_CLASS];
     }
 
     elementRef(): ElementRef<any> {
@@ -33,12 +34,41 @@ export class TestDirective implements OnInit, OnChanges, CssClassBuilder {
     }
 }
 
+const TEST_CONTENT_CHILD_PHASE_CLASS_NAME = 'content-child-phase-class-name';
+
+@Component({
+    selector: 'fd-test-proxy-component',
+    template: '<ng-content></ng-content>'
+})
+export class TestProxyComponent {
+    @ContentChild(ButtonComponent, { read: ElementRef })
+    set buttonElementRef(elementRef: ElementRef<HTMLElement>) {
+        this._renderer.addClass(elementRef?.nativeElement, TEST_CONTENT_CHILD_PHASE_CLASS_NAME);
+    }
+
+    constructor(private _renderer: Renderer2) {}
+}
+
+const TEST_VIEW_CHILD_PHASE_CLASS_NAME = 'view-child-phase-class-name';
+
 @Component({
     selector: 'fd-test-component',
-    template: '<button #element fd-test-directive fd-button>Button</button>'
+    template: `
+        <fd-test-proxy-component>
+            <button class="initial-button-class" #element fd-test-directive fd-button>Button</button>
+        </fd-test-proxy-component>
+    `
 })
 export class TestComponent {
-    @ViewChild('element') element: ElementRef;
+    @ViewChild('element')
+    element: ButtonComponent;
+
+    @ViewChild('element', { read: ElementRef })
+    set viewChildClassName(elementRef: ElementRef<HTMLElement>) {
+        this._renderer.addClass(elementRef?.nativeElement, TEST_VIEW_CHILD_PHASE_CLASS_NAME);
+    }
+
+    constructor(private _renderer: Renderer2) {}
 }
 
 describe('ButtonComponent', () => {
@@ -48,30 +78,56 @@ describe('ButtonComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [TestDirective, ButtonComponent, TestComponent]
+            imports: [CommonModule],
+            declarations: [TestDirective, ButtonComponent, TestComponent, TestProxyComponent]
         });
     }));
 
     beforeEach(async () => {
         fixture = TestBed.createComponent(TestComponent);
         fixture.detectChanges();
-        await fixture.whenStable();
         buttonInstance = (fixture.componentInstance.element as unknown) as ButtonComponent;
         testDirectiveInstance = (fixture.componentInstance.element as unknown) as TestDirective;
+    });
+
+    beforeEach(() => {
+        /**
+         * We have to check that calling buildComponentCssClass() in a row
+         * at the beginning does not impact on expected result
+         */
+        buttonInstance.buildComponentCssClass();
+        buttonInstance.buildComponentCssClass();
+    });
+
+    it('should keep initial class', async () => {
+        const componentClasses = (buttonInstance.elementRef().nativeElement as HTMLElement).className;
+        expect(componentClasses).toContain('initial-button-class');
     });
 
     it('should handle styles for 2 directives', async () => {
         buttonInstance.compact = true;
         buttonInstance.fdType = 'standard';
 
-        buttonInstance.ngOnInit();
         testDirectiveInstance.ngOnInit();
 
         await fixture.whenStable();
 
         const componentClasses = (buttonInstance.elementRef().nativeElement as HTMLElement).className;
+
         expect(componentClasses).toContain('standard');
         expect(componentClasses).toContain('compact');
-        expect(componentClasses).toContain(testDirectiveClass);
+        expect(componentClasses).toContain(TEST_DIRECTIVE_CLASS);
+    });
+
+    it('should keep classes added at @ContentChild phase', async () => {
+        const componentClasses = (buttonInstance.elementRef().nativeElement as HTMLElement).className;
+
+        expect(componentClasses).toContain(TEST_CONTENT_CHILD_PHASE_CLASS_NAME);
+    });
+
+    it('should keep classes added at @ViewChild phase', async () => {
+        const componentClasses = (buttonInstance.elementRef().nativeElement as HTMLElement).className;
+
+        expect(componentClasses).toContain(TEST_VIEW_CHILD_PHASE_CLASS_NAME);
     });
 });
