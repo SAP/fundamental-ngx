b969304663239cb4073cb1ca9bc3c38e0bf446f3
Michal Salamon <msalamon@divante.com>
2020-09-25 13:01:38 +0200

    fix(core) Change Info Label into component | Improve tests

--- libs/core/src/lib/info-label/info-label.component.ts
+++ libs/core/src/lib/info-label/info-label.component.ts
@@ -1,69 +1,75 @@
 import {
-    Component,
-    ViewEncapsulation,
     ChangeDetectionStrategy,
-    Input,
-    OnInit,
+    Component,
     ElementRef,
-    OnChanges
+    Input,
+    OnChanges,
+    OnInit,
+    ViewEncapsulation
 } from '@angular/core';
-import { CssClassBuilder, applyCssClass } from '../utils/public_api';
+import { applyCssClass, CssClassBuilder } from '../utils/public_api';
 
 export type LabelType = 'numeric' | 'icon';
 
 @Component({
     // tslint:disable-next-line:component-selector
-    selector: '[fd-info-label]',
-    template: `<ng-content></ng-content>`,
+    // TODO @salarenko: DEPRECATED DIRECTIVE APPROACH - Remove in v0.23.0
+    selector: 'fd-info-label, [fd-info-label]',
+    templateUrl: './info-label.component.html',
     styleUrls: ['./info-label.component.scss'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InfoLabelComponent implements OnInit, OnChanges, CssClassBuilder {
-    /** user's custom classes */
+    /** User's custom classes */
     @Input()
     class = '';
 
     /**
      * The LabelType represented by the info label .
-     * Can be one of the following: 'numeric' | 'only-icon' | 'icon'
      * For default info label omit this property
      */
     @Input()
     type: LabelType;
 
-    /** glyph define the icon of info label */
+    /** Glyph define the icon of info label */
     @Input()
     glyph: string;
 
-    /**define the colour of the info label starting form 1 to 10 */
+    /** Define the colour of the info label starting form 1 to 10 */
     @Input()
     color: string;
 
-    @applyCssClass
-    buildComponentCssClass(): string[] {
-        return [
-            'fd-info-label',
-            this.type ? `fd-info-label--${this.type}` : '',
-            this.glyph ? `sap-icon--${this.glyph}` : '',
-            this.color ? `fd-info-label--accent-color-${this.color}` : '',
-            this.class
-        ];
-    }
+    /** Define the text content of the info label */
+    @Input()
+    label: string;
 
     /** @hidden */
     constructor(private _elementRef: ElementRef) {}
 
+    /** @hidden */
     ngOnInit(): void {
         this.buildComponentCssClass();
     }
 
+    /** @hidden */
     ngOnChanges(): void {
         this.buildComponentCssClass();
     }
 
     /** @hidden */
-    elementRef(): ElementRef<any> {
+    elementRef(): ElementRef {
         return this._elementRef;
+    }
+
+    /** @hidden */
+    @applyCssClass
+    buildComponentCssClass(): string[] {
+        return [
+            'fd-info-label',
+            this.type ? `fd-info-label--${this.type}` : '',
+            this.color ? `fd-info-label--accent-color-${this.color}` : '',
+            this.class
+        ];
     }
 }
