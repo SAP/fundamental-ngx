d71e71b09c02f94192980e9d0365c57009f1d63b
InnaAtanasova <i.atanasova@sap.com>
2020-09-28 17:50:50 -0400

    fix: refactor Object Status to use label as input prop

--- libs/core/src/lib/object-status/object-status.component.spec.ts
+++ libs/core/src/lib/object-status/object-status.component.spec.ts
@@ -1,19 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, ViewChild } from '@angular/core';
 
 import { ObjectStatusComponent } from './object-status.component';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'fd-test-object-status',
-    template: ` <span fd-object-status>Test Object Status</span> `
+    template: `
+        <span
+            fd-object-status
+            [status]="status"
+            [glyph]="glyph"
+            [label]="label"
+            [indicationColor]="indicationColor"
+            [clickable]="clickable"
+            [inverted]="inverted"
+            [large]="large"
+        >
+        </span>
+    `
 })
 class TestObjectStatusComponent {
-    @ViewChild(ObjectStatusComponent, { static: true })
-    objectStatusComponent: ObjectStatusComponent;
+    @ViewChild(ObjectStatusComponent, { static: true, read: ElementRef })
+    objectStatusElementRef: ElementRef;
+
+    status: 'negative' | 'critical' | 'positive' | 'informative';
+    glyph: string;
+    label: string;
+    indicationColor: number;
+    clickable: boolean;
+    inverted: boolean;
+    large: boolean;
 }
 
 describe('ObjectStatusComponent', () => {
-    let component: ObjectStatusComponent;
+    let objectStatusElementRef: ElementRef;
+    let testComponent: TestObjectStatusComponent;
     let fixture: ComponentFixture<TestObjectStatusComponent>;
 
     beforeEach(async(() => {
@@ -24,46 +45,63 @@ describe('ObjectStatusComponent', () => {
 
     beforeEach(() => {
         fixture = TestBed.createComponent(TestObjectStatusComponent);
-        component = fixture.componentInstance.objectStatusComponent;
+        objectStatusElementRef = fixture.componentInstance.objectStatusElementRef;
+        testComponent = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('Should create', () => {
+        expect(testComponent).toBeTruthy();
+        expect(objectStatusElementRef).toBeTruthy();
     });
 
-    it('Should Add Status', () => {
-        component.status = 'positive';
-        component.buildComponentCssClass();
+    it('Should add status', () => {
+        testComponent.status = 'positive';
         fixture.detectChanges();
-        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--positive')).toBe(true);
+        expect(objectStatusElementRef.nativeElement.classList.contains('fd-object-status--positive')).toBeTrue();
     });
 
-    it('Should Add Indication Color', () => {
-        component.indicationColor = 2;
-        component.buildComponentCssClass();
+    it('Should add indication color', () => {
+        testComponent.indicationColor = 2;
         fixture.detectChanges();
-        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--indication-2')).toBe(true);
+        expect(objectStatusElementRef.nativeElement.classList.contains('fd-object-status--indication-2')).toBeTrue();
     });
 
-    it('Should Add Clickable Class', () => {
-        component.clickable = true;
-        component.buildComponentCssClass();
+    it('Should add icon', () => {
+        testComponent.glyph = 'future';
         fixture.detectChanges();
-        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--link')).toBe(true);
+        const iconElement = fixture.nativeElement.querySelector('i');
+
+        expect(iconElement).toBeTruthy();
+        expect(iconElement.classList.contains('sap-icon--future')).toBeTrue();
     });
 
-    it('Should Add Inverted Class', () => {
-        component.inverted = true;
-        component.buildComponentCssClass();
+    it('Should add inverted class', () => {
+        testComponent.inverted = true;
         fixture.detectChanges();
-        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--inverted')).toBe(true);
+        expect(objectStatusElementRef.nativeElement.classList.contains('fd-object-status--inverted')).toBeTrue();
     });
 
-    it('Should Apply Large Design', () => {
-        component.large = true;
-        component.buildComponentCssClass();
+    it('Should add large design', () => {
+        testComponent.large = true;
         fixture.detectChanges();
-        expect(component.elementRef().nativeElement.classList.contains('fd-object-status--large')).toBe(true);
+        expect(objectStatusElementRef.nativeElement.classList.contains('fd-object-status--large')).toBeTrue();
+    });
+
+    it('Should add clickable class', () => {
+        testComponent.clickable = true;
+        fixture.detectChanges();
+        expect(objectStatusElementRef.nativeElement.classList.contains('fd-object-status--link')).toBeTrue();
+    });
+
+    it('Should display label', () => {
+        const label = 'Test label';
+        testComponent.label = label;
+        fixture.detectChanges();
+
+        const labelTextElement = fixture.nativeElement.querySelector('.fd-object-status__text');
+
+        expect(labelTextElement).toBeTruthy();
+        expect(labelTextElement.textContent.trim()).toBe(label);
     });
 });
