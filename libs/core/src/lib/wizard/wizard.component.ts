10c79bb592946d0fd4576c4494ea6f46e714320c
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-09-29 14:45:55 -0600

    moving through steps works

--- libs/core/src/lib/wizard/wizard.component.ts
+++ libs/core/src/lib/wizard/wizard.component.ts
@@ -1,5 +1,6 @@
 import {
     AfterViewInit,
+    ChangeDetectionStrategy,
     ChangeDetectorRef,
     Component,
     ContentChildren,
@@ -17,7 +18,8 @@ import { Subscription } from 'rxjs';
     selector: 'fd-wizard',
     templateUrl: './wizard.component.html',
     styleUrls: ['./wizard.component.scss'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WizardComponent implements AfterViewInit, OnDestroy {
     /** @hidden */
@@ -33,47 +35,65 @@ export class WizardComponent implements AfterViewInit, OnDestroy {
     /** @hidden */
     private _previousWidth: number;
 
-    constructor(private _cdRef: ChangeDetectorRef, private _elRef: ElementRef) {}
+    constructor(private _elRef: ElementRef, private _cdRef: ChangeDetectorRef) {}
 
     /** @hidden */
     @HostListener('window:resize')
     resizeHandler(): void {
         // Check if any step is narrower than 168px and also get the current step
         const wizardWidth = this._elRef.nativeElement.getBoundingClientRect().width;
-        if (wizardWidth < this._previousWidth) {
-            this.steps.forEach((step) => {
-                if (step.status === 'active' || step.status === 'current') {
-                    const currentStep = step;
-                    if (step.wizardLabel && step.elRef.nativeElement.clientWidth < 168) {
-                        this._hideSomeStep(currentStep);
-                    }
-                }
-            });
+        if (wizardWidth <= this._previousWidth) {
+            this._wizardShrinking();
+        } else if (wizardWidth > this._previousWidth) {
+            this._wizardGrowing();
         }
         this._previousWidth = wizardWidth;
     }
 
     /** @hidden */
     ngAfterViewInit(): void {
-        this._setContentTemplate();
-        this._subscriptions.add(
-            this.steps.changes.subscribe(() => {
-                this._setContentTemplate();
-            })
-        );
-        this.steps.forEach((step) => {
+        setTimeout(() => { // fixes ExpressionChangedAfterItHasBeenCheckedError
+            this._setContentTemplate();
             this._subscriptions.add(
-                step.statusChange.subscribe(() => {
-                    this._setContentTemplate();
+                this.steps.changes.subscribe(() => {
+                    this._handleStepOrStatusChanges();
                 })
             );
+            this.steps.forEach(step => {
+                this._subscriptions.add(
+                    step.statusChange.subscribe(() => {
+                        this._handleStepOrStatusChanges();
+                    })
+                );
+                // need to call wizardShrinking for each step < 168px on first load
+                if (step.wizardLabel && step.elRef.nativeElement.clientWidth < 168) {
+                    this._wizardShrinking();
+                }
+            });
+            this._cdRef.detectChanges();
         });
-        this._previousWidth = this._elRef.nativeElement.getBoundingClientRect().width;
     }
 
     /** @hidden */
     ngOnDestroy(): void {
         this._subscriptions.unsubscribe();
+    }
+
+    /** @hidden */
+    private _wizardShrinking(): void {
+        this.steps.forEach((step) => {
+            if (step.status === 'active' || step.status === 'current') {
+                const currentStep = step;
+                if (step.wizardLabel && step.elRef.nativeElement.clientWidth < 168) {
+                    this._hideSomeStep(currentStep);
+                }
+            }
+        });
+    }
+
+    /** @hidden */
+    private _wizardGrowing(): void {
+
     }
 
     /** @hidden */
@@ -117,5 +137,37 @@ export class WizardComponent implements AfterViewInit, OnDestroy {
                 step.elRef.nativeElement.classList.add('fd-wizard__step--stacked-top');
             }
         });
+    }
+
+    /** @hidden */
+    private _resetStepClasses(): void {
+        this.steps.forEach(step => {
+            step.elRef.nativeElement.classList.remove('fd-wizard__step--stacked-top');
+            step.elRef.nativeElement.classList.remove('fd-wizard__step--stacked');
+            step.elRef.nativeElement.classList.remove('fd-wizard__step--no-label');
+        });
+    }
+
+    /** @hidden */
+    private _handleStepOrStatusChanges(): void {
+        this._resetStepClasses();
+        this._setContentTemplate();
+        let i = 0;
+        while (this._anyStepIsTooNarrow() && i < this.steps.length - 1) {
+            i++;
+            this.resizeHandler();
+        }
+        this._cdRef.detectChanges();
+    }
+
+    /** @hidden */
+    private _anyStepIsTooNarrow(): boolean {
+        let foundNarrowStep = false;
+        this.steps.forEach(step => {
+            if (step.elRef.nativeElement.clientWidth < 168) {
+                foundNarrowStep = true;
+            }
+        });
+        return foundNarrowStep;
     }
 }
