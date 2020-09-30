1231601edd20a88e06901869934990914a731e4b
Michal Salamon <msalamon@divante.com>
2020-09-30 12:35:40 +0200

    fix(core) Remove shadow dom example | Add composedPath method check

--- apps/docs/src/app/core/component-docs/popover/examples/popover-dialog/popover-dialog-example.component.ts
+++ apps/docs/src/app/core/component-docs/popover/examples/popover-dialog/popover-dialog-example.component.ts
@@ -1,5 +1,5 @@
 import { Component, TemplateRef } from '@angular/core';
-import { DialogConfig, DialogService } from '@fundamental-ngx/core';
+import { DialogService } from '@fundamental-ngx/core';
 
 @Component({
     selector: 'fd-popover-dialog-example',
