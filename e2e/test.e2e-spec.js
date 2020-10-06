ba3b97c0b981119f9fdd211110034b06b62fab4c
anton <anymac@TonyMac.local>
2020-10-06 20:29:55 +0300

    test(e2e): sauclab connect init

---
+++ e2e/test.e2e-spec.js
@@ -0,0 +1,19 @@
+
+
+describe('Test Sauclab', function() {
+
+    it('should work',  async function() {
+        await browser.waitForAngularEnabled(false);
+        await browser.sleep(10000);
+        await browser.get('/');
+        expect(true).toBe(true);
+    });
+    it('should fail', function() {
+        expect(false).toBe(true);
+    });
+
+    afterEach( async ()=> {
+
+        //browser.executeScript("sauce:job-result=" + (this.children[0].result.status ? "pass" : "failed"))
+    });
+});
---
+++ protractor-ci.conf.js
@@ -0,0 +1,31 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+require('dotenv').config();
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+exports.config = {
+    allScriptsTimeout: 11000,
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+    specs: [
+        './e2e/**/*.e2e-spec.js'
+    ],
+    multiCapabilities: [
+        {
+            'browserName': 'internet explorer',
+            'version': 'latest',
+            'name': 'tests-Protractor',
+        }
+    ],
+    directConnect: false,
+    baseUrl: 'http://www.google.com',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function() {}
+    },
+    onPrepare() {
+        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    }
+};
