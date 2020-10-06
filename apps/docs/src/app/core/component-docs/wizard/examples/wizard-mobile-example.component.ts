c18e07cfca6c15c5e13d8c460ae669f95ff4a554
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-10-06 13:56:15 -0600

    some progress on PR comments

--- apps/docs/src/app/core/component-docs/wizard/examples/wizard-mobile-example.component.ts
+++ apps/docs/src/app/core/component-docs/wizard/examples/wizard-mobile-example.component.ts
@@ -21,12 +21,20 @@ import { Component, ViewEncapsulation } from '@angular/core';
 export class WizardMobileExampleComponent {
     step1status = 'current';
     step2status = 'upcoming';
+    step3status = 'upcoming';
 
     goToStep(step: number): void {
         switch (step) {
             case 2: {
                 this.step1status = 'completed';
                 this.step2status = 'current';
+                this.step3status = 'upcoming';
+                break;
+            }
+            case 3: {
+                this.step1status = 'completed';
+                this.step2status = 'completed';
+                this.step3status = 'current';
                 break;
             }
         }
