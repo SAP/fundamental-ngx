6d8ddce45343f5eff93dd4a1bc0296d79a8b6424
Kevin Okamoto <48103491+KevinOkamoto@users.noreply.github.com>
2020-10-06 07:44:52 -0700

    Adding highlight.pipe and auto-complete.directive to exports (#3556)

--- libs/platform/src/lib/components/form/combobox/index.ts
+++ libs/platform/src/lib/components/form/combobox/index.ts
@@ -3,3 +3,5 @@ export * from './combobox/combobox.component';
 export * from './commons/base-combobox';
 export * from './combobox-mobile/combobox/combobox-mobile.component';
 export * from './combobox-mobile/combobox-mobile.module';
+export * from './pipes/highlight.pipe';
+export * from './directives/auto-complete.directive';
