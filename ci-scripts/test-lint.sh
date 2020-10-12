fec7fdcc97e30a8b0d856d4727327c76e372c0d3
Jedrzej <jedrzej.markowski@sap.com>
2020-10-12 19:15:28 +0200

    fix shell script, to handle errors from test/lint

--- ci-scripts/test-lint.sh
+++ ci-scripts/test-lint.sh
@@ -5,11 +5,19 @@ CORE_PATH=libs/core/
 PLATFORM_PATH=libs/platform/
 DOCS_PATH=apps/docs/
 
-
 core=$(git diff --name-only ${COMMIT_RANGE_BRANCH} | sort -u | uniq | grep ${CORE_PATH})
 platform=$(git diff --name-only ${COMMIT_RANGE_BRANCH} | sort -u | uniq | grep ${PLATFORM_PATH})
 docs=$(git diff --name-only ${COMMIT_RANGE_BRANCH} | sort -u | uniq | grep ${DOCS_PATH})
 
+
+handle_error() {
+    if [ $? -eq 0 ]
+    then
+        echo Tests passed
+    else
+        exit 1
+    fi
+}
 
 # Check for core
 if [ -z "$core"  ]
@@ -17,6 +25,7 @@ then
     echo There are no changes at core
 else
     npm run test && npm run lint:core
+    handle_error
 fi
 
 
@@ -26,6 +35,7 @@ then
     echo There are no changes at platform
 else
     npm run test:platform && npm run lint:platform
+    handle_error
 fi
 
 
@@ -35,5 +45,5 @@ then
     echo There are no changes at docs
 else
     npm run lint:docs
+    handle_error
 fi
-
