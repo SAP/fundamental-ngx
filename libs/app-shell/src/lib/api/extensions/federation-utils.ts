534f8812a3ab455b2e1d13b64c55abc983d380a2
Dmitriy Shekhovtsov <valorkin@gmail.com>
2020-10-15 17:59:30 +0300

    feat(app-shell): iframe merged in plugin launcher

--- libs/app-shell/src/lib/api/extensions/federation-utils.ts
+++ libs/app-shell/src/lib/api/extensions/federation-utils.ts
@@ -14,7 +14,10 @@ interface Container {
 const moduleMap = {};
 
 function loadRemoteEntry(remoteEntry: string): Promise<void> {
-    return new Promise<any>((resolve, reject) => {
+    if (moduleMap[remoteEntry]) {
+        return moduleMap[remoteEntry];
+    }
+    moduleMap[remoteEntry] = new Promise<any>((resolve, reject) => {
 
         if (moduleMap[remoteEntry]) {
             resolve();
@@ -27,11 +30,12 @@ function loadRemoteEntry(remoteEntry: string): Promise<void> {
         script.onerror = reject;
 
         script.onload = () => {
-            moduleMap[remoteEntry] = true;
             resolve(); // window is the global namespace
         };
+
         document.body.append(script);
     });
+    return moduleMap[remoteEntry];
 }
 
 async function lookupExposedModule<T>(remoteName: string, exposedModule: string): Promise<T> {
@@ -46,8 +50,8 @@ async function lookupExposedModule<T>(remoteName: string, exposedModule: string)
     return Module as T;
 }
 
-
 export async function loadRemoteModule<T = any>(options: Partial<PluginDescriptor>, module: AngularIvyComponentDescriptor): Promise<T> {
     await loadRemoteEntry(options.uri);
+
     return await lookupExposedModule<T>(options.name, module.exposedModule);
 }
--- libs/app-shell/src/lib/components/plugin-launcher/plugin-launcher.component.ts
+++ libs/app-shell/src/lib/components/plugin-launcher/plugin-launcher.component.ts
@@ -1,28 +1,39 @@
 import {
+    AfterViewChecked,
     ChangeDetectionStrategy,
     ChangeDetectorRef,
     Component,
     ComponentFactoryResolver,
-    ComponentRef, ElementRef,
+    ElementRef,
     Injector,
     Input,
-    OnChanges, Renderer2,
+    OnChanges,
+    Renderer2,
+    SimpleChanges,
     ViewChild,
     ViewContainerRef
 } from '@angular/core';
 import { loadRemoteModule } from '../../api/extensions/federation-utils';
 import { AngularIvyComponentDescriptor, PluginDescriptor } from '../../api/extensions/lookup/plugin-descriptor.model';
 import { LookupService } from '../../api/extensions/lookup/lookup.service';
-import { isPluginComponent } from '../../api/extensions/component/plugin-component';
 import { PluginManagerService } from '../../api/extensions/plugin-manager.service';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'fds-plugin-launcher',
-    template: '<ng-container #view></ng-container>',
+    styleUrls: ['./plugin-launcher.component.scss'],
+    template: `
+        <ng-container #view></ng-container>
+        <iframe
+            *ngIf="_safeIframeUri"
+            #iframe
+            class="responsive-wrapper"
+            [src]="_safeIframeUri"
+            [style.minHeight]="iframeAttrs.height"
+        ></iframe>`,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PluginLauncherComponent implements OnChanges {
+export class PluginLauncherComponent implements OnChanges, AfterViewChecked {
     /** plugin name */
     @Input()
     name: string;
@@ -31,38 +42,68 @@ export class PluginLauncherComponent implements OnChanges {
     @Input()
     module: string;
 
-    descriptor: Partial<PluginDescriptor>;
+    /** Iframe URI */
+    @Input() iframeUri: string;
+    @Input() iframeAttrs: Record<string, string | number>;
 
     @ViewChild('view', { read: ViewContainerRef, static: true })
-    viewContainer: ViewContainerRef;
+    ngContentView: ViewContainerRef;
 
-    constructor(private injector: Injector,
-                private _elementRef: ElementRef,
-                private cfr: ComponentFactoryResolver,
-                private _cd: ChangeDetectorRef,
-                private _render: Renderer2,
-                private _pluginMgr: PluginManagerService,
-                private lookupService: LookupService) {
+    @ViewChild('iframe', { static: false }) iframeEl: ElementRef;
+
+    _safeIframeUri: SafeResourceUrl;
+
+    private descriptor: Partial<PluginDescriptor>;
+
+    constructor(private readonly _injector: Injector,
+                private readonly _elementRef: ElementRef,
+                private readonly cfr: ComponentFactoryResolver,
+                private readonly _cd: ChangeDetectorRef,
+                private readonly _render: Renderer2,
+                private readonly _pluginMgr: PluginManagerService,
+                private readonly lookupService: LookupService,
+                private readonly sanitizer: DomSanitizer) {
     }
 
-    async ngOnChanges(): Promise<void> {
-        this.viewContainer.clear();
-
-        if (!this.descriptor) {
-            const item = this.lookupService.lookup(this.initQuery());
-            if (!item) {
-                return;
-            }
-            this.descriptor = item.descriptor;
+    async ngOnChanges(changes: SimpleChanges): Promise<void> {
+        if ('iframeUri' in changes) {
+            this._safeIframeUri = this.sanitizer.bypassSecurityTrustResourceUrl(this.iframeUri);
         }
-        this.doCreateComponent(this.descriptor, this.module);
+
+        if ('name' in changes) {
+            const { descriptor } = this.lookupService.lookup(new Map([['name', this.name]]));
+            if (!this.descriptor || this.descriptor.name !== descriptor.name) {
+                this.descriptor = descriptor;
+                this.renderPlugin(this.descriptor);
+            }
+        }
     }
 
+    ngAfterViewChecked(): void {
+        if (this._safeIframeUri) {
+            this.updateAttrs(this.iframeAttrs);
+        }
+    }
 
-    async doCreateComponent(descriptor: Partial<PluginDescriptor>, moduleName: string): Promise<void> {
-        const _module = descriptor.modules.find(module => module.name === moduleName);
-        const _component = await loadRemoteModule(descriptor, _module as AngularIvyComponentDescriptor)
-            .then(m => m[_module.name]);
+    async renderPlugin(descriptor: Partial<PluginDescriptor>): Promise<void> {
+        if (!descriptor) {
+            return;
+        }
+
+        const _module = descriptor.modules.find(module => module.name === this.module);
+        const _component = await loadRemoteModule<AngularIvyComponentDescriptor>(descriptor, _module as AngularIvyComponentDescriptor)
+            .then(m => m[_module.name])
+            .catch(err => console.error(err));
+
+        if (_module.type !== 'iframe' && !this.iframeUri) {
+            this._safeIframeUri = null;
+        }
+
+        if (_module.type === 'iframe') {
+            const _url = descriptor.uri + _module.html;
+            this._safeIframeUri = this.sanitizer.bypassSecurityTrustResourceUrl(_url);
+            return;
+        }
 
         if (_module.type === 'custom-element') {
             const element = document.createElement(_component);
@@ -70,23 +111,27 @@ export class PluginLauncherComponent implements OnChanges {
             return;
         }
 
-        if (_module.type === 'angular-ivy-component') {
+        if (_module.type === 'angular-ivy-component' && this.ngContentView) {
+            this.ngContentView.clear();
             const factory = this.cfr.resolveComponentFactory(_component);
-            const componentRef: ComponentRef<any> = this.viewContainer.createComponent(factory, null, this.injector);
-
-            if (isPluginComponent(componentRef.instance)) {
-                this._pluginMgr.register(descriptor, componentRef.instance);
-            }
+            this.ngContentView.createComponent(factory, null, this._injector);
             this._cd.detectChanges();
-            return;
         }
     }
 
-    private initQuery(): Map<string, any> {
-        const query = new Map();
-        if (this.name) {
-            query.set('name', this.name);
+    private updateAttrs(newValue: Record<string, string | number>, oldValue?: Record<string, string>): void {
+        if (!this._safeIframeUri) {
+            return;
         }
-        return query;
+        if (oldValue) {
+            for (const key of Object.keys(oldValue)) {
+                this._render.removeAttribute(this.iframeEl.nativeElement, key);
+            }
+        }
+        if (newValue) {
+            for (const key of Object.keys(newValue)) {
+                this._render.setAttribute(this.iframeEl.nativeElement, key, `${newValue[key]}`);
+            }
+        }
     }
 }
