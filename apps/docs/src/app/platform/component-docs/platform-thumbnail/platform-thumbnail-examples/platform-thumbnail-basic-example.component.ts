5e7773cdc4bce226fca6238dfed583d49f896c5a
Jedrzej <jedrzej.markowski@sap.com>
2020-10-22 10:47:12 +0200

    fix thumbnail link, put various images in examples

--- apps/docs/src/app/platform/component-docs/platform-thumbnail/platform-thumbnail-examples/platform-thumbnail-basic-example.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-thumbnail/platform-thumbnail-examples/platform-thumbnail-basic-example.component.ts
@@ -10,7 +10,7 @@ export class PlatformThumbnailBasicExampleComponent {
     data: Media[] = [{
         thumbnailUrl: 'http://picsum.photos/id/320/640/480',
         mediaType: 'image',
-        mediaUrl: 'http://picsum.photos/id/320/640/480/1',
+        mediaUrl: 'http://picsum.photos/id/320/640/480',
         alt: 'Failed to load http://picsum.photos/id/320/640/480',
         label: 'nature'
     },
@@ -25,7 +25,7 @@ export class PlatformThumbnailBasicExampleComponent {
         thumbnailUrl: 'http://picsum.photos/id/316/600',
         mediaType: 'image',
         mediaUrl: 'http://picsum.photos/id/316/600',
-        alt: 'Failed to loadhttp://picsum.photos/id/316/600',
+        alt: 'Failed to load http://picsum.photos/id/316/600',
         label: 'culture'
     },
     {
