b477e5e2c509f21c8fb518a936a2f4167f572d5e
Evgeny Beregovoy <evgeny.beregovoy@valor-software.com>
2020-10-22 18:28:48 +0300

    remove unused code

--- apps/docs/src/app/core/component-docs/feed-input/feed-input-header/feed-input-header.component.ts
+++ apps/docs/src/app/core/component-docs/feed-input/feed-input-header/feed-input-header.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fd-feed-input-header',
   templateUrl: './feed-input-header.component.html',
   styleUrls: ['./feed-input-header.component.scss']
 })
-export class FeedInputHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-}
+export class FeedInputHeaderComponent {}
