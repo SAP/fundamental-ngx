0d066cf8d412dcf1353184678c6c903565f18ce9
anton <anymac@TonyMac.local>
2020-11-02 14:09:19 +0200

    test(e2e): fix base url

--- protractor.conf.js
+++ protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
 },
   directConnect: true,
     // baseUrl: 'https://sap.github.io/fundamental-ngx/#',
-    baseUrl: 'http://localhost:4200/#/',
+    baseUrl: 'http://localhost:4200/#',
     // baseUrl: 'https://fundamental-ngx.netlify.app/#/',
   framework: 'jasmine',
   jasmineNodeOpts: {
