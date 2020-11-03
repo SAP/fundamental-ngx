51b9b3e9ae0030895e2029cdc9e49e734060f744
Evgeny Beregovoy <evgeny.beregovoy@valor-software.com>
2020-11-03 14:31:28 +0200

    remove unnecessary files

--- apps/docs/src/app/core/component-docs/feed-input/feed-input-header/feed-input-header.component.ts
+++ apps/docs/src/app/core/component-docs/feed-input/feed-input-header/feed-input-header.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'fd-feed-input-header',
-  templateUrl: './feed-input-header.component.html',
-  styleUrls: ['./feed-input-header.component.scss']
+  templateUrl: './feed-input-header.component.html'
 })
 export class FeedInputHeaderComponent {}
