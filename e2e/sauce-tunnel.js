fb01750d69e5694d68d2f7f87f2145dd918e9925
anton <anymac@TonyMac.local>
2020-11-04 14:11:08 +0200

    test(e2e): update sauce tunnel file with correct creds

--- e2e/sauce-tunnel.js
+++ e2e/sauce-tunnel.js
@@ -2,8 +2,8 @@ const sauceConnectLauncher = require('sauce-connect-launcher');
 
 
 sauceConnectLauncher({
-    username: 'tonyolkh6',
-    accessKey: 'a2a89026-78b9-4ce4-8438-346bedd934cf',
+    username: process.env.SAUCE_USERNAME,
+    accessKey: process.env.SAUCE_ACCESS_KEY,
     x: 'https://eu-central-1.saucelabs.com/rest/v1'
 
 }, function (err, sauceConnectProcess) {
