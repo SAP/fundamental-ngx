22dd5b234bfc7e2b87d8c69eee116eea76d7ac29
Sean Moore <sean.moore@valor-software.com>
2020-11-05 19:27:12 +0200

    test(e2e): add menu button component e2e tests

---
+++ e2e/platform/fixtures/appData/menu-button-contents.ts
@@ -0,0 +1,33 @@
+export default {
+
+    disabledState: 'ng-reflect-disabled',
+    cozyAndCompactBtnTextArr: [
+        'Default Button',
+        'Emphasized Button',
+        'Ghost Button',
+        'Positive Button',
+        'Negative Button',
+        'Attention Button',
+        'Transparent Button'
+    ],
+    cozyAndCompactBtnColorArr:  [
+        'rgba(8, 84, 160, 1)',
+        'rgba(255, 255, 255, 1)',
+        'rgba(8, 84, 160, 1)',
+        'rgba(16, 126, 62, 1)',
+        'rgba(187, 0, 0, 1)',
+        'rgba(233, 115, 12, 1)',
+        'rgba(8, 84, 160, 1)'
+    ],
+    textColorAttr: 'color',
+    textAlignmentAttr: 'align-items',
+    alignmentCenter: 'center',
+    selectedItem: 'Item Selected: First Item',
+    compactAttr: 'ng-reflect-compact',
+    iconAttr: 'ng-reflect-glyph',
+    icon: 'world',
+    truncatedBtnText: 'Default Button with long text',
+    truncatedBtnTooltipText: 'Default menu button with icon and long text',
+    truncatedBtnNoIconTooltipText: 'Default menu button with long text',
+    tooltipAttr: 'title'
+}
