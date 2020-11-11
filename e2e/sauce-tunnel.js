9f98d769885dec9a5f35f76061204f28ed66be38
anton <anymac@TonyMac.local>
2020-11-11 18:51:29 +0200

    test(e2e): update sc options with no ssl bump

--- e2e/sauce-tunnel.js
+++ e2e/sauce-tunnel.js
@@ -4,7 +4,10 @@ const sauceConnectLauncher = require('sauce-connect-launcher');
 sauceConnectLauncher({
     username: process.env.SAUCE_USERNAME,
     accessKey: process.env.SAUCE_ACCESS_KEY,
-    x: 'https://eu-central-1.saucelabs.com/rest/v1'
+    x: 'https://eu-central-1.saucelabs.com/rest/v1',
+    noSslBumpDomains: 'sap.dev',
+    // logfile: 'sauce_connect_e2e.log',
+    //doctor: true
 
 }, function (err, sauceConnectProcess) {
     if (err) {
