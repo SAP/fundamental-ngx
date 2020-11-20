b0ccdbf3b9d1b3e60875acbaa559abf77b004f25
Deepak Kumar <deepak.kumar14@sap.com>
2020-11-20 17:12:01 +0530

    removed patching styles from examples

--- apps/docs/src/app/core/component-docs/carousel/examples/carousel-loading-content-example.component.ts
+++ apps/docs/src/app/core/component-docs/carousel/examples/carousel-loading-content-example.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     selector: 'fd-carousel-loading-content-example',
     templateUrl: './carousel-loading-content-example.component.html',
-    styles: ['fd-carousel:focus {outline: 1px dotted}', 'img {pointer-events: none;}'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CarouselLoadingContentExampleComponent {
