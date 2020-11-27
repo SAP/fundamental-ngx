0feb00807f4cc19675b3933538b5861833d68bb8
Jedrzej <jedrzej.markowski@sap.com>
2020-11-27 10:53:34 +0100

    chore: Add hotfix CI pipeline, remove archive pipeline

---
+++ .ci-env/flags.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+latest=false
---
+++ scripts/hotfix-publish-release.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+NOCOLOR='\033[0m'
+ERROR='\033[31m'
+
+HOTFIX_TMP_BRANCH="hotfix_tmp_branch_for_automated_release_do_not_use"
+
+# make sure the current branch has non-rc tag
+rcVersion=$(git describe)
+[[ "$rcVersion" =~ "rc" ]] && echo -e "\n\t${ERROR}Sorry, this branch is a release candidate.${NOCOLOR}\n\tIn order to publish a hot-fix release, the package must be an non-rc version.\n\n" && exit 0
+
+# make sure the branch is clean
+git_status=$(git status --porcelain)
+[ -n "$git_status" ] && echo -e "\n\t${ERROR}Sorry, you have uncommitted changes.${NOCOLOR}\n\tIn order to publish a release, your working directory must be clean.\n\n" && exit 0
+
+
+git checkout -b $HOTFIX_TMP_BRANCH
+
+# Check if should be treated as a latest version
+[[ $1 == "latest" ]] && sed -i -e "s/\(latest=\).*/\1true/" .ci-env/flags.sh
+
+[[ $1 != "latest" ]] && sed -i -e "s/\(latest=\).*/\1false/" .ci-env/flags.sh
+
+set -o errexit
+git commit -m "chore(release): create new release via script" -n
+
+# push new branch to trigger travis build
+git push --set-upstream origin $HOTFIX_TMP_BRANCH
+
+# delete branch on local machine
+git checkout master
+git branch -D $HOTFIX_TMP_BRANCH
