e6ccbc258bbbc829e189914863ffc558f280bd88
mykolavorobiov <nikolay.vorobyev@valor-software.com>
2020-12-01 11:37:55 +0200

    fix review notes

--- libs/core/src/lib/feed-list-item/directives/byline.directive.ts
+++ libs/core/src/lib/feed-list-item/directives/byline.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: '[fdItemFooterByline]'
+  selector: '[fd-item-footer-byline]'
 })
 export class FeedListFooterBylineDirective {
   @HostBinding('class.fd-feed-list__footer--byline')
