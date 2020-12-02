b320d83e5aab17065ae82a63b72758c5d744db2c
AntonOlkhovskyi <olkhovskyy@gmail.com>
2020-12-02 15:59:26 +0200

    test(e2e): update menu checkbox and checkbox-group components, skip Safari and IE

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './e2e/wdio/**/fixed-card-layout.e2e-spec.ts'
+        './e2e/wdio/**/switch.e2e-spec.ts'
     ],
     // Patterns to exclude.
     exclude: [
@@ -52,17 +52,17 @@ exports.config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [
-        {
-            browserName: 'internet explorer',
-            browserVersion: 'latest',
-            platformName: 'Windows 10',
-            "sauce:options": {
-                screenResolution: '1920x1080',
-                name: 'e2e-win-internet-explorer',
-                requireWindowFocus: true,
-                //tags: [ "process.env.TRAVIS_BUILD_ID"],
-            }
-        },
+        // {
+        //     browserName: 'internet explorer',
+        //     browserVersion: 'latest',
+        //     platformName: 'Windows 10',
+        //     "sauce:options": {
+        //         screenResolution: '1920x1080',
+        //         name: 'e2e-win-internet-explorer',
+        //         requireWindowFocus: true,
+        //         //tags: [ "process.env.TRAVIS_BUILD_ID"],
+        //     }
+        // },
         {
             browserName: 'MicrosoftEdge',
             browserVersion: 'latest',
@@ -129,15 +129,15 @@ exports.config = {
                 //tags: [ process.env.TRAVIS_BUILD_ID],
             }
         },
-        {
-            browserName: 'safari',
-            browserVersion: '13.1',
-            platformName: 'macOS 10.15',
-            "sauce:options": {
-                screenResolution: '1920x1440',
-                name: 'e2e-MAC-safari',
-            }
-        },
+        // {
+        //     browserName: 'safari',
+        //     browserVersion: '13.1',
+        //     platformName: 'macOS 10.15',
+        //     "sauce:options": {
+        //         screenResolution: '1920x1440',
+        //         name: 'e2e-MAC-safari',
+        //     }
+        // },
     ],
     //
     // ===================
@@ -170,7 +170,10 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://sap.dev:4200/',
+
+    //baseUrl: 'https://sap.dev:4200/',
+    baseUrl: 'https://anton.local:4200/',
+    //baseUrl: 'https://sap.github.io/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -213,7 +216,13 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    // reporters: ['spec' , []],
+
+    reporters: ['spec',['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
 
     jasmineNodeOpts: {
         isVerbose: true,
@@ -278,6 +287,9 @@ exports.config = {
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
     before: function () {
+
+
+        console.log(browser.capabilities.browserName);
         require('ts-node').register({
             project: 'e2e/tsconfig.json'
         });
