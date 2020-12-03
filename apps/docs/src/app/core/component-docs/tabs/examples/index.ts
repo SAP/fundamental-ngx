3ac980370e6da6b1dbc186e4d068758f50afa79a
Michal Salamon <msalamon@divante.com>
2020-12-03 13:09:15 +0100

    fix(core): Add exmaples

---
+++ apps/docs/src/app/core/component-docs/tabs/examples/index.ts
@@ -0,0 +1,46 @@
+import { AddingTabExampleComponent } from './adding-tab-example/adding-tab-example.component';
+import { ComplexTitleExampleComponent } from './complex-title-example/complex-title-example.component';
+import { TabCollapsibleExampleComponent } from './tab-collapsible-example/tab-collapsible-example.component';
+import { TabCounterComponent } from './tab-counter/tab-counter.component';
+import { TabFilterExampleComponent } from './tab-filter-example/tab-filter-example.component';
+import { TabIconOnlyExampleComponent } from './tab-icon-only-example/tab-icon-only-example.component';
+import {
+    TabNavigationExampleChildFirst,
+    TabNavigationExampleChildSecond,
+    TabNavigationExampleChildThird
+} from './tab-navigation-children/tab-navigation-children';
+import { TabProcessExampleComponent } from './tab-process-example/tab-process-example.component';
+import { TabStackedContentExampleComponent } from './tab-stacked-content-example/tab-stacked-content-example.component';
+import { TabsNavigationModeExampleComponent } from './tab-navigation-mode-example-component';
+import { TabSelectionExampleComponent } from './tab-selection-example.component';
+import { TabsExampleComponent } from './tabs-examples-component';
+
+export * from './tab-stacked-content-example/tab-stacked-content-example.component';
+export * from './tab-collapsible-example/tab-collapsible-example.component';
+export * from './complex-title-example/complex-title-example.component';
+export * from './tab-icon-only-example/tab-icon-only-example.component';
+export * from './tab-process-example/tab-process-example.component';
+export * from './adding-tab-example/adding-tab-example.component';
+export * from './tab-filter-example/tab-filter-example.component';
+export * from './tab-navigation-children/tab-navigation-children';
+export * from './tab-navigation-mode-example-component';
+export * from './tab-counter/tab-counter.component';
+export * from './tab-selection-example.component';
+export * from './tabs-examples-component';
+
+export const examples = [
+    TabCounterComponent,
+    TabsExampleComponent,
+    TabFilterExampleComponent,
+    AddingTabExampleComponent,
+    TabProcessExampleComponent,
+    TabIconOnlyExampleComponent,
+    TabSelectionExampleComponent,
+    ComplexTitleExampleComponent,
+    TabCollapsibleExampleComponent,
+    TabNavigationExampleChildThird,
+    TabNavigationExampleChildFirst,
+    TabNavigationExampleChildSecond,
+    TabStackedContentExampleComponent,
+    TabsNavigationModeExampleComponent
+];
