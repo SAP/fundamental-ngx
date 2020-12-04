8e669a87a9e594028b5206fa68189f642fbc6ce3
Deepak Kumar <deepak.kumar14@sap.com>
2020-12-04 16:02:43 +0530

    revreting test focus

--- libs/core/src/lib/flexible-column-layout/flexible-column-layout.component.spec.ts
+++ libs/core/src/lib/flexible-column-layout/flexible-column-layout.component.spec.ts
@@ -155,7 +155,7 @@ describe('FlexibleColumnLayoutComponent', () => {
         expect(separators.length).toBe(1);
     });
 
-    it('TWO_COLUMNS_END_EXPANDED should render 2 columns, mid open, end expanded', async () => {
+    fit('TWO_COLUMNS_END_EXPANDED should render 2 columns, mid open, end expanded', async () => {
         whenStable(fixture);
         viewport.set(1023, 900);
 
