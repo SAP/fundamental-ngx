8b7f60476993a0eced1795c876f4470dbe867627
Dmitriy Tkachenko <dmitriy.tkachenko@valor-software.com>
2020-12-04 17:58:57 +0200

    remove extra pipe option

--- libs/platform/src/lib/components/table/pipes/value-by-path.pipe.ts
+++ libs/platform/src/lib/components/table/pipes/value-by-path.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import { getNestedValue } from '../../../utils/object';
 
-@Pipe({ name: 'valueByPath', pure: true })
+@Pipe({ name: 'valueByPath' })
 export class ValueByPathPipe implements PipeTransform {
     transform(value: any, key: string): any {
         return getNestedValue(key, value);
