da43d16071dc0d7048559af77762f9def8baf6f0
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-12-07 15:41:13 -0700

    Revert "start on fullscreen thing"
    This reverts commit 3141be0a5d9655b024803e76bcd01a4321e859ef.

--- apps/docs/src/app/core/component-docs/wizard/wizard-docs.module.ts
+++ apps/docs/src/app/core/component-docs/wizard/wizard-docs.module.ts
@@ -19,7 +19,6 @@ import { SharedDocumentationPageModule } from '../../../documentation/shared-doc
 import { WizardCustomizableExampleComponent } from './examples/wizard-customizable-example.component';
 import { WizardMobileExampleComponent } from './examples/wizard-mobile-example.component';
 import { WizardBranchingExampleComponent } from './examples/wizard-branching-example.component';
-import { CdkScrollableModule } from '@angular/cdk/scrolling';
 
 const routes: Routes = [
     {
@@ -43,8 +42,7 @@ const routes: Routes = [
         FormGroupModule,
         FormModule,
         TitleModule,
-        LayoutGridModule,
-        CdkScrollableModule
+        LayoutGridModule
     ],
     exports: [RouterModule],
     declarations: [
