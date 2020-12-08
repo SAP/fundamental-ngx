fcc08072a88f384bfd79ddf39862f49575d8b8bd
Ilya Surmay <ilya.surmay@valor-software.com>
2020-12-08 16:16:15 +0200

    fix members search

--- libs/platform/src/lib/components/approval-flow/approval-flow-user-details/approval-flow-user-details.component.ts
+++ libs/platform/src/lib/components/approval-flow/approval-flow-user-details/approval-flow-user-details.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, Input, OnInit } from '@angular/core';
 import { DIALOG_REF, DialogRef } from '@fundamental-ngx/core';
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { ApprovalDataSource, ApprovalNode, User } from '../public_api';
-import { DataProvider, ListDataSource } from '../../../domain';
 
 @Component({
     selector: 'fdp-approval-flow-user-details',
@@ -17,16 +16,25 @@ export class ApprovalFlowUserDetailsComponent implements OnInit {
 
     @Input() approvalFlowDataSource: ApprovalDataSource;
 
+    /** @hidden */
     _isListMode = false;
 
-    _dataSource: ListDataSource<User>;
+    /** @hidden */
+    _listItems: User[] = [];
+
+    /** @hidden */
     _selectedItems: any[] = [];
+
+    /** @hidden */
     _userToShowDetails: User;
+
+    /** @hidden */
     _userToShowDetailsData$: Observable<any>;
 
     constructor(@Inject(DIALOG_REF) public dialogRef: DialogRef, private _cdr: ChangeDetectorRef) {
     }
 
+    /** @hidden */
     ngOnInit(): void {
         this._isListMode = this.dialogRef.data.node?.approvers.length > 1;
         if (this._isListMode) {
@@ -36,35 +44,42 @@ export class ApprovalFlowUserDetailsComponent implements OnInit {
         }
     }
 
-    setListItems(users: User[]): void {
-        this._dataSource = new ListDataSource<User>(new ListDataProvider(users));
+    /** @hidden */
+    setListItems(items: User[]): void {
+        this._listItems = [...items];
         this._cdr.detectChanges();
     }
 
+    /** @hidden */
     onUserClick(user: User): void {
         this.setUserToShowDetails(user);
     }
 
+    /** @hidden */
     backToListFromDetails(): void {
         this._userToShowDetails = undefined;
     }
 
+    /** @hidden */
     setUserToShowDetails(user: User): void {
         this._userToShowDetails = user;
         this._userToShowDetailsData$ = this.dialogRef.data.approvalFlowDataSource.fetchUser(user.id);
     }
 
+    /** @hidden */
     sendReminder(): void {
         const reminderTargets = this._isListMode ? this.getUsersFromSelectedItems() : this.dialogRef.data.node.approvers;
         this.dialogRef.close(reminderTargets);
     }
 
+    /** @hidden */
     getUsersFromSelectedItems(): User[] {
         return this._selectedItems.map(item => {
-            return this.dialogRef.data.node.approvers.find(user => user.imgUrl === item.avatarSrc);
+            return this.dialogRef.data.node.approvers.find(user => user.imgUrl === item.avatarSrc && user.name === item.title);
         });
     }
 
+    /** @hidden */
     onSearchStringChange(searchString: string): void {
         this._selectedItems = [];
         if (!searchString) {
@@ -72,22 +87,8 @@ export class ApprovalFlowUserDetailsComponent implements OnInit {
             return;
         }
 
-        const result = this.dialogRef.data.node.approvers.filter(
+        this.setListItems(this.dialogRef.data.node.approvers.filter(
             user => user.name.toLowerCase().indexOf(searchString.toLowerCase()) > -1
-        );
-        this.setListItems(result);
-    }
-}
-
-export class ListDataProvider extends DataProvider<User> {
-    data: User[];
-
-    constructor(data: User[]) {
-        super();
-        this.data = data;
-    }
-
-    fetch(params: Map<string, string>): Observable<User[]> {
-        return of(this.data);
+        ));
     }
 }
