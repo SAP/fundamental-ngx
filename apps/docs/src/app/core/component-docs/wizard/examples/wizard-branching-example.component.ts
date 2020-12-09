7649325b9d4b2f813153c70b7c3cc50b820989a8
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-12-09 15:28:03 -0700

    wizard branching dialog

--- apps/docs/src/app/core/component-docs/wizard/examples/wizard-branching-example.component.ts
+++ apps/docs/src/app/core/component-docs/wizard/examples/wizard-branching-example.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { WizardStepStatus } from '@fundamental-ngx/core';
+import { Component, OnInit, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { DialogService, WizardStepStatus } from '@fundamental-ngx/core';
 
 @Component({
     selector: 'fd-wizard-branching-example',
@@ -9,13 +9,41 @@ import { WizardStepStatus } from '@fundamental-ngx/core';
         class: 'fd-wizard-example'
     }
 })
-export class WizardBranchingExampleComponent {
+export class WizardBranchingExampleComponent implements OnInit {
     step1status: WizardStepStatus = 'current';
     step2status: WizardStepStatus = 'upcoming';
     step3status: WizardStepStatus = 'upcoming';
 
-    @Input()
-    paymentSelection: any;
+    paymentSelection = '';
+
+    oldPayment = '';
+
+    init = true;
+
+    constructor(private _dialogService: DialogService) {}
+
+    ngOnInit(): void {
+        this.oldPayment = this.paymentSelection;
+    }
+
+    paymentSelectionChanged(dialog: TemplateRef<any>): void {
+        if (this.oldPayment !== this.paymentSelection) {
+            if (!this.init) {
+                const dialogRef = this._dialogService.open(dialog, { responsivePadding: true });
+
+                dialogRef.afterClosed.subscribe(
+                    () => {
+                        this.oldPayment = this.paymentSelection;
+                    },
+                    () => {
+                        this.paymentSelection = this.oldPayment;
+                    }
+                );
+            } else {
+                this.init = false;
+            }
+        }
+    }
 
     goToStep(step: number): void {
         switch (step) {
