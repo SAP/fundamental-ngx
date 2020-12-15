ac7307216a5ddab47788a5b3c59e9bd903ee7495
olshansky <olshansky0@gmail.com>
2020-12-15 16:02:17 +0200

    fix: demos and HostBinding

--- apps/docs/src/app/core/component-docs/quick-view/quick-view-docs.component.ts
+++ apps/docs/src/app/core/component-docs/quick-view/quick-view-docs.component.ts
@@ -55,38 +55,4 @@ export class QuickViewDocsComponent {
             component: 'QuickViewDialogExampleComponent'
         }
     ];
-
-    data = DATA;
 }
-
-export const DATA = {
-    id: 'employee-1',
-    title: 'Employee',
-    subHeader: {
-        title: 'Michael Adams',
-        subtitle: 'Account Manager',
-        avatar: 'http://placeimg.com/500/500/people'
-    },
-    groups: [{
-        title: 'Contact Details',
-        items: [{
-            label: 'Mobile',
-            value: '+1 605 555 5555'
-        }, {
-            label: 'Phone',
-            value: '+1 316 555 5555'
-        }, {
-            label: 'Email',
-            value: 'michael_adams@example.com'
-        }]
-    }, {
-        title: 'Company',
-        items: [{
-            label: 'Name',
-            value: 'Company A'
-        }, {
-            label: 'Address',
-            value: '718 Main Street, Anytown, SD 57401, USA'
-        }]
-    }]
-};
