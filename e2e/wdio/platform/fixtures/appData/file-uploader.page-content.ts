53c68e5d6f45f5020a0937ef46ce4ba523f0b340
nikvalor <nikita.lastovych@valor-software.com>
2020-12-15 09:50:43 +0200

    Refactor

--- e2e/wdio/platform/fixtures/appData/file-uploader.page-content.ts
+++ e2e/wdio/platform/fixtures/appData/file-uploader.page-content.ts
@@ -13,4 +13,13 @@ export const titleValue = [
     'file valid count1'
 ];
 
+export const titleValue = [
+    'file valid count0',
+    'file uploaded1',
+    'file uploaded0',
+    'file invalid uploaded1',
+    'file invalid uploaded0',
+    'file valid count1',
+];
+
 export const imagePath = 'e2e/wdio/platform/tests/screen.png';
