631836350b1183ee64dbfb3aec3a29fa4578122f
nikvalor <nikita.lastovych@valor-software.com>
2020-12-15 09:50:43 +0200

    Refactor

--- e2e/wdio/platform/fixtures/appData/file-uploader.page-content.ts
+++ e2e/wdio/platform/fixtures/appData/file-uploader.page-content.ts
@@ -4,4 +4,13 @@ export const placeholderValue = [
         'Choose file to upload'
 ];
 
+export const titleValue = [
+    'file valid count0',
+    'file uploaded1',
+    'file uploaded0',
+    'file invalid uploaded1',
+    'file invalid uploaded0',
+    'file valid count1',
+];
+
 export const imagePath = 'e2e/wdio/platform/tests/screen.png';
--- e2e/wdio/platform/tests/file-uploader.e2e-spec.ts
+++ e2e/wdio/platform/tests/file-uploader.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { webDriver } from '../../driver/wdio';
 import { FileUploaderPo } from '../pages/file-uploader.po';
-import {placeholderValue, imagePath} from '../fixtures/appData/file-uploader.page-content';
+import { placeholderValue, titleValue, imagePath } from '../fixtures/appData/file-uploader.page-content';
 import { BaseComponentPo } from '../pages/base-component.po';
 
 
-describe('File uploader test suite', function () {
+describe('File uploader test suite', function() {
     const fileUploader = new FileUploaderPo();
-    const BaseComponent = new BaseComponentPo();
+    const baseComponent = new BaseComponentPo();
 
     beforeAll(() => {
         fileUploader.open();
@@ -29,27 +29,27 @@ describe('File uploader test suite', function () {
         for (let i = 0; i < arrLength; i++) {
             webDriver.click(fileUploader.browseButton, i);
         }
-    })
+    });
 
     it('Verify file upload', () => {
-        const arrLength = webDriver.getElementArrayLength(fileUploader.browseButton) - 1;
+        const arrLength = webDriver.getElementArrayLength(fileUploader.browseButton);
         for (let i = 0; i < arrLength; i++) {
-            webDriver.uploadFile(fileUploader.fileUploaderInputFile, imagePath, i);
-            expect(imagePath).toContain(webDriver.getText(fileUploader.fileSelectedText, i))
-            expect(imagePath).toContain(webDriver.getAttributeByName(fileUploader.fileUploaderInput, 'title', i).slice(1))
+            webDriver.uploadFile(fileUploader.fileUploaderInputFile, imagePath);
+            expect(imagePath).toContain(webDriver.getText(fileUploader.fileSelectedText, i));
+            expect(imagePath).toContain(webDriver.getAttributeByName(fileUploader.fileUploaderInput, 'title', i).slice(1));
         }
-    })
+    });
 
     it('Verify file uploaded message', () => {
-        const arrLength = webDriver.getElementArrayLength(fileUploader.browseButton) - 1;
+        const arrLength = webDriver.getElementArrayLength(fileUploader.browseButton);
         for (let i = 0; i < arrLength; i++) {
             webDriver.uploadFile(fileUploader.fileUploaderInputFile, imagePath, i);
-            expect(webDriver.getAlertText()).toContain('file uploaded');
+            expect(titleValue).toContain(webDriver.getAlertText());
         }
-    })
+    });
 
     it('should check LTR and RTL orientation', () => {
-        BaseComponent.checkRtlSwitch();
-    })
+        baseComponent.checkRtlSwitch();
+    });
 
 });
