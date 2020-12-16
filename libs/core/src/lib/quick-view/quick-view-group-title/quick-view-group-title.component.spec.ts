3d0bc585998370d7f986e041f9a8109fa5eca51a
olshansky <olshansky0@gmail.com>
2020-12-16 12:36:48 +0200

    fix: add tests

--- libs/core/src/lib/quick-view/quick-view-group-title/quick-view-group-title.component.spec.ts
+++ libs/core/src/lib/quick-view/quick-view-group-title/quick-view-group-title.component.spec.ts
@@ -1,24 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
+import { QuickViewModule } from '../quick-view.module';
 import { QuickViewGroupTitleComponent } from './quick-view-group-title.component';
 
+@Component({
+    template: `
+        <fd-quick-view-group-title>
+            {{ title }}
+        </fd-quick-view-group-title>
+    `
+})
+class TestComponent {
+    title = 'Contact Details';
+}
+
 describe('QuickViewGroupTitleComponent', () => {
-    let component: QuickViewGroupTitleComponent;
-    let fixture: ComponentFixture<QuickViewGroupTitleComponent>;
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [QuickViewGroupTitleComponent]
+            declarations: [TestComponent],
+            imports: [QuickViewModule]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(QuickViewGroupTitleComponent);
-        component = fixture.componentInstance;
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.debugElement.componentInstance;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should assign classes to h5 tag', () => {
+        const h5 = fixture.debugElement.query(By.css('h5[fd-title]'));
+
+        expect(h5.nativeElement.classList).toContain('fd-title');
+        expect(h5.nativeElement.classList).toContain('fd-title--h5');
     });
 });
--- libs/core/src/lib/quick-view/quick-view-subheader-title/quick-view-subheader-title.component.spec.ts
+++ libs/core/src/lib/quick-view/quick-view-subheader-title/quick-view-subheader-title.component.spec.ts
@@ -1,24 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
+import { QuickViewModule } from '../quick-view.module';
 import { QuickViewSubheaderTitleComponent } from './quick-view-subheader-title.component';
 
+@Component({
+    template: `
+        <fd-quick-view-subheader-title>
+            {{ title }}
+        </fd-quick-view-subheader-title>
+    `
+})
+class TestComponent {
+    title = 'Michael Adams';
+}
+
 describe('QuickViewSubheaderTitleComponent', () => {
-    let component: QuickViewSubheaderTitleComponent;
-    let fixture: ComponentFixture<QuickViewSubheaderTitleComponent>;
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [QuickViewSubheaderTitleComponent]
+            declarations: [TestComponent],
+            imports: [QuickViewModule]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(QuickViewSubheaderTitleComponent);
-        component = fixture.componentInstance;
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.debugElement.componentInstance;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should assign classes to h5 tag', () => {
+        const h5 = fixture.debugElement.query(By.css('h5[fd-title]'));
+
+        expect(h5.nativeElement.classList).toContain('fd-title');
+        expect(h5.nativeElement.classList).toContain('fd-title--h5');
     });
 });
