0571a05f1e4832cd6a6de7cd0a98f4952092c185
sKudum <53487468+sKudum@users.noreply.github.com>
2020-12-21 20:26:16 +0530

    fix: (Platform) Add examples in list with long text for accessibility test (#4131)
    * added long title for list
    
    * aria labels for unread data
    
    add Lorem ipsum text

--- apps/docs/src/app/platform/component-docs/platform-standard-list-item/platform-standard-list-item-examples/platform-standard-list-item-border-less-example.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-standard-list-item/platform-standard-list-item-examples/platform-standard-list-item-border-less-example.component.ts
@@ -6,8 +6,18 @@ import { Component } from '@angular/core';
 export class PlatformStandardListItemBorderLessExampleComponent {
     items: any[] = [
         {
-            'title': 'Item1',
-            'avatarTitle': 'nature', 'description': 'First text item in Byline (Standard text item)', 'secondary': 'Second text item in Byline (Can be semantic (Status) or not)', 'avatarSrc': 'https://placeimg.com/400/400/nature'
+            'title': 'Item1 A laptop computer, sometimes called a notebook computer by manufacturers,' +
+             'is a battery- or AC-powered personal computer generally smaller than a briefcase that can ' +
+             'easily be transported and conveniently' ,
+            'avatarTitle': 'nature', 'description': 'First text item in Byline (Standard text item)' +
+            'A laptop computer, sometimes called a notebook computer by manufacturers,' +
+             'is a battery- or AC-powered personal computer generally smaller than a briefcase that can ' +
+             'easily be transported and conveniently',
+             'secondary': 'Second text item in Byline (Can be semantic (Status) or not)' +
+             'A laptop computer, sometimes called a notebook computer by manufacturers,' +
+              'is a battery- or AC-powered personal computer generally smaller than a briefcase that can ' +
+              'easily be transported and conveniently',
+              'avatarSrc': 'https://placeimg.com/400/400/nature'
         },
         {
             'title': 'Item2', 'avatarSrc': 'https://placeimg.com/400/400/nature',
