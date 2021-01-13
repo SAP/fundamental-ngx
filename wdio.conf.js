d71540c493ddd3f4a296bdf02bfcec583b59695a
AntonOlkhovskyi <olkhovskyy@gmail.com>
2021-01-13 22:41:43 +0200

    test(e2e): add retry for failed spec

--- wdio.conf.js
+++ wdio.conf.js
@@ -199,13 +199,13 @@ exports.config = {
     framework: 'jasmine',
     //
     // The number of times to retry the entire specfile when it fails as a whole
-    // specFileRetries: 1,
+    specFileRetries: 2,
     //
     // Delay in seconds between the spec file retry attempts
     // specFileRetriesDelay: 0,
     //
     // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-    // specFileRetriesDeferred: false,
+     specFileRetriesDeferred: true,
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
