8dbee737daab583d87521ae72ada0ce8fb2d4c90
mykolavorobiov <38053194+mykolavorobiov@users.noreply.github.com>
2021-01-14 16:15:13 +0200

    docs: (Core) add rating indicator example with display mode (#4329)
    * feat: (Core) add rating indicator example with display mode
    
    * fix PR notes, remove unnecessary styles, fix stackblitz examples

--- apps/docs/src/app/core/component-docs/rating-indicator/examples/rating-indicator-sizes.component.ts
+++ apps/docs/src/app/core/component-docs/rating-indicator/examples/rating-indicator-sizes.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-rating-indicator-sizes',
-    templateUrl: './rating-indicator-sizes.component.html',
-    styleUrls: ['./rating-indicator-style.component.scss']
+    selector: 'fd-rating-indicator-sizes',
+    templateUrl: './rating-indicator-sizes.component.html'
 })
 export class RatingIndicatorSizesExampleComponent {
     sizes = ['xs', 'sm', 'md', 'lg', 'cozy', 'compact', 'condensed'];
