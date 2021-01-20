b860504d886c3f116b8acecd5e300e8a11dab836
manu-kr <53512176+manu-kr@users.noreply.github.com>
2021-01-20 22:44:29 +0530

    fix: (Platform) Thumbnail accessibility bug fixes. (#4385)
    * fix(platform):Thumbnail accessbility bug fixes.
    
    * fix: (Platform): Thumbnail accessibility bug fix changes

--- apps/docs/src/app/platform/component-docs/platform-thumbnail/platform-thumbnail-examples/platform-thumbnail-video-media-example.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-thumbnail/platform-thumbnail-examples/platform-thumbnail-video-media-example.component.ts
@@ -11,36 +11,51 @@ export class PlatformThumbnailVideoMediaExampleComponent {
         thumbnailUrl: 'http://picsum.photos/id/1008/400',
         mediaType: 'video',
         mediaUrl: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+        captionFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
+        audioDescFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
         alt: 'Failed to load http://picsum.photos/id/1008/400',
-        label: 'nature'
+        label: 'nature',
+
     },
     {
         thumbnailUrl: 'http://picsum.photos/id/1004/400',
         mediaType: 'video',
         mediaUrl: 'https://cdn.bitdegree.org/learn/Pexels%20Videos%203373.mp4?raw=true',
+        captionFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
+        audioDescFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
         alt: 'Failed to load http://picsum.photos/id/1004/400',
         label: 'animal'
+
     },
     {
         thumbnailUrl: 'http://picsum.photos/id/1001/400',
         mediaType: 'video',
         mediaUrl: 'https://cdn.bitdegree.org/learn/Pexels%20Videos%203373.mp4?raw=true',
+        captionFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
+        audioDescFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
         alt: 'Failed to load http://picsum.photos/id/1001/400',
-        label: 'culture'
+        label: 'culture',
+
     },
     {
         thumbnailUrl: 'http://picsum.photos/id/1002/400',
         mediaType: 'video',
         mediaUrl: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+        captionFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
+        audioDescFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
         alt: 'Failed to load http://picsum.photos/id/1002/400',
-        label: 'dance'
+        label: 'dance',
+        selected: false
     },
     {
         thumbnailUrl: 'http://picsum.photos/id/1019/400',
         mediaType: 'video',
         mediaUrl: 'https://cdn.bitdegree.org/learn/Pexels%20Videos%203373.mp4?raw=true',
+        captionFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
+        audioDescFile: 'https://gist.github.com/samdutton/ca37f3adaf4e23679957b8083e061177',
         alt: 'Failed to load http://picsum.photos/id/1019/400',
-        label: 'nature'
+        label: 'nature',
+        selected: false
     }
     ];
 
