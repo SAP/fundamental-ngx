064955eb5cfab0e0b1407c0258242b1793c1d050
Evgeny Beregovoy <evgeny.beregovoy@valor-software.com>
2021-01-21 10:39:00 +0200

    feat: (Platform) Form Group enhancement phase 1 (#4213)
    * feat: (platform) form grid styles for Platform Forms
    
    * address review comments
    
    * address review comments
    
    * easy layout
    
    * 4 column view
    
    * add column property aligment
    
    * feat(platform): add field-group component
    
    * feat(platform): form group enhancement phase 1
    
    * feat(platform): rewrite column template
    
    * feat(platform): fix field group in others components
    
    * feat(platform): fix circular dependency
    
    * feat(platform): add field group padding
    
    * feat(platform): update docs and fix PR comments
    
    * fix(platform): fix adressed comments
    
    * feat(platform): fix review comments
    
    * feat(platform): change default column layout value
    
    * feat(platform): add form helpers && address comments
    
    * test(e2e): update e2e test locators
    
    * feat(platform): fix circular dependency
    
    * feat(platform): enchance fields render
    
    * feat(platform): fix travis build
    
    * feat(platform): remove multilayout property clean up leftover properties in components
    
    * feat(platform): remove unused styles
    
    * Fixed failed scenarios. Small refactoring
    
    Co-authored-by: Kavya <kavya.bhat@sap.com>
    Co-authored-by: Sean Moore <sean.moore@valor-software.com>
    Co-authored-by: nikvalor <nikita.lastovych@valor-software.com>

---
+++ apps/docs/src/app/platform/component-docs/platform-forms/platform-form-container/platform-form-container-docs.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ApiComponent } from '../../../../documentation/core-helpers/api/api.component';
+import { API_FILES } from '../../../api-files';
+import { SharedDocumentationPageModule } from '../../../../documentation/shared-documentation-page.module';
+
+import {
+    PlatformTextAreaModule,
+    FdpFormGroupModule,
+    PlatformButtonModule,
+    PlatformRadioGroupModule,
+    PlatformInputModule,
+    PlatformCheckboxGroupModule,
+    PlatformStepInputModule,
+    PlatformInputGroupModule,
+    PlatformSwitchModule
+} from '@fundamental-ngx/platform';
+import { PlatformFormContainerDocsComponent } from './platform-form-container-docs.component';
+import { PlatformFormContainerRecommendedExampleComponent } from './platform-form-container-examples/platform-form-container-recommended-example.component';
+import { PlatformFormContainerPossibleExampleComponent } from './platform-form-container-examples/platform-form-container-possible-example.component';
+import { PlatformFormContainerNotRecommendedExampleComponent } from './platform-form-container-examples/platform-form-container-not-recommended-example.component';
+import { PlatformFormContainerComplexExampleComponent } from './platform-form-container-examples/platform-form-container-complex-example.component';
+import { PlatformFormContainerHeaderComponent } from './platform-form-container-header/platform-form-container-header.component';
+import { PlatformFormBasicExampleComponent } from './platform-form-container-examples/platform-form-basic/platform-form-basic-example.component';
+import { PlatformFormGroupExampleComponent } from './platform-form-container-examples/platform-form-group/platform-form-group-example.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PlatformFormContainerHeaderComponent,
+        children: [
+            { path: '', component: PlatformFormContainerDocsComponent },
+            { path: 'api', component: ApiComponent, data: { content: API_FILES.formContainer } }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes),
+        SharedDocumentationPageModule,
+        PlatformTextAreaModule,
+        PlatformButtonModule,
+        PlatformRadioGroupModule,
+        PlatformInputModule,
+        PlatformCheckboxGroupModule,
+        PlatformStepInputModule,
+        PlatformInputGroupModule,
+        PlatformSwitchModule,
+        FdpFormGroupModule
+    ],
+    exports: [RouterModule],
+    declarations: [
+        PlatformFormContainerDocsComponent,
+        PlatformFormContainerHeaderComponent,
+        PlatformFormContainerRecommendedExampleComponent,
+        PlatformFormContainerPossibleExampleComponent,
+        PlatformFormContainerNotRecommendedExampleComponent,
+        PlatformFormContainerComplexExampleComponent,
+
+        PlatformFormBasicExampleComponent,
+        PlatformFormGroupExampleComponent
+    ]
+})
+export class PlatformFormContainerDocsModule {}
