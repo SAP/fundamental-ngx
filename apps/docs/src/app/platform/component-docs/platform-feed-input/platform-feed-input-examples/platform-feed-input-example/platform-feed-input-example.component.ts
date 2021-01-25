8a800124828d0dcaf3801ac4d1fb8149add22042
Yury Betrozov <betrozov91@gmail.com>
2021-01-25 15:57:34 +0200

    fix: (platform) fix Feed input and File uploader examples for stackblitz

--- apps/docs/src/app/platform/component-docs/platform-feed-input/platform-feed-input-examples/platform-feed-input-example/platform-feed-input-example.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-feed-input/platform-feed-input-examples/platform-feed-input-example/platform-feed-input-example.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'fdp-feed-input-example',
+    selector: 'fdp-platform-feed-input-example',
     templateUrl: './platform-feed-input-example.component.html'
 })
 export class PlatformFeedInputExampleComponent {
