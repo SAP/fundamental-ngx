f150c1dae6b0b003beda9af5d057d27ba81393a2
Yury Betrozov <betrozov91@gmail.com>
2021-01-29 13:19:42 +0200

    fix: (platform) fix after review

--- apps/docs/src/app/platform/component-docs/platform-thumbnail/platform-thumbnail-examples/platform-thumbnail-basic-example.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-thumbnail/platform-thumbnail-examples/platform-thumbnail-basic-example.component.ts
@@ -8,38 +8,38 @@ import { Media } from '@fundamental-ngx/platform';
 export class PlatformThumbnailBasicExampleComponent {
 
     data: Media[] = [{
-        thumbnailUrl: 'http://picsum.photos/id/320/640/480',
+        thumbnailUrl: '//picsum.photos/id/320/640/480',
         mediaType: 'image',
-        mediaUrl: 'http://picsum.photos/id/320/640/480',
-        alt: 'Failed to load http://picsum.photos/id/320/640/480',
+        mediaUrl: '//picsum.photos/id/320/640/480',
+        alt: 'Failed to load //picsum.photos/id/320/640/480',
         label: 'nature'
     },
     {
-        thumbnailUrl: 'http://picsum.photos/id/315/640/480',
+        thumbnailUrl: '//picsum.photos/id/315/640/480',
         mediaType: 'image',
-        mediaUrl: 'http://picsum.photos/id/315/640/480',
-        alt: 'Failed to load http://picsum.photos/id/315/640/480',
+        mediaUrl: '//picsum.photos/id/315/640/480',
+        alt: 'Failed to load //picsum.photos/id/315/640/480',
         label: 'animal'
     },
     {
-        thumbnailUrl: 'http://picsum.photos/id/316/600',
+        thumbnailUrl: '//picsum.photos/id/316/600',
         mediaType: 'image',
-        mediaUrl: 'http://picsum.photos/id/316/600',
-        alt: 'Failed to load http://picsum.photos/id/316/600',
+        mediaUrl: '//picsum.photos/id/316/600',
+        alt: 'Failed to load //picsum.photos/id/316/600',
         label: 'culture'
     },
     {
-        thumbnailUrl: 'http://picsum.photos/id/316/600',
+        thumbnailUrl: '//picsum.photos/id/316/600',
         mediaType: 'image',
-        mediaUrl: 'http://picsum.photos/id/316/600',
-        alt: 'Failed to load http://picsum.photos/id/316/600',
+        mediaUrl: '//picsum.photos/id/316/600',
+        alt: 'Failed to load //picsum.photos/id/316/600',
         label: 'sports'
     },
     {
-        thumbnailUrl: 'http://picsum.photos/id/317/600',
+        thumbnailUrl: '//picsum.photos/id/317/600',
         mediaType: 'image',
-        mediaUrl: 'http://picsum.photos/id/317/600',
-        alt: 'Failed to load http://picsum.photos/id/317/600',
+        mediaUrl: '//picsum.photos/id/317/600',
+        alt: 'Failed to load //picsum.photos/id/317/600',
         label: 'art'
     }
     ];
