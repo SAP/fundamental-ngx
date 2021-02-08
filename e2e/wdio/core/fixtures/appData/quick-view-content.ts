e74c77b4bcd6405ae7d802fe93eb1c43cd67c763
nikvalor <nikita.lastovych@valor-software.com>
2021-02-08 13:29:36 +0200

    Covered quick view component

---
+++ e2e/wdio/core/fixtures/appData/quick-view-content.ts
@@ -0,0 +1,9 @@
+const mobile = '+1 605 555 5555';
+const phone = '+1 316 555 5555';
+const email = 'michael_adams@example.com';
+const address = '718 Main Street, Anytown, SD 57401, USA'
+const companyName = 'Company A'
+const popoverHeaderValue = 'Employee';
+export {
+    mobile, phone, email, address, companyName, popoverHeaderValue
+};
