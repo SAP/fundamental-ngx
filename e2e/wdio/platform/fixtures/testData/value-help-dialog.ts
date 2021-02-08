deb96f34ad9a685886b5a2d0ce82b8666f8abe29
Sean Moore <sean.moore@valor-software.com>
2021-02-08 20:05:32 +0200

    test(e2e): add value help dialog e2e tests

---
+++ e2e/wdio/platform/fixtures/testData/value-help-dialog.ts
@@ -0,0 +1,13 @@
+const searchValues = [
+    'name 1',
+    '31127',
+    'city 0',
+    'zipcode 0',
+    'address 0',
+    'nickname 0'
+];
+
+const valueZero = '0';
+const valueOne = '1';
+
+export {searchValues, valueZero, valueOne};
