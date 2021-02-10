5d41b00c381c66fc257ae38874a27727ea5032ce
Sean Moore <sean.moore@valor-software.com>
2021-02-10 19:47:30 +0200

    test: add dynamic side content visual tests

---
+++ e2e/wdio/core/tests/dynamic-side-content.e2e-spec.ts
@@ -0,0 +1,22 @@
+import { DynamicSideContentPo } from '../pages/dynamic-side-content.po';
+
+describe('dynamic side content test suite', function() {
+    const dynamicSideContentPage = new DynamicSideContentPo();
+
+    beforeAll(() => {
+        dynamicSideContentPage.open();
+    }, 1);
+
+    describe('check orientation', function() {
+        it('should check RTL and LTR orientation', () => {
+            dynamicSideContentPage.checkRtlSwitch();
+        });
+    });
+
+    describe('Check visual regression', function() {
+        it('should check examples visual regression', () => {
+            dynamicSideContentPage.saveExampleBaselineScreenshot('dynamic-side-content');
+            expect(dynamicSideContentPage.compareWithBaseline('dynamic-side-content')).toEqual(0);
+        });
+    });
+});
