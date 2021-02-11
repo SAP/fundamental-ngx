562d842130dd9487406092a83b9efb57eb8b35b9
Sean Moore <sean.moore@valor-software.com>
2021-02-11 16:55:30 +0200

    test(e2e): add afterEach hook

--- e2e/wdio/core/tests/dynamic-side-content.e2e-spec.ts
+++ e2e/wdio/core/tests/dynamic-side-content.e2e-spec.ts
@@ -1,10 +1,16 @@
 import { DynamicSideContentPo } from '../pages/dynamic-side-content.po';
+import { refreshPage, waitForPresent } from '../../driver/wdio';
 
 describe('dynamic side content test suite', function() {
     const dynamicSideContentPage = new DynamicSideContentPo();
 
     beforeAll(() => {
         dynamicSideContentPage.open();
+    }, 1);
+
+    afterEach(() => {
+       refreshPage();
+       waitForPresent(dynamicSideContentPage.pageHeader);
     }, 1);
 
     describe('check orientation', function() {
