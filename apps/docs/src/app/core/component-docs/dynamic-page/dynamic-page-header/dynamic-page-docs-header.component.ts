1bc7e97c209332bb33dec976ea4dfa51d9bf7204
Jedrzej <jedrzej.markowski@sap.com>
2021-02-19 11:33:10 +0100

    remove leftovers

--- apps/docs/src/app/core/component-docs/dynamic-page/dynamic-page-header/dynamic-page-docs-header.component.ts
+++ apps/docs/src/app/core/component-docs/dynamic-page/dynamic-page-header/dynamic-page-docs-header.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-dynamic-page-header',
-    templateUrl: './platform-dynamic-page-header.component.html'
+    templateUrl: './dynamic-page-header.component.html'
 })
 export class DynamicPageDocsHeaderComponent {}
--- libs/core/src/lib/dynamic-page/dynamic-page.service.ts
+++ libs/core/src/lib/dynamic-page/dynamic-page.service.ts
@@ -3,15 +3,19 @@ import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable()
 export class DynamicPageService {
+    /** @hidden */
     collapsed = new BehaviorSubject<boolean>(false);
+
+    /** @hidden */
     pinned = new BehaviorSubject<boolean>(false);
 
-
+    /** @hidden */
     pixelsSizeChanged = new BehaviorSubject<number>(0)
 
+    /** @hidden */
     subheaderVisibilityChange = new Subject<void>();
 
-
+    /** @hidden */
     toggleCollapsed(): void {
         this.collapsed.next(!this.collapsed.value);
     }
