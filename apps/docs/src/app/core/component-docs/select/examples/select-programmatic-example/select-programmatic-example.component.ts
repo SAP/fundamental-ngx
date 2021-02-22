1c1074825563d414d362ab4e8ada928c5f3e3bb8
skudum <sushma.kudum@sap.com>
2021-02-22 21:52:29 +0530

    updating example as requested by QA for testing

--- apps/docs/src/app/core/component-docs/select/examples/select-programmatic-example/select-programmatic-example.component.ts
+++ apps/docs/src/app/core/component-docs/select/examples/select-programmatic-example/select-programmatic-example.component.ts
@@ -8,17 +8,40 @@ import { Component } from '@angular/core';
 export class SelectProgrammaticExampleComponent {
 
     options: string[] = ['Apple', 'Pineapple', 'Tomato', 'Strawberry'];
+    selectedPosition = 0;
 
     selectedValue: string = this.options[0];
+    newValue = this.options[0];
 
     changeValue(): void {
         this.selectedValue = this.getNewValue();
     }
 
+    getPosition(selectedValue: string): number {
+        this.options.forEach((item, index) => {
+            if (selectedValue === item) {
+                if (this.selectedPosition < this.options.length) {
+                    this.selectedPosition = index;
+                }
+                return this.selectedPosition;
+            }
+        });
+        return this.selectedPosition;
+    }
+
     getNewValue(): string {
-        const newValue = this.options[Date.now() % this.options.length];
-        return this.selectedValue === newValue
+
+        this.selectedPosition = this.getPosition(this.selectedValue);
+
+        if (this.selectedPosition < this.options.length - 1) {
+            this.selectedPosition++;
+        } else {
+            this.selectedPosition = 0;
+        }
+
+        this.newValue = this.options[this.selectedPosition];
+        return this.selectedValue === this.newValue
             ? this.getNewValue()
-            : newValue;
+            : this.newValue;
     }
 }
