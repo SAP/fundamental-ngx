53be5923b580dcdb709174a0bd859a026b6aa3a6
Sean Moore <47522152+Seamoo13@users.noreply.github.com>
2021-02-23 17:02:51 +0200

    test: (e2e) add object status e2e tests platform (#4721)

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,6 +55,7 @@ exports.config = {
             './e2e/wdio/platform/**/multi-input.e2e-spec.ts',
             './e2e/wdio/platform/**/object-list-item.e2e-spec.ts',
             './e2e/wdio/platform/**/object-marker.e2e-spec.ts',
+            './e2e/wdio/platform/**/object-status.e2e-spec.ts',
             './e2e/wdio/platform/**/object-attribute.e2e-spec.ts',
             './e2e/wdio/platform/**/panel.e2e-spec.ts',
             './e2e/wdio/platform/**/radio-button-group.e2e-spec.ts',
