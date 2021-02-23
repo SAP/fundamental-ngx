94bf2399b98b3ac787837d9baab13aedf6cdf320
jmarkowski <jedrzej.markowski@sap.com>
2021-02-23 14:35:10 +0100

    feat: (Core) introduce ControlValueAccessor to SegmentedButton  (#4579)
    * feat: Add control value accesor and examples to segmented button
    
    * Add tests
    
    * Apply changes in documentations
    
    * add complex example
    
    * Add documentation, put deisabled example
    
    * PR Comments
    
    * button margin
    
    * fix pagintion example
    
    * fix test
    
    * change selected to toggle
    
    Co-authored-by: Mike O'Donnell <mikerodonnell89@users.noreply.github.com>

--- apps/docs/src/app/core/component-docs/fixed-card-layout/examples/default/fixed-card-layout-examples.component.ts
+++ apps/docs/src/app/core/component-docs/fixed-card-layout/examples/default/fixed-card-layout-examples.component.ts
@@ -6,23 +6,16 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FixedCardLayoutExampleComponent {
-    card1Visibility = true;
-    card2Visibility = true;
-    card3Visibility = true;
+    cardsHidden = [];
 
     constructor(private _changeDetectorRef: ChangeDetectorRef) {}
 
-    public showHideCard(card: string): void {
-        if (card === 'card1') {
-            this.card1Visibility = !this.card1Visibility;
-        } else if (card === 'card2') {
-            this.card2Visibility = !this.card2Visibility;
-        } else if (card === 'card3') {
-            this.card3Visibility = !this.card3Visibility;
-        }
+    isHidden(card: string): boolean {
+        return this.cardsHidden.some(_card => _card === card);
     }
 
-    public onResized(): void {
-        this._changeDetectorRef.markForCheck();
+    update(): void {
+        this._changeDetectorRef.detectChanges();
     }
+
 }
