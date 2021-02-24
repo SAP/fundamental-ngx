367b204c548d1af71cff81a87360dd5596ac0643
Art Olshansky <olshansky0@gmail.com>
2021-02-24 17:35:04 +0200

    fix: remove unused keycodes
    Co-authored-by: Yurii Betrozov <betrozov91@gmail.com>

--- libs/platform/src/lib/components/table/directives/table-cell-selectable.directive.ts
+++ libs/platform/src/lib/components/table/directives/table-cell-selectable.directive.ts
@@ -1,6 +1,6 @@
 import { AfterContentChecked, AfterViewChecked, Directive, HostListener } from '@angular/core';
 import { KeyUtil } from '@fundamental-ngx/core';
-import { DOWN_ARROW, PAGE_DOWN, PAGE_UP, SPACE, UP_ARROW } from '@angular/cdk/keycodes';
+import { SPACE } from '@angular/cdk/keycodes';
 
 @Directive({
     // tslint:disable-next-line:directive-selector
