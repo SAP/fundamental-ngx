daecc19ea6cbcbb2ed5fb0faebfe503c1a721a7f
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2021-02-25 14:07:34 -0500

    feat: (Core) introduce Illustrated Message component (#4750)
    * initial commit
    
    * remove extra files
    
    * adding examples and tests
    
    * remove empty files
    
    * PR review comments
    
    * more PR review comments

--- libs/core/src/lib/fundamental-ngx-core.module.ts
+++ libs/core/src/lib/fundamental-ngx-core.module.ts
@@ -83,6 +83,7 @@ import { ObjectNumberModule } from './object-number/object-number.module';
 import { TitleModule } from './title/title.module';
 import { SliderModule } from './slider/public_api';
 import { OnlyDigitsModule } from './utils/directives/only-digits/only-digits.module';
+import { IllustratedMessageModule } from './illustrated-message/illustrated-message.module';
 
 @NgModule({
     imports: [CommonModule, FormsModule],
@@ -166,7 +167,8 @@ import { OnlyDigitsModule } from './utils/directives/only-digits/only-digits.mod
         MessageToastModule,
         DynamicSideContentModule,
         SliderModule,
-        OnlyDigitsModule
+        OnlyDigitsModule,
+        IllustratedMessageModule
     ],
     providers: [AlertService, DialogService, NotificationService, MessageToastService, ThemesService],
 })
