072458f46e263b1ef7047817d0abdafdc9e5bd0d
Evgeny Beregovoy <evgeny.beregovoy@valor-software.com>
2021-02-26 17:28:46 +0200

    fix: (Core | Platform) feed input accessibility issues (#4488)
    * fix: (Core | Platform) feed input accessibility issues

--- libs/core/src/lib/feed-input/feed-input.component.spec.ts
+++ libs/core/src/lib/feed-input/feed-input.component.spec.ts
@@ -74,7 +74,7 @@ describe('FeedInputComponent', () => {
         textareaEl.triggerEventHandler('keyup', { target: { value: 'Feed message' } });
         fixture.detectChanges();
 
-        expect((<any>buttonDirective)._elementRef.nativeElement.disabled).toBeFalse();
+        expect((<any>buttonDirective)._elementRef.nativeElement.getAttribute('aria-disabled')).toBe('false');
     });
 
     it('should button enable when textarea have not a value', () => {
@@ -82,7 +82,7 @@ describe('FeedInputComponent', () => {
         textareaEl.triggerEventHandler('keyup', { target: { value: '' } });
         fixture.detectChanges();
 
-        expect((<any>buttonDirective)._elementRef.nativeElement.disabled).toBeTrue();
+        expect((<any>buttonDirective)._elementRef.nativeElement.getAttribute('aria-disabled')).toBe('true');
     });
 
     it('should emit value properly', () => {
@@ -93,15 +93,14 @@ describe('FeedInputComponent', () => {
 
         expect(textareaDirective.valueChange.emit).toHaveBeenCalledWith(event.target.value)
     });
-    // TODO: Unskip after fix
-    xit('should textarea grow by default with normal line height', () => {
-        textareaEl.nativeElement.style.lineHeight = 'normal';
-        textareaEl.nativeElement.style.fontSize = '14px';
+
+    it('should textarea grow by default', () => {
+        const defaultHeight = textareaEl.nativeElement.style.height;
         textareaEl.nativeElement.value = '1 \n 2 \n 3 \n 4 \n';
         textareaDirective.resize();
         fixture.detectChanges();
 
-        expect(textareaEl.nativeElement.style.height).toEqual('92px');
+        expect(parseInt(textareaEl.nativeElement.style.height, 10)).toBeGreaterThan(defaultHeight);
     });
 
     it('should set textarea max height', () => {
@@ -110,16 +109,16 @@ describe('FeedInputComponent', () => {
         fixture.detectChanges();
 
         textareaDirective.ngOnInit();
-        expect(textareaEl.nativeElement.style.maxHeight).toEqual('190px');
+        expect(parseInt(textareaEl.nativeElement.style.maxHeight, 10)).toEqual(190);
     });
-    // TODO: Unskip after fix
-    xit('should set textarea max height with normal line height', () => {
+
+    it('should set textarea max height with normal line height', () => {
         component.maxRows = 10;
         textareaEl.nativeElement.style.lineHeight = 'normal';
         textareaEl.nativeElement.style.fontSize = '14px';
         fixture.detectChanges();
 
         textareaDirective.ngOnInit();
-        expect(textareaEl.nativeElement.style.maxHeight).toEqual('154px');
+        expect(parseInt(textareaEl.nativeElement.style.maxHeight, 10)).toEqual(154);
     });
 });
