6f1b809e7e56b0116693602cfb7070aa53031eb7
nikvalor <nikita.lastovych@valor-software.com>
2021-03-02 14:46:38 +0200

    Debugging

--- deploy-to-preview-channel-platform-a.sh
+++ deploy-to-preview-channel-platform-a.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-DEPLOY_TO_PREVIEW_CHANNEL_RESULT=$(firebase hosting:channel:deploy pr-$TRAVIS_PULL_REQUEST --expires 30d --token $FIREBASE_TOKEN --json)
+DEPLOY_TO_PREVIEW_CHANNEL_RESULT=$(firebase hosting:channel:deploy platform-a-pr-$TRAVIS_PULL_REQUEST --expires 30d --token $FIREBASE_TOKEN --json)
 
 RESULT=`echo ${DEPLOY_TO_PREVIEW_CHANNEL_RESULT} | jq -r '.result'`
 ALLURE_RESULTS=`echo ${RESULT} | jq -r '."allure-results"'`
 SITE=`echo ${ALLURE_RESULTS} | jq -r .site`
-URL=`echo ${ALLURE_RESULTS} | jq -r .url '.platform-a'`
+URL=`echo ${ALLURE_RESULTS} | jq -r .url`
 EXPIRE_TIME_UTC=`echo ${ALLURE_RESULTS} | jq -r .expireTime`
 EXPIRE_TIME=$(TZ='GMT' date -d $EXPIRE_TIME_UTC +%c)
 
 NEW_COMMENT="Project: $SITE Platform A \n Url: $URL \n This link will expire at $EXPIRE_TIME"
 COMMENTS=$(curl -X GET "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments")
 
-SUBSTRING="Project: allure-results"
+SUBSTRING="Project: allure-results Platform A"
 COMMENT_ID=-1
 
 for row in $(echo "${COMMENTS}" | jq -r '.[] | @base64'); do
