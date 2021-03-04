f1e9099d9916a7071512933d11658e96be92ccd1
nikvalor <74654139+nikvalor@users.noreply.github.com>
2021-03-04 18:17:32 +0200

    test: added allure to Travis CI build (#4829)
    * Added allure to Travis CI build

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,8 @@
 /*require('ts-node').register({ transpileOnly: true });
 module.exports = require('./wdio.conf.ts');*/
-const {join} = require('path');
+const { join } = require('path');
 require('ts-node').register({ transpileOnly: true });
+AllureReporter = require('@wdio/allure-reporter').default;
 exports.config = {
     //
     // ====================
@@ -66,7 +67,7 @@ exports.config = {
             './e2e/wdio/platform/**/step-input.e2e-spec.ts',
             './e2e/wdio/platform/**/switch.e2e-spec.ts',
             './e2e/wdio/platform/**/textarea.e2e-spec.ts',
-         //   './e2e/wdio/platform/**/thumbnail.e2e-spec.ts',
+            //   './e2e/wdio/platform/**/thumbnail.e2e-spec.ts',
             './e2e/wdio/platform/**/value-help-dialog.e2e-spec.ts',
         ]
     },
@@ -264,7 +265,7 @@ exports.config = {
     }], ['allure', {
         outputDir: 'allure-results',
         disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: true
+        disableWebdriverScreenshotsReporting: false
     }]],
 
     jasmineNodeOpts: {
@@ -344,7 +345,7 @@ exports.config = {
 
         browser.resetUrl = 'about:blank';
         browser.maximizeWindow();
-    }
+    },
 
 //     const processedConfig = await browser.getProcessedConfig();
 //
@@ -385,9 +386,13 @@ exports.config = {
     /**
      * Function to be executed after a test (in Mocha/Jasmine).
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
-
+    afterTest: function(test, context, { error, result, duration, passed, retries }) {
+        if (error !== undefined) {
+            browser.takeScreenshot();
+            const html = browser.getPageSource();
+            AllureReporter.addAttachment('page.html', html, 'text/html');
+        }
+    },
 
     /**
      * Hook that gets executed after the suite has ended
