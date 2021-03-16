3191365c8862022195d24f40dd46a0be1c2cfadf
AntonOlkhovskyi <olkhovskyy@gmail.com>
2021-03-16 11:34:10 +0200

    test(e2e): fix display list item and file uploader tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -25,8 +25,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './e2e/wdio/**/breadcrumb.e2e-spec.ts',
-        './e2e/wdio/**/bar.e2e-spec.ts',
+        './e2e/wdio/**/*.e2e-spec.ts',
     ],
     // Patterns to exclude.
     exclude: [
@@ -248,7 +247,7 @@ exports.config = {
     framework: 'jasmine',
     //
     // The number of times to retry the entire specfile when it fails as a whole
-    specFileRetries: 0,
+    specFileRetries: 2,
     //
     // Delay in seconds between the spec file retry attempts
     specFileRetriesDelay: 0,
