ae96a167f1afea1e2471e38bb178e5ff8c0619be
Kavya <kavya.bhat@sap.com>
2021-03-17 14:36:19 +0530

    feat: (Core) implement facets
    - implement Form, Key-value, Image, and Rating Indicator facets

---
+++ apps/docs/src/app/core/component-docs/facets/facet-examples/key-value-facet-alignment-example.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'fd-key-value-facet-alignment-example',
+    templateUrl: './key-value-facet-alignment-example.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class KeyValueFacetAlignmentExampleComponent {
+}
---
+++ apps/docs/src/app/core/component-docs/facets/facet-examples/key-value-facet-example.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'fd-key-value-facet-example',
+    templateUrl: './key-value-facet-example.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class KeyValueFacetExampleComponent {
+}
