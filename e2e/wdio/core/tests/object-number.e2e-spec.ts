51d6a09173d832f30ca4f938ecf57a65e666e4ba
droshev <mladen.droshev@sap.com>
2021-03-17 22:13:19 -0400

    test: disable object number e2e visual test [ci skip]

--- e2e/wdio/core/tests/object-number.e2e-spec.ts
+++ e2e/wdio/core/tests/object-number.e2e-spec.ts
@@ -132,7 +132,7 @@ describe('object number test suite', function () {
     });
 
     describe('Check visual regression', function() {
-        it('should check examples visual regression', () => {
+        xit('should check examples visual regression', () => {
             objectNumberPage.saveExampleBaselineScreenshot();
             expect(objectNumberPage.compareWithBaseline()).toBeLessThan(1);
         });
