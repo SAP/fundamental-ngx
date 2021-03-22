97f4b2937afb3c1e9ccfb92269bd599cc0596ef4
Kavya <kavya.bhat@sap.com>
2021-03-22 14:55:55 +0530

    unit tests for facets and code cleanup

--- apps/docs/src/app/core/component-docs/facets/facet-examples/form-link-facet-example.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'fd-form-link-facet-example',
-    templateUrl: './form-link-facet-example.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class FormLinkFacetExampleComponent {
-}
