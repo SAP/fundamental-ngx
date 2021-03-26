92a9c565c897a939005a5ba7d903582ebcbe69ae
Deepak Kumar <deepak.kumar14@sap.com>
2021-03-26 14:56:05 +0530

    docs: examples added for link list and carousel card

---
+++ apps/docs/src/app/core/component-docs/card/examples/link-list-card/card-link-list-carousel-example.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fd-card-link-list-carousel-example',
+    templateUrl: './card-link-list-carousel-example.component.html',
+    styleUrls: ['card-link-list-carousel-example.component.scss']
+})
+export class CardLinkListCarouselExampleComponent {}
