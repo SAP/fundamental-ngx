5043b5e9643fa0dd441f654033829d09c000034a
Deepak Kumar <deepak.kumar14@sap.com>
2021-03-26 16:07:04 +0530

    fix:data change shown in example

--- apps/docs/src/app/core/component-docs/card/examples/list-card/card-list-example.component.ts
+++ apps/docs/src/app/core/component-docs/card/examples/list-card/card-list-example.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnChanges, OnInit } from '@angular/core';
+import { FdSelectChange } from '@fundamental-ngx/core';
+
+interface ListData {
+    title?: string;
+    value?: number;
+    change?: number;
+}
 
 @Component({
     selector: 'fd-card-list-example',
-    templateUrl: 'card-list-example.component.html'
+    templateUrl: 'card-list-example.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardListExampleComponent {
-    selectedValue: string;
+export class CardListExampleComponent implements OnInit {
+    selectedValue = 'By Supplier';
     options: string[] = ['By Supplier', 'By Zone'];
+    showData: ListData[] = [];
+
+    bySupplierData: ListData[] = [
+        { title: 'Delbont Industries', value: 91, change: 4 },
+        { title: 'Brazil Technologies', value: 52, change: 3 },
+        { title: 'Anav Ideon', value: 23, change: 1 },
+        { title: 'Loremt', value: 21, change: 1 }
+    ];
+
+    byZoneData: ListData[] = [
+        { title: 'North Zone', value: 91, change: 14 },
+        { title: 'West Zone', value: 80, change: 9 },
+        { title: 'South Zone', value: 45, change: 12 },
+        { title: 'east Zone', value: 21, change: 9 }
+    ];
+
+    constructor(private _cd: ChangeDetectorRef) {}
+
+    ngOnInit(): void {
+        this.showData = this.bySupplierData;
+    }
+
+    onValueChange(event: FdSelectChange): void {
+        this.showData = this.selectedValue === this.options[0] ? this.bySupplierData : this.byZoneData;
+        this._cd.markForCheck();
+    }
 }
