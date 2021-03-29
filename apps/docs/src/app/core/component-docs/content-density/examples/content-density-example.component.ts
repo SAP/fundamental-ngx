3709cef5f39fa8ebd6eef321aaafe034f1aa3d9f
Jedrzej <jedrzej.markowski@sap.com>
2021-03-29 15:44:48 +0200

    bring adjustments to documentation

--- apps/docs/src/app/core/component-docs/content-density/examples/content-density-example.component.ts
+++ apps/docs/src/app/core/component-docs/content-density/examples/content-density-example.component.ts
@@ -10,14 +10,10 @@ import { map } from 'rxjs/operators';
 export class ContentDensityExampleComponent implements OnInit {
     selectedDensity: ContentDensity;
 
-    compact$: Observable<boolean>;
-
     constructor(private _contentDensityService: ContentDensityService) {}
 
     ngOnInit(): void {
         this.selectedDensity = this._contentDensityService.contentDensity.value;
-
-        this.compact$ = this._contentDensityService.contentDensity.pipe(map((density) => density === 'compact'));
     }
 
     onChange(): void {
