7352f9955b95090f6b7c18e3d8371ee39e36859b
Deepak Kumar <deepak.kumar14@sap.com>
2021-04-01 09:39:39 +0530

    link list combined

--- apps/docs/src/app/core/component-docs/card/examples/link-list-card/card-link-list-example.component.ts
+++ apps/docs/src/app/core/component-docs/card/examples/link-list-card/card-link-list-example.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-card-link-list-example',
-    templateUrl: 'card-link-list-example.component.html'
+    templateUrl: 'card-link-list-example.component.html',
+    styleUrls: ['card-link-list-example.component.scss']
 })
 export class CardLinkListExampleComponent {}
--- apps/docs/src/app/core/component-docs/card/examples/link-list-card/card-quick-link-list-example.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'fd-card-quick-link-list-example',
-    templateUrl: 'card-quick-link-list-example.component.html'
-})
-export class CardQuickLinkListExampleComponent {}
