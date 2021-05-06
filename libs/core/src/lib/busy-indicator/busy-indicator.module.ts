15ff7c6e15f884c227a094ec3cfd0b0ae9a1ec5d
I505460 <lokanathan.k@sap.com>
2021-05-06 10:11:28 +0530

    Update busy-indicator.module.ts

--- libs/core/src/lib/busy-indicator/busy-indicator.module.ts
+++ libs/core/src/lib/busy-indicator/busy-indicator.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BusyIndicatorComponent } from './busy-indicator.component';
-import { FormModule } from '../form/form.module';
 
 @NgModule({
     declarations: [BusyIndicatorComponent],
     exports: [BusyIndicatorComponent],
-    imports: [CommonModule, FormModule]
+    imports: [CommonModule]
 })
 export class BusyIndicatorModule {}
