353305540af8d59928ef47313684fa0ac5c84913
nikvalor <74654139+nikvalor@users.noreply.github.com>
2021-05-17 17:22:46 +0300

    test: (e2e) added popover core e2e tests  (#5329)
    * test: added popover core e2e

---
+++ e2e/wdio/core/fixtures/appData/popover-contents.ts
@@ -0,0 +1,18 @@
+export const headerTestText = 'Header';
+export const cozyHeaderTestText = 'Cozy Header';
+export const cozySubheaderTestText = 'Cozy Subheader';
+export const subheaderTestText = 'SubHeader Middle section with Full Width Element';
+export const popoverTestText = 'This popover is connected to trigger element';
+export const fruitsTestArr = [
+    'Pineapple',
+    'Apple',
+    'Pepper',
+    'Tomato'
+];
+export const popoverExampleTestText = 'Popover Example';
+export const popoverParagraphTestText = 'The popover disappears if the attached reference object is outside the view!';
+export const buttonsPopoverTestText = 'This is the button\'s popover!';
+export const hoverTestText = 'Control Element - Hover over me!';
+export const containerTestText = 'Trigger';
+export const triggerTestText = 'This popover is connected to other element';
+export const messageTestText = 'Start scrolling to see behaviour';
---
+++ e2e/wdio/core/tests/popover.e2e-spec.ts
@@ -0,0 +1,417 @@
+import { PopoverPo } from '../pages/popover.po';
+import {
+    click, getAttributeByName,
+    getElementArrayLength, getText, isElementClickable, isElementDisplayed,
+    refreshPage, scrollIntoView
+} from '../../driver/wdio';
+
+import {
+    headerTestText,
+    cozyHeaderTestText,
+    subheaderTestText,
+    popoverTestText,
+    cozySubheaderTestText,
+    fruitsTestArr,
+    popoverExampleTestText,
+    popoverParagraphTestText,
+    buttonsPopoverTestText,
+    hoverTestText,
+    containerTestText,
+    triggerTestText, messageTestText
+} from '../fixtures/appData/popover-contents';
+
+describe('Popover test suite', function() {
+    const popoverPage = new PopoverPo();
+    const {
+        avatar, popover, option, icon, iconMoney, standardMoneyButton, barElement, basicPopoverButton,
+        simplePopoverButton, popoverMessage, headerPopoverButton, placementPopoverButton, programmaticControlButton,
+        popoverDialogsButton, multiInputButton, multiInputOption, multiInputSpan, popoverDialogsHeader, popoverDialogParagraph,
+        clickMeButton, popoverDialogMessage, hoverElement, triggerButton2, triggerButton, popoverContainer, scrollButton,
+        scrollMessage, plusButton, dynamicOption, dropdownButton, dropdownOption, dynamicSubOption, cdkButton, segmentButton
+    } = popoverPage;
+
+    beforeAll(() => {
+        popoverPage.open();
+    }, 1);
+
+    afterEach(() => {
+        refreshPage();
+    }, 1);
+
+    describe('Check Basic Popovers', function() {
+
+        it('should check that avatars have popovers and all options are clickable', () => {
+            scrollIntoView(avatar);
+            const avatarLength = getElementArrayLength(avatar);
+            for (let i = 0; i < avatarLength - 1; i++) {
+                click(avatar, i);
+                expect(isElementDisplayed(popover, 1)).toBe(true, `popover not displayed`);
+                const optionLength = getElementArrayLength(option);
+                for (let j = 3; j < optionLength; j++) {
+                    expect(isElementClickable(option, j)).toBe(true, `option with index ${j} not clickable`);
+                }
+            }
+        });
+
+        it('should check that icon has popover and all options are clickable', () => {
+            scrollIntoView(icon, 1);
+            click(icon, 1);
+            const iconLength = getElementArrayLength(option);
+            for (let i = 3; i < iconLength; i++) {
+                expect(isElementClickable(option, i)).toBe(true, `option with index ${i} not clickable`);
+            }
+        });
+
+        it('should check that icon money has popover and all options and arrow button are clickable', () => {
+            scrollIntoView(iconMoney);
+            click(iconMoney);
+            expect(isElementDisplayed(popover, 1)).toBe(true, `popover not displayed`);
+            expect(isElementClickable(standardMoneyButton)).toBe(true, `button not clickable`);
+            expect(getText(barElement, 1)).toBe(headerTestText);
+            const iconMoneyLength = getElementArrayLength(option);
+            for (let i = 3; i < iconMoneyLength; i++) {
+                expect(isElementClickable(option, i)).toBe(true, `option with index ${i} not clickable`);
+            }
+        });
+
+        it('should check that basic popover buttons have popovers and all options are clickable', () => {
+            scrollIntoView(basicPopoverButton);
+            const buttonsLength = getElementArrayLength(basicPopoverButton);
+            for (let i = 0; i < buttonsLength; i++) {
+                click(basicPopoverButton, i);
+                expect(isElementDisplayed(popover, 1)).toBe(true, `popover not displayed`);
+                const optionLength = getElementArrayLength(option);
+                for (let j = 3; j < optionLength; j++) {
+                    expect(isElementClickable(option, j)).toBe(true, `option with index ${j} not clickable`);
+                }
+            }
+        });
+
+        it('should check that button popover have header, subheader and buttons are clickable', () => {
+            scrollIntoView(basicPopoverButton);
+            click(basicPopoverButton);
+            expect(getText(barElement)).toBe(cozyHeaderTestText);
+            expect(getText(barElement, 1)).toBe(subheaderTestText);
+
+            expect(isElementClickable(barElement, 2)).toBe(true, `save button not clickable`);
+            expect(isElementClickable(barElement, 3)).toBe(true, `cancel button not clickable`);
+        });
+    });
+
+    describe('Check Simple Popover', function() {
+
+        it('check that after clicking the button a message with text is displayed', () => {
+            scrollIntoView(simplePopoverButton);
+            click(simplePopoverButton);
+            expect(isElementDisplayed(popoverMessage)).toBe(true, `message not displayed`);
+            expect(getText(popoverMessage)).toBe(popoverTestText);
+        });
+    });
+
+    describe('Check Popover with Header and Footer', function() {
+
+        it('should check that buttons have popovers and all options are clickable', () => {
+            scrollIntoView(headerPopoverButton);
+            const buttonLength = getElementArrayLength(headerPopoverButton);
+            for (let i = 0; i < buttonLength; i++) {
+                click(headerPopoverButton, i);
+                const optionLength = getElementArrayLength(option);
+                for (let j = 3; j < optionLength; j++) {
+                    expect(isElementClickable(option, j)).toBe(true, `option with index ${j} not clickable`);
+                }
+            }
+        });
+
+        it('should check that button "with header" has header', () => {
+            scrollIntoView(headerPopoverButton);
+            click(headerPopoverButton, 1);
+            expect(getText(barElement)).toBe(cozyHeaderTestText);
+        });
+
+        it('should check that button "with header and footer" has header and all buttons are clickable', () => {
+            scrollIntoView(headerPopoverButton);
+            click(headerPopoverButton, 2);
+            expect(getText(barElement)).toBe(cozyHeaderTestText);
+
+            expect(isElementClickable(barElement, 1)).toBe(true, `save button not clickable`);
+            expect(isElementClickable(barElement, 2)).toBe(true, `cancel button not clickable`);
+        });
+
+        it('should check that button "with header, subHeader and footer" has header, subheader and all buttons are clickable', () => {
+            scrollIntoView(headerPopoverButton);
+            click(headerPopoverButton, 3);
+            expect(getText(barElement)).toBe(cozyHeaderTestText);
+            expect(getText(barElement, 1)).toBe(cozySubheaderTestText);
+
+            expect(isElementClickable(barElement, 2)).toBe(true, `save button not clickable`);
+            expect(isElementClickable(barElement, 3)).toBe(true, `cancel button not clickable`);
+        });
+    });
+
+    describe('Check Popover Placement', function() {
+
+        it('should check that arrow buttons have popovers and all options are clickable', () => {
+            scrollIntoView(placementPopoverButton);
+            const buttonsLength = getElementArrayLength(placementPopoverButton);
+            for (let i = 0; i < buttonsLength; i++) {
+                scrollIntoView(placementPopoverButton, i);
+                click(placementPopoverButton, i);
+                expect(isElementDisplayed(popover, 1)).toBe(true, `popover not displayed`);
+                const optionLength = getElementArrayLength(option);
+                for (let j = 3; j < optionLength; j++) {
+                    expect(isElementClickable(option, j)).toBe(true, `option with index ${j} not clickable`);
+                }
+            }
+        });
+    });
+
+    describe('Check Programmatic Control', function() {
+
+        it('should check that avatar has popover and all options are clickable', () => {
+            scrollIntoView(avatar, 2);
+            expect(isElementDisplayed(avatar, 2)).toBe(true, `avatar not displayed`);
+            click(programmaticControlButton);
+            expect(isElementDisplayed(popover, 1)).toBe(true, `popover not displayed`);
+            const buttonLength = getElementArrayLength(option);
+            for (let i = 3; i < buttonLength; i++) {
+                expect(isElementClickable(option, i)).toBe(true, `option with index ${i} not clickable`);
+            }
+            click(programmaticControlButton, 1);
+        });
+    });
+
+    describe('Check Popovers in Dialogs', function() {
+
+        it('should check popovers in dialogs example', () => {
+            scrollIntoView(popoverDialogsButton);
+            click(popoverDialogsButton);
+            click(multiInputButton);
+            const optionLength = getElementArrayLength(multiInputOption);
+            for (let i = 3; i < optionLength; i++) {
+                click(multiInputOption, i);
+            }
+            click(multiInputButton);
+            const spanLength = getElementArrayLength(multiInputSpan);
+            for (let i = 0; i < spanLength; i++) {
+                expect(getText(multiInputSpan, i)).toBe(fruitsTestArr[i]);
+            }
+        });
+
+        it('should check dialog have header and paragraph', () => {
+            scrollIntoView(popoverDialogsButton);
+            click(popoverDialogsButton);
+            expect(getText(popoverDialogsHeader)).toBe(popoverExampleTestText);
+            expect(getText(popoverDialogParagraph)).toBe(popoverParagraphTestText);
+        });
+
+        it('should check that after clicking button "Click me!" message appears', () => {
+            scrollIntoView(popoverDialogsButton);
+            click(popoverDialogsButton);
+            click(clickMeButton);
+            expect(isElementDisplayed(popoverDialogMessage)).toBe(true, 'message not displayed');
+            expect(getText(popoverDialogMessage)).toBe(buttonsPopoverTestText);
+        });
+    });
+
+    describe('Check Fill Control Width', function() {
+
+        it('should check fill control width example', () => {
+            scrollIntoView(hoverElement);
+            expect(getText(hoverElement)).toBe(hoverTestText);
+        });
+    });
+
+    describe('Check Popover Focus Trap', function() {
+
+        it('should check that after clicking trigger button appears message with text and container have some text', () => {
+            scrollIntoView(triggerButton);
+            click(triggerButton);
+            expect(isElementDisplayed(popoverMessage)).toBe(true, `message not displayed`);
+            expect(getText(popoverMessage)).toBe(triggerTestText);
+            expect(getText(popoverContainer)).toBe(containerTestText);
+        });
+    });
+
+    describe('Check Popover Focus Trap', function() {
+
+        it('should check that after clicking trigger button has popover and all options are clickable', () => {
+            scrollIntoView(triggerButton2);
+            click(triggerButton2);
+            const buttonLength = getElementArrayLength(option);
+            for (let i = 3; i < buttonLength; i++) {
+                expect(isElementClickable(option, i)).toBe(true, `option with index ${i} not clickable`);
+            }
+        });
+
+        it('should check header and clickable buttons', () => {
+            scrollIntoView(triggerButton2);
+            click(triggerButton2);
+            const buttonsLength = getElementArrayLength(barElement);
+            for (let i = 0; i < buttonsLength; i++) {
+                if (i === 1) {
+                    continue;
+                }
+                expect(isElementClickable(barElement)).toBe(true, `button with index ${i} not clickable`);
+            }
+            expect(getText(barElement, 1)).toBe(headerTestText);
+        });
+
+    });
+
+    describe('Check Popover Scroll Example', function() {
+
+        it('should check that after clicking messages appears', () => {
+            scrollIntoView(scrollButton);
+            const buttonLength = getElementArrayLength(scrollButton);
+            for (let i = 0; i < buttonLength; i++) {
+                scrollIntoView(scrollButton, i);
+                click(scrollButton, i);
+                expect(isElementDisplayed(scrollMessage)).toBe(true, 'message not displayed');
+                expect(getText(scrollMessage)).toBe(messageTestText);
+                click(scrollButton, i);
+            }
+        });
+    });
+
+    describe('Check Dynamic Body Height', function() {
+
+        it('should check that first dropdown option has clickable suboptions', () => {
+            scrollIntoView(plusButton);
+            click(plusButton);
+            click(dynamicOption);
+            const subOptionsLength = getElementArrayLength(dynamicSubOption);
+            for (let i = 0; i < subOptionsLength - 12; i++) {
+                scrollIntoView(dynamicSubOption, i);
+                expect(isElementClickable(dynamicSubOption, i)).toBe(true, `suboption with index ${i} not clickable`);
+            }
+        });
+
+        it('should check that second dropdown option has clickable suboptions', () => {
+            scrollIntoView(plusButton);
+            click(plusButton);
+            click(dynamicOption, 1);
+            const subOptionsLength = getElementArrayLength(dynamicSubOption);
+            for (let i = 3; i < subOptionsLength - 9; i++) {
+                scrollIntoView(dynamicSubOption, i);
+                expect(isElementClickable(dynamicSubOption, i)).toBe(true, `suboption with index ${i} not clickable`);
+            }
+        });
+
+        it('should check that third dropdown option has clickable suboptions', () => {
+            scrollIntoView(plusButton);
+            click(plusButton);
+            click(dynamicOption, 2);
+            const subOptionsLength = getElementArrayLength(dynamicSubOption);
+            for (let i = 6; i < subOptionsLength - 6; i++) {
+                scrollIntoView(dynamicSubOption, i);
+                expect(isElementClickable(dynamicSubOption, i)).toBe(true, `suboption with index ${i} not clickable`);
+            }
+        });
+
+        it('should check that forth dropdown option has clickable suboptions', () => {
+            scrollIntoView(plusButton);
+            click(plusButton);
+            click(dynamicOption, 3);
+            const subOptionsLength = getElementArrayLength(dynamicSubOption);
+            for (let i = 9; i < subOptionsLength - 3; i++) {
+                scrollIntoView(dynamicSubOption, i);
+                expect(isElementClickable(dynamicSubOption, i)).toBe(true, `suboption with index ${i} not clickable`);
+            }
+        });
+
+        it('should check that fifth dropdown option has clickable suboptions', () => {
+            scrollIntoView(plusButton);
+            click(plusButton);
+            click(dynamicOption, 4);
+            const subOptionsLength = getElementArrayLength(dynamicSubOption);
+            for (let i = 12; i < subOptionsLength; i++) {
+                scrollIntoView(dynamicSubOption, i);
+                expect(isElementClickable(dynamicSubOption, i)).toBe(true, `suboption with index ${i} not clickable`);
+            }
+        });
+    });
+
+    describe('Check Dropdown Popover', function() {
+
+        it('should check disable button', () => {
+            scrollIntoView(dropdownButton);
+            expect(getAttributeByName(dropdownButton, 'ng-reflect-disabled', 1)).toBe('true');
+        });
+
+        it('should check that buttons have dropdown and options', () => {
+            scrollIntoView(dropdownButton);
+            const buttonLength = getElementArrayLength(dropdownButton);
+            for (let i = 0; i < buttonLength; i++) {
+                if (i === 1) {
+                    continue;
+                }
+                click(dropdownButton, i);
+                expect(isElementDisplayed(popover, 1)).toBe(true, 'popover not displayed');
+                const optionsLength = getElementArrayLength(dropdownOption);
+                for (let j = 0; j < optionsLength; j++) {
+                    expect(isElementClickable(dropdownOption, i)).toBe(true, `option with index ${j} not clickable`);
+                }
+            }
+        });
+    });
+
+    describe('Check Dropdown Popover', function() {
+
+        it('should check that arrow button is clickable and it has popover and clickable options', () => {
+            scrollIntoView(cdkButton);
+            click(cdkButton, 5);
+            click(cdkButton, 9);
+            scrollIntoView(cdkButton, 12);
+            expect(isElementClickable(cdkButton, 12)).toBe(true, 'arrow button not clickable');
+            expect(isElementDisplayed(popover)).toBe(true, 'popover not displayed');
+            const optionsLength = getElementArrayLength(option);
+            for (let i = 0; i < optionsLength; i++) {
+                expect(isElementClickable(option, i)).toBe(true, `option with index ${i} not clickable`);
+            }
+        });
+
+        it('should check that popover can move around the button', () => {
+            scrollIntoView(cdkButton);
+            click(cdkButton);
+            // Origin X
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model')).toBe('start');
+            click(cdkButton, 1);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model')).toBe('center');
+            click(cdkButton, 2);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model')).toBe('end');
+            // Origin Y
+            click(cdkButton, 3);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 1)).toBe('bottom');
+            click(cdkButton, 4);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 1)).toBe('center');
+            click(cdkButton, 5);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 1)).toBe('top');
+            // Overlay X
+            click(cdkButton, 6);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 2)).toBe('start');
+            click(cdkButton, 7);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 2)).toBe('center');
+            click(cdkButton, 8);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 2)).toBe('end');
+            // Overlay Y
+            click(cdkButton, 9);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 3)).toBe('bottom');
+            click(cdkButton, 10);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 3)).toBe('center');
+            click(cdkButton, 11);
+            expect(getAttributeByName(segmentButton, 'ng-reflect-model', 3)).toBe('top');
+        });
+    });
+
+    it('should check orientation', () => {
+        popoverPage.checkRtlSwitch();
+    });
+
+    describe('visual regression', function() {
+
+        it('should check examples visual regression', () => {
+            popoverPage.saveExampleBaselineScreenshot();
+            expect(popoverPage.compareWithBaseline()).toBeLessThan(5);
+        });
+    });
+});
