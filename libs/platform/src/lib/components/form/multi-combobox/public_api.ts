1e37336036cae7c806077f939500d2f32f82f006
Artur Olshanskyi <olshansky0@gmail.com>
2021-05-21 11:54:39 +0300

    feat: (Platform) Multi-Combobox component (#5107)

---
+++ libs/platform/src/lib/components/form/multi-combobox/public_api.ts
@@ -0,0 +1,5 @@
+export { PlatformMultiComboboxModule } from './multi-combobox.module';
+export { BaseMultiCombobox, MultiComboboxSelectionChangeEvent } from './commons/base-multi-combobox';
+export { MultiComboboxComponent } from './multi-combobox/multi-combobox.component';
+export { PlatformMultiComboboxMobileModule } from './multi-combobox-mobile/multi-combobox-mobile.module';
+export { MultiComboboxMobileComponent } from './multi-combobox-mobile/multi-combobox/multi-combobox-mobile.component';
