afb01df812071f940701a3764b8104757ac3284b
Artur Olshanskyi <olshansky0@gmail.com>
2021-05-21 11:54:39 +0300

    feat: (Platform) Multi-Combobox component (#5107)

--- libs/platform/src/lib/domain/array-data-source.ts
+++ libs/platform/src/lib/domain/array-data-source.ts
@@ -1,10 +1,16 @@
 /**
  * Default implementation for Arrays.
  */
-import { ComboBoxDataSource, ListDataSource } from './data-source';
+import { ComboBoxDataSource, ListDataSource, MultiComboBoxDataSource } from './data-source';
 import { BaseDataProvider } from './base-data-provider';
 
 export class ArrayComboBoxDataSource<T> extends ComboBoxDataSource<T> {
+    constructor(private data: T[]) {
+        super(new BaseDataProvider(data));
+    }
+}
+
+export class ArrayMultiComboBoxDataSource<T> extends MultiComboBoxDataSource<T> {
     constructor(private data: T[]) {
         super(new BaseDataProvider(data));
     }
