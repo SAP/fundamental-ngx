0c313bd98eba8fbe2632c92857f825447f8a4a81
Yurii Betrozov <betrozov91@gmail.com>
2021-05-25 22:51:19 +0300

    chore: (deps-dev) update commitlint rules (#5452)

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,7 +7,9 @@ const Configuration = {
             'always',
             ['build', 'ci', 'docs', 'feat', 'fix', 'style', 'refactor', 'test', 'revert', 'chore']
         ],
-        'scope-enum': [2, 'always', ['Core', 'Platform', 'doc-app', 'release', 'e2e', 'deps', 'deps-dev']]
+        'scope-enum': [2, 'always', ['Core', 'Platform', 'doc-app', 'release', 'e2e', 'deps', 'deps-dev']],
+        'body-max-line-length': [2, 'always', 200],
+        'footer-max-line-length': [2, 'always', 200]
     }
 };
 
