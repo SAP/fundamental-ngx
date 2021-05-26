e8612066daead87837d26ed246a14ad5d65c5c48
nikvalor <74654139+nikvalor@users.noreply.github.com>
2021-05-26 10:07:16 +0300

    test: (e2e) add e2e tests for File Uploader component (#5354)
    * test: (e2e) add file uploader e2e tests

---
+++ e2e/wdio/core/tests/file-uploader.e2e-spec.ts
@@ -0,0 +1,71 @@
+import { FileUploaderPo } from '../pages/file-uploader.po';
+import {
+    getAlertText,
+    getAttributeByName,
+    getElementArrayLength, getText, isElementClickable,
+    refreshPage,
+    scrollIntoView, uploadFile
+} from '../../driver/wdio';
+
+import { placeholderTestTextArr, imagePath, titleValue } from '../fixtures/appData/file-uploader-contents';
+
+describe('File uploader component test', function() {
+    const fileUploaderPage = new FileUploaderPo();
+    const { fileUploaderInput, browseButton, fileUploaderInputFile, fileSelectedText } = fileUploaderPage;
+
+    beforeAll(() => {
+        fileUploaderPage.open();
+    }, 1);
+
+    afterEach(() => {
+        refreshPage();
+    }, 2);
+
+    it('verify placeholders', () => {
+        const arrLength = getElementArrayLength(fileUploaderInput);
+        for (let i = 0; i < arrLength; i++) {
+            scrollIntoView(fileUploaderInput, i);
+            expect(getAttributeByName(fileUploaderInput, 'placeholder', i)).toBe(placeholderTestTextArr[i]);
+        }
+    });
+
+    it('verify browser button', () => {
+        const arrLength = getElementArrayLength(browseButton);
+        for (let i = 0; i < arrLength; i++) {
+            scrollIntoView(browseButton, i);
+            expect(isElementClickable(browseButton, i)).toBe(true, `browse button with index ${i} not clickable`);
+        }
+    });
+
+    // skipped due to issue with file uploader - browser is stuck after uploading file
+    xit('verify file upload', () => {
+        const arrLength = getElementArrayLength(browseButton);
+        for (let i = 0; i < arrLength; i++) {
+            uploadFile(fileUploaderInputFile, imagePath, i);
+            expect(imagePath).toContain(getText(fileSelectedText, i));
+            expect(imagePath).toContain(getAttributeByName(fileUploaderInput, 'title', i).slice(1));
+        }
+    });
+
+    // skipped due to issue with file uploader - browser is stuck after uploading file
+    xit('verify file uploaded message', () => {
+        const arrLength = getElementArrayLength(browseButton);
+        for (let i = 0; i < arrLength; i++) {
+            uploadFile(fileUploaderInputFile, imagePath, i);
+            expect(titleValue).toContain(getAlertText());
+        }
+    });
+
+
+    it('should check RTL and LTR orientation', () => {
+        fileUploaderPage.checkRtlSwitch();
+    });
+
+    describe('Should check visual regression', function() {
+
+        it('should check visual regression for all examples', () => {
+            fileUploaderPage.saveExampleBaselineScreenshot();
+            expect(fileUploaderPage.compareWithBaseline()).toBeLessThan(5);
+        });
+    });
+});
