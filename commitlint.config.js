3845fef7791d906e8eb354eca8613e909991de0d
Yury Betrozov <betrozov91@gmail.com>
2021-05-27 12:34:59 +0300

    chore: update changelog

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,12 @@
 const Configuration = {
     extends: ['@commitlint/config-conventional'],
-    parserPreset: './parser-preset',
     rules: {
-        'type-enum': [
-            2,
-            'always',
-            ['build', 'ci', 'docs', 'feat', 'fix', 'style', 'refactor', 'test', 'revert', 'chore']
-        ],
-        'scope-enum': [2, 'always', ['Core', 'Platform', 'doc-app', 'release', 'e2e', 'deps', 'deps-dev']],
+        // 'type-enum': [
+        //     2,
+        //     'always',
+        //     ['build', 'ci', 'docs', 'feat', 'fix', 'style', 'refactor', 'test', 'revert', 'chore']
+        // ],
+        'scope-enum': [2, 'always', ['core', 'platform', 'docs', 'release', 'e2e', 'deps', 'deps-dev']],
         'body-max-line-length': [2, 'always', 200],
         'footer-max-line-length': [2, 'always', 200]
     }
