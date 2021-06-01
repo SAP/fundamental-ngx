64eeda52e05e0dbc82241f223cec29af858a541a
Dmitriy Shekhovtsov <valorkin@gmail.com>
2021-06-01 17:19:35 +0300

    chore(ci): check config transfer

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 const Configuration = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'scope-enum': [2, 'always', ['core', 'platform', 'docs', 'e2e', 'release', 'deps', 'deps-dev', 'changelog']],
+        'scope-enum': [2, 'always', ['core', 'platform', 'docs', 'e2e', 'release', 'deps', 'deps-dev', 'changelog', 'ci']],
         'body-max-line-length': [2, 'always', 200],
         'footer-max-line-length': [2, 'always', 200],
         'header-max-length': [2, 'always', 200]
