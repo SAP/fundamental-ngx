369bff36925d4e91b06b11969bd46c2eb4e5203a
Anton Okhrimenko <anton.okhrimenko@valor-software.com>
2021-06-03 15:36:51 +0300

    fix(core): layout grid examples

---
+++ apps/docs/src/app/core/component-docs/layout-grid/examples/layout-grid-basic-example.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fd-layout-grid-basic-example',
+    templateUrl: './layout-grid-basic-example.component.html'
+})
+export class LayoutGridBasicExampleComponent {}
