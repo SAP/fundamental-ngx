fbb0448dbc3b093fb0ff5551449120a3302d7549
Denis Severin <Denis_severin@icloud.com>
2021-06-07 20:21:27 +0300

    fix(platform): remove unused core and imports

--- libs/platform/src/lib/components/form/form-generator/base-dynamic-form-generator-control.ts
+++ libs/platform/src/lib/components/form/form-generator/base-dynamic-form-generator-control.ts
@@ -1,9 +1,8 @@
-import { Directive, Input, ViewChild } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 import { DynamicFormItem } from './interfaces/dynamic-form-item';
 import { FormGroup } from '@angular/forms';
 import { FormField } from '../form-field';
 import { dynamicFormFieldProvider, dynamicFormGroupChildProvider } from './providers/providers';
-import { BaseInput } from '../base.input';
 
 /**
  * Abstract class that used as a base for the Dynamic Form Generator components.
