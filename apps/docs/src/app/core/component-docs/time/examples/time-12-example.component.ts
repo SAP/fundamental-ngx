6097562711409101a1ae81ade97bfdace588fa2e
Anton Okhrimenko <74180264+web-dev23@users.noreply.github.com>
2021-06-10 12:37:34 +0300

    docs(core): time stackblitz examples (#5555)
    Co-authored-by: Anton Okhrimenko <anton.okhrimenko@valor-software.com>

--- apps/docs/src/app/core/component-docs/time/examples/time-12-example.component.ts
+++ apps/docs/src/app/core/component-docs/time/examples/time-12-example.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { FdDate } from '@fundamental-ngx/core/datetime';
+import { DatetimeAdapter, FdDate, FdDatetimeAdapter } from '@fundamental-ngx/core/datetime';
 
 @Component({
     selector: 'fd-time-12-example',
-    templateUrl: './time-12-example.component.html'
+    templateUrl: './time-12-example.component.html',
+    providers: [
+        {
+            provide: DatetimeAdapter,
+            useClass: FdDatetimeAdapter
+        }
+    ]
 })
 export class Time12ExampleComponent {
     timeMeridian = new FdDate().setTime(9, 0, 0);
