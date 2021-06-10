b8febcf0c6134c0e107a744531cfd6a4a0d1d18b
Anton Okhrimenko <74180264+web-dev23@users.noreply.github.com>
2021-06-10 12:32:54 +0300

    docs(core): fix calendar stackblitz examples (#5390)
    Co-authored-by: Anton Okhrimenko <anton.okhrimenko@valor-software.com>
    Co-authored-by: Mike O'Donnell <mikerodonnell89@users.noreply.github.com>

--- apps/docs/src/app/core/component-docs/calendar/examples/calendar-single-example.component.ts
+++ apps/docs/src/app/core/component-docs/calendar/examples/calendar-single-example.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FdDate } from '@fundamental-ngx/core/datetime';
+import { DatetimeAdapter, DATE_TIME_FORMATS, FdDate, FdDatetimeAdapter, FD_DATETIME_FORMATS } from '@fundamental-ngx/core/datetime';
 
 @Component({
     selector: 'fd-calendar-single-example',
@@ -15,6 +15,16 @@ import { FdDate } from '@fundamental-ngx/core/datetime';
                 margin-top: 1rem;
             }
         `
+    ],
+    providers: [
+        {
+            provide: DatetimeAdapter,
+            useClass: FdDatetimeAdapter
+        },
+        {
+            provide: DATE_TIME_FORMATS,
+            useValue: FD_DATETIME_FORMATS
+        }
     ]
 })
 export class CalendarSingleExampleComponent {
