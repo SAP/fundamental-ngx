4c94a5039b604e5e4b054a3141ba58d037f9974a
Anton Okhrimenko <74180264+web-dev23@users.noreply.github.com>
2021-06-10 12:35:45 +0300

    docs(core): fix formatted text stackblitz examples (#5406)
    Co-authored-by: Anton Okhrimenko <anton.okhrimenko@valor-software.com>
    Co-authored-by: Mike O'Donnell <mikerodonnell89@users.noreply.github.com>

--- apps/docs/src/app/core/component-docs/formatted-text/examples/script/formatted-text-script-example.component.ts
+++ apps/docs/src/app/core/component-docs/formatted-text/examples/script/formatted-text-script-example.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-formatted-text-script-example',
-    templateUrl: './formatted-text-example.component.html'
+    templateUrl: './formatted-text-script-example.component.html'
 })
 export class FormattedTextScriptExampleComponent {
     rawHTMLWithScript = `<script>alert(1);</script>`;
