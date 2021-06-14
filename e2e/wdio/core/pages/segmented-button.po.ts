6336a8b3e2adf2c11fd21aee296911aa4b6c7df1
maxym-valor <maxym.riabchuk@valor-software.com>
2021-06-14 10:17:00 +0300

    test(e2e): add e2e tests for segmented button

---
+++ e2e/wdio/core/pages/segmented-button.po.ts
@@ -0,0 +1,40 @@
+import { CoreBaseComponentPo } from './core-base-component.po';
+import { waitForElDisplayed, waitForPresent } from '../../driver/wdio';
+
+export class SegmentedButtonPo extends CoreBaseComponentPo {
+    url = '/segmentedButton';
+
+   defaultExample = 'fd-segmented-button-default-example ';
+   toggleExample = 'fd-segmented-button-toggle-example ';
+   complexExample = 'fd-segmented-button-complex-example ';
+   formExample = 'fd-segmented-button-form-example ';
+
+   firstDefaulSegment =  this.defaultExample + '.fd-segmented-button:nth-child(2) ';
+   secondDefaulSegment =  this.defaultExample + '.fd-segmented-button:nth-child(4) ';
+   firstFormSegment = this.formExample + 'form div:nth-child(1) fd-segmented-button ';
+   firstFormButtonsSection = 'div:nth-child(1) ';
+   secondFormButtonsSection = 'div:nth-child(2) ';
+   button = '.fd-button';
+
+   toggled = 'fd-button--toggled';
+   chosenValue = 'small';
+
+
+    open(): void {
+        super.open(this.url);
+        waitForElDisplayed(this.root);
+        waitForPresent(this.title);
+    }
+
+    getScreenshotFolder(): object {
+        return super.getScreenshotFolder(this.url);
+    }
+
+    saveExampleBaselineScreenshot(specName: string = 'segmented-button'): void {
+        super.saveExampleBaselineScreenshot(specName, this.getScreenshotFolder());
+    }
+
+    compareWithBaseline(specName: string = 'segmented-button'): any {
+        return super.compareWithBaseline(specName, this.getScreenshotFolder());
+    }
+}
