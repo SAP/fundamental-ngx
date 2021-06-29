6f25b02b5150b072dd7bb86eb02bfe6f0343546f
nikvalor <nikita.lastovych@valor-software.com>
2021-06-29 16:42:00 +0300

    test(e2e): add e2e tests for select component

---
+++ e2e/wdio/platform/fixtures/appData/select-contents.ts
@@ -0,0 +1,8 @@
+export const testTextValue = 'Selected value: PlumsC';
+export const disableSelectModeValueTestText = 'Selected value:watermelonE';
+export const selectWithTwoColumnsTestText = 'Selected Item: Lorem ipsum dolor sit, amet';
+export const titleTestText = 'Title';
+export const mobileExampleTestText = 'Selected: PlumsC';
+export const maxHeightTestText = 'Selected value: "PlumsC"';
+export const testTextValue6 = 'Selected Item: "Apple"';
+export const testTextValue7 = 'Form Selected Item: { "field": "Apple" }';
