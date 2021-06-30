4a3c4643619871a9bcb516b129c39500dd560176
i505485 <manu.k.r@sap.com>
2021-06-30 15:14:51 +0530

    fix(core): revoking changes to have feed list item style import only at the parent level

--- libs/core/src/lib/feed-list-item/components/feed-list-footer/feed-list-footer.component.ts
+++ libs/core/src/lib/feed-list-item/components/feed-list-footer/feed-list-footer.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'fd-feed-list-footer',
-  styleUrls: ['./feed-list-footer.component.scss'],
   template: '<ng-content></ng-content>',
   host: { class: 'fd-feed-list__footer'}
 })
