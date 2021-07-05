c709cf9f6fd6f19b182a31ac95bd53b1ccd79b2a
DeepakSap14 <deepak.kumar14@sap.com>
2021-07-05 13:07:12 +0530

    fix(platform): correct import

--- libs/platform/src/lib/components/form/checkbox-group/checkbox-group.module.ts
+++ libs/platform/src/lib/components/form/checkbox-group/checkbox-group.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { FormGroupModule } from '@fundamental-ngx/core';
+import { FormGroupModule } from '@fundamental-ngx/core/form';
 import { CheckboxGroupComponent } from './checkbox-group.component';
 import { PlatformCheckboxModule } from '../checkbox/checkbox.module';
 
