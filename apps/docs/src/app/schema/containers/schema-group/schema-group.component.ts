51a4313e8593f77534b86c8d130c660f518d22bd
Denis Severin <Denis_severin@icloud.com>
2021-07-05 14:54:44 +0300

    fix(core,docs): add resettable flag for playground and reset message strip

--- apps/docs/src/app/schema/containers/schema-group/schema-group.component.ts
+++ apps/docs/src/app/schema/containers/schema-group/schema-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { Property } from '../../models/schema.model';
 
@@ -11,6 +11,11 @@ export class SchemaGroupComponent implements OnInit {
     @Input() schemaGroup: FormGroup;
     @Input() properties: Property;
 
+    /**
+     * Is current playground can be resetted.
+     */
+    @Input() resettable = false;
+
     controls: Array<{
         key: string;
         control: AbstractControl;
@@ -18,9 +23,14 @@ export class SchemaGroupComponent implements OnInit {
         enum: [any];
     }> = [];
 
+    /**
+     * Emits when playground needs to be resetted.
+     */
+    @Output() onReset: EventEmitter<void> = new EventEmitter<void>();
+
     ngOnInit(): void {
         const controls = this.schemaGroup.controls;
-        
+
         for (const key in controls) {
             if (controls.hasOwnProperty(key)) {
                 this.controls.push({
@@ -36,5 +46,12 @@ export class SchemaGroupComponent implements OnInit {
     /** @hidden */
     _isFormControl(form: FormControl | FormGroup): boolean {
         return form instanceof FormControl;
+    }
+
+    /**
+     * Emits reset event.
+     */
+    reset(): void {
+        this.onReset.emit()
     }
 }
--- apps/docs/src/app/schema/containers/schema/schema.component.ts
+++ apps/docs/src/app/schema/containers/schema/schema.component.ts
@@ -1,30 +1,91 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Properties, Schema } from '../../models/schema.model';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
     selector: 'schema',
     templateUrl: 'schema.component.html',
     styleUrls: ['schema.component.scss']
 })
-export class SchemaComponent implements OnInit {
+export class SchemaComponent implements OnInit, OnChanges, OnDestroy {
     @Input() schema: Schema;
 
     @Input() initialValues: any;
 
+    /**
+     * Is current playground can be resetted.
+     */
+    @Input() resettable = false;
+
     @Output() onSchemaValues: EventEmitter<any> = new EventEmitter<any>();
+
+    /**
+     * Emits when playground needs to be resetted.
+     */
+    @Output() onReset: EventEmitter<void> = new EventEmitter<void>();
 
     schemaGroup: FormGroup;
 
+    /**
+     * @hidden
+     */
+    private _resetted = false;
+
+    /**
+     * @hidden
+     */
+    private _allowSubscribe = true;
+
+    /**
+     * @hidden
+     */
+    ngOnDestroy(): void {
+        this._allowSubscribe = false;
+    }
+
+    /**
+     * @hidden
+     */
     ngOnInit(): void {
+        this._constructSchemaGroup();
+    }
+
+    /**
+     * Resets the form and emits event.
+     */
+    reset(): void {
+        this._resetted = true;
+        this.onReset.emit();
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+
+        if (this._resetted && changes.initialValues) {
+            this.initialValues = changes.initialValues.currentValue;
+            this._resetted = false;
+            this.schemaGroup.patchValue(this.initialValues);
+        }
+    }
+
+    /**
+     * @hidden
+     */
+    private _constructSchemaGroup(): void {
         this.schemaGroup = this._constructProperties(this.schema.properties);
+
         this.schemaGroup.patchValue(this.initialValues);
 
-        this.schemaGroup.valueChanges.subscribe((values) => {
+        this.schemaGroup.valueChanges
+        .pipe(takeWhile(() => this._allowSubscribe)).subscribe((values) => {
             this.onSchemaValues.emit(values);
         });
     }
 
+    /**
+     * @hidden
+     */
     private _constructProperties(properties: Properties): FormGroup {
         const formGroup = {};
         for (const property in properties) {
