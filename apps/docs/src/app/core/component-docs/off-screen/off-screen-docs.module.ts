cf58d18f7cb7bcf1de793b0a1cb9642c0721d749
DeepakSap14 <deepak.kumar14@sap.com>
2021-07-07 15:11:00 +0530

    docs(core): correct order for imports [ci skip]

--- apps/docs/src/app/core/component-docs/off-screen/off-screen-docs.module.ts
+++ apps/docs/src/app/core/component-docs/off-screen/off-screen-docs.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SharedDocumentationPageModule } from '../../../documentation/shared-documentation-page.module';
-import { ApiComponent } from '../../../documentation/core-helpers/api/api.component';
-import { API_FILES } from '../../api-files';
-import { OffScreenHeaderComponent } from './off-screen-header/off-screen-header.component';
-import { OffScreenDocsComponent } from './off-screen-docs.component';
-import { OffScreenExampleComponent } from './examples/off-screen-example.component';
 
 import { ButtonModule } from '@fundamental-ngx/core/button';
 import { ComboboxModule } from '@fundamental-ngx/core/combobox';
 import { OffScreenElementModule } from '@fundamental-ngx/core/utils';
+
+import { SharedDocumentationPageModule } from '../../../documentation/shared-documentation-page.module';
+import { ApiComponent } from '../../../documentation/core-helpers/api/api.component';
+import { API_FILES } from '../../api-files';
+
+import { OffScreenHeaderComponent } from './off-screen-header/off-screen-header.component';
+import { OffScreenDocsComponent } from './off-screen-docs.component';
+import { OffScreenExampleComponent } from './examples/off-screen-example.component';
 
 const routes: Routes = [
     {
