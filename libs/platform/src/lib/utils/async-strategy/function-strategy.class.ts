596fa27aec3d9abffabe164b4f685b64a629671d
Denis Severin <Denis_severin@icloud.com>
2021-07-07 11:53:33 +0300

    feat(platform): correct interface and return value for async strategies

--- libs/platform/src/lib/utils/async-strategy/function-strategy.class.ts
+++ libs/platform/src/lib/utils/async-strategy/function-strategy.class.ts
@@ -5,8 +5,8 @@ import { SubscriptionStrategy } from './subscription-strategy.interface';
  * @description Executes function and passes returned value into callback function.
  */
 export class FunctionStrategy implements SubscriptionStrategy {
-    createSubscription(fn: Function, updateLatestValue: (v: any) => any): void {
+    createSubscription(fn: Function, updateLatestValue: (v: any) => any): Promise<void> {
         const result = isFunction(fn) ? fn() : fn;
-        updateLatestValue(result);
+        return Promise.resolve(result).then(updateLatestValue);
     }
 }
--- libs/platform/src/lib/utils/async-strategy/value-strategy.class.ts
+++ libs/platform/src/lib/utils/async-strategy/value-strategy.class.ts
@@ -4,7 +4,7 @@ import { SubscriptionStrategy } from './subscription-strategy.interface';
  * @description Passes value object into callback function.
  */
 export class ValueStrategy implements SubscriptionStrategy {
-    createSubscription(value: any, updateLatestValue: (v: any) => any): void {
-        updateLatestValue(value);
+    createSubscription(value: any, updateLatestValue: (v: any) => any): Promise<void> {
+        return Promise.resolve(value).then(updateLatestValue);
     }
 }
