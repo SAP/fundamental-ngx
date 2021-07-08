82bca36db3ac01933c6409c30b9b658054432ccc
kavya-b <kavya.bhat@sap.com>
2021-07-08 13:08:32 +0530

    fix(platform): add missing exports for classes in Platform library (#5882)

--- libs/platform/src/lib/components/form/multi-input/public_api.ts
+++ libs/platform/src/lib/components/form/multi-input/public_api.ts
@@ -1,4 +1,4 @@
-export { MultiInputSelectionChangeEvent } from './base-multi-input';
+export { BaseMultiInput, MultiInputSelectionChangeEvent } from './base-multi-input';
 export { PlatformMultiInputComponent } from './multi-input.component';
 export { PlatformMultiInputModule } from './multi-input.module';
 export { PlatformMultiInputMobileComponent } from './multi-input-mobile/multi-input-mobile.component';
