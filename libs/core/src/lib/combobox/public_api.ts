1a208794971c23f8eda250c8d306819c062616d9
Denis Severin <Denis_severin@icloud.com>
2021-07-13 16:39:05 +0300

    fix(core): add autocomplete for multi-input

--- libs/core/src/lib/combobox/public_api.ts
+++ libs/core/src/lib/combobox/public_api.ts
@@ -3,5 +3,4 @@ export * from './combobox.component';
 export * from './combobox-item';
 export * from './combobox-mobile/combobox-mobile.component';
 export * from './combobox-mobile/combobox-mobile.module';
-export * from './auto-complete.directive';
 export * from './combobox.interface';
