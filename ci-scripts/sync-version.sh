be01802b9e3fb96d6d3ea07e87a482a52ea5fc0a
Denis Severin <Denis_severin@icloud.com>
2021-07-15 11:46:55 +0300

    fix(core): remove hammerjs version placeholder

--- ci-scripts/sync-version.sh
+++ ci-scripts/sync-version.sh
@@ -14,7 +14,6 @@ RXJS_VERSION=$(node -p "require('./package.json').dependencies['rxjs']")
 CDK_VERSION=$(node -p "require('./package.json').dependencies['@angular/cdk']")
 FDSTYLES_VERSION=$(node -p "require('./package.json').dependencies['fundamental-styles']")
 FOCUSTRAP_VERSION=$(node -p "require('./package.json').dependencies['focus-trap']")
-HAMMERJS_VERSION=$(node -p "require('./package.json').dependencies['hammerjs']")
 THEMING_VERSION=$(node -p "require('./package.json').dependencies['@sap-theming/theming-base-content']")
 
 cd ./dist
@@ -25,7 +24,5 @@ grep -rl 'RXJS_VER_PLACEHOLDER' . | xargs  perl -X -p -i -e "s/RXJS_VER_PLACEHOL
 grep -rl 'CDK_VER_PLACEHOLDER' . | xargs  perl -X -p -i -e "s/CDK_VER_PLACEHOLDER/${CDK_VERSION}/g"
 grep -rl 'FDSTYLES_VER_PLACEHOLDER' . | xargs  perl -X -p -i -e "s/FDSTYLES_VER_PLACEHOLDER/${FDSTYLES_VERSION}/g"
 grep -rl 'FOCUSTRAP_VER_PLACEHOLDER' . | xargs  perl -X -p -i -e "s/FOCUSTRAP_VER_PLACEHOLDER/${FOCUSTRAP_VERSION}/g"
-grep -rl 'THEMING_VER_PLACEHOLDER' . | xargs  perl -X -p -i -e "s/THEMING_VER_PLACEHOLDER/${THEMING_VERSION}/g"
-grep -rl 'HAMMERJS_VER_PLACEHOLDER' . | xargs  perl -X -p -i -e "s/HAMMERJS_VER_PLACEHOLDER/${HAMMERJS_VERSION}/g"
 
 cd ../
