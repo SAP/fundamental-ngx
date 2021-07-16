7ccd9f540ace70561556c507756501c131a495ed
Sean Moore <47522152+Seamoo13@users.noreply.github.com>
2021-07-16 12:23:45 +0300

    test(e2e): add e2e tests for message-strip component (#5975)
    * test(e2e): add e2e tests for message-strip component
    
    * test(e2e): add pictures for visual regression for message-strip core
    
    * test(e2e): fix formatting, spelling, file names
    
    Co-authored-by: maxym-valor <maxym.riabchuk@valor-software.com>

---
+++ e2e/wdio/core/fixtures/appData/message-strip.contents.ts
@@ -0,0 +1,20 @@
+export const sections = [
+    'fd-message-strip-example ',
+    'fd-message-strip-noicon-example ',
+    'fd-message-strip-width-example '
+];
+
+export const messageTypes = [
+    'warning',
+    'success',
+    'information',
+    'error'
+];
+
+export const playgroundStates = [
+    'default',
+    'warning',
+    'error',
+    'success',
+    'information'
+];
