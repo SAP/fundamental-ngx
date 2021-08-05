1fdd4ab2450e8880d5955ed6e61772e66bc922da
I505460 <lokanathan.k@sap.com>
2021-08-05 14:08:16 +0530

    fix(core): fixing the accessability test cases
    fix(core): fixing the accessability test cases
    
    fix(core): adding the aria model tag
    
    fix(core): adding the aria model tag
    
    fix(core): adding the aria model tag
    
    fix(core): adding the aria model tag
    
    fix(core): fixing the accessability test cases
    
    fix(core): fixing the accessability test cases
    
    fix(core): fixing the accessability test cases for the default dialog component
    
    fix(core): fixing the accessability test cases for the default dialog component
    
    fix(core): fixing the accessability test case Object dialog component
    
    fix(core): fixing the accessability test case Object dialog component
    
    fix(core): adding focus to the dialog window
    
    fix(core): adding focus to the dialog window
    
    fix(core): adding role to the dialog body at host
    
    fix(core): adding role to the dialog body at host
    
    fix(core): adding formatting text
    
    fix(core): adding formatting text
    
    fix(core): adding tool tip to the close button
    
    fix(core): adding tool tip to the close button
    
    fix(core): adding role to the dialog component at host
    
    fix(core): adding role to the dialog component at host
    
    fix(core): adding role to the dialog component
    
    fix(core): adding role to the dialog component
    
    fix(core): adding role to the dialog component
    
    fix(core): adding role to the dialog component
    
    Revert "fix(core): adding role to the dialog component"
    
    This reverts commit 78066eaf3646163b16574cf8fe155dc400b1856f.
    
    fix(core): addidng focus to the dialog box
    
    fix(core): addidng focus to the dialog box
    
    fix(core): addidng role as docuement
    
    fix(core): addidng role as docuement
    
    fix(core): removing the document on the li element
    
    fix(core): removing the document on the li element
    
    fix(core): fixing the aria label to read button type
    
    fix(core): fixing the aria label to read button type
    
    fix(core): adding i18n tag to the X button
    
    fix(core): adding i18n tag to the X button
    
    fix(core): fixing the aria label to read button type
    
    fix(core): fixing the aria label to read button type
    
    fix(core): seperating title form aria label
    
    fix(core): seperating title form aria label
    
    fix(core): reverting the button type reading the fdtype
    
    fix(core): reverting the button type reading the fdtype
    
    fix(core): adding focus trap to the code
    
    fix(core): adding focus trap to the code
    
    fix(core): removing unused imports and variables
    
    fix(core): removing unused imports and variables
    
    fix(core): skipping the regression test case
    
    fix(core): skipping the regression test case
    
    fix(core): removing the default focus on the dialog
    
    fix(core): removing the default focus on the dialog
    
    test(e2e): skip visual regression case

--- libs/core/src/lib/dialog/base/dialog-content-base.class.ts
+++ libs/core/src/lib/dialog/base/dialog-content-base.class.ts
@@ -1,4 +1,6 @@
 export class DialogContentBase {
+    /** Dialog Title id */
+    titleId?: string;
 
     /** Dialog Title */
     title?: string;
@@ -8,6 +10,18 @@ export class DialogContentBase {
 
     /** Approve Button Label */
     approveButton?: string;
+
+    /** Aria Modal for the dialog component element */
+    ariaModal?: boolean;
+
+    /** Aria label for the dialog component element. */
+    ariaLabel?: string = null;
+
+    /** Aria labelby for the dialog component element. */
+    ariaLabelledBy?: string = null;
+
+    /** Aria DescribedBy for the dialog component element.. */
+    ariaDescribedBy?: string = null;
 
     /** Approve Button Click Callback */
     approveButtonCallback?: Function;
@@ -20,4 +34,13 @@ export class DialogContentBase {
 
     /** Close Button Callback */
     closeButtonCallback?: Function;
+
+    /** close button title */
+    closeButtonTitle?: string;
+
+    /** Aria label for the dialog approve button element. */
+    approveButtonAriaLabel?: string;
+
+    /** Aria labelby for the dialog close button element. */
+    closeButtonAriaLabel?: string;
 }
