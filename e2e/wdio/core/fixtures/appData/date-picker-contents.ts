8407a9b59213d6bbba43831e7403727248155602
Sean Moore <47522152+Seamoo13@users.noreply.github.com>
2021-08-09 11:34:27 +0300

    test(e2e): add e2e tests for date-picker component (#6199)
    Co-authored-by: maxym-valor <maxym.riabchuk@valor-software.com>

---
+++ e2e/wdio/core/fixtures/appData/date-picker-contents.ts
@@ -0,0 +1,13 @@
+export const blockExamples = [
+    'fd-date-picker-single-example ',
+    'fd-date-picker-range-example ',
+    'fd-datepicker-i18n-example ',
+    'fd-date-picker-format-example ',
+    'fd-date-picker-allow-null-example ',
+    'fd-date-picker-form-example ',
+    'fd-date-picker-form-range-example ',
+    'fd-date-picker-disable-func-example ',
+    'fd-date-picker-range-disabled-example ',
+    'fd-date-picker-position-example ',
+    'fd-date-picker-disabled-example '
+];
