fb2f8b0fe09df18de3ac49125b2b80a794b2d834
nikvalor <nikita.lastovych@valor-software.com>
2021-08-27 11:56:07 +0300

    test(e2e): improve cypress tests

--- e2e/cypress/integration/core/tests/cy-toolbar.e2e-spec.ts
+++ e2e/cypress/integration/core/tests/cy-toolbar.e2e-spec.ts
@@ -1,5 +1,10 @@
 import { ToolbarPo } from '../../../support/core/pages/cy-toolbar.po';
-import { headerTestText, testTextArr, testDate, fruitArr } from '../../../support/core/fixtures/cy-toolbar.content';
+import {
+    headerTestText,
+    testTextArr,
+    testDate,
+    fruitArr,
+} from '../../../support/core/fixtures/cy-toolbar.content';
 
 
 describe('Toolbar test suite:', () => {
@@ -7,7 +12,7 @@ describe('Toolbar test suite:', () => {
     const {
         toolbarTitleHeader, toolbarTypeExampleLabel, overFlowButton, moreButton, okButton, inputField, dropdownMenu, dropdownOption,
         inputFieldText, activeInfoToolbar, checkbox, checkboxValue, overflowPriorityButton, moreBtn, overflowPriorityExample,
-        overflowBody, alwaysButton, overflowGroupingButton, overflowGroupingExample
+        overflowBody, alwaysButton, overflowGroupingButton, overflowGroupingExample,
     } = toolbarPage;
 
     before(() => {
@@ -18,14 +23,22 @@ describe('Toolbar test suite:', () => {
         cy.reload();
     });
 
+    describe('Check orientation', function() {
+
+        it('should check RTL and LTR orientation', () => {
+            toolbarPage.checkRtlSwitch();
+        });
+    });
+
     xit('verify  text in Toolbar Types example', () => {
-        for (let i = 0; i < 5; i++) {
-            toolbarPage.getText(toolbarTypeExampleLabel, testTextArr[i], i);
+        const textLength = toolbarPage.getElementArrayLength(toolbarTypeExampleLabel);
+        for (let i = 0; i < textLength; i++) {
+            toolbarPage.getTexts(toolbarTypeExampleLabel, testTextArr[i], i);
         }
     });
 
     xit('verify header text in Toolbar Title example', () => {
-        toolbarPage.getText(toolbarTitleHeader, headerTestText);
+        toolbarPage.getTexts(toolbarTitleHeader, headerTestText);
     });
 
     it('verify info active toolbar is clickable', () => {
@@ -35,7 +48,8 @@ describe('Toolbar test suite:', () => {
     describe('Check Toolbar Overflow example', () => {
 
         it('verify is clickable buttons', () => {
-            for (let i = 0; i < 6; i++) {
+            const buttonLength = toolbarPage.getElementArrayLength(overFlowButton);
+            for (let i = 0; i < buttonLength; i++) {
                 if (i === 3) {
                     continue;
                 }
@@ -62,9 +76,10 @@ describe('Toolbar test suite:', () => {
         it('verify dropdown menu', () => {
             cy.get(moreButton).click();
             cy.get(dropdownMenu).click();
-            for (let i = 0; i < 4; i++) {
+            const optionLength = toolbarPage.getElementArrayLength(dropdownOption);
+            for (let i = 0; i < optionLength; i++) {
                 toolbarPage.clickOnElement(dropdownOption, i);
-                toolbarPage.getText(inputFieldText, fruitArr[i]);
+                toolbarPage.getTexts(inputFieldText, fruitArr[i]);
                 if (i !== 3) {
                     cy.get(dropdownMenu).click();
                 }
@@ -75,7 +90,8 @@ describe('Toolbar test suite:', () => {
     describe('Check Toolbar Overflow Priority', function() {
 
         it('should check Toolbar Overflow Priority example', () => {
-            for (let i = 0; i < 6; i++) {
+            const buttonLength = toolbarPage.getElementArrayLength(overflowPriorityButton);
+            for (let i = 0; i < buttonLength; i++) {
                 toolbarPage.clickOnElement(overflowPriorityButton, i);
             }
 
@@ -89,7 +105,8 @@ describe('Toolbar test suite:', () => {
     describe('Check Toolbar Overflow Grouping', function() {
 
         it('should check Toolbar Overflow Grouping example', () => {
-            for (let i = 0; i < 7; i++) {
+            const buttonLength = toolbarPage.getElementArrayLength(overflowGroupingButton);
+            for (let i = 0; i < buttonLength; i++) {
                 toolbarPage.clickOnElement(overflowGroupingButton, i);
             }
 
--- e2e/cypress/support/core/pages/cy-core-base-component.po.ts
+++ e2e/cypress/support/core/pages/cy-core-base-component.po.ts
@@ -9,7 +9,7 @@ export class BaseComponent {
         cy.visit(this.coreUrl + url);
     }
 
-    getText(selector: string, expectedValue: string, index?: number): void {
+    getTexts(selector: string, expectedValue: string, index?: number): void {
         cy.get(selector).eq(index ? index : 0).contains(expectedValue);
     }
 
@@ -17,12 +17,28 @@ export class BaseComponent {
         cy.get(selector).eq(index ? index : 0).click();
     }
 
-    checkRTLOrientation(element: string, index: number): void {
-        cy.get(element).eq(index).invoke('attr', 'dir').should('contain', 'rtl');
+    checkRTLOrientation(element: string, index?: number): void {
+        cy.get(element).eq(index ? index : 0).invoke('attr', 'dir').should('contain', 'rtl');
+        cy.get(element).eq(index ? index : 0).invoke('css', 'direction').should('contain', 'rtl');
     }
 
-    checkLTROrientation(element: string): void {
-        cy.get(element).invoke('attr', 'dir').should('contain', 'ltr');
+    checkLTROrientation(element: string, index?: number): void {
+        cy.get(element).eq(index ? index : 0).invoke('attr', 'dir').should('contain', 'ltr');
+        cy.get(element).eq(index ? index : 0).invoke('css', 'direction').should('contain', 'ltr');
+    }
+
+     getElementArrayLength(selector: string): number {
+        return Cypress.$(selector).length;
+    };
+
+    checkRtlSwitch(switchers: string = this.rtlSwitcherArr, areas: string = this.exampleAreaContainersArr, index?: number): void {
+        const areasLength = this.getElementArrayLength(areas);
+        for (let i = 0; i < areasLength; i++) {
+            this.clickOnElement(switchers, i);
+            this.checkRTLOrientation(areas, i);
+            this.clickOnElement(switchers, i);
+            this.checkLTROrientation(areas, i);
+        }
     }
 }
 
