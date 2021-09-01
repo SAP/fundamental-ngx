3ab51ef9db568d4898b9867d1c57a75593bb5ebd
smiranin <suprunandruha@gmail.com>
2021-09-01 15:03:28 +0300

    continue positionating

--- libs/core/src/lib/timeline/services/position-strategies/vertical-double-sides-strategy.ts
+++ libs/core/src/lib/timeline/services/position-strategies/vertical-double-sides-strategy.ts
@@ -4,25 +4,53 @@ import { TimelineNodeComponent } from '../../components/timeline-node/timeline-n
 
 export class VerticalDoubleSidesStrategy extends BaseStrategy {
 
+    previousLeftElTop = 0;
+    previousLeftElBottom = 0;
+    previousRightElBottom = 0;
+
+    arrowOffset: number;
+
+    constructor() {
+        super();
+        this.arrowOffset = this._convertRemToPixels();
+    }
+
     getCoords(): TimelineNodePosition {
         return undefined;
     }
 
     calculatePosition(nodes: TimelineNodeComponent[]): void {
         const lineWidth = 24;
-        const parentWidth = nodes[0].el.nativeElement.parentElement.offsetWidth;
-        let previousHeight;
+        const parentWidth = nodes[0].el.nativeElement.parentElement.clientWidth;
+        const nodeWidth = (parentWidth / 2);
         nodes.forEach((node, index) => {
             const el = node.el.nativeElement;
-            el.style.width = (parentWidth / 2 - lineWidth / 2) + 'px';
+            el.style.width = nodeWidth + 'px';
             if (index % 2 === 0) {
-                previousHeight = el.offsetTop + el.offsetHeight;
+                this.previousLeftElTop = el.offsetTop;
+                this.previousLeftElBottom = el.offsetTop + el.offsetHeight;
+                el.classList.add('fd-timeline__node-wrapper--left');
                 node.setClasses(['fd-timeline__post--right']);
             } else {
                 el.style.position = 'absolute';
-                el.style.top = `${previousHeight}px`;
+                el.style.left = (parentWidth / 2 - lineWidth) + 'px';
                 node.setClasses(['fd-timeline__post--left']);
+                el.style.top = `${this._initTopPosition()}px`;
+                this.previousRightElBottom = el.offsetTop + el.offsetHeight;
             }
         });
+    }
+
+    private _initTopPosition(): number {
+        const arrowPosition = this.previousLeftElTop + this.arrowOffset;
+        if (arrowPosition > this.previousRightElBottom) {
+            return arrowPosition;
+        } else {
+            return this.previousRightElBottom;
+        }
+    }
+
+    private _convertRemToPixels(rem: number = 1): number {
+        return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
     }
 }
