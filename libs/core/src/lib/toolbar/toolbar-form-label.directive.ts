b029db6a3e4b2326932c69daae6304f8dec14373
Yury Betrozov <betrozov91@gmail.com>
2021-09-01 19:11:19 +0300

    Revert "fix(core): remove hardcoded style from ngx for toolbar component (#6396)"
    This reverts commit 68ae34adc2407cffb4b569d602fcba8a781d80fc.

--- libs/core/src/lib/toolbar/toolbar-form-label.directive.ts
+++ libs/core/src/lib/toolbar/toolbar-form-label.directive.ts
@@ -1,11 +1,11 @@
-import { Directive } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Directive } from '@angular/core';
 import { ToolbarItemDirective } from './toolbar-item.directive';
 
 @Directive({
     // tslint:disable-next-line: directive-selector
     selector: '[fd-toolbar-form-label]',
     host: {
-        class: 'fd-form-label fd-toolbar__overflow-form-label fd-toolbar__overflow-form-label--text'
+        class: 'fd-form-label fd-toolbar__overflow__form-label'
     }
 })
-export class ToolbarFormLabelDirective extends ToolbarItemDirective { }
+export class ToolbarFormLabelDirective extends ToolbarItemDirective {}
--- libs/core/src/lib/toolbar/toolbar-label.directive.ts
+++ libs/core/src/lib/toolbar/toolbar-label.directive.ts
@@ -1,11 +1,11 @@
-import {  Directive } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Directive } from '@angular/core';
 import { ToolbarItemDirective } from './toolbar-item.directive';
 
 @Directive({
     // tslint:disable-next-line: directive-selector
     selector: '[fd-toolbar-label]',
     host: {
-        class: 'fd-label fd-toolbar__overflow-label'
+        class: 'fd-label fd-toolbar__overflow__label'
     }
 })
 export class ToolbarLabelDirective extends ToolbarItemDirective {}
