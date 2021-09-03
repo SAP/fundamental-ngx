81562dbee7c6a51fb39663d32dd3d7f1457a8c08
smiranin <suprunandruha@gmail.com>
2021-09-03 12:08:14 +0300

    fix(platform): rename overflow item selector

--- libs/core/src/lib/utils/directives/overflow-list/overflow-list-item.directive.ts
+++ libs/core/src/lib/utils/directives/overflow-list/overflow-list-item.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[fdOverflowListItem], [fd-overflowList-item]'
+  selector: '[fdOverflowListItem], [fd-overflow-list-item]'
 })
 export class OverflowListItemDirective {
 
