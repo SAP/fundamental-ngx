9698e4362c7188c01b68e4fd732ca48ac8d51b25
Deepak Kumar <57661487+DeepakSap14@users.noreply.github.com>
2021-09-08 16:48:15 +0530

    fix(core): remove grid list component css from Ngx (#6425)

--- libs/core/src/lib/grid-list/components/grid-list-filter-bar/grid-list-filter-bar.component.ts
+++ libs/core/src/lib/grid-list/components/grid-list-filter-bar/grid-list-filter-bar.component.ts
@@ -3,7 +3,6 @@ import { Component, Output, EventEmitter, ChangeDetectionStrategy } from '@angul
 @Component({
     selector: 'fd-grid-list-filter-bar',
     templateUrl: './grid-list-filter-bar.component.html',
-    styleUrls: ['./grid-list-filter-bar.component.scss'],
     host: {
         class: 'fd-col fd-col--12'
     },
