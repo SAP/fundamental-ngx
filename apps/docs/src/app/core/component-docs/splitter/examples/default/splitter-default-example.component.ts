450e2c3f967ab0856cbf7abe37478baf76a77b31
Platon Rov <platon.rov@gmail.com>
2021-09-09 21:47:50 +0400

    fix(core,docs): splitter documentation, css fixes

--- apps/docs/src/app/core/component-docs/splitter/examples/default/splitter-default-example.component.ts
+++ apps/docs/src/app/core/component-docs/splitter/examples/default/splitter-default-example.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { SplitterPaneResizeEvent } from '@fundamental-ngx/core/splitter';
 
 @Component({
-    selector: 'fd-slider-basic-example',
+    selector: 'fd-slider-default-example',
     templateUrl: './splitter-default-example.component.html'
 })
 export class SplitterDefaultExampleComponent {
--- apps/docs/src/app/core/component-docs/splitter/splitter-docs.component.ts
+++ apps/docs/src/app/core/component-docs/splitter/splitter-docs.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 import { ExampleFile } from '../../../documentation/core-helpers/code-example/example-file';
 
-import * as splitterBasicHtml from '!raw-loader!./examples/default/splitter-default-example.component.html';
-import * as splitterBasicTs from '!raw-loader!./examples/default/splitter-default-example.component.ts';
+import * as splitterDefaultHtml from '!raw-loader!./examples/default/splitter-default-example.component.html';
+import * as splitterDefaultTs from '!raw-loader!./examples/default/splitter-default-example.component.ts';
+
 import * as splitterRequiredParentWidthHtml from '!raw-loader!./examples/required-parent-width/splitter-required-parent-width-example.component.html';
 import * as splitterRequiredParentWidthTs from '!raw-loader!./examples/required-parent-width/splitter-required-parent-width-example.component.ts';
+
+import * as splitterApiHtml from '!raw-loader!./examples/api/splitter-api-example.component.html';
+import * as splitterApiTs from '!raw-loader!./examples/api/splitter-api-example.component.ts';
 
 
 @Component({
@@ -13,19 +17,19 @@ import * as splitterRequiredParentWidthTs from '!raw-loader!./examples/required-
     templateUrl: './splitter-docs.component.html'
 })
 export class SplitterDocsComponent {
-    basic: ExampleFile[] = [
+    default: ExampleFile[] = [
         {
             language: 'html',
-            code: splitterBasicHtml,
+            code: splitterDefaultHtml,
             fileName: 'splitter-default-example',
-            name: 'splitter-required-parent-width-example.component.html'
+            name: 'splitter-default-example.component.html'
         },
         {
             language: 'typescript',
-            code: splitterBasicTs,
+            code: splitterDefaultTs,
             fileName: 'splitter-default-example',
-            component: 'SplitterBasicExampleComponent',
-            name: 'splitter-required-parent-width-example.component.ts'
+            component: 'SplitterDefaultExampleComponent',
+            name: 'splitter-default-example.component.ts'
         }
     ];
 
@@ -42,6 +46,22 @@ export class SplitterDocsComponent {
             fileName: 'splitter-required-parent-width-example',
             component: 'SplitterRequiredParentWidthExampleComponent',
             name: 'splitter-required-parent-width-example.component.ts'
+        }
+    ];
+
+    splitterApi: ExampleFile[] = [
+        {
+            language: 'html',
+            code: splitterApiHtml,
+            fileName: 'splitter-api-example',
+            name: 'splitter-api-example.component.html'
+        },
+        {
+            language: 'typescript',
+            code: splitterApiTs,
+            fileName: 'splitter-api-example',
+            name: 'splitter-api-example.component.ts',
+            component: 'SplitterApiExampleComponent'
         }
     ];
 }
