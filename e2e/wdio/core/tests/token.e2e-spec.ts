76db478b4a76a6dba24538026c004e017b3c907f
maxym-valor <maxym.riabchuk@valor-software.com>
2021-09-14 15:55:02 +0300

    test(e2e): add e2e tests for token

---
+++ e2e/wdio/core/tests/token.e2e-spec.ts
@@ -0,0 +1,156 @@
+import { TokenPo } from '../pages/token.po';
+import {
+    click,
+    getText,
+    refreshPage,
+    doesItExist,
+    getElementClass,
+    scrollIntoView,
+    getElementArrayLength, 
+    setValue, 
+    sendKeys, 
+    waitForElDisplayed
+} from '../../driver/wdio';
+import { checkElArrIsClickable } from '../../helper/assertion-helper';
+
+describe('Token component test', function () {
+    const tokenPage = new TokenPo();
+    const {
+        defaultExample, 
+        tokenizerExample, 
+        selectedExample, 
+        readOnlyExample, 
+        compactExample, 
+        token, 
+        closeBtn, 
+        input,
+        compactTokenizer
+    } = tokenPage;
+
+    beforeAll(() => {
+        tokenPage.open();
+    }, 1);
+
+    beforeEach(() => {
+        refreshPage();
+        waitForElDisplayed(tokenPage.title);
+    }, 2);
+
+    describe('default example', function () {
+        it('should check tokens clickable in default example', () => {
+            checkElArrIsClickable(defaultExample + token);
+        });
+    });
+
+    describe('compact example', () => {
+        it('should check tokens clickable in compact example', () => {
+            checkElArrIsClickable(compactExample + token);
+        });
+
+        it('should check size of tokens in compact example', () => {
+            const tokensLength = getElementArrayLength(compactExample + token);
+            for (let i = 0; i < tokensLength; i++) {
+                expect(getElementClass(compactExample + token, i)).toContain('compact');
+            }
+        });
+    });
+
+    describe('selected example', () => {
+        it('should check tokens clickable in selected example', () => {
+            checkElArrIsClickable(selectedExample + token);
+        });
+
+        it('should check selected example', () => {
+            const tokensLength = getElementArrayLength(selectedExample + token);
+            for (let i = 0; i < tokensLength; i++) {
+                expect(getElementClass(selectedExample + token, i)).toContain('selected');
+            }
+        });
+    });
+
+    describe('read only example', () => {
+        it('should check tokens clickable in read only example', () => {
+            checkElArrIsClickable(readOnlyExample + token);
+        });
+
+        it('should check that impossible to select token in read only example', () => {
+            const tokensLength = getElementArrayLength(readOnlyExample + token);
+            for (let i = 0; i < tokensLength; i++) {
+                click(readOnlyExample + token, i);
+                expect(getElementClass(readOnlyExample + token, i)).not.toContain('selected');
+            }
+        });
+
+        it('should check that no ability to remove token in read only example', () => {
+            expect(doesItExist(readOnlyExample + closeBtn)).toBe(false);
+        });
+    });
+
+    describe('tokenizer example', () => {
+        it('should check tokens clickable in tokenizer example', () => {
+            checkElArrIsClickable(tokenizerExample + token);
+        });
+
+        it('should check closing tokens in tokenizer example', () => {
+            checkClosingTokens(tokenizerExample);
+        });
+
+        it('should check selecting tokens in tokenizer example', () => {
+            checkSelectingTokens(tokenizerExample);
+        });
+
+        it('should check adding tokens to tokenizer example', () => {
+            checkAddingTokens(tokenizerExample);
+        });
+    });
+
+    describe('compact tokenizer example', () => {
+        it('should check tokens clickable in compact tokenizer example', () => {
+            checkElArrIsClickable(compactTokenizer + token);
+        });
+
+        it('should check closing tokens in compact tokenizer example', () => {
+            checkClosingTokens(compactTokenizer);
+        });
+
+        it('should check selecting tokens in tokenizer example', () => {
+            checkSelectingTokens(compactTokenizer);
+        });
+
+        it('should check adding tokens to compact tokenizer example', () => {
+            checkAddingTokens(compactTokenizer);
+        });
+    });
+
+    it('should check orientations', () => {
+        tokenPage.checkRtlSwitch();
+    });
+
+    function checkAddingTokens(section: string): void {
+        const tokensLengthBefore = getElementArrayLength(section + token);
+        click(section + input);
+        setValue(section + input, 'asd');
+        sendKeys('Enter');
+        const tokensLengthAfter = getElementArrayLength(section + token);
+        expect(tokensLengthAfter).toEqual(tokensLengthBefore + 1);
+        expect(getText(section + token, tokensLengthAfter - 1)).toEqual('asd');
+    }
+
+    function checkSelectingTokens(section: string): void {
+        const tokensLength = getElementArrayLength(section + token) - 1;
+        for (let i = tokensLength; i !== -1; i--) {
+            scrollIntoView(section + token, i);
+            click(section + token, i);
+            expect(getElementClass(section + token, i)).toContain('selected');
+        }
+    }
+
+    function checkClosingTokens(section: string): void {
+        const tokensLength = getElementArrayLength(section + closeBtn) - 1;
+        for (let i = tokensLength; i !== -1; i--) {
+            scrollIntoView(section + closeBtn, i);
+            click(section + closeBtn, i);
+        }
+        expect(doesItExist(section + token)).toBe(false);
+    }
+});
