4973b620808daac7253eeab9fb338e6da3f4cb51
Sean Moore <47522152+Seamoo13@users.noreply.github.com>
2021-09-20 11:35:41 +0300

    test(e2e): add e2e tests for form-generator (#6574)
    Co-authored-by: maxym-valor <maxym.riabchuk@valor-software.com>

---
+++ e2e/wdio/platform/pages/form-generator.po.ts
@@ -0,0 +1,34 @@
+import { BaseComponentPo } from './base-component.po';
+import { waitForElDisplayed, waitForPresent } from '../../driver/wdio';
+
+export class FormGeneratorPo extends BaseComponentPo {
+    private url = '/form-generator';
+    
+    defaultExample = 'fdp-platform-form-generator-example ';
+    observableExample = 'fdp-platform-form-generator-observable-example ';
+    customExample = 'fdp-platform-form-generator-custom-component-example ';
+    programmaticExample = 'fdp-platform-form-generator-programatic-submit ';
+    errorExample = 'fdp-platform-form-generator-custom-error-example ';
+    fieldLayoutExample = 'fdp-platform-form-generator-field-layout-example ';
+
+    nameInput = '.fd-input[aria-labelledby*="name"]';
+    passwordInput = '.fd-input[aria-labelledby*="password"]';
+    ageInput = '.fd-input[aria-labelledby*="age"]';
+    dateInput = '.fd-input[aria-label*="Date"]';
+    radioButton = '.fd-radio[aria-labelledby*="radio"]';
+    radioButtonLabel = '.fd-radio__label';
+    checkbox = '.fd-checkbox[aria-labelledby*="citizen"]';
+    submitButton = '.fd-button[type="submit"]';
+    calendarInputGroup = '.fd-input-group';
+    mainSpecialitySelect = ' .fd-select__control[aria-labelledby="Main speciality"]';
+    errorMessage = '.fd-form-message--error span';
+    formValue = 'p.ng-star-inserted';
+    sliderPoint = '.fd-slider__handle';
+    validationInput = '.fd-input[aria-labelledby*="validation"]';
+
+    open(): void {
+        super.open(this.url);
+        waitForPresent(this.root);
+        waitForElDisplayed(this.title);
+    }
+}
