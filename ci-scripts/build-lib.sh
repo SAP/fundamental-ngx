43f5f207eb77a3f03c3b6df7c6c11b1abfeca4f6
smiranin <suprunandruha@gmail.com>
2021-09-20 21:06:16 +0300

    fix(core): added build lib script

--- ci-scripts/build-lib.sh
+++ ci-scripts/build-lib.sh
@@ -3,3 +3,4 @@
 
 nx run-many --target=build --all --with-deps --parallel --maxParallel=3 --exclude=docs,core,platform,docs-e2e --configuration=production
 nx run-many --target=build --projects=core,platform --with-deps --parallel --maxParallel=3  --configuration=production
+npm run build:schematics && npm run build:platform-schematics && npm run build:moment-adapter-schematics && npm run sync-versions && cd dist/libs/core/ && npm pack && cd ../platform && npm pack && cd ../moment-adapter && npm pack && cd ../..
