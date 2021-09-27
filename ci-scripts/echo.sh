ed696bc53c2a50ede41b7007aebdc870f876e007
Dmitriy Shekhovtsov <valorkin@gmail.com>
2021-09-27 16:28:21 +0300

    chore: test env vars

--- ci-scripts/echo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo $TRAVIS_BUILD_STAGE_NAME
-echo $TRAVIS_BRANCH
-echo "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG"
-echo ":$TRAVIS_BRANCH"
-echo "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG"
-echo $CURRENT_BRANCH
