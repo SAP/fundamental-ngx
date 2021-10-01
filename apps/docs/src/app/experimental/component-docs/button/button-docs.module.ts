a1084b5edfe3d325ec4eba062e167f7995c840ec
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2021-09-30 19:03:51 -0600

    feat(core): fix docs module imports

--- apps/docs/src/app/experimental/component-docs/button/button-docs.module.ts
+++ apps/docs/src/app/experimental/component-docs/button/button-docs.module.ts
@@ -7,10 +7,6 @@ import { ButtonHeaderComponent } from './button-header/button-header.component';
 import { ButtonDocsComponent } from './button-docs.component';
 import { examples } from './examples';
 import { ExperimentalButtonModule } from '@fundamental-ngx/experimental/button';
-import { ExperimentalCheckboxModule } from '@fundamental-ngx/experimental/checkbox';
-import { ExperimentalRadioModule } from '@fundamental-ngx/experimental/radio';
-import { ExperimentalFormModule } from '@fundamental-ngx/experimental/form';
-import { ExperimentalSwitchModule } from '@fundamental-ngx/experimental/switch';
 
 const routes: Routes = [
     {
@@ -30,11 +26,6 @@ const routes: Routes = [
     imports: [
         RouterModule.forChild(routes),
         SharedDocumentationPageModule,
-        ExperimentalButtonModule,
-        ExperimentalCheckboxModule,
-        ExperimentalRadioModule,
-        ExperimentalFormModule,
-        ExperimentalSwitchModule,
         ExperimentalButtonModule
     ],
     exports: [RouterModule],
