a7b890a8dd722d76976c0ff474b6dc0868d2f00f
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2021-10-01 11:43:19 -0600

    feat(core): docs fixes

--- apps/docs/src/app/experimental/component-docs/button/button-docs.component.ts
+++ apps/docs/src/app/experimental/component-docs/button/button-docs.component.ts
@@ -1,16 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { Schema } from '../../../schema/models/schema.model';
-import { SchemaFactoryService } from '../../../schema/services/schema-factory/schema-factory.service';
 
 import * as buttonrc from '!raw-loader!./examples/button-example/button-example.component.html';
-import * as buttosecondarynrc from '!raw-loader!./examples/button-example/button-secondary-example.component.html';
-import * as buttoflatnrc from '!raw-loader!./examples/button-example/button-flat-example.component.html';
-import * as buttooutlinenrc from '!raw-loader!./examples/button-example/button-outline-example.component.html';
-import * as buttolinknrc from '!raw-loader!./examples/button-example/button-link-example.component.html';
-import * as buttonakednrc from '!raw-loader!./examples/button-example/button-naked-example.component.html';
+import * as buttonsecondaryrc from '!raw-loader!./examples/button-example/button-secondary-example.component.html';
+import * as buttonflatrc from '!raw-loader!./examples/button-example/button-flat-example.component.html';
+import * as buttonoutlinerc from '!raw-loader!./examples/button-example/button-outline-example.component.html';
+import * as buttonlinkrc from '!raw-loader!./examples/button-example/button-link-example.component.html';
+import * as buttonnakedrc from '!raw-loader!./examples/button-example/button-naked-example.component.html';
 
 import { ExampleFile } from '../../../documentation/core-helpers/code-example/example-file';
-import { Icons } from '../../../documentation/utilities/icons';
 
 @Component({
     selector: 'app-button',
@@ -19,112 +16,6 @@ import { Icons } from '../../../documentation/utilities/icons';
     encapsulation: ViewEncapsulation.None
 })
 export class ButtonDocsComponent {
-    static schema: Schema = {
-        properties: {
-            properties: {
-                type: 'object',
-                properties: {
-                    items: {
-                        type: 'object',
-                        properties: {
-                            mode: {
-                                type: 'string',
-                                enum: ['', 'icon-only', 'filter', 'process']
-                            },
-                            compact: {
-                                type: 'boolean'
-                            }
-                        }
-                    },
-                    item1: {
-                        type: 'object',
-                        properties: {
-                            title: {
-                                type: 'string'
-                            },
-                            counter: {
-                                type: 'string'
-                            },
-                            content: {
-                                type: 'string'
-                            },
-                            icon: {
-                                type: 'string',
-                                enum: Icons
-                            }
-                        }
-                    },
-                    item2: {
-                        type: 'object',
-                        properties: {
-                            title2: {
-                                type: 'string'
-                            },
-                            counter2: {
-                                type: 'string'
-                            },
-                            content2: {
-                                type: 'string'
-                            },
-                            icon2: {
-                                type: 'string',
-                                enum: Icons
-                            }
-                        }
-                    },
-                    item3: {
-                        type: 'object',
-                        properties: {
-                            title3: {
-                                type: 'string'
-                            },
-                            counter3: {
-                                type: 'string'
-                            },
-                            content3: {
-                                type: 'string'
-                            },
-                            icon3: {
-                                type: 'string',
-                                enum: Icons
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        type: 'object'
-    };
-
-    schema: Schema;
-
-    data: any = {
-        properties: {
-            items: {
-                mode: '',
-                compact: false
-            },
-            item1: {
-                title: 'Title1',
-                counter: '1',
-                content: 'Content 1',
-                icon: 'menu'
-            },
-            item2: {
-                title2: 'Title2',
-                counter2: '2',
-                content2: 'Content 2',
-                icon2: 'menu'
-            },
-            item3: {
-                title3: 'Title3',
-                counter3: '3',
-                content3: 'Content 3',
-                icon3: 'menu'
-            }
-        }
-    };
-
     buttonExample: ExampleFile[] = [
         {
             language: 'html',
@@ -136,7 +27,7 @@ export class ButtonDocsComponent {
     buttonSecondaryExample: ExampleFile[] = [
         {
             language: 'html',
-            code: buttonrc,
+            code: buttonsecondaryrc,
             fileName: 'button-secondary-example'
         }
     ];
@@ -144,7 +35,7 @@ export class ButtonDocsComponent {
     buttonFlatExample: ExampleFile[] = [
         {
             language: 'html',
-            code: buttonrc,
+            code: buttonflatrc,
             fileName: 'button-flat-example'
         }
     ];
@@ -152,7 +43,7 @@ export class ButtonDocsComponent {
     buttonLinkExample: ExampleFile[] = [
         {
             language: 'html',
-            code: buttonrc,
+            code: buttonlinkrc,
             fileName: 'button-link-example'
         }
     ];
@@ -160,7 +51,7 @@ export class ButtonDocsComponent {
     buttonOutlineExample: ExampleFile[] = [
         {
             language: 'html',
-            code: buttonrc,
+            code: buttonoutlinerc,
             fileName: 'button-outline-example'
         }
     ];
@@ -168,16 +59,8 @@ export class ButtonDocsComponent {
     buttonNakedExample: ExampleFile[] = [
         {
             language: 'html',
-            code: buttonrc,
+            code: buttonnakedrc,
             fileName: 'button-naked-example'
         }
     ];
-
-    constructor(private schemaFactory: SchemaFactoryService) {
-        this.schema = this.schemaFactory.getComponent('button');
-    }
-
-    onSchemaValues(data): void {
-        this.data = data;
-    }
 }
