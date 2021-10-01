12569ae576a8d40d3247e607e4b04cd24e562338
dmitry-stepanenko <stepanenkodmitri@gmail.com>
2021-10-01 12:58:13 +0300

    fix(core): rating indicator keyboard a11y
    ISSUES CLOSED: #6183

--- libs/core/src/lib/rating-indicator/pipes/rating-star-label.pipe.ts
+++ libs/core/src/lib/rating-indicator/pipes/rating-star-label.pipe.ts
@@ -4,8 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RatingStarLabelPipe implements PipeTransform {
     transform(index: number, controlsCount: number, useHalves: boolean): string {
         if (useHalves) {
-           return `${index / 2 + 0.5} of ${controlsCount / 2}` 
+           return `${index / 2 + 0.5} of ${controlsCount / 2}`;
         }
-        return `${index + 1} of ${controlsCount}`
+        return `${index + 1} of ${controlsCount}`;
     }
 }
