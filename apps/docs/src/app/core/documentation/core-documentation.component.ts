b8c6fee24a83f125710b8ed3ec4391896183b761
Platon Rov <platon.rov@gmail.com>
2021-10-05 16:27:35 +0300

    feat(core): splitter component (#6468)

--- apps/docs/src/app/core/documentation/core-documentation.component.ts
+++ apps/docs/src/app/core/documentation/core-documentation.component.ts
@@ -96,6 +96,7 @@ export class CoreDocumentationComponent extends DocumentationBaseComponent {
             { url: 'core/grid-list', name: 'Grid List' },
             { url: 'core/slider', name: 'Slider' },
             { url: 'core/timeline', name: 'Timeline' },
+            { url: 'core/splitter', name: 'Splitter/Responsive Splitter' }
         ];
 
         this.layouts = [
---
+++ libs/core/src/lib/splitter/constants.ts
@@ -0,0 +1,7 @@
+export const PANE_AUTO_SIZE = 'auto';
+
+export const PANE_NONE_SIZE = 'none';
+
+export const RESIZER_SIZE_PX = 16;
+
+export const ROOT_PAGE = 'FD_SPLITTER_ROOT_PAGE';
