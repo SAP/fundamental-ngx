2cb533fac759d408d28f5ee9c0840bbba30d7eee
Evgeny Beregovoy <evgeny.beregovoy@valor-software.com>
2021-10-06 15:15:25 +0300

    feat(platform): add code example for mobile mode and description for refresh button

--- apps/docs/src/app/platform/component-docs/platform-search-field/platform-search-field-docs.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-search-field/platform-search-field-docs.component.ts
@@ -10,6 +10,8 @@ import * as platformCategoriesSearchFieldTsCode from '!raw-loader!./platform-sea
 import * as platformDataSourceSearchFieldSrc from '!raw-loader!./platform-search-field-examples/platform-search-field-data-source-example.component.html';
 import * as platformDataSourceSearchFieldTsCode from '!raw-loader!./platform-search-field-examples/platform-search-field-data-source-example.component';
 import * as platformDataSourceSearchFieldServiceTsCode from '!raw-loader!./platform-search-field-examples/platform-search-field-data-source-example.service';
+import * as platformSearchFieldMobileModeSrc from '!raw-loader!./platform-search-field-examples/platform-search-field-mobile/platform-search-field-mobile-example.component.html';
+import * as platformSearchFieldMobileModeTsCode from '!raw-loader!./platform-search-field-examples/platform-search-field-mobile/platform-search-field-mobile-example.component.ts';
 
 @Component({
     selector: 'app-search-field',
@@ -67,4 +69,19 @@ export class PlatformSearchFieldDocsComponent {
             component: 'SearchFieldDataProvider'
         }
     ];
+
+    searchFieldMobileMode: ExampleFile[] = [
+        {
+            language: 'html',
+            code: platformSearchFieldMobileModeSrc,
+            fileName: 'platform-search-field-mobile-example'
+        },
+        {
+            language: 'typescript',
+            code: platformSearchFieldMobileModeTsCode,
+            fileName: 'platform-search-field-mobile-example',
+            component: 'PlatformSearchFieldMobileExampleComponent',
+            scssFileCode: platformSearchFieldExampleScss
+        }
+    ]
 }
