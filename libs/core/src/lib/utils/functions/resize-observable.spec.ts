f7e997d4daadd7bc6be298b679230ad2e0152ac8
Denis Severin <Denis_severin@icloud.com>
2021-10-21 12:04:27 +0300

    fix(core): fix resize observable unit test

--- libs/core/src/lib/utils/functions/resize-observable.spec.ts
+++ libs/core/src/lib/utils/functions/resize-observable.spec.ts
@@ -2,16 +2,14 @@ import { Component, ElementRef } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { resizeObservable } from './resize-observable';
 
-const ELEMENT_DIMENSIONS = { width: 100, margin: 2, padding: 5 };
+const ELEMENT_DIMENSIONS = { width: 100 };
 
 @Component({
     template: '',
     host: {
         '[style.display]': '"block"',
-        '[style.box-sizing]': '"content-box"',
-        '[style.width]': `elementDimensions.width + 'px'`,
-        '[style.margin]': `elementDimensions.margin + 'px'`,
-        '[style.padding]': `elementDimensions.padding + 'px'`
+        '[style.box-sizing]': '"border-box"',
+        '[style.width]': `elementDimensions.width + 'px'`
     }
 })
 class TestComponent {
@@ -43,9 +41,11 @@ describe('Resize Observable utils', () => {
 
     it('should spy on element resize', () => {
         resizeObservable(elementRef.nativeElement).subscribe((entries) => {
+            console.log(entries[0]);
             const entry = entries[0];
             expect(entry.contentRect.width).toEqual(200);
         });
         fixture.componentInstance.elementDimensions.width = 200;
+        fixture.detectChanges();
     });
 });
