1a41eefcddf61e0888cd2eac2d4d7cf5d70dc9c4
Oleksiy Drozdov <oleksiydrozdov@gmail.com>
2021-10-24 17:13:24 +0300

    docs(core): add new css classes to card bar component

--- apps/docs/src/app/core/component-docs/card/examples/bar-chart-list-card/card-bar.component.ts
+++ apps/docs/src/app/core/component-docs/card/examples/bar-chart-list-card/card-bar.component.ts
@@ -8,28 +8,10 @@ import { Component, ChangeDetectionStrategy, Input, ViewEncapsulation, OnInit } 
     encapsulation: ViewEncapsulation.None,
     host: { class: 'fd-card-bar-example' }
 })
-export class CardBarComponent implements OnInit {
+export class CardBarComponent {
     /** value in percentage, minimum 0, maximum 100 */
     @Input() value = 0;
 
-    /** bar hex color */
-    @Input() bgColor: string = null;
-
-    /** color name */
-    @Input() color = 'Information';
-
-    ngOnInit(): void {
-        if (this.color === 'Information') {
-            this.bgColor = '#1866b4';
-        }
-        if (this.color === 'Warning') {
-            this.bgColor = '#DA5A1B';
-        }
-        if (this.color === 'Critical') {
-            this.bgColor = '#C67A0C';
-        }
-        if (this.color === 'Success') {
-            this.bgColor = '#358A4D';
-        }
-    }
+    /** color name: information, warning, critical, success */
+    @Input() color: string = null;
 }
