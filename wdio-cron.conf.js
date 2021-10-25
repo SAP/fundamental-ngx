44caee68b5eab8d92c6f1f82093d2b3c68fd2cc6
Maxym Riabchuk <83698366+maxym-valor@users.noreply.github.com>
2021-10-25 15:19:29 +0300

    test(e2e): add e2e tests for icon-tab-bar (#6955)

--- wdio-cron.conf.js
+++ wdio-cron.conf.js
@@ -139,6 +139,7 @@ exports.config = {
             './e2e/wdio/platform/**/file-uploader.e2e-spec.ts',
             './e2e/wdio/platform/**/form-container.e2e-spec.ts',
             './e2e/wdio/platform/**/form-generator.e2e-spec.ts',
+            './e2e/wdio/platform/**/icon-tab-bar.e2e-spec.ts',
             './e2e/wdio/platform/**/info-label.e2e-spec.ts',
             './e2e/wdio/platform/**/input.e2e-spec.ts',
             './e2e/wdio/platform/**/input-group.e2e-spec.ts',
