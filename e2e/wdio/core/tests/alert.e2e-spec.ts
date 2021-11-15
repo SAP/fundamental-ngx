74da33307cc92660d0275c85fbf19cb9935cfd1b
maxym-valor <maxym.riabchuk@valor-software.com>
2021-11-15 17:23:21 +0200

    test(e2e): add additional test cases for alert

--- e2e/wdio/core/tests/alert.e2e-spec.ts
+++ e2e/wdio/core/tests/alert.e2e-spec.ts
@@ -13,7 +13,12 @@ import {
     pause,
     waitForPresent,
     waitForNotDisplayed,
-    scrollIntoView
+    scrollIntoView,
+    getAttributeByName,
+    setValue,
+    getText,
+    sendKeys,
+    getElementClass
 } from '../../driver/wdio';
 
 describe('Alert test suite', () => {
@@ -27,7 +32,12 @@ describe('Alert test suite', () => {
         openCustomAlertButton,
         playgroundAlert,
         select,
-        option
+        option,
+        playgroundAlertText,
+        alertWidthField,
+        messageField,
+        checkbox,
+        openAlertButton
     } = alertPage;
 
     beforeAll(() => {
@@ -57,6 +67,92 @@ describe('Alert test suite', () => {
         it('should check custom popup alerts appear and disappear', () => {
             const customAlertCount = getElementArrayLength(openCustomAlertButton);
             checkPopupAlert(openCustomAlertButton, customAlertCount);
+        });
+
+        it('should check RTL/LTR orientations', () => {
+            alertPage.checkRtlSwitch();
+        });
+    });
+
+    describe('Alert with customizable width example', () => {
+        it('check alert with width 250px', () => {
+            click(openCustomAlertButton);
+            expect(getAttributeByName(popupAlert, 'style')).toContain('width: 250px');
+        });
+
+        it('check alert with width 550px', () => {
+            click(openCustomAlertButton, 1);
+            expect(getAttributeByName(popupAlert, 'style')).toContain('width: 550px');
+        });
+
+        it('check alert with width 70 vw', () => {
+            click(openCustomAlertButton, 2);
+            expect(getAttributeByName(popupAlert, 'style')).toContain('width: 70vw');
+        });
+
+        it('check alert with width 100px', () => {
+            click(openCustomAlertButton, 3);
+            expect(getAttributeByName(popupAlert, 'style')).toContain('width: 100vw');
+        });
+    });
+
+    describe('Playground example', () => {
+        it('should check changing alert text', () => {
+            const newValue = '123';
+            setValue(messageField, newValue);
+            expect(getText(playgroundAlertText)).toBe(newValue);
+            expect(getText(playgroundAlertText, 1)).toBe(newValue);
+        });
+
+        it('should check that we can keep alert without test', () => {
+            const defaulTextLength = getText(playgroundAlert).length;
+            click(messageField);
+            for (let i = 0; i < defaulTextLength; i++) {
+                sendKeys('Backspace');
+            }
+            expect(getText(playgroundAlertText, 0)).toBe('');
+            expect(getText(playgroundAlertText, 1)).toBe('');
+        });
+
+        it('should check changing width of the alert', () => {
+            setValue(alertWidthField, '90%');
+            expect(getAttributeByName(playgroundAlert, 'style')).toContain('width: 90%');
+            expect(getAttributeByName(playgroundAlert, 'style', 1)).toContain('width: 90%');
+        });
+
+        it('should check that we can put different width meter(px, vw, etc.)', () => {
+            setValue(alertWidthField, '100px');
+            expect(getAttributeByName(playgroundAlert, 'style')).toContain('width: 100px');
+            expect(getAttributeByName(playgroundAlert, 'style', 1)).toContain('width: 100px');
+        });
+
+        it('should check that width will not change if miss width meter(%, px, vw_)', () => {
+            setValue(alertWidthField, '90');
+            expect(getAttributeByName(playgroundAlert, 'style')).toContain('width: 100%');
+            expect(getAttributeByName(playgroundAlert, 'style', 1)).toContain('width: 100%');
+        });
+
+        it('should check changing type of warning', () => {
+            click(playgroundAlert + button);
+            click(select);
+            click(option, 1);
+            expect(getElementClass(playgroundAlert)).toContain('warning');
+        });
+
+        it('should turn on/turn off dissmising alerts', () => {
+            click(checkbox);
+            expect(doesItExist(playgroundAlert + button)).toBe(false);
+            click(checkbox);
+            expect(doesItExist(playgroundAlert + button)).toBe(true);
+        });
+
+        // skipped due to https://github.com/SAP/fundamental-ngx/issues/7189
+        xit('should check close - open alert', () => {
+            scrollIntoView(playgroundAlert);
+            click(playgroundAlert + button);
+            expect(getElementArrayLength(playgroundAlert)).toBe(1);
+            click(openAlertButton);
+            expect(getElementArrayLength(playgroundAlert)).toBe(2);
         });
 
         // skipped due to https://github.com/SAP/fundamental-ngx/issues/6405
