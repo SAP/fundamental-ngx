fe8558fde6d57d1d26ba7f6e4a7b4d946f3784d1
nikvalor <nikita.lastovych@valor-software.com>
2021-12-20 16:27:29 +0200

    test(e2e): add e2e tests for upload collection core

---
+++ e2e/wdio/core/tests/upload-collection.e2e-spec.ts
@@ -0,0 +1,168 @@
+import { UploadCollectionPo } from '../pages/upload-collection.po';
+import {
+    acceptAlert,
+    click,
+    getAlertText,
+    getAttributeByName,
+    getElementArrayLength,
+    getElementSize,
+    getText,
+    isElementClickable,
+    refreshPage,
+    scrollIntoView,
+    setValue,
+    uploadFile
+} from '../../driver/wdio';
+import {
+    acceptAlertText,
+    declineAlertText,
+    formatArr,
+    imagePath,
+    imageText,
+    testText
+} from '../fixtures/appData/upload-collection-contents';
+import { checkElArrIsClickable } from '../../helper/assertion-helper';
+
+describe('File uploader component test', () => {
+    const uploadCollectionPage = new UploadCollectionPo();
+    const {
+        uploadCollectionComplexExample,
+        uploadCollectionExample,
+        link,
+        editButton,
+        input,
+        okButton,
+        declineButton,
+        item,
+        uploadCollectionSmallExample,
+        uploadCollectionCustomExample,
+        emphasizedButton,
+        fileUploaderInputFile,
+        checkbox,
+        standardButton
+    } = uploadCollectionPage;
+
+    beforeAll(() => {
+        uploadCollectionPage.open();
+    }, 1);
+
+    afterEach(() => {
+        refreshPage();
+    }, 2);
+
+    describe('Should check default example', () => {
+        it('should verify basic checks', () => {
+            checkElArrIsClickable(uploadCollectionExample + link);
+            checkFileNameChange(uploadCollectionExample);
+            checkItemDecline(uploadCollectionExample);
+        });
+    });
+
+    describe('Should check Small Mode example', () => {
+        it('should verify basic checks', () => {
+            checkElArrIsClickable(uploadCollectionSmallExample + link);
+            checkFileNameChange(uploadCollectionSmallExample);
+            checkItemDecline(uploadCollectionSmallExample);
+        });
+
+        it('should check small item be smaller than basic item', () => {
+            const basicItem = getElementSize(uploadCollectionExample + item);
+            const smallItem = getElementSize(uploadCollectionSmallExample + item);
+
+            expect(smallItem.width).toBeLessThan(basicItem.width);
+        });
+    });
+
+    describe('Should check Customization example', () => {
+        it('should verify basic checks', () => {
+            checkElArrIsClickable(uploadCollectionCustomExample + link);
+        });
+
+        it('should disabled buttons', () => {
+            expect(getAttributeByName(uploadCollectionCustomExample + editButton, 'disabled')).toBe('true');
+            expect(getAttributeByName(uploadCollectionCustomExample + declineButton, 'disabled')).toBe('true');
+        });
+
+        it('should check the editing of the file name', () => {
+            scrollIntoView(uploadCollectionCustomExample);
+            click(uploadCollectionCustomExample + editButton, 1);
+            setValue(uploadCollectionCustomExample + input, testText);
+            click(okButton);
+            expect(getAlertText()).toBe(acceptAlertText);
+            acceptAlert();
+            expect(getText(uploadCollectionCustomExample + link, 2)).toBe(testText + formatArr[2]);
+        });
+    });
+
+    describe('Should check Complex example', () => {
+        it('should verify basic checks', () => {
+            checkElArrIsClickable(uploadCollectionComplexExample + link);
+            checkFileNameChange(uploadCollectionComplexExample);
+            checkItemDecline(uploadCollectionComplexExample);
+        });
+
+        it('should check upload files', () => {
+            scrollIntoView(uploadCollectionComplexExample);
+            uploadFile(fileUploaderInputFile, imagePath);
+            const afterUploadItems = getElementArrayLength(uploadCollectionComplexExample + item);
+            expect(afterUploadItems).toEqual(4);
+            expect(getText(uploadCollectionComplexExample + link, 3)).toBe(imageText);
+        });
+
+        it('should check if file download button is active', () => {
+            scrollIntoView(uploadCollectionComplexExample);
+            click(uploadCollectionComplexExample + checkbox);
+            expect(isElementClickable(uploadCollectionComplexExample + standardButton, 1)).toBe(
+                true,
+                'download button not clickable'
+            );
+        });
+
+        it('should check functionality delete 2 items', () => {
+            scrollIntoView(uploadCollectionComplexExample);
+            click(uploadCollectionComplexExample + checkbox);
+            click(uploadCollectionComplexExample + checkbox, 1);
+            click(uploadCollectionComplexExample + standardButton, 2);
+            const afterDeleteItems = getElementArrayLength(uploadCollectionComplexExample + item);
+            expect(afterDeleteItems).toEqual(1);
+        });
+    });
+
+    it('should check LTR and RTL orientation', () => {
+        uploadCollectionPage.checkRtlSwitch();
+    });
+
+    xdescribe('Should check visual regression', () => {
+        it('should check visual regression for all examples', () => {
+            uploadCollectionPage.saveExampleBaselineScreenshot();
+            expect(uploadCollectionPage.compareWithBaseline()).toBeLessThan(5);
+        });
+    });
+
+    function checkFileNameChange(example: string): void {
+        scrollIntoView(example);
+        const buttonLength = getElementArrayLength(example + editButton);
+        for (let i = 0; i < buttonLength; i++) {
+            click(example + editButton, i);
+            setValue(example + input, testText);
+            click(okButton);
+            expect(getAlertText()).toBe(acceptAlertText);
+            acceptAlert();
+            expect(getText(example + link, i)).toBe(testText + formatArr[i]);
+        }
+        refreshPage();
+    }
+
+    function checkItemDecline(example: string): void {
+        scrollIntoView(example);
+        click(example + declineButton);
+        if (example === uploadCollectionComplexExample) {
+            click(emphasizedButton);
+        }
+        expect(getAlertText()).toBe(declineAlertText);
+        acceptAlert();
+        const itemCount = getElementArrayLength(example + item);
+        expect(itemCount).toEqual(2);
+        refreshPage();
+    }
+});
