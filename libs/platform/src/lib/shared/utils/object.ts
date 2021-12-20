31acd27c149c6a79fa29fd8c8f246e26a47ad649
dmitry-stepanenko <stepanenkodmitri@gmail.com>
2021-12-20 14:48:46 +0200

    fix(core): remove getNestedValue in favour of lodash.get

--- libs/platform/src/lib/shared/utils/object.ts
+++ libs/platform/src/lib/shared/utils/object.ts
@@ -1,17 +1,5 @@
 import { isObject } from './lang';
 
-export function getNestedValue(key: string, object: any): any {
-    const path = key.split('.');
-    const pathLength = path.length;
-    let index = 0;
-
-    while (object != null && index < pathLength) {
-        object = object[path[index++]];
-    }
-
-    return index && index === pathLength ? object : undefined;
-}
-
 /**
  * @description Deep merge two objects.
  * @param target
