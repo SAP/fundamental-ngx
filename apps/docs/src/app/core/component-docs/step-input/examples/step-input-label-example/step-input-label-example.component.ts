8ad09eb45abfe64e08a96ce9c07ea534bef8c663
Yury Betrozov <betrozov91@gmail.com>
2021-12-23 16:07:00 +0200

    fix(core): step-input, fix a11y issues (#7235)

--- apps/docs/src/app/core/component-docs/step-input/examples/step-input-label-example/step-input-label-example.component.ts
+++ apps/docs/src/app/core/component-docs/step-input/examples/step-input-label-example/step-input-label-example.component.ts
@@ -6,19 +6,19 @@ import { Component } from '@angular/core';
         <div class="step-input-example-container">
             <div class="step-input-example">
                 <label fd-form-label for="kg-input">Kilograms</label>
-                <fd-step-input id="kg-input" [(value)]="value1" unit="kg"></fd-step-input>
+                <fd-step-input inputId="kg-input" [(value)]="value1" unit="kg"></fd-step-input>
                 <small>Value: {{ value1 }}</small>
             </div>
 
             <div class="step-input-example">
                 <label fd-form-label for="mm-input">Millimeters</label>
-                <fd-step-input id="mm-input" [(value)]="value2" unit="mm"></fd-step-input>
+                <fd-step-input inputId="mm-input" [(value)]="value2" unit="mm"></fd-step-input>
                 <small>Value: {{ value2 }}</small>
             </div>
 
             <div class="step-input-example">
                 <label fd-form-label for="mol-input">Moles</label>
-                <fd-step-input id="mol-input" [(value)]="value3" unit="mol"></fd-step-input>
+                <fd-step-input inputId="mol-input" [(value)]="value3" unit="mol"></fd-step-input>
                 <small>Value: {{ value3 }}</small>
             </div>
         </div>
