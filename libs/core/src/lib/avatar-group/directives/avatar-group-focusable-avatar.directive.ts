a4a5f1f959a42451be69c1d9f0465d97ec80652f
Yury Betrozov <betrozov91@gmail.com>
2022-01-04 13:17:10 +0200

    fix(core): avatar-group, add keyboard support

--- libs/core/src/lib/avatar-group/directives/avatar-group-focusable-avatar.directive.ts
+++ libs/core/src/lib/avatar-group/directives/avatar-group-focusable-avatar.directive.ts
@@ -1,13 +1,38 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, ElementRef, Host, HostBinding, HostListener, Input } from '@angular/core';
+import { FocusableOption } from '@angular/cdk/a11y';
+
+import { AvatarGroupOverflowBodyDirective } from './avatar-group-overflow-body.directive';
 
 @Directive({
     // eslint-disable-next-line @angular-eslint/directive-selector
     selector: '[fd-avatar-group-focusable-avatar]',
     host: { class: 'fd-avatar-group__focusable-avatar' }
 })
-export class AvatarGroupFocusableAvatarDirective {
+export class AvatarGroupFocusableAvatarDirective implements FocusableOption {
     /** Tabindex of the Avatar. */
     @Input()
     @HostBinding('attr.tabindex')
     tabindex = 0;
+
+    /** @hidden */
+    constructor(
+        private readonly _elementRef: ElementRef<HTMLElement>,
+        @Host() private readonly _component: AvatarGroupOverflowBodyDirective
+    ) {}
+
+    /** @hidden */
+    get _element(): HTMLElement {
+        return this._elementRef.nativeElement;
+    }
+
+    /** Handler for mouse events */
+    @HostListener('click')
+    onClick(): void {
+        this._component._setActiveItem(this);
+    }
+
+    /** @hidden */
+    focus(): void {
+        this._element.focus();
+    }
 }
--- libs/core/src/lib/avatar-group/directives/avatar-group-item.directive.ts
+++ libs/core/src/lib/avatar-group/directives/avatar-group-item.directive.ts
@@ -1,10 +1,35 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Host, HostListener } from '@angular/core';
+import { FocusableOption } from '@angular/cdk/a11y';
+
+import { AvatarGroupComponent } from '../avatar-group.component';
 
 @Directive({
     // eslint-disable-next-line @angular-eslint/directive-selector
     selector: '[fd-avatar-group-item]',
     host: { class: 'fd-avatar-group__item' }
 })
-export class AvatarGroupItemDirective {
-    constructor(public elementRef: ElementRef) {}
+export class AvatarGroupItemDirective implements FocusableOption {
+    disabled = false;
+
+    /** @hidden */
+    constructor(
+        private readonly _elementRef: ElementRef<HTMLElement>,
+        @Host() private readonly _component: AvatarGroupComponent
+    ) {}
+
+    /** @hidden */
+    get _element(): HTMLElement {
+        return this._elementRef.nativeElement;
+    }
+
+    /** Handler for mouse events */
+    @HostListener('click')
+    onClick(): void {
+        this._component._setActiveItem(this);
+    }
+
+    /** @hidden */
+    focus(): void {
+        this._element.focus();
+    }
 }
