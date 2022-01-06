bd53a41072577890d03ef9b19251106f76ca14a3
Denis Severin <Denis_severin@icloud.com>
2022-01-06 15:49:36 +0200

    fix(platform): disable strict mode

--- libs/platform/src/lib/smart-filter-bar/smart-filter-bar.class.ts
+++ libs/platform/src/lib/smart-filter-bar/smart-filter-bar.class.ts
@@ -2,8 +2,8 @@ import { FILTER_STRATEGY } from '@fundamental-ngx/platform/table';
 import { SmartFilterBarCondition } from './interfaces/smart-filter-bar-condition';
 
 export abstract class SmartFilterBar {
-    defineStrategyLabels: {
+    defineStrategyLabels!: {
         [key in typeof FILTER_STRATEGY[keyof typeof FILTER_STRATEGY]]: string;
     };
-    getDisplayValue: (condition: SmartFilterBarCondition, filterType: string) => Promise<string>;
+    getDisplayValue!: (condition: SmartFilterBarCondition, filterType: string) => Promise<string>;
 }
