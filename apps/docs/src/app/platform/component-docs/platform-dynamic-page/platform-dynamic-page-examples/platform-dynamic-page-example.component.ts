e8699b89638259c62a1bf0de32b8d61f8397e693
Platon Rov <platon.rov@gmail.com>
2022-01-10 17:00:17 +0200

    chore(docs): lint errors fixes

--- apps/docs/src/app/platform/component-docs/platform-dynamic-page/platform-dynamic-page-examples/platform-dynamic-page-example.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-dynamic-page/platform-dynamic-page-examples/platform-dynamic-page-example.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
-import { DynamicPageCollapseChangeEvent } from '@fundamental-ngx/platform/dynamic-page';
 import { PlatformDynamicPagePageOverflowService } from './platform-dynamic-page-page-overflow.service';
 
 @Component({
@@ -19,7 +18,7 @@ export class PlatformDynamicPageExampleComponent implements OnDestroy {
 
     constructor(private _overflowHandlingService: PlatformDynamicPagePageOverflowService) {}
 
-    onCollapseChange(event: DynamicPageCollapseChangeEvent): void {
+    onCollapseChange(): void {
         console.log('collapse changed');
     }
 
