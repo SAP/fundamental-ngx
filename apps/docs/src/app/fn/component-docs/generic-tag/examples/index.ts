e7254097f20c523b0034b6c2713b235978b832e9
Denis Severin <Denis_severin@icloud.com>
2022-02-09 13:36:50 +0200

    feat(fn): generic tag component

---
+++ apps/docs/src/app/fn/component-docs/generic-tag/examples/index.ts
@@ -0,0 +1,5 @@
+import { GenericTagExampleComponent } from './generic-tag-example/generic-tag-example-component';
+
+export * from './generic-tag-example/generic-tag-example-component';
+
+export const examples = [GenericTagExampleComponent];
