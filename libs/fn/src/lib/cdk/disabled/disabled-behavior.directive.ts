ff02b1e51fcddae7277ff2e513bdfeb696c953d8
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-03-02 18:02:56 +0400

    feat(fn): created disabled observer and splitted them up

--- libs/fn/src/lib/cdk/disabled/disabled-behavior.directive.ts
+++ libs/fn/src/lib/cdk/disabled/disabled-behavior.directive.ts
@@ -1,13 +1,12 @@
-import { Directive, ElementRef, Inject, Input, OnDestroy, Optional, Self } from '@angular/core';
+import { Directive, ElementRef, Input, OnDestroy } from '@angular/core';
 import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ReplaySubject } from 'rxjs';
 import { FN_DISABLED } from '../tokens/disabled';
 import { DisabledBehavior } from '../interfaces/disabled-behavior.interface';
 import { setDisabledState } from './set-disabled-state';
 import { takeUntil, tap } from 'rxjs/operators';
-import { AttributeObserver } from '../observers/attribute.observer';
 import { DestroyedBehavior } from '../common-behaviors/destroyed-behavior';
+import { DisabledObserver } from './disabled.observer';
 
 @Directive({
     selector: '[fnDisabled]',
@@ -32,30 +31,15 @@ export class DisabledBehaviorDirective extends ReplaySubject<boolean> implements
     _disabled = false;
 
     constructor(
-        @Optional() @Self() @Inject(NG_VALUE_ACCESSOR) private valueAccessors: ControlValueAccessor[],
         private _elementRef: ElementRef<HTMLElement>,
-        private _contentObserver: AttributeObserver,
+        private _disabledObserver: DisabledObserver,
         private _destroy$: DestroyedBehavior
     ) {
         super(1);
-
-        if (valueAccessors?.length > 0) {
-            for (const valueAccessor of valueAccessors) {
-                const originalSetDisabledState = valueAccessor.setDisabledState;
-                valueAccessor.setDisabledState = (isDisabled: boolean) => {
-                    if (originalSetDisabledState) {
-                        originalSetDisabledState.call(valueAccessor, isDisabled);
-                    }
-                    this.fnDisabled = isDisabled;
-                };
-            }
-        }
-
-        _contentObserver
+        this._disabledObserver
             .observe(this._elementRef)
             .pipe(
-                tap(() => {
-                    const isDisabled = this.isDisabled(this._elementRef.nativeElement);
+                tap((isDisabled) => {
                     if (isDisabled !== this._disabled) {
                         this._disabled = isDisabled;
                         this.next(isDisabled);
@@ -64,14 +48,6 @@ export class DisabledBehaviorDirective extends ReplaySubject<boolean> implements
                 takeUntil(this._destroy$)
             )
             .subscribe();
-    }
-
-    isDisabled(el: Element): boolean {
-        return (
-            el.hasAttribute('disabled') ||
-            el.getAttribute('aria-disabled') === 'true' ||
-            el.classList.contains('is-disabled')
-        );
     }
 
     ngOnDestroy(): void {
