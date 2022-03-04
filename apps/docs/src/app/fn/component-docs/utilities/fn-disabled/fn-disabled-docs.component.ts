d58c9296a594726d30a861b1e62ff380a479e5f8
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-03-05 01:22:53 +0400

    chore(docs): fixed path to DI example files

--- apps/docs/src/app/fn/component-docs/utilities/fn-disabled/fn-disabled-docs.component.ts
+++ apps/docs/src/app/fn/component-docs/utilities/fn-disabled/fn-disabled-docs.component.ts
@@ -1,7 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { ExampleFile } from '../../../../documentation/core-helpers/code-example/example-file';
+
 import defaultExampleHtml from '!./examples/default-example/default-example.component.html?raw';
 import defaultExampleTs from '!./examples/default-example/default-example.component.ts?raw';
+
+import diExampleHtml from '!./examples/di-example/di-example.component.html?raw';
+import diExampleTs from '!./examples/di-example/di-example.component.ts?raw';
+import diRecipientExampleTs from '!./examples/di-example/fn-disabled-recipient.directive.ts?raw';
 
 @Component({
     selector: 'app-tabs',
@@ -21,6 +26,26 @@ export class FnDisabledDocsComponent {
             language: 'ts',
             fileName: 'fn-disabled-default-example',
             component: 'FnDisabledDefaultExample'
+        }
+    ];
+    diExample: ExampleFile[] = [
+        {
+            code: diExampleHtml,
+            language: 'html',
+            fileName: 'fn-disabled-di-example',
+            component: 'FnDisabledDIExample'
+        },
+        {
+            code: diExampleTs,
+            language: 'ts',
+            fileName: 'fn-disabled-di-example',
+            component: 'FnDisabledDIExample'
+        },
+        {
+            code: diRecipientExampleTs,
+            language: 'ts',
+            fileName: 'fn-disabled-recipient.directive',
+            component: 'FnDisabledRecipientDirective'
         }
     ];
 
