249db4b18a26bb13446a9df8016b15ba384b1318
Platon Rov <platon.rov@gmail.com>
2022-03-04 12:53:23 +0200

    fix(docs): file uploader disabled example

--- apps/docs/src/app/core/component-docs/file-uploader/examples/file-uploader-example/file-uploader-example.component.ts
+++ apps/docs/src/app/core/component-docs/file-uploader/examples/file-uploader-example/file-uploader-example.component.ts
@@ -9,8 +9,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 export class FileUploaderExampleComponent {
     files: File[];
 
-    filesDisabled: File[];
-
     handleFileSelection(files: File[]): void {
         alert(files.length + ' Files selected successfully!!!');
     }
