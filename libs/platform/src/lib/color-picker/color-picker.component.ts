f8d4d463239b2f219f43b667e742d9fb7cc90784
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-03-11 14:39:57 -0700

    feat(platform): added RTL and content density support for color picker, should be abstracted to shared component

--- libs/platform/src/lib/color-picker/color-picker.component.ts
+++ libs/platform/src/lib/color-picker/color-picker.component.ts
@@ -7,10 +7,16 @@ import {
     SkipSelf,
     Host,
     ChangeDetectionStrategy,
-    HostListener
+    HostListener,
+    AfterViewInit,
+    ElementRef,
+    OnDestroy
 } from '@angular/core';
 import { BaseInput, FormField, FormFieldControl } from '@fundamental-ngx/platform/shared';
 import { NgControl, NgForm } from '@angular/forms';
+import { isCompactDensity, RtlService } from '@fundamental-ngx/core/utils';
+
+import applyDirection from '@ui5/webcomponents-base/dist/locale/applyDirection.js';
 
 @Component({
     selector: 'fdp-color-picker',
@@ -18,7 +24,7 @@ import { NgControl, NgForm } from '@angular/forms';
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [{ provide: FormFieldControl, useExisting: PlatformColorPickerComponent, multi: true }]
 })
-export class PlatformColorPickerComponent extends BaseInput {
+export class PlatformColorPickerComponent extends BaseInput implements AfterViewInit, OnDestroy {
     /** @hidden */
     _value: string;
 
@@ -49,9 +55,40 @@ export class PlatformColorPickerComponent extends BaseInput {
         @Optional() @Self() public ngControl: NgControl,
         @Optional() @Self() public ngForm: NgForm,
         @Optional() @SkipSelf() @Host() formField: FormField,
-        @Optional() @SkipSelf() @Host() formControl: FormFieldControl<any>
+        @Optional() @SkipSelf() @Host() formControl: FormFieldControl<any>,
+        @Optional() private _rtlService: RtlService,
+        private _wcElRef: ElementRef
     ) {
         super(_changeDetectorRef, ngControl, ngForm, formField, formControl);
+        if (this._rtlService) {
+            this._subscriptions.add(
+                this._rtlService.rtl.subscribe((value: boolean) => {
+                    this._wcElRef.nativeElement.dir = value ? 'rtl' : 'ltr';
+                    applyDirection();
+                })
+            );
+        }
+        if (this._contentDensityService) {
+            this._subscriptions.add(
+                this._contentDensityService._contentDensityListener.subscribe((density) => {
+                    const classList = this._wcElRef.nativeElement.classList;
+                    isCompactDensity(density)
+                        ? classList.add('ui5-content-density-compact')
+                        : classList.remove('ui5-content-density-compact');
+                })
+            );
+        }
+    }
+
+    /** @hidden */
+    ngAfterViewInit(): void {
+        super.ngAfterViewInit();
+    }
+
+    /** @hidden */
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+        this._subscriptions.unsubscribe();
     }
 
     /** @hidden */
