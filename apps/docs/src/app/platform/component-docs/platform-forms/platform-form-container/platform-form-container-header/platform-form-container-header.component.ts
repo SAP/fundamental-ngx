2ea7cf8e50fad8860ec3c63b448957c6af6501bc
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-03-15 12:21:57 +0400

    chore(platform): added explanation for global inline help configs

--- apps/docs/src/app/platform/component-docs/platform-forms/platform-form-container/platform-form-container-header/platform-form-container-header.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-forms/platform-form-container/platform-form-container-header/platform-form-container-header.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import hljs from 'highlight.js/lib';
 
 @Component({
     selector: 'app-form-container-header',
     templateUrl: './platform-form-container-header.component.html'
 })
-export class PlatformFormContainerHeaderComponent {}
+export class PlatformFormContainerHeaderComponent implements AfterViewInit {
+    constructor(private elementRef: ElementRef<HTMLElement>) {}
+
+    ngAfterViewInit(): void {
+        this.elementRef.nativeElement.querySelectorAll('.hljs').forEach((block) => {
+            hljs.highlightBlock(block);
+        });
+    }
+}
--- apps/docs/src/app/platform/component-docs/platform-forms/platform-form-generator/platform-form-generator-header/platform-form-generator-header.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-forms/platform-form-generator/platform-form-generator-header/platform-form-generator-header.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import hljs from 'highlight.js/lib';
 
 @Component({
     selector: 'fd-platform-form-generator-header',
     templateUrl: './platform-form-generator-header.component.html'
 })
-export class PlatformFormGeneratorHeaderComponent {}
+export class PlatformFormGeneratorHeaderComponent implements AfterViewInit {
+    constructor(private elementRef: ElementRef<HTMLElement>) {}
+
+    ngAfterViewInit(): void {
+        this.elementRef.nativeElement.querySelectorAll('.hljs').forEach((block) => {
+            hljs.highlightBlock(block);
+        });
+    }
+}
