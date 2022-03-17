52741da37126705abfe558a0ed938947430acf98
Steven <50121934+StevenZhao318@users.noreply.github.com>
2022-03-17 15:48:11 -0700

    feat(platform): cleanup code, and use colorPaletteItem for colorPalettePopover example

--- libs/platform/src/lib/color-palette-popover/color-palette-popover.module.ts
+++ libs/platform/src/lib/color-palette-popover/color-palette-popover.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { PipeModule } from '@fundamental-ngx/core/utils';
 import { ButtonModule } from '@fundamental-ngx/core/button';
+import { PlatformColorPaletteModule } from '@fundamental-ngx/platform/color-palette';
+
 import { ColorPalettePopoverComponent } from './color-palette-popover.component';
 import '@ui5/webcomponents/dist/ColorPalettePopover.js';
 import '@ui5/webcomponents/dist/features/ColorPaletteMoreColors.js';
@@ -10,7 +12,7 @@ import '@ui5/webcomponents/dist/features/ColorPaletteMoreColors.js';
 @NgModule({
     declarations: [ColorPalettePopoverComponent],
     imports: [CommonModule, ButtonModule, PipeModule],
-    exports: [ColorPalettePopoverComponent],
+    exports: [ColorPalettePopoverComponent, PlatformColorPaletteModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PlatformColorPalettePopoverModule {}
