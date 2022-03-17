94091ed7006e3c3b67ae21e8e65d5d0943e9688b
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-03-17 23:03:17 +0400

    refactor(docs): removed redundant custom code snippet

--- apps/docs/src/app/platform/component-docs/platform-forms/platform-form-container/platform-form-container-header/platform-form-container-header.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-forms/platform-form-container/platform-form-container-header/platform-form-container-header.component.ts
@@ -1,16 +1,7 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
-import hljs from 'highlight.js/lib';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-form-container-header',
     templateUrl: './platform-form-container-header.component.html'
 })
-export class PlatformFormContainerHeaderComponent implements AfterViewInit {
-    constructor(private elementRef: ElementRef<HTMLElement>) {}
-
-    ngAfterViewInit(): void {
-        this.elementRef.nativeElement.querySelectorAll('.hljs').forEach((block) => {
-            hljs.highlightBlock(block);
-        });
-    }
-}
+export class PlatformFormContainerHeaderComponent {}
--- apps/docs/src/app/platform/component-docs/platform-forms/platform-form-generator/platform-form-generator-header/platform-form-generator-header.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-forms/platform-form-generator/platform-form-generator-header/platform-form-generator-header.component.ts
@@ -1,16 +1,7 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
-import hljs from 'highlight.js/lib';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-platform-form-generator-header',
     templateUrl: './platform-form-generator-header.component.html'
 })
-export class PlatformFormGeneratorHeaderComponent implements AfterViewInit {
-    constructor(private elementRef: ElementRef<HTMLElement>) {}
-
-    ngAfterViewInit(): void {
-        this.elementRef.nativeElement.querySelectorAll('.hljs').forEach((block) => {
-            hljs.highlightBlock(block);
-        });
-    }
-}
+export class PlatformFormGeneratorHeaderComponent {}
