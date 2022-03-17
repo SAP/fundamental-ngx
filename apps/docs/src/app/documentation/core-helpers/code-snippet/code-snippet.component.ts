60f170e077bcd222ba3cb9eaf25f96eed8f64a47
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-03-18 01:47:51 +0400

    feat(docs): added content based code snipped support

--- apps/docs/src/app/documentation/core-helpers/code-snippet/code-snippet.component.ts
+++ apps/docs/src/app/documentation/core-helpers/code-snippet/code-snippet.component.ts
@@ -1,4 +1,13 @@
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    Input,
+    isDevMode,
+    OnChanges,
+    SimpleChanges,
+    ViewChild
+} from '@angular/core';
 import { ExampleFile } from '../code-example/example-file';
 import hljs from 'highlight.js/lib';
 
@@ -7,19 +16,38 @@ import hljs from 'highlight.js/lib';
     styles: ['.bordered { border: 1px solid beige }'],
     template: `
         <pre [class.bordered]="standAlone">
-            <code #code class="hljs" [class]="file.language">{{ file.code }}</code>
+            <code #code class="hljs" [class]="file?.language || language">
+                <ng-container *ngIf="contentBased; else fileBased">
+                    <ng-content></ng-content>
+                </ng-container>
+                <ng-template #fileBased>
+                    {{ file.code }}
+                </ng-template>
+            </code>
         </pre>
     `
 })
-export class CodeSnippetComponent implements AfterViewInit {
+export class CodeSnippetComponent implements AfterViewInit, OnChanges {
     @Input()
     file: ExampleFile;
 
     @Input()
     standAlone: boolean;
 
+    @Input()
+    contentBased: boolean;
+
+    @Input()
+    language: string;
+
     @ViewChild('code', { read: ElementRef })
     codeContainerRef: ElementRef;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.contentBased && this.contentBased && !this.language && isDevMode()) {
+            console.warn('Provide language');
+        }
+    }
 
     ngAfterViewInit(): void {
         /** Highlight.js init */
