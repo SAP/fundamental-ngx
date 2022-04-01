440b8e25ea0bcebc41e238227111b92b4cae579a
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-04-02 01:54:55 +0400

    chore(docs): added typedocs

--- libs/core/src/lib/utils/directives/ignore-click-on-selection/ignore-click-on-selection.directive.ts
+++ libs/core/src/lib/utils/directives/ignore-click-on-selection/ignore-click-on-selection.directive.ts
@@ -2,6 +2,11 @@ import { Directive, ElementRef, HostListener, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { IgnoreClickOnSelectionDirectiveToken } from './tokens';
 
+/**
+ * Directive will stop propagation and prevent default of click, if selected area is coming from
+ * directive's host element. This gives ability to not cancel all clicks on element, just the ones
+ * which include selection
+ */
 @Directive({
     selector: '[fdIgnoreClickOnSelection]',
     providers: [
@@ -12,8 +17,10 @@ import { IgnoreClickOnSelectionDirectiveToken } from './tokens';
     ]
 })
 export class IgnoreClickOnSelectionDirective {
+    /** @hidden */
     constructor(@Inject(DOCUMENT) private document: Document, private _elementRef: ElementRef<HTMLElement>) {}
 
+    /** Host click listener. Checks for selection existence and if finds one, checks anchor */
     @HostListener('click', ['$event'])
     clicked($event: MouseEvent): void {
         const selection = this.document.getSelection();
@@ -26,9 +33,5 @@ export class IgnoreClickOnSelectionDirective {
                 $event.preventDefault();
             }
         }
-    }
-
-    nativeElement(): Element {
-        return this._elementRef.nativeElement;
     }
 }
