380e0fc0412d8724e938413a63b29b99a42cf61d
Platon Rov <platon.rov@gmail.com>
2022-04-29 12:21:08 +0300

    test(core): e2e fixes

--- e2e/wdio/core/pages/quick-view.po.ts
+++ e2e/wdio/core/pages/quick-view.po.ts
@@ -4,9 +4,7 @@ import { waitForElDisplayed, waitForPresent } from '../../driver/wdio';
 export class QuickViewPo extends CoreBaseComponentPo {
     private url = '/quick-view';
     root = '#page-content';
-    pageHeader = 'app-quick-view-docs-header h1';
 
-    popoverContent = '.fd-popover__popper .fd-quick-view__content';
     mobileLabel = '(//fd-quick-view-group-item-content//child::a[contains(@href, "tel")])[1]';
     phoneLabel = '(//fd-quick-view-group-item-content//child::a[contains(@href, "tel")])[2]';
     mobilePopoverLabel = '(//fd-quick-view-group-item-content//child::a[contains(@href, "tel")])[3]';
@@ -17,15 +15,12 @@ export class QuickViewPo extends CoreBaseComponentPo {
     companyAddressLabel = '(//fd-quick-view-group-item-content//child::div)[2]';
     companyPopoverNameLabel = '(//fd-quick-view-group-item-content//child::div)[3]';
     companyPopoverAddressLabel = '(//fd-quick-view-group-item-content//child::div)[4]';
-    subTitle = '[class="fd-quick-view__subtitle"]';
     title = '[bardesign="header-with-subheader"] .fd-title';
-    popoverSubTitle = '.fd-popover__popper div.fd-quick-view__subtitle';
-    popoverTitle = '.fd-popover__popper h5';
     avatar = '.fd-quick-view fd-avatar';
     popoverAvatar = '(//*[contains(@class, "fd-quick-view__content")]//child::fd-avatar)[2]';
     popoverHeader = '.fd-bar--header fd-bar-element';
     popoverWithoutHeaderButton = '.fd-docs-flex-display-helper :nth-child(2) button';
-    popoverWithHeaderButton = '.fd-popover-custom:nth-child(1) button';
+    popoverWithHeaderButton = 'fd-quick-view-popover-example button:nth-child(1)';
     popoverSendReminderButton = '.fd-bar__right :nth-child(1) button';
     popoverCancelButton = '.fd-bar__right :nth-child(2) button';
     openDialogButton = '#in-dialog + component-example button';
