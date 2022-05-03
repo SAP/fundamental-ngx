cb5118ba56cbf5061cdc5585f48b8f7110632890
N1XUS <Denis_severin@icloud.com>
2022-05-03 18:15:12 +0300

    fix(core,platform): adjust toolbar component for dynamic page (#8052)

--- libs/core/src/lib/shared/public_api.ts
+++ libs/core/src/lib/shared/public_api.ts
@@ -8,3 +8,4 @@ export * from './interfaces/nullable';
 export * from './interfaces/special-day-rule';
 export * from './services/value-state-message.service';
 export * from './utils/form';
+export * from './tokens/dynamic-page-header.token';
