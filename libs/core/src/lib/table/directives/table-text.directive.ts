4aec05e1bb24d7a2af47acddd6a72a8786c4e6ce
Platon Rov <platon.rov@gmail.com>
2022-06-09 23:32:24 +0200

    fix(core): table text documentation

--- libs/core/src/lib/table/directives/table-text.directive.ts
+++ libs/core/src/lib/table/directives/table-text.directive.ts
@@ -4,22 +4,22 @@ import { Directive, HostBinding, Input } from '@angular/core';
     selector: '[fdTableText], [fd-table-text]'
 })
 export class TableTextDirective {
-    /** @hidden */
+    /** Whether the text should have shadow (halo) & overflow should be hidden. */
     @HostBinding('class.fd-table__text')
     @Input()
     fdTableTextClass = true;
 
-    /** Whether or not  the text should wrap, when text is too long for 1 line */
+    /** Whether the text should wrap when the text is too long for 1 line */
     @HostBinding('class.fd-table__text--no-wrap')
     @Input()
     noWrap = false;
 
-    /** Whether or not  the text should be bolded and marked as a title */
+    /** Whether the text should be bolded and marked as a title */
     @HostBinding('class.fd-table__text--title')
     @Input()
     title = false;
 
-    /** Maximum width of text element */
+    /** Maximum width of a text element */
     @HostBinding('style.max-width')
     @Input()
     maxWidth: string;
