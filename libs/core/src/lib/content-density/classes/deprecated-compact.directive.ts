e403910e0a4794bac47a9b3eac2df315cce536e2
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-06-22 03:52:53 +0400

    chore(docs,core): re-imported some changes from another branch

--- libs/core/src/lib/content-density/classes/deprecated-compact.directive.ts
+++ libs/core/src/lib/content-density/classes/deprecated-compact.directive.ts
@@ -2,6 +2,7 @@ import { Directive, Input, isDevMode, OnDestroy } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 import { ContentDensityMode } from '../content-density.types';
+import { ModuleDeprecation } from '@fundamental-ngx/core/utils';
 
 /**
  * Directive decorator is only for allowing @Input
