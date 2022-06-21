7c9ba1b0209dba20e7e5012569e0ec9a156dc7fe
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-06-21 22:57:46 +0400

    chore(core): updated example to use global value for real

--- apps/docs/src/app/core/component-docs/content-density/examples/content-density-example.component.ts
+++ apps/docs/src/app/core/component-docs/content-density/examples/content-density-example.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { GlobalContentDensityService, ContentDensityMode } from '@fundamental-ngx/core/content-density';
+import { Component } from '@angular/core';
+import { GlobalContentDensityService } from '@fundamental-ngx/core/content-density';
 
 @Component({
     selector: 'fd-content-density-example',
     templateUrl: './content-density-example.component.html'
 })
-export class ContentDensityExampleComponent implements OnInit {
-    selectedDensity: ContentDensityMode;
-
+export class ContentDensityExampleComponent {
     constructor(readonly _contentDensityService: GlobalContentDensityService) {}
-
-    ngOnInit(): void {
-        this.selectedDensity = this._contentDensityService.currentContentDensity;
-    }
 
     onChange($event: any): void {
         this._contentDensityService.updateContentDensity($event);
