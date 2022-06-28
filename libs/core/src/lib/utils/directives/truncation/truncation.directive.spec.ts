3a9bbe5005ecab9a21de145ccd534d6e0dde6e65
Shurvir <shurvir.sisodiya@sap.com>
2022-06-28 18:33:57 -0400

    fix(core): new truncation by pixel width

--- libs/core/src/lib/utils/directives/truncation/truncation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TruncationDirective } from './truncation.directive';
-
-describe('TruncationDirective', () => {
-    it('should create an instance', () => {
-        const directive = new TruncationDirective();
-        expect(directive).toBeTruthy();
-    });
-});
