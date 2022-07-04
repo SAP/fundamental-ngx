fec587acc18241d497014228060e1534b46a68ba
N1XUS <Denis_severin@icloud.com>
2022-07-04 23:06:20 +0300

    feat(core): info label different icon fonts (#8308)
    * feat(core): info label different icon fonts
    
    * feat(platform): info label font option

--- libs/core/src/lib/info-label/info-label.module.ts
+++ libs/core/src/lib/info-label/info-label.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconModule } from '@fundamental-ngx/core/icon';
 import { InfoLabelComponent } from './info-label.component';
 
 @NgModule({
     declarations: [InfoLabelComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, IconModule],
     exports: [InfoLabelComponent]
 })
 export class InfoLabelModule {}
