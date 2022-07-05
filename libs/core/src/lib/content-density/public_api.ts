94e44fd48a05294b9c8f397249013f96d5a6b482
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-05 20:01:13 +0400

    feat(core): renamed file to match exported function

--- libs/core/src/lib/content-density/public_api.ts
+++ libs/core/src/lib/content-density/public_api.ts
@@ -5,7 +5,7 @@ export * from './classes/abstract-content-density-storage';
 
 export * from './directives/content-density.directive';
 
-export * from './providers/consumer-provider-factory';
+export * from './providers/content-density-observer-providers';
 export * from './services/content-density-observer.service';
 
 export * from './services/global-content-density.service';
