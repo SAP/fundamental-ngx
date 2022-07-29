62c0fbe3615e65d6a306092b828b7bc6e06cc1ee
Platon Rov <platon.rov@gmail.com>
2022-07-29 12:15:17 +0200

    fix(core,platform): revert "schematics, deps, migrations, assets & styles (#8465)"
    This reverts commit 1dc00d1671a0dccef49c28ef11e7b54337ce0652.

---
+++ libs/core/schematics/migrations/migrate-theme-fonts/styles.ts
@@ -0,0 +1,17 @@
+export const sapFioriFonts = `
+@font-face {
+    font-family: 'SAP-icons';
+    src: url('~@sap-theming/theming-base-content/content/Base/baseLib/baseTheme/fonts/SAP-icons.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+`;
+
+export const sapHorizonFonts = `
+@font-face {
+    font-family: 'SAP-icons';
+    src: url('~@sap-theming/theming-base-content/content/Base/baseLib/sap_horizon/fonts/SAP-icons.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+`;
