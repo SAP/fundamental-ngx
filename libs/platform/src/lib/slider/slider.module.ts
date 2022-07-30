1db9da721eaaeebe629c76a7a2a090a0a0dda09c
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-31 00:48:59 +0400

    chore(docs,platform): removed usage of deprecated inputs in docs and exported cd module from slider module

--- libs/platform/src/lib/slider/slider.module.ts
+++ libs/platform/src/lib/slider/slider.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { SliderModule } from '@fundamental-ngx/core/slider';
-import { SliderComponent } from './slider.component';
+import { ContentDensityModule } from '@fundamental-ngx/core/content-density';
+
 import { PlatformContentDensityDeprecationsModule } from '@fundamental-ngx/platform/shared';
+
+import { SliderComponent } from './slider.component';
 
 @NgModule({
     declarations: [SliderComponent],
-    imports: [CommonModule, SliderModule, FormsModule, PlatformContentDensityDeprecationsModule],
-    exports: [SliderComponent, PlatformContentDensityDeprecationsModule]
+    imports: [CommonModule, SliderModule, FormsModule, PlatformContentDensityDeprecationsModule, ContentDensityModule],
+    exports: [SliderComponent, PlatformContentDensityDeprecationsModule, ContentDensityModule]
 })
 export class PlatformSliderModule {}
