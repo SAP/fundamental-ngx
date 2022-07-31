04264e9b8247e0b80c86afe046f931ba21d3ac0a
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-31 17:39:06 +0400

    chore(platform): added deprecated provider factory for platform

--- libs/platform/src/lib/shared/platform-content-density-deprecations/index.ts
+++ libs/platform/src/lib/shared/platform-content-density-deprecations/index.ts
@@ -1,3 +1,4 @@
 export * from './platform-content-density-deprecations.module';
 export * from './directives/platform-content-density-deprecation.directive';
 export * from './directives/platform-compact-deprecation.directive';
+export * from './providers/module-deprecation.provider';
