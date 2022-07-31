a261d89bea29530df91239b2a51bba6df921a3a5
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-31 17:37:39 +0400

    chore(core): added deprecated factory provider

--- libs/core/src/lib/utils/functions/module-deprecations-provider.ts
+++ libs/core/src/lib/utils/functions/module-deprecations-provider.ts
@@ -1,10 +1,18 @@
-import { Provider } from '@angular/core';
+import { FactorySansProvider, Provider } from '@angular/core';
 import { ModuleDeprecations } from '../tokens/module-deprecations.token';
 
 export function moduleDeprecationsProvider(classRef: any): Provider {
     return {
         provide: ModuleDeprecations,
         useClass: classRef,
+        multi: true
+    };
+}
+
+export function moduleDeprecationsFactory(factory: FactorySansProvider): Provider {
+    return {
+        provide: ModuleDeprecations,
+        ...factory,
         multi: true
     };
 }
