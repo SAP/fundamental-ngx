ae91b8e45761fdc8698ada64201d2d65026358df
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-08-10 12:33:04 -0400

    fix(core): format

--- libs/core/src/lib/fixed-card-layout/fixed-card-layout.component.spec.ts
+++ libs/core/src/lib/fixed-card-layout/fixed-card-layout.component.spec.ts
@@ -36,15 +36,13 @@ describe('FixedCardLayoutComponent', () => {
     let component: TestFixedCardLayoutComponent;
     let fixture: ComponentFixture<TestFixedCardLayoutComponent>;
 
-    beforeEach(
-        waitForAsync(() => {
-            TestBed.configureTestingModule({
-                imports: [CardModule, FixedCardLayoutModule],
-                declarations: [TestFixedCardLayoutComponent],
-                providers: [RtlService]
-            }).compileComponents();
-        })
-    );
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [CardModule, FixedCardLayoutModule],
+            declarations: [TestFixedCardLayoutComponent],
+            providers: [RtlService]
+        }).compileComponents();
+    }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(TestFixedCardLayoutComponent);
