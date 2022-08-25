243299f22bf93f889151c3aeaff893d69ca33f6e
Denis Severin <Denis_severin@icloud.com>
2022-08-25 11:43:52 +0300

    fix(docs): extend sorting example

--- apps/docs/src/app/core/component-docs/table/examples/table-column-sorting-example.component.ts
+++ apps/docs/src/app/core/component-docs/table/examples/table-column-sorting-example.component.ts
@@ -1,6 +1,44 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 
+const rows = [
+    {
+        column1: 'Apple',
+        column2: 'Row 1',
+        column3: 'Row 1',
+        date: '09-07-18',
+        type: 'search'
+    },
+    {
+        column1: 'Banana',
+        column2: 'Row 2',
+        column3: 'Row 2',
+        date: '09-08-18',
+        type: 'cart'
+    },
+    {
+        column1: 'Kiwi',
+        column2: 'Row 3',
+        column3: 'Row 3',
+        date: '02-14-18',
+        type: 'calendar'
+    },
+    {
+        column1: 'Peach',
+        column2: 'Row 4',
+        column3: 'Row 4',
+        date: '12-30-17',
+        type: 'search'
+    },
+    {
+        column1: 'Strawberry',
+        column2: 'Row 5',
+        column3: 'Row 5',
+        date: '11-12-18',
+        type: 'search'
+    }
+];
+
 interface ExampleRow {
     column1: any;
     column2?: any;
@@ -17,54 +55,36 @@ interface ExampleRow {
 export class TableColumnSortingExampleComponent implements OnInit {
     tableRows: ExampleRow[];
     displayedRows: ExampleRow[];
+    tableRows2: ExampleRow[];
+    displayedRows2: ExampleRow[];
     ascending = true;
+    ascending2 = true;
     filterVal = '';
+    filterVal2 = '';
+    open = false;
 
     constructor(private liveAnnouncer: LiveAnnouncer) {}
 
-    changeSort(asc: boolean): void {
+    sortColumn1(asc: boolean): void {
+        this.ascending2 = asc;
+        this.open = false;
+    }
+
+    inputKeyup(event: KeyboardEvent): void {
+        if (event.key === 'Enter' || event.key === 'Esc') {
+            this.open = false;
+        }
+    }
+
+    async changeSort(asc: boolean): Promise<void> {
         this.ascending = asc;
-        this.liveAnnouncer.announce(`Set sort ${asc ? 'ascending' : 'descending'}`, 'assertive');
+        await this.liveAnnouncer.announce(`Set sort ${asc ? 'ascending' : 'descending'}`, 'assertive');
     }
 
     ngOnInit(): void {
-        this.tableRows = [
-            {
-                column1: 'Apple',
-                column2: 'Row 1',
-                column3: 'Row 1',
-                date: '09-07-18',
-                type: 'search'
-            },
-            {
-                column1: 'Banana',
-                column2: 'Row 2',
-                column3: 'Row 2',
-                date: '09-08-18',
-                type: 'cart'
-            },
-            {
-                column1: 'Kiwi',
-                column2: 'Row 3',
-                column3: 'Row 3',
-                date: '02-14-18',
-                type: 'calendar'
-            },
-            {
-                column1: 'Peach',
-                column2: 'Row 4',
-                column3: 'Row 4',
-                date: '12-30-17',
-                type: 'search'
-            },
-            {
-                column1: 'Strawberry',
-                column2: 'Row 5',
-                column3: 'Row 5',
-                date: '11-12-18',
-                type: 'search'
-            }
-        ];
+        this.tableRows = [...rows];
         this.displayedRows = this.tableRows;
+        this.tableRows2 = [...rows];
+        this.displayedRows2 = this.tableRows2;
     }
 }
