d9d2b48703faf8bbea35c14dcc6cd5a66196442d
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-09-09 11:04:19 -0600

    fix(core,platform): use lodash

--- libs/core/src/lib/utils/pipes/search-highlight.pipe.ts
+++ libs/core/src/lib/utils/pipes/search-highlight.pipe.ts
@@ -1,15 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import _ from 'lodash-es';
 
 @Pipe({
     name: 'highlight'
 })
 export class SearchHighlightPipe implements PipeTransform {
     transform(value: string, args: string, active: boolean = true, includeSpans: boolean = false): string {
-        value = value.replace(/</g, '&lt;');
-        value = value.replace(/>/g, '&gt;');
+        value = _.escape(value);
         let result: string = value;
         if (args && value && active) {
-            const testStr: string = args.trim().toLowerCase();
+            const testStr: string = _.escape(args.trim().toLowerCase());
             const startIndex = value.toLowerCase().indexOf(testStr);
             if (startIndex !== -1) {
                 const matchingString = value.substr(startIndex, testStr.length);
