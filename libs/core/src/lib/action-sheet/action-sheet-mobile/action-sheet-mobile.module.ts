b9dc4e0194d919c4249381f57a6ff92966863201
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-09-14 16:01:12 -0600

    fix(core): action sheet mobile tab bug, also list tab fix

--- libs/core/src/lib/action-sheet/action-sheet-mobile/action-sheet-mobile.module.ts
+++ libs/core/src/lib/action-sheet/action-sheet-mobile/action-sheet-mobile.module.ts
@@ -3,10 +3,11 @@ import { ActionSheetMobileComponent } from './action-sheet-mobile.component';
 import { ButtonModule } from '@fundamental-ngx/core/button';
 import { DialogModule } from '@fundamental-ngx/core/dialog';
 import { CommonModule } from '@angular/common';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
     declarations: [ActionSheetMobileComponent],
-    imports: [DialogModule, ButtonModule, CommonModule],
+    imports: [DialogModule, ButtonModule, CommonModule, A11yModule],
     exports: [ActionSheetMobileComponent]
 })
 export class ActionSheetMobileModule {}
