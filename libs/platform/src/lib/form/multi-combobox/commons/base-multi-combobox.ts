e8f914392a6d6ba379b7fa7d706bd927c2c30849
skaquib123 <sk.aquib.javed@sap.com>
2022-09-21 13:12:11 +0530

    Revert "fix(platform): added internal auto-incremented id"
    This reverts commit 7852701f7e9767299e8a21ec8e8b7edc11aa6f63.

--- libs/platform/src/lib/form/multi-combobox/commons/base-multi-combobox.ts
+++ libs/platform/src/lib/form/multi-combobox/commons/base-multi-combobox.ts
@@ -79,8 +79,6 @@ export class MultiComboboxSelectionChangeEvent {
     ) {}
 }
 
-let randomId = 0;
-
 @Directive()
 export abstract class BaseMultiCombobox extends CollectionBaseInput implements OnChanges, AfterViewInit, OnDestroy {
     /** Provides selected items. */
@@ -305,9 +303,6 @@ export abstract class BaseMultiCombobox extends CollectionBaseInput implements O
 
     /** @hidden */
     protected _dataSource: FdpMultiComboboxDataSource<any>;
-
-    /** @hidden */
-    private _defaultId = `fdp-multi-combobox-lookupkey-${randomId++}`;
 
     /** @hidden */
     private _inputTextValue: string;
@@ -938,10 +933,9 @@ export abstract class BaseMultiCombobox extends CollectionBaseInput implements O
         const selectItems: SelectableOptionItem[] = [];
         for (let i = 0; i < items.length; i++) {
             const value = items[i];
-            const lookupKey = this._defaultId;
             selectItems.push({
                 label: value,
-                id: this.lookupValue(value) ? this.lookupValue(value) : lookupKey,
+                id: this.lookupValue(value),
                 value,
                 selected: this.selectedItems?.includes(value) || false
             });
@@ -959,10 +953,9 @@ export abstract class BaseMultiCombobox extends CollectionBaseInput implements O
 
         for (let i = 0; i < items.length; i++) {
             const value = items[i];
-            const lookupKey = this._defaultId;
             selectItems.push({
                 label: this.displayValue(value),
-                id: this.lookupValue(value) ? this.lookupValue(value) : lookupKey,
+                id: this.lookupValue(value),
                 value,
                 selected: this.selectedItems?.includes(value) || false
             });
