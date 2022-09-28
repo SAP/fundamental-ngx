d7f8c5b1cd3210f653ac5702a100381142a78842
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-09-28 04:33:55 -0600

    fix(core,platform): remove platform highlighting and account for lt and gt in highlight pipe (#8658)
    * fix(core,platform): remove platform highlighting and account for lt and gt in highlight pipe
    
    * fix(core,platform): no need for the while loop
    
    * fix(core,platform): use lodash
    
    * fix(core,platform): only import escape
    
    Co-authored-by: Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>

--- libs/platform/src/lib/form/combobox/index.ts
+++ libs/platform/src/lib/form/combobox/index.ts
@@ -3,4 +3,3 @@ export * from './combobox/combobox.component';
 export * from './combobox.module';
 export * from './combobox-mobile/combobox/combobox-mobile.component';
 export * from './combobox-mobile/combobox-mobile.module';
-export * from './pipes/highlight.pipe';
