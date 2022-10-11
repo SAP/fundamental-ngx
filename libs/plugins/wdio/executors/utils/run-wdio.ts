edab1c20696740cc7aba792a16bcd7739dfc3be6
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-10-11 16:49:17 +0400

    refactor: using spawn instead of execSync

--- libs/plugins/wdio/executors/utils/run-wdio.ts
+++ libs/plugins/wdio/executors/utils/run-wdio.ts
@@ -1,8 +1,8 @@
 import { ExecutorContext, logger } from '@nrwl/devkit';
-import { execSync } from 'child_process';
+import { spawn } from 'child_process';
 import { unlinkSync, writeFileSync } from 'fs';
 
-export async function runWdio(baseUrl: string, e2eFiles: string[], context: ExecutorContext) {
+export async function runWdio(baseUrl: string, e2eFiles: string[], context: ExecutorContext): Promise<boolean> {
     const wdioConfig = `wdio_${context.projectName}.conf.js`;
     writeFileSync(
         wdioConfig,
@@ -17,13 +17,21 @@ module.exports.config = require('./e2e/wdio.base-config.js')({
     const flags = ` ${baseUrl ? `--baseUrl ${baseUrl}` : ''}`;
     const command = `npx cross-env TS_NODE_PROJECT=./e2e/tsconfig.json npx wdio ${wdioConfig} ${flags}`;
     logger.info(`Running command ${command}`);
-    try {
-        const wdioProcess = execSync(command, { encoding: 'utf-8' });
-        logger.log(wdioProcess);
-        unlinkSync(wdioConfig);
-        return true;
-    } catch (e) {
-        logger.error(e.stdout);
-        return false;
-    }
+    return new Promise((resolve, reject) => {
+        const testProcess = spawn(command, [], { shell: true });
+        testProcess.stdout.on('data', (data) => {
+            logger.log(data.toString());
+        });
+        testProcess.stderr.on('data', (data) => {
+            logger.error(data.toString());
+        });
+        testProcess.on('exit', (code) => {
+            unlinkSync(wdioConfig);
+            if (code === 0) {
+                resolve(true);
+            } else {
+                reject(new Error(`Test failed with code ${code}`));
+            }
+        });
+    });
 }
