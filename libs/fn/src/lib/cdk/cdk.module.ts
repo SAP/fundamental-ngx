aa0efc0116d76a0610e4b2ba976ab3c592677d26
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2022-10-14 09:11:29 -0600

    fix(fn): remove unused fnResize directive/service (#8841)

--- libs/fn/src/lib/cdk/cdk.module.ts
+++ libs/fn/src/lib/cdk/cdk.module.ts
@@ -1,26 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FocusableListModule } from './focusable-list/focusable-list.module';
-import { ResizeBehaviorModule } from './resize/resize.module';
 import { DisabledBehaviorModule } from './disabled/disabled-behavior.module';
 import { ReadonlyBehaviorModule } from './readonly/readonly-behavior.module';
 import { ClickedBehaviorModule } from './clicked/clicked-behavior.module';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FocusableListModule,
-        DisabledBehaviorModule,
-        ReadonlyBehaviorModule,
-        ClickedBehaviorModule,
-        ResizeBehaviorModule
-    ],
-    exports: [
-        FocusableListModule,
-        DisabledBehaviorModule,
-        ReadonlyBehaviorModule,
-        ClickedBehaviorModule,
-        ResizeBehaviorModule
-    ]
+    imports: [CommonModule, FocusableListModule, DisabledBehaviorModule, ReadonlyBehaviorModule, ClickedBehaviorModule],
+    exports: [FocusableListModule, DisabledBehaviorModule, ReadonlyBehaviorModule, ClickedBehaviorModule]
 })
 export class CdkModule {}
