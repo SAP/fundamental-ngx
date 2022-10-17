81e1b635e57246076436361d6903c6579c2709d9
shrvr <shurvir04@gmail.com>
2022-10-17 13:10:11 -0400

    fix(core): condensed mode key navigation

--- libs/core/src/lib/list/list-component.interface.ts
+++ libs/core/src/lib/list/list-component.interface.ts
@@ -1,5 +1,11 @@
 import { QueryList } from '@angular/core';
 
 export interface ListComponentInterface {
-    _navItems: QueryList<{ indicated: boolean; _innerText: string; expanded: boolean; _isItemVisible: boolean }>;
+    _navItems: QueryList<{
+        indicated: boolean;
+        _innerText: string;
+        expanded: boolean;
+        _isItemVisible: boolean;
+        focus(): void;
+    }>;
 }
