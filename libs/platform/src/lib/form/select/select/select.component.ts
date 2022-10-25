af3ae02caf8f201bb82fad0c4cb5a84a5c5e58df
shrvr <shurvir04@gmail.com>
2022-10-24 22:46:02 -0400

    fix(platform): remove style for list-title-text

--- libs/platform/src/lib/form/select/select/select.component.ts
+++ libs/platform/src/lib/form/select/select/select.component.ts
@@ -103,12 +103,6 @@ export class SelectComponent extends BaseSelect implements AfterViewInit, AfterV
         this.select._options.forEach((option) => {
             const optionItem = option._getHtmlElement();
 
-            // set title-text items to center - useful for two columns mainly
-            const listTitleText = optionItem.querySelector('.fd-list__title-text');
-            if (listTitleText) {
-                listTitleText.setAttribute('style', 'display:flex; align-items:center');
-            }
-
             // set maxWidth default is 40rem
             if (this.maxWidth) {
                 optionItem.setAttribute('style', 'max-width: ' + this.maxWidth + 'px');
