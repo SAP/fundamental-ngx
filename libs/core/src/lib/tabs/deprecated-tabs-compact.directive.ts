086ba594b857cae78cd48fae4e69c3df1304f3af
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2022-12-09 16:12:08 +0400

    fix(core): fixed deprecated tab list compact directive (#9061)

--- libs/core/src/lib/tabs/deprecated-tabs-compact.directive.ts
+++ libs/core/src/lib/tabs/deprecated-tabs-compact.directive.ts
@@ -1,8 +1,14 @@
-import { DeprecatedCompactDirective } from '@fundamental-ngx/core/content-density';
-import { Directive } from '@angular/core';
+import { CONTENT_DENSITY_DIRECTIVE, DeprecatedCompactDirective } from '@fundamental-ngx/core/content-density';
+import { Directive, forwardRef } from '@angular/core';
 
 @Directive({
-    selector: '[fd-tab-nav][compact], fd-tab-list[compact]'
+    selector: '[fd-tab-nav][compact], fd-tab-list[compact]',
+    providers: [
+        {
+            provide: CONTENT_DENSITY_DIRECTIVE,
+            useExisting: forwardRef(() => DeprecatedTabsCompactDirective)
+        }
+    ]
 })
 export class DeprecatedTabsCompactDirective extends DeprecatedCompactDirective {
     /** @hidden */
