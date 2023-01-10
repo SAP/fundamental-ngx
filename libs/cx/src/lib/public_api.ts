bf0fbb5a9d4ee928f18570c480a8c9a32a37dec5
Mike O'Donnell <mikerodonnell89@gmail.com>
2023-01-10 11:10:05 -0700

    fix(cx): fix prod build and add schematics

--- libs/cx/src/lib/public_api.ts
+++ libs/cx/src/lib/public_api.ts
@@ -1,3 +1,3 @@
-export * from './fundamental-ngx.module';
+export * from './fundamental-ngx-cx.module';
 export * from '@fundamental-ngx/cx/nested-list';
 export * from '@fundamental-ngx/cx/side-navigation';
