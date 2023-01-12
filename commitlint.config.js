e05571e7821297bd8a1f1370ed760c0dded24ca9
Platon Rov <platon.rov@gmail.com>
2023-01-12 12:17:10 +0100

    feat(ci): cdk scope for commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ const Configuration = {
         'scope-enum': [
             2,
             'always',
-            ['core', 'platform', 'docs', 'e2e', 'release', 'deps', 'deps-dev', 'changelog', 'ci', 'fn', 'cx']
+            ['core', 'platform', 'docs', 'e2e', 'release', 'deps', 'deps-dev', 'changelog', 'ci', 'fn', 'cx', 'cdk']
         ],
         'body-max-line-length': [2, 'always', 200],
         'footer-max-line-length': [2, 'always', 200],
