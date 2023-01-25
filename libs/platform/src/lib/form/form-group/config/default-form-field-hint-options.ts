7fcce9b4bd11e89794890b76d2a7c9847abeafc1
Mike O'Donnell <mikerodonnell89@gmail.com>
2023-01-25 11:50:54 -0700

    fix(platform): fix form generator inline help open/close issue

--- libs/platform/src/lib/form/form-group/config/default-form-field-hint-options.ts
+++ libs/platform/src/lib/form/form-group/config/default-form-field-hint-options.ts
@@ -3,7 +3,7 @@ import { FieldHintOptions } from '@fundamental-ngx/platform/shared';
 export const defaultFormFieldHintOptions: Omit<FieldHintOptions, 'text'> = {
     placement: 'right',
     position: 'after',
-    trigger: ['click', 'focusout'],
+    trigger: ['mouseenter', 'mouseleave', 'focusin', 'focusout'],
     glyph: 'hint',
     target: 'auto'
 };
