d5599b0a4e4ff12a7d8786bbafe4fe7f041aa4e6
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-02-23 14:26:32 +0400

    chore: removed redundant common module

--- libs/core/src/lib/scrollbar/scrollbar.module.ts
+++ libs/core/src/lib/scrollbar/scrollbar.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ScrollbarDirective } from './scrollbar.directive';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule, ScrollbarDirective],
+    imports: [ScrollbarDirective],
     exports: [ScrollbarDirective]
 })
 export class ScrollbarModule {}
