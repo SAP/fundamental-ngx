54d067de58e95d7f69d004b850669d77e4b019e3
Platon Rov <platon.rov@gmail.com>
2023-02-28 23:48:18 +0100

    fix(core,platform): form message issues (#9179)

--- libs/docs/core/form-message/e2e/form-message-contents.ts
+++ libs/docs/core/form-message/e2e/form-message-contents.ts
@@ -8,7 +8,9 @@ export const button = 'button';
 export const message = 'message';
 export const placeholderText = 'Field placeholder text';
 export const hoverEventMessageInput = 'This message is triggered by a click event.';
-export const clickEventMessage = 'This message is controlled by open variable.';
+export const clickEventMessage = `This message is controlled by
+open
+variable.`;
 export const hoverEventMessageInputGroup = 'This message is only triggered by hovering over the InputGroup.';
 export const eventMessageTextArea =
     'This message is only triggered by focusing inside the textarea. The message box placement is also changed.';
