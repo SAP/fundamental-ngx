16bcc3ee4a77684ebb14ba361881d01f37c91db0
skaquib123 <sk.aquib.javed@sap.com>
2023-03-01 15:57:39 +0530

    fix(platform): areaLabel value is removed

--- libs/core/src/lib/select/select.component.ts
+++ libs/core/src/lib/select/select.component.ts
@@ -424,7 +424,6 @@ export class SelectComponent<T = any>
 
     /** @hidden */
     ngOnInit(): void {
-        this.ariaLabel = this.placeholder;
         this._initializeCommonBehavior();
     }
 
