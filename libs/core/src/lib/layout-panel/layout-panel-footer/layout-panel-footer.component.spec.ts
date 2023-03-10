3e51e74774fc6eed0b2cef277f009835bfe7bf97
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2023-03-10 16:30:00 -0500

    fix(core): update Layout Panel to latest design (#9499)

--- libs/core/src/lib/layout-panel/layout-panel-footer/layout-panel-footer.component.spec.ts
+++ libs/core/src/lib/layout-panel/layout-panel-footer/layout-panel-footer.component.spec.ts
@@ -1,28 +1,52 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { LayoutPanelFooterComponent, FooterPosition } from './layout-panel-footer.component';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
-import { LayoutPanelFooterComponent } from './layout-panel-footer.component';
+@Component({
+    selector: 'fd-test-layout-panel-footer',
+    template: `<fd-layout-panel-footer [position]="position"> </fd-layout-panel-footer> `
+})
+class TestLayoutPanelFooterComponent {
+    @ViewChild(LayoutPanelFooterComponent, { static: true, read: ElementRef })
+    layoutPanelFooterElementRef: ElementRef;
+
+    position: FooterPosition;
+}
 
 describe('LayoutPanelFooterComponent', () => {
-    let component: LayoutPanelFooterComponent;
-    let fixture: ComponentFixture<LayoutPanelFooterComponent>;
+    let layoutPanelFooterElementRef: ElementRef;
+    let testComponent: TestLayoutPanelFooterComponent;
+    let fixture: ComponentFixture<TestLayoutPanelFooterComponent>;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            declarations: [LayoutPanelFooterComponent]
+            declarations: [LayoutPanelFooterComponent, TestLayoutPanelFooterComponent]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(LayoutPanelFooterComponent);
-        component = fixture.componentInstance;
+        fixture = TestBed.createComponent(TestLayoutPanelFooterComponent);
+        layoutPanelFooterElementRef = fixture.componentInstance.layoutPanelFooterElementRef;
+        testComponent = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('Should create', () => {
+        expect(testComponent).toBeTruthy();
+        expect(layoutPanelFooterElementRef).toBeTruthy();
     });
 
-    it('should have panel footer class', () => {
-        expect(fixture.nativeElement.className).toBe('fd-layout-panel__footer');
+    it('should apply modifier class for footer content position start/left', () => {
+        testComponent.position = 'start';
+        fixture.detectChanges();
+        expect(
+            layoutPanelFooterElementRef.nativeElement.classList.contains('fd-layout-panel__footer--start')
+        ).toBeTrue();
+    });
+
+    it('should apply modifier class for footer content position end/right', () => {
+        testComponent.position = 'end';
+        fixture.detectChanges();
+        expect(layoutPanelFooterElementRef.nativeElement.classList.contains('fd-layout-panel__footer--end')).toBeTrue();
     });
 });
