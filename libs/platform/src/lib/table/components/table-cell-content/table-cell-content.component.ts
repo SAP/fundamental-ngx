2bd8f2d2f4e0fcd9c4f8d7bb2a58af3145b497ab
Mike O'Donnell <mikerodonnell89@gmail.com>
2023-04-19 11:15:05 -0600

    fix: manual change detection

--- libs/platform/src/lib/table/components/table-cell-content/table-cell-content.component.ts
+++ libs/platform/src/lib/table/components/table-cell-content/table-cell-content.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Input,
+    OnChanges,
+    SimpleChanges,
+    ViewEncapsulation
+} from '@angular/core';
 import { TableRow } from '../../models/table-row.model';
 import { TableColumn } from '../table-column/table-column';
 
 @Component({
     selector: 'fdp-table-cell-content',
     templateUrl: './table-cell-content.component.html',
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TableCellContentComponent {
+export class TableCellContentComponent implements OnChanges {
     /** Table column definition. */
     @Input()
     column: TableColumn;
@@ -19,4 +28,18 @@ export class TableCellContentComponent {
     /** Whether the row is tree. */
     @Input()
     isTreeRowFirstCell = false;
+
+    /** @hidden Whether the row is expanded. */
+    @Input()
+    expanded = false;
+
+    /** @hidden */
+    constructor(private _cdRef: ChangeDetectorRef) {}
+
+    /** @hidden */
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.expanded) {
+            this._cdRef.markForCheck();
+        }
+    }
 }
