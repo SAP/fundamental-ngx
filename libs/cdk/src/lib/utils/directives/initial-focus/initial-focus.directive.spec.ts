06ce13810ce91bf6fd3326622d496fa2e15e6283
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-04-24 23:37:48 +0400

    fix: fixed initial focus directive

--- libs/cdk/src/lib/utils/directives/initial-focus/initial-focus.directive.spec.ts
+++ libs/cdk/src/lib/utils/directives/initial-focus/initial-focus.directive.spec.ts
@@ -1,49 +1,82 @@
 import { InitialFocusDirective } from './initial-focus.directive';
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, waitForAsync } from '@angular/core/testing';
+import { TabbableElementService } from '../../services/tabbable-element.service';
 
 @Component({
+    hostDirectives: [InitialFocusDirective],
+    // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+    host: {
+        '[attr.tabindex]': 'rootElementTabIndex'
+    },
+    standalone: true,
+    providers: [
+        {
+            provide: TabbableElementService,
+            useFactory: () => {
+                function getTabbableElement(root: HTMLElement): HTMLElement | null {
+                    if (root.getAttribute('tabindex') === '0') {
+                        return root;
+                    }
+                    const rootChildren = root.children;
+
+                    for (let i = 0; i < rootChildren.length; i++) {
+                        const tabbableChild =
+                            rootChildren[i].nodeType === document.ELEMENT_NODE
+                                ? getTabbableElement(rootChildren[i] as HTMLElement)
+                                : null;
+
+                        if (tabbableChild) {
+                            return tabbableChild;
+                        }
+                    }
+
+                    return null;
+                }
+                return {
+                    getTabbableElement
+                };
+            }
+        }
+    ],
     template: `
-        <button fd-initial-focus [attr.tabindex]="rootElementTabIndex" #elementToFocus>
-            <span>Non Focusable</span>
-            <span tabindex="0" #nestedElementToFocus>Focusable</span>
-            <button>Focusable</button>
-        </button>
+        <span>Non Focusable</span>
+        <span tabindex="0" id="nestedElementToFocus">Focusable</span>
     `
 })
 class TestComponent {
-    @ViewChild('elementToFocus') elementToFocus: ElementRef;
-    @ViewChild('nestedElementToFocus') nestedElementToFocus: ElementRef;
-
     rootElementTabIndex = 0;
 }
 
 describe('InitialFocusDirective', () => {
     let component: TestComponent;
     let fixture: ComponentFixture<TestComponent>;
+    let hostFocusSpy: jest.SpyInstance;
+    let childFocusSpy: jest.SpyInstance;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            declarations: [TestComponent],
-            imports: [InitialFocusDirective]
+            imports: [TestComponent]
         }).compileComponents();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(TestComponent);
         component = fixture.componentInstance;
+        hostFocusSpy = jest.spyOn(fixture.nativeElement, 'focus');
+        childFocusSpy = jest.spyOn(fixture.nativeElement.querySelector('#nestedElementToFocus'), 'focus');
     });
 
-    it('should focus element', fakeAsync(() => {
+    it('should focus element', async () => {
         fixture.detectChanges();
-        tick(10);
-        expect(document.activeElement).toBe(component.elementToFocus.nativeElement);
-    }));
+        expect(hostFocusSpy).toHaveBeenCalled();
+        expect(childFocusSpy).not.toHaveBeenCalled();
+    });
 
     it('should focus nested element', fakeAsync(() => {
         component.rootElementTabIndex = -1;
         fixture.detectChanges();
-        tick(10);
-        expect(document.activeElement).toBe(component.nestedElementToFocus.nativeElement);
+        expect(hostFocusSpy).not.toHaveBeenCalled();
+        expect(childFocusSpy).toHaveBeenCalled();
     }));
 });
