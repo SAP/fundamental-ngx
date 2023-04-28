e13cb85106093b8efa8da4c3348371d624a5c393
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-04-29 01:55:47 +0400

    feat: added post generator step

--- libs/nx-plugin/src/generators/migrate-to-jest/generator.ts
+++ libs/nx-plugin/src/generators/migrate-to-jest/generator.ts
@@ -1,5 +1,7 @@
 import { createProjectGraphAsync, readProjectConfiguration, Tree, updateProjectConfiguration } from '@nrwl/devkit';
 import { jestProjectGenerator } from '@nrwl/jest';
+import { relative } from 'path';
+import { workspaceRoot } from 'nx/src/utils/app-root';
 
 export default async function generator(tree: Tree) {
     const graph = await createProjectGraphAsync();
@@ -9,7 +11,7 @@ export default async function generator(tree: Tree) {
             return;
         }
         const [targetName, targetWithKarma] =
-        Object.entries(projectConfig.targets || {}).find(([, target]) => target.executor?.includes('karma')) || [];
+            Object.entries(projectConfig.targets || {}).find(([, target]) => target.executor?.includes('karma')) || [];
         if (targetWithKarma && targetName) {
             const options = projectConfig.targets[targetName].options;
             tree.delete(options.main);
@@ -26,6 +28,23 @@ export default async function generator(tree: Tree) {
                     skipFormat: true,
                     skipPackageJson: false
                 });
+                const fileContents = tree.read(`${projectConfig.root}/jest.config.ts`, 'utf-8') as string;
+                const setupFilesAfterEnv = (fileContents.match(/setupFilesAfterEnv: \[(.*)]/) as RegExpMatchArray)[1];
+                const pathToRoot = relative(projectConfig.root, workspaceRoot).replace(/\\/g, '/');
+                tree.write(
+                    `${projectConfig.root}/jest.config.ts`,
+                    `
+import baseConfig from '${pathToRoot}/jest.config.base';
+
+export default {
+    ...baseConfig,
+    displayName: '${projectConfig.name}',
+    preset: '${pathToRoot}/jest.preset.js',
+    setupFilesAfterEnv: [${setupFilesAfterEnv}, '${pathToRoot}/jest-extended-matchers.ts'],
+    coverageDirectory: '${pathToRoot}/dist/coverage/${projectConfig.name}',
+};
+`
+                );
             }
         }
     }
