febd554f6663a8853ba8229882c6c92dec36c3c1
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-05-05 20:51:37 +0400

    chore: added brief description to the fdk-dynamic-portal

--- libs/cdk/src/lib/utils/components/dynamic-portal/dynamic-portal.component.ts
+++ libs/cdk/src/lib/utils/components/dynamic-portal/dynamic-portal.component.ts
@@ -11,6 +11,11 @@ import {
 import { ComponentPortal, ComponentType, DomPortal, Portal, PortalModule, TemplatePortal } from '@angular/cdk/portal';
 import { coerceElement } from '@angular/cdk/coercion';
 
+/**
+ * A component that can be used to attach a portal to a DOM element, without explicitly creating portal instances on place.
+ * This is useful when you have a list of the components or templates, or HTMLElmets, and you want to attach them to the DOM
+ * dynamically in a loop.
+ */
 @Component({
     selector: 'fdk-dynamic-portal',
     standalone: true,
