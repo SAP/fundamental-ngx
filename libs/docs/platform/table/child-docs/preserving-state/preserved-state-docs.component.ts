17df8c97959189fe6ce0c1bc88a7fea0b700ba6e
Denis Severin <Denis_severin@icloud.com>
2023-05-22 13:41:43 +0300

    fix(docs): correct example url

--- libs/docs/platform/table/child-docs/preserving-state/preserved-state-docs.component.ts
+++ libs/docs/platform/table/child-docs/preserving-state/preserved-state-docs.component.ts
@@ -3,8 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ExampleChildService, ExampleFile, getAssetFromModuleAssets } from '@fundamental-ngx/docs/shared';
 import { PlatformTablePreservedStateExampleComponent } from '../../examples/preserved-state/platform-table-preserved-state-example.component';
 
-const platformTableActivableRowSrc = 'platform-table-preserved-state-example.component.html';
-const platformTableActivableRowTsSrc = 'platform-table-preserved-state-example.component.ts';
+const platformTableActivableRowSrc = 'preserved-state/platform-table-preserved-state-example.component.html';
+const platformTableActivableRowTsSrc = 'preserved-state/platform-table-preserved-state-example.component.ts';
 
 @Component({
     selector: 'fdp-doc-preserved-state-docs',
