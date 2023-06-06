96b4624f92eb61abab71f0eb593e71b196667b19
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-06-06 14:50:22 +0400

    fix(core): feature parity with alert is done

--- libs/core/src/lib/message-strip/auto-dismiss-message-strip.directive.ts
+++ libs/core/src/lib/message-strip/auto-dismiss-message-strip.directive.ts
@@ -1,55 +1,42 @@
-import { AfterViewInit, Directive, ElementRef, inject, Input, isDevMode } from '@angular/core';
+import { Directive, ElementRef, inject, Input, isDevMode } from '@angular/core';
 import { MessageStripComponent } from './message-strip.component';
-import { BehaviorSubject, combineLatest, finalize, fromEvent, map, merge, startWith, takeUntil, tap } from 'rxjs';
+import { fromEvent, map, merge, Observable, of, startWith, takeUntil } from 'rxjs';
 import { DestroyedService } from '@fundamental-ngx/cdk/utils';
-import { filter } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 @Directive({
     // eslint-disable-next-line @angular-eslint/directive-selector
     selector: 'fd-message-strip[mousePersist], fd-message-strip[duration], fd-message-strip[autoDismiss]',
+    exportAs: 'fdAutoDismissMessageStrip',
     standalone: true,
-    providers: [DestroyedService]
-})
-export class AutoDismissMessageStripDirective implements AfterViewInit {
-    /** Whether the message strip is dismissible */
-    @Input()
-    set dismissible(dismissible: boolean) {
-        this.dismissible$.next(dismissible);
+    providers: [DestroyedService],
+    host: {
+        '[style.display]': '!opened ? "none" : null'
     }
+})
+export class AutoDismissMessageStripDirective {
+    /** @hidden */
+    private messageStripComponent = inject(MessageStripComponent, { optional: false, host: true });
+
+    /** Whether the message strip is dismissible */
+    @Input() dismissible = true;
 
     /** Whether the alert should be automatically dismissed. */
-    @Input()
-    set autoDismiss(autoDismiss: boolean) {
-        this.autoDismiss$.next(autoDismiss);
-    }
+    @Input() autoDismiss = true;
 
     /** Duration of time *in milliseconds* that the alert will be visible. Set to -1 for indefinite. */
-    @Input()
-    set duration(duration: number) {
-        this.duration$.next(duration);
-    }
+    @Input() duration = 10000;
 
     /** Whether the alert should stay open if the mouse is hovering over it. */
-    @Input()
-    set mousePersist(mousePersist: boolean) {
-        this.mousePersist$.next(mousePersist);
-    }
+    @Input() mousePersist = false;
+
+    /** Whether the message strip is currently opened. */
+    opened = false;
 
     /** @hidden */
     private autoDismissTimeout?: ReturnType<typeof setTimeout>;
     /** @hidden */
     private elementRef = inject(ElementRef);
-    /** @hidden */
-    private messageStripComponent = inject(MessageStripComponent, { optional: false, host: true });
-
-    /** @hidden */
-    private dismissible$ = new BehaviorSubject<boolean>(this.messageStripComponent.dismissible); // Take the initial value from the message strip component
-    /** @hidden */
-    private autoDismiss$ = new BehaviorSubject<boolean>(true);
-    /** @hidden */
-    private duration$ = new BehaviorSubject<number>(10000);
-    /** @hidden */
-    private mousePersist$ = new BehaviorSubject<boolean>(false);
 
     /** @hidden */
     private destroy$ = inject(DestroyedService);
@@ -63,51 +50,52 @@ export class AutoDismissMessageStripDirective implements AfterViewInit {
     ).pipe(startWith(false));
 
     /** @hidden */
-    ngAfterViewInit(): void {
-        combineLatest([this.autoDismiss$, this.dismissible$])
-            .pipe(
-                takeUntil(this.destroy$),
-                finalize(() => this.stopAutoDismiss())
-            )
-            .subscribe(([autoDismiss, dismissible]) => {
-                this.stopAutoDismiss();
-                if (autoDismiss && !dismissible && isDevMode()) {
-                    console.warn(
-                        'Auto dismiss is enabled but the message strip is not dismissible. Please set the dismissible input to true.'
-                    );
-                }
-                if (autoDismiss && dismissible) {
-                    this.startAutoDismiss();
-                }
-            });
-    }
-
-    /** @hidden */
-    private stopAutoDismiss(): void {
-        if (this.autoDismissTimeout) {
-            clearTimeout(this.autoDismissTimeout);
-            this.autoDismissTimeout = undefined;
+    open(): void {
+        this.opened = true;
+        this.elementRef.nativeElement.classList.remove('fd-has-display-block');
+        this.elementRef.nativeElement.classList.remove('fd-has-display-none');
+        this.stopAutoDismiss();
+        if (this.autoDismiss && !this.dismissible && isDevMode()) {
+            console.warn(
+                'Auto dismiss is enabled but the message strip is not dismissible. Please set the dismissible input to true.'
+            );
+        }
+        if (this.autoDismiss && this.dismissible) {
+            this.startAutoDismiss();
         }
     }
 
     /** @hidden */
+    private stopAutoDismiss = (): void => {
+        if (this.autoDismissTimeout) {
+            clearTimeout(this.autoDismissTimeout);
+            this.autoDismissTimeout = undefined;
+        }
+    };
+
+    /** @hidden */
     private startAutoDismiss(): void {
-        combineLatest([this.duration$, this.mousePersist$, this.mouseIn$])
-            .pipe(
-                tap(([duration, mousePersist, mouseIn]) => {
-                    if (mouseIn && mousePersist && this.autoDismissTimeout) {
-                        this.stopAutoDismiss();
-                        return;
-                    }
-                    if (duration > -1) {
-                        this.autoDismissTimeout = setTimeout(this.dismiss, duration);
-                    } else {
-                        this.stopAutoDismiss();
-                    }
-                }),
-                takeUntil(merge(this.destroy$, this.autoDismiss$.pipe(filter((autoDismiss) => !autoDismiss))))
-            )
-            .subscribe();
+        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+        const startAutoDismissTimer$ = new Observable((res) => {
+            this.autoDismissTimeout = setTimeout(() => {
+                this.dismiss();
+                res.next();
+            }, this.duration);
+            return this.stopAutoDismiss;
+        });
+        if (this.duration > -1) {
+            const source$ = this.mousePersist
+                ? this.mouseIn$.pipe(
+                      switchMap((mouseIn) => {
+                          if (mouseIn) {
+                              return of(null);
+                          }
+                          return startAutoDismissTimer$;
+                      })
+                  )
+                : startAutoDismissTimer$;
+            source$.pipe(takeUntil(merge(this.destroy$, this.messageStripComponent.onDismiss))).subscribe();
+        }
     }
 
     /** @hidden */
