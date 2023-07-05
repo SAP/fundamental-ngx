753e9361edb0b55f9f21d5a36c8b52d2cbe8524b
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-07-06 01:25:43 +0400

    fix(e2e): fixed e2e tests for checkbox

--- libs/docs/platform/checkbox-group/e2e/checkbox-group.po.ts
+++ libs/docs/platform/checkbox-group/e2e/checkbox-group.po.ts
@@ -5,30 +5,21 @@ export class CheckboxGroupPO extends PlatformBaseComponentPo {
 
     stringValueCheckboxesArr = 'fdp-platform-checkbox-group-list input';
     stringValueCheckboxLabelArr = 'fdp-platform-checkbox-group-list fd-checkbox label';
-    stringValuecheckboxGroupLabelsArr = 'fdp-platform-checkbox-group-list .fd-form-label';
-    stringValuecheckboxGroupsArr = 'fdp-platform-checkbox-group-list fd-form-group';
     stringValueoutputLabelsArr = 'fdp-platform-checkbox-group-list > span';
-    winterCheckbox = '.fd-checkbox__label[for=seasons0]';
+    winterCheckbox = '.fd-checkbox__label[for=seasons0] span.fd-checkbox__checkmark';
 
     objectValueCheckboxesArr = 'fdp-platform-checkbox-group-list-object input';
     objectValueCheckboxLabelArr = 'fdp-platform-checkbox-group-list-object fd-checkbox label';
-    objectValuecheckboxGroupLabelsArr = 'fdp-platform-checkbox-group-list-object .fd-form-label';
-    objectValuecheckboxGroupsArr = 'fdp-platform-checkbox-group-list-object fd-form-group';
     objectValueoutputLabelsArr = 'fdp-platform-checkbox-group-list-object > span';
 
     projectedValueCheckboxesArr = 'fdp-platform-checkbox-group-content-checkbox input';
     projectedValueCheckboxLabelArr = 'fdp-platform-checkbox-group-content-checkbox fd-checkbox label';
-    projectedValuecheckboxGroupLabelsArr = 'fdp-platform-checkbox-group-content-checkbox .fd-form-label';
-    projectedValuecheckboxGroupsArr = 'fdp-platform-checkbox-group-content-checkbox fd-form-group';
     projectValueoutputLabelsArr = 'fdp-platform-checkbox-group-content-checkbox > span';
 
     formValidationCheckboxesArr = 'fdp-platform-checkbox-group-examples input';
     formValidationCheckboxLabelArr = 'fdp-platform-checkbox-group-examples fd-checkbox label';
-    formValidationcheckboxGroupLabelsArr = 'fdp-platform-checkbox-group-examples .fd-form-label';
-    formValidationcheckboxGroupsArr = 'fdp-platform-checkbox-group-examples fd-form-group';
     formvalidationValueoutputLabelsArr = 'fdp-platform-checkbox-group-examples > span';
     errorTooltip = '.fd-form-message span';
-    sectiontitle = 'fdp-platform-checkbox-group-examples h3';
 
     async open(): Promise<void> {
         await super.open(this.url);
--- libs/docs/platform/checkbox/e2e/checkbox.po.ts
+++ libs/docs/platform/checkbox/e2e/checkbox.po.ts
@@ -4,20 +4,14 @@ export class CheckboxPO extends PlatformBaseComponentPo {
     url = '/checkbox';
 
     binaryTempCheckbox = 'fdp-platform-binary-checkbox input';
-    binaryTempCheckboxLable = 'fdp-platform-binary-checkbox label.fd-checkbox__label';
     disabledBinaryCheckbox = 'fdp-platform-binary-checkbox input#disabled';
     checkboxWithoutForm = 'fdp-platform-binary-checkbox-no-form input';
     disabledCheckboxWithoutForm = 'fdp-platform-binary-checkbox-no-form fdp-checkbox:nth-of-type(5) input';
     checkboxWithValue = 'fdp-platform-multiselect-checkbox input';
     tristateCheckboxes = 'fdp-platform-tristate-checkbox input';
-    tristateCheckboxParis = '.fd-checkbox__label[for=paris]';
-    acceptAllCheckbox = 'fdp-platform-tristate-checkbox #acceptAll';
-    termsAndConditionsCheckbox = 'fdp-platform-tristate-checkbox #termsAndConditions';
-    marketingCheckbox = 'fdp-platform-tristate-checkbox #marketing';
-    newsletterCheckbox = 'fdp-platform-tristate-checkbox #newsletter';
+    tristateCheckboxParis = '.fd-checkbox__label[for=paris] span.fd-checkbox__checkmark';
     errorCheckboxes = 'fdp-platform-checkbox-error-handling .fd-checkbox__label';
     presenceCheckbox = 'fdp-platform-checkbox-error-handling #presence';
-    errorExampleTitle = 'fdp-platform-checkbox-error-handling h3';
     submitBtn = 'fdp-platform-checkbox-error-handling button';
     errorTooltip = '[type="error"] span';
     accessibilityCheckboxes = 'fdp-platform-checkbox-a11y input';
