3bb76d85d9a15327524956b1e3e83e48cfe481a6
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-07-11 22:36:50 +0400

    chore: removed unnecessary comments

--- .github/actions/helpers/git-semver-tags.js
+++ .github/actions/helpers/git-semver-tags.js
@@ -22,29 +22,4 @@ module.exports = (maxVersion = null, skipUnstable = false) => {
             return !(maxVersion && semver.gt(tag, maxVersion));
         })
         .sort(semver.rcompare);
-    // return new Promise((resolve, reject) => {
-    //
-    //     exec('git tag -l', { maxBuffer: Infinity }, (err, data) => {
-    //         if (err) {
-    //             reject(err);
-    //         } else {
-    //             debugger;
-    //             resolve(
-    //                 data
-    //                     .split('\n')
-    //                     .filter((tag) => {
-    //                         const valid = semver.valid(tag);
-    //                         if (!valid) {
-    //                             return false;
-    //                         }
-    //                         if (skipUnstable && semver.prerelease(tag)) {
-    //                             return false;
-    //                         }
-    //                         return !(maxVersion && semver.gt(tag, maxVersion));
-    //                     })
-    //                     .sort(semver.rcompare)
-    //             );
-    //         }
-    //     });
-    // });
 };
