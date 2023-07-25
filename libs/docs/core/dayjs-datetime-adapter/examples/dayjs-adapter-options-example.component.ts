06aaa26777e81268e849c97a44741eb523641e14
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2023-07-25 10:35:57 -0700

    fix(docs): dayjs datetime adapter docs (#10220)
    * fix: dayjs datetime adapter docs
    
    * fix: update console error message

--- libs/docs/core/dayjs-datetime-adapter/examples/dayjs-adapter-options-example.component.ts
+++ libs/docs/core/dayjs-datetime-adapter/examples/dayjs-adapter-options-example.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-import { DatetimeAdapter } from '@fundamental-ngx/core/datetime';
-import { DAYJS_DATE_TIME_ADAPTER_OPTIONS, DayjsDatetimeAdapter } from '@fundamental-ngx/datetime-adapter';
+import { Component, LOCALE_ID } from '@angular/core';
+import { DATE_TIME_FORMATS, DatetimeAdapter } from '@fundamental-ngx/core/datetime';
+import {
+    DAYJS_DATE_TIME_ADAPTER_OPTIONS,
+    DAYJS_DATETIME_FORMATS,
+    DayjsDatetimeAdapter
+} from '@fundamental-ngx/datetime-adapter';
 
 @Component({
     selector: 'fd-dayjs-adapter-options-example',
     templateUrl: './dayjs-adapter-options-example.component.html',
     providers: [
+        // Note that this is usually provided in the root of your application.
+        // Due to the limit of this example we must provide it on this level.
+        { provide: DATE_TIME_FORMATS, useValue: DAYJS_DATETIME_FORMATS },
         { provide: DatetimeAdapter, useClass: DayjsDatetimeAdapter },
         { provide: DAYJS_DATE_TIME_ADAPTER_OPTIONS, useValue: { useUtc: true, strict: true } }
     ]
