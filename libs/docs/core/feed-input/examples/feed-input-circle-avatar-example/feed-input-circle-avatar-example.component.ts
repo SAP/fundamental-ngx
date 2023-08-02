91898564843b83ef59214b4d0e1c0056f6817b01
IB002 <132930816+IB002@users.noreply.github.com>
2023-08-02 16:05:03 -0400

    feat(core): added feed input example

---
+++ libs/docs/core/feed-input/examples/feed-input-circle-avatar-example/feed-input-circle-avatar-example.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fd-feed-input-circle-avatar-example',
+    templateUrl: './feed-input-circle-avatar-example.component.html'
+})
+export class FeedInputCircleAvatarExampleComponent {}
