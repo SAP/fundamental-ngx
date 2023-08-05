485b6f0d0effd2c29ab02e2a250ea1247488faf1
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-08-06 02:09:42 +0400

    chore(platform): removed deprecated input table filter rule
    BREAKING CHANGE: fdp-table-filter-rule[compact] selector is removed, use [fdCompact] directive instead

--- libs/platform/src/lib/shared/platform-content-density-deprecations/platform-content-density-deprecations.module.ts
+++ libs/platform/src/lib/shared/platform-content-density-deprecations/platform-content-density-deprecations.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlatformContentDensityDeprecationDirective } from './directives/platform-content-density-deprecation.directive';
-import { PlatformCompactDeprecationDirective } from './directives/platform-compact-deprecation.directive';
 
 @NgModule({
-    declarations: [PlatformContentDensityDeprecationDirective, PlatformCompactDeprecationDirective],
+    declarations: [PlatformContentDensityDeprecationDirective],
     imports: [CommonModule],
-    exports: [PlatformContentDensityDeprecationDirective, PlatformCompactDeprecationDirective]
+    exports: [PlatformContentDensityDeprecationDirective]
 })
 export class PlatformContentDensityDeprecationsModule {}
