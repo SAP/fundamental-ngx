f2f520249d14cea0a73d7092d7d7ca562241e608
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-08-09 23:51:42 +0400

    chore(core): removed deprecated class
    BREAKING CHANGE: removed deprecated `TimeI18n` class, use `coreTime` i18n keys instead

--- libs/core/src/lib/time/index.ts
+++ libs/core/src/lib/time/index.ts
@@ -2,4 +2,3 @@ export * from './time.module';
 export * from './time.component';
 export * from './time-column/time-column.component';
 export * from './time-column/time-column-config';
-export * from './i18n/time-i18n';
