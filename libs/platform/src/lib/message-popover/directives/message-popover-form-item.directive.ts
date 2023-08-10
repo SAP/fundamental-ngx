261f5ac8c689c410175ae5c10e03af9946480d12
N1XUS <denis.severin@valor-software.com>
2023-08-10 09:33:15 +0300

    feat(platform): message popover horizon update (#10317)
    * feat(platform): message popover horizon update
    
    * fix(platform): correct button state

--- libs/platform/src/lib/message-popover/directives/message-popover-form-item.directive.ts
+++ libs/platform/src/lib/message-popover/directives/message-popover-form-item.directive.ts
@@ -1,14 +1,20 @@
 import { Directive, ElementRef, Input, Optional } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { MessagePopoverConfig } from '../default-config';
 
 @Directive({
-    selector: '[fdpMessagePopoverFormItem]'
+    selector: '[fdpMessagePopoverFormItem]',
+    standalone: true
 })
 export class MessagePopoverFormItemDirective {
     /** Form item name. */
     @Input('fdpMessagePopoverFormItem')
     label: string;
 
+    /** Error type definition. */
+    @Input()
+    errorTypes: MessagePopoverConfig['errors'];
+
     /** @hidden */
-    constructor(public elementRef: ElementRef, @Optional() public control: NgControl) {}
+    constructor(public readonly elementRef: ElementRef, @Optional() public readonly control: NgControl) {}
 }
