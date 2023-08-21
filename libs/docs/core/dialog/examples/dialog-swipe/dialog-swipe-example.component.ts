cfc39e70a3aae8255283403c97cac69bcf94bd40
IB002 <132930816+IB002@users.noreply.github.com>
2023-08-21 15:28:17 -0400

    docs(core): added extra layer to swipe example

--- libs/docs/core/dialog/examples/dialog-swipe/dialog-swipe-example.component.ts
+++ libs/docs/core/dialog/examples/dialog-swipe/dialog-swipe-example.component.ts
@@ -1,40 +1,61 @@
 import { Component, TemplateRef } from '@angular/core';
 import { DialogService } from '@fundamental-ngx/core/dialog';
 
+type message = { icon: string; messageTitle: string; messageInfo: string };
+
 @Component({
     selector: 'fd-dialog-swipe-example',
     templateUrl: './dialog-swipe-example.component.html'
 })
 export class DialogSwipeExampleComponent {
-    elementSelected: boolean = false;
+    messageSelected: boolean = false;
+    subMessageSelected: boolean = false;
     dialogTitle: string = 'Swipe Dialog';
-    messageList = [
-        {icon: 'error',messageTitle: 'File not uploaded',messageInfo: 'Uploaded file does not meet security requirements'},
-        {icon: 'error',messageTitle: 'Missing required documents',messageInfo: 'The following documents were not found ...'},
-        {icon: 'warning', messageTitle: 'Missing Data', messageInfo: 'Missing the folowing data ...'},
-        {icon: 'message-information',messageTitle: 'Changes to upload policy',messageInfo: 'For better security measures the following ...'},
-        {icon: 'incident', messageTitle: 'Improvement Survey', messageInfo: 'Complete the following survey'}
+    messageList: message[] = [
+        {
+            icon: 'error',
+            messageTitle: 'File not uploaded',
+            messageInfo: 'Uploaded file does not meet security requirements'
+        },
+        { icon: 'error', messageTitle: 'Missing documents', messageInfo: 'The following documents were not found ...' },
+        { icon: 'warning', messageTitle: 'Missing Data', messageInfo: 'Missing the folowing data ...' },
+        {
+            icon: 'message-information',
+            messageTitle: 'Changes to upload policy',
+            messageInfo: 'For better security measures the following ...'
+        },
+        { icon: 'incident', messageTitle: 'Improvement Survey', messageInfo: 'Complete the following survey' }
     ];
-    content: string = '';
+    arrList = [1, 2, 3];
+    newMessage: message = { icon: '', messageTitle: '', messageInfo: '' };
 
     constructor(private _dialogService: DialogService) {}
 
     openDialog(dialog: TemplateRef<any>): void {
         const dialogRef = this._dialogService.open(dialog, {
             draggable: true,
-            verticalPadding: false
+            verticalPadding: false,
+            width: '500px',
+            height: '600px'
         });
     }
 
-    openElement(numb: number) {
-        this.content = this.messageList[numb].messageInfo;
-        this.dialogTitle = this.messageList[numb].messageTitle;
-        this.elementSelected = true;
+    openMessage(temp: message) {
+        this.newMessage = temp;
+        this.dialogTitle = temp.messageTitle;
+        this.messageSelected = true;
     }
-
-    returnToMain() {
-        this.content = '';
-        this.dialogTitle = 'Swipe Dialog';
-        this.elementSelected = false;
+    openSubMessage(numb: number) {
+        this.dialogTitle = this.newMessage.messageTitle + ' ' + numb;
+        this.subMessageSelected = true;
+    }
+    returnToPrevious() {
+        if (this.subMessageSelected === true) {
+            this.dialogTitle = this.newMessage.messageTitle;
+            this.subMessageSelected = false;
+        } else {
+            this.dialogTitle = 'Swipe Dialog';
+            this.messageSelected = false;
+        }
     }
 }
