7bf4342b600c69e744104c9d5d0297a9c0e94fd0
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-08-31 17:16:14 +0400

    feat(platform): added aria-labelledby to the table

--- libs/platform/src/lib/table/components/table-toolbar/table-toolbar.ts
+++ libs/platform/src/lib/table/components/table-toolbar/table-toolbar.ts
@@ -1,8 +1,12 @@
 import { InjectionToken, TemplateRef } from '@angular/core';
 
-export interface TableToolbarWithTemplate {
+export interface TableToolbarInterface {
+    tableToolbarTitleId: string;
     contentTemplateRef: TemplateRef<any>;
 }
+
+/** @deprecated Use TableToolbarInterface instead */
+export type TableToolbarWithTemplate = TableToolbarInterface;
 
 /**
  * Used to provide a table-toolbar to the table component without causing a circular dependency.
