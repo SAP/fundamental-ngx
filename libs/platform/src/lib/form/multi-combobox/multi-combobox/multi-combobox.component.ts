5d69453665b5bb99cccd9ca0673defa2bb5ff14b
skaquib123 <sk.aquib.javed@sap.com>
2023-09-08 21:17:21 +0530

    fix(platform): fdp multi combobox fires multiple times after value is selected

--- libs/platform/src/lib/form/multi-combobox/multi-combobox/multi-combobox.component.ts
+++ libs/platform/src/lib/form/multi-combobox/multi-combobox/multi-combobox.component.ts
@@ -368,8 +368,6 @@ export class MultiComboboxComponent extends BaseMultiCombobox implements OnInit,
         // selected items should be displayed in the same order as options
         const valueIndexes = new Map<any, number>(this._suggestions.map((s, i) => [s.value, i]));
         this._selectedSuggestions.sort((a, b) => valueIndexes.get(a.value)! - valueIndexes.get(b.value)!);
-        this._propagateChange();
-
         this._tokenizer.onResize();
 
         this._tokenizer.tokenizerInnerEl.nativeElement.scrollLeft =
@@ -402,8 +400,6 @@ export class MultiComboboxComponent extends BaseMultiCombobox implements OnInit,
 
         // setting value, it will call setValue()
         this.value = selectedItems;
-
-        this._emitChangeEvent();
     }
 
     /** @hidden */
