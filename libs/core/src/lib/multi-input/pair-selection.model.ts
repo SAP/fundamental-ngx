781046cff13b0693eee975757b26a13a3f7d2dc1
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-09-29 17:52:51 +0400

    feat(core): multi input value identifier (#10601)
    * chore: moved object nesting models into the cdk library
    
    * feat(core): added ability to pass identifier to the option items
    
    * fix: fixed unit tests

---
+++ libs/core/src/lib/multi-input/pair-selection.model.ts
@@ -0,0 +1,44 @@
+import { EventEmitter } from '@angular/core';
+
+export class PairSelectionModel<KeyType, ValueType> {
+    /** @hidden */
+    _selected = new Map<KeyType, ValueType>();
+    /** Event emitted when the selection has changed. */
+    selectionChanged = new EventEmitter<[KeyType, ValueType][]>();
+
+    /** Array of selected items */
+    get selected(): ValueType[] {
+        return [...this._selected.values()];
+    }
+
+    /** Whether the given key exists in the record */
+    isSelected(key: KeyType): boolean {
+        return this._selected.has(key);
+    }
+
+    /** Selects the given key-value pair */
+    select(values: [KeyType, ValueType][]): void;
+    /** Selects the given key-value pair */
+    select(key: KeyType, value: ValueType): void;
+    /** Selects the given key-value pair */
+    select(keyOrValues: KeyType | Array<[KeyType, ValueType]>, value?: ValueType): void {
+        if (value) {
+            this._selected.set(keyOrValues as KeyType, value);
+        } else {
+            (keyOrValues as Array<[KeyType, ValueType]>).forEach(([key, v]) => this._selected.set(key, v));
+        }
+        this.selectionChanged.emit([...this._selected]);
+    }
+
+    /** Deselects the given key */
+    deselect(key: KeyType): void {
+        this._selected.delete(key);
+        this.selectionChanged.emit([...this._selected]);
+    }
+
+    /** Clears the selection information */
+    clear(): void {
+        this._selected.clear();
+        this.selectionChanged.emit([...this._selected]);
+    }
+}
