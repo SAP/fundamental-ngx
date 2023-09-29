989873eda6c56f88cc3e9ea55f51601b6a6af6fb
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-09-29 17:52:25 +0400

    feat(cdk): created object nesting utility types

--- libs/cdk/src/lib/utils/datatypes/index.ts
+++ libs/cdk/src/lib/utils/datatypes/index.ts
@@ -1,3 +1,4 @@
-export * from './hash.datatype';
-export * from './size.datatype';
 export * from './color-accent.datatype';
+export * from './hash.datatype';
+export * from './object-nesting.types';
+export * from './size.datatype';
