4acd9185f0b2cac670850f4fa6ada627110c9961
Denis Severin <Denis_severin@icloud.com>
2023-10-13 16:42:02 +0300

    fix(btp): fix unit test run

--- libs/btp/navigation/jest.config.ts
+++ libs/btp/navigation/jest.config.ts
@@ -3,7 +3,7 @@ import baseConfig from '../../../jest.config.base';
 export default {
     ...baseConfig,
     displayName: 'btp-navigation',
-    preset: '../../../../../jest.preset.js',
+    preset: '../../../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts', '../../../jest-extended-matchers.ts'],
     coverageDirectory: '../../../dist/coverage/btp-navigation'
 };
