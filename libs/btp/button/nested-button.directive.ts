7d0c5fbf5d0a7f6dab4963d673a7f5da9d125fd1
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-11-02 16:37:39 +0400

    feat(btp): introduced squared nested buttons

--- libs/btp/button/nested-button.directive.ts
+++ libs/btp/button/nested-button.directive.ts
@@ -1,12 +1,23 @@
-import { Directive, Input } from '@angular/core';
+import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
+import { Directive, Input, OnChanges } from '@angular/core';
 import { ButtonTypeGuard } from './button-type-guard';
 
 @Directive({
     selector: '[fd-button][fdbNestedButton]',
     standalone: true
 })
-export class NestedButtonDirective extends ButtonTypeGuard {
+export class NestedButtonDirective extends ButtonTypeGuard implements OnChanges {
     /** Type of the button. In case of fdbNestedButton it is always a `nested` */
     @Input()
-    fdType: 'nested' = 'nested' as const;
+    fdType: 'nested' | 'nested-square' = 'nested' as const;
+
+    /** Whether the button should have squared form */
+    @Input({ transform: coerceBooleanProperty })
+    square: BooleanInput;
+
+    /** @hidden */
+    ngOnChanges(): void {
+        this.fdType = this.square ? 'nested-square' : 'nested';
+        super.ngOnChanges();
+    }
 }
--- libs/docs/btp/button/examples/nested-example.component.ts
+++ libs/docs/btp/button/examples/nested-example.component.ts
@@ -6,9 +6,15 @@ import { ButtonComponent } from '@fundamental-ngx/core/button';
 @Component({
     selector: 'fdb-button-nested-example',
     template: `
-        <div [ngStyle]="{ display: 'flex', gap: '0.5rem' }">
+        <h4 id="roundedNestedButtonHeading">Rounded nested button</h4>
+        <div [ngStyle]="{ display: 'flex', gap: '0.5rem' }" aria-describedby="roundedNestedButtonHeading">
             <button fd-button fdbNestedButton ariaLabel="Go Home" glyph="home"></button>
             <button fd-button fdbNestedButton ariaLabel="Cancel" glyph="decline"></button>
+        </div>
+        <h4 id="squaredNestedButtonHeading">Squared nested button</h4>
+        <div [ngStyle]="{ display: 'flex', gap: '0.5rem' }" aria-describedby="squaredNestedButtonHeading">
+            <button fd-button fdbNestedButton square ariaLabel="Go Home" glyph="home"></button>
+            <button fd-button fdbNestedButton square ariaLabel="Cancel" glyph="decline"></button>
         </div>
     `,
     standalone: true,
