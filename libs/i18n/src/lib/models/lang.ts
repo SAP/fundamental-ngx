28e7b20f69c370cd3a4fc083beb2fba53228ec0b
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2023-11-08 10:41:47 -0500

    fix(platform): remove title tooltip from table checkbox (#10907)
    Co-authored-by: deno <mladen.droshev@sap.com>

--- libs/i18n/src/lib/models/lang.ts
+++ libs/i18n/src/lib/models/lang.ts
@@ -674,8 +674,6 @@ export interface FdLanguage {
         deselectAllCheckboxLabel: FdLanguageKey;
         deselectSingleRow: FdLanguageKey;
         selectSingleRow: FdLanguageKey;
-        deselectSingleRowTitle: FdLanguageKey;
-        selectSingleRowTitle: FdLanguageKey;
     };
     platformThumbnail: {
         detailsGotoPreviousButtonTitle: FdLanguageKey;
