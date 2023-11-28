fff9ddace08aa3794f1313fd515defaf238fd692
droshev <droshev@users.noreply.github.com>
2023-11-28 17:44:44 +0000

    Deploying to gh-pages from @ SAP/fundamental-ngx@5ca289a4c79f10dbdb0fb7607c142fd0626b6900 🚀

--- docs/core/segmented-button/examples/segmented-button-toggle-example.component.ts
+++ docs/core/segmented-button/examples/segmented-button-toggle-example.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FocusableItemDirective } from '@fundamental-ngx/cdk/utils';
 import { ButtonComponent } from '@fundamental-ngx/core/button';
 import { SegmentedButtonModule } from '@fundamental-ngx/core/segmented-button';
 
@@ -7,7 +8,7 @@ import { SegmentedButtonModule } from '@fundamental-ngx/core/segmented-button';
     selector: 'fd-segmented-button-toggle-example',
     templateUrl: './segmented-button-toggle-example.component.html',
     standalone: true,
-    imports: [SegmentedButtonModule, FormsModule, ButtonComponent]
+    imports: [SegmentedButtonModule, FormsModule, ButtonComponent, FocusableItemDirective]
 })
 export class SegmentedButtonToggleExampleComponent {
     value: string[] = ['first'];
