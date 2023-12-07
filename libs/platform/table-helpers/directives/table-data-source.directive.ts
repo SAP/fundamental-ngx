2a173442babbd58c43d45610cc44baf2353e9f4c
Denis Severin <Denis_severin@icloud.com>
2023-12-07 16:32:00 +0200

    fix(platform): reset table rows before new rows emitted

--- libs/platform/table-helpers/directives/table-data-source.directive.ts
+++ libs/platform/table-helpers/directives/table-data-source.directive.ts
@@ -233,5 +233,6 @@ export class TableDataSourceDirective<T> extends DataSourceDirective<T, TableDat
 
         this._tableDsSubscription?.unsubscribe();
         this._tableDsSubscription = null;
+        this._table.clearTableRows();
     }
 }
