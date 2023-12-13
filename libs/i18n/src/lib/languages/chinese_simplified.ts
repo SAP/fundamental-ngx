eee0844ab1e66ef14faf9c5cfe329ef8d8ac297e
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-12-13 21:45:53 +0400

    chore: fixed missing export name

--- libs/i18n/src/lib/languages/chinese_simplified.ts
+++ libs/i18n/src/lib/languages/chinese_simplified.ts
@@ -2,3 +2,4 @@ import { FdLanguage } from '../models';
 import json from '../translations/translations_zh_CN';
 
 export const FD_LANGUAGE_CHINESE_SIMPLIFIED = json as FdLanguage;
+export const FD_LANGUAGE_CHINESE = json as FdLanguage;
