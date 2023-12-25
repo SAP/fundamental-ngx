d5aa7b96ccd85d18209d539a5940aa8956727e64
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-12-26 00:27:48 +0400

    chore: removed redundant splitter index and updated fd styles version

--- libs/btp/splitter/index.ts
+++ libs/btp/splitter/index.ts
@@ -1,6 +1,7 @@
 export * from './interfaces/splitter-pane-resize-event.interface';
 export * from './splitter-pagination/splitter-pagination.component';
 export * from './splitter-pane-container/splitter-pane-container.component';
+export * from './splitter-pane-container/splitter-pane-orientation.enum';
 export * from './splitter-resizer/splitter-resizer.component';
 export * from './splitter-split-pane/splitter-split-pane.component';
 export * from './splitter.component';
