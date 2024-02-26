d6f522099a0628b03afaf2d3812ac013aa26403a
N1XUS <denis.severin@valor-software.com>
2024-02-26 13:25:58 +0200

    feat(core): migrate switch to use cva directive (#11460)

--- libs/core/switch/switch.component.spec.ts
+++ libs/core/switch/switch.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { CVATestSteps, runValueAccessorTests } from 'ngx-cva-test-suite';
 
-import { ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectorRef, Type } from '@angular/core';
 import { SwitchComponent } from './switch.component';
+import { ControlValueAccessor } from '@angular/forms';
 
 describe('SwitchComponent', () => {
     let component: SwitchComponent;
@@ -51,11 +53,11 @@ describe('SwitchComponent', () => {
 
     it('should accept custom name', () => {
         const name = 'custom-name';
-        component.name = name;
+        component._cva.name = name;
 
         detectChangesOnPush();
 
-        expect(input.getAttribute('ng-reflect-name')).toEqual(component.name);
+        expect(input.getAttribute('ng-reflect-name')).toEqual(component._cva.name);
     });
 
     it('should auto-generate id', () => {
@@ -95,5 +97,41 @@ describe('SwitchComponent', () => {
 
         const switchComp = fixture.nativeElement.querySelector('.fd-switch');
         expect(switchComp.classList).toContain('fd-switch--semantic');
+    });
+});
+
+describe('SwitchComponent component CVA', () => {
+    runValueAccessorTests({
+        /** Component, that is being tested */
+        component: SwitchComponent as unknown as Type<Required<ControlValueAccessor>>,
+        /**
+         * All the metadata required for this test to run.
+         * Under the hood calls TestBed.configureTestingModule with provided config.
+         */
+        testModuleMetadata: {
+            imports: [SwitchComponent]
+        },
+        hostTemplate: {
+            hostComponent: SwitchComponent,
+            getTestingComponent: (fixture) => fixture.componentInstance._cva
+        },
+        /** Whether component is able to track "onBlur" events separately */
+        supportsOnBlur: false,
+        /**
+         * CSS selector for the element, that should dispatch `blur` event.
+         * Required and used only if `supportsOnBlur` is set to true.
+         */
+        nativeControlSelector: '.fd-switch__input',
+        /**
+         * Tests the correctness of an approach that is used to set value in the component,
+         * when the change is internal. It's optional and can be omitted by passing "null"
+         */
+        internalValueChangeSetter: (fixture, value) => {
+            fixture.componentInstance._cva.setValue(value, true);
+        },
+        /** Function to get the value of a component in a runtime. */
+        getComponentValue: (fixture: ComponentFixture<SwitchComponent>) => fixture.componentInstance.isChecked,
+
+        excludeSteps: [CVATestSteps.ValueChangedInternally],
     });
 });
