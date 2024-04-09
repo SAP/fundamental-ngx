c84f9401ae6d2e02492745d2fc44e2817ef19c46
droshev <mladen.droshev@sap.com>
2024-04-08 20:44:12 -0400

    docs(core): notification stackblits

--- libs/docs/core/notification/notification-docs.component.ts
+++ libs/docs/core/notification/notification-docs.component.ts
@@ -17,6 +17,7 @@ import { NotificationOpenTemplateExampleComponent } from './examples/template-as
 
 const componentAsContentTs = 'component-as-content/notification-component-as-content-example.component.ts';
 const contentTs = 'component-as-content/notification-content.component.ts';
+const contentHtml = 'component-as-content/notification-content.component.html';
 const optionsTs = 'notification-options/notification-options-example.component.ts';
 const optionsH = 'notification-options/notification-options-example.component.html';
 const templateTs = 'template-as-content/notification-open-template-example.component.ts';
@@ -60,6 +61,12 @@ export class NotificationDocsComponent {
             fileName: 'notification-content',
             component: 'NotificationExampleContentComponent',
             entryComponent: true
+        },
+        {
+            language: 'html',
+            code: getAssetFromModuleAssets(contentHtml),
+            fileName: 'notification-content',
+            component: 'NotificationExampleContentComponent'
         }
     ];
 
