7a5b655d8add101525176c664285865b5f14e8b0
droshev <mladen.droshev@sap.com>
2024-04-12 14:24:16 -0400

    docs(core): tabs stackblitz

--- libs/docs/core/tabs/examples/tab-selection-example/tab-selection-example.component.ts
+++ libs/docs/core/tabs/examples/tab-selection-example/tab-selection-example.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '@fundamental-ngx/core/button';
 import { FormLabelComponent } from '@fundamental-ngx/core/form';
 import { TabsModule } from '@fundamental-ngx/core/tabs';
+import { TextComponent } from '@fundamental-ngx/core/text';
 
 @Component({
     selector: 'fd-tab-selection-example',
     templateUrl: './tab-selection-example.component.html',
     styleUrls: ['tab-selection-example.component.scss'],
     standalone: true,
-    imports: [TabsModule, FormLabelComponent, ButtonComponent]
+    imports: [TabsModule, FormLabelComponent, ButtonComponent, TextComponent]
 })
 export class TabSelectionExampleComponent {}
