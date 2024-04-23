449f1e3706b9ba18d5157315450327d72255d750
deno <mladen.droshev@sap.com>
2024-04-23 14:52:32 -0400

    docs(platform): button stackblitz (#11805)

--- libs/docs/platform/button/examples/platform-button-examples.component.ts
+++ libs/docs/platform/button/examples/platform-button-examples.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ContentDensityDirective } from '@fundamental-ngx/core/content-density';
 import { PlatformButtonModule } from '@fundamental-ngx/platform/button';
 
 @Component({
@@ -12,15 +11,6 @@ import { PlatformButtonModule } from '@fundamental-ngx/platform/button';
 export class PlatformButtonTypesExampleComponent {}
 
 @Component({
-    selector: 'fdp-button-sizes-example',
-    templateUrl: './platform-button-sizes-example.component.html',
-    styleUrls: ['./platform-button-examples.scss'],
-    standalone: true,
-    imports: [PlatformButtonModule, ContentDensityDirective]
-})
-export class PlatformButtonSizesExampleComponent {}
-
-@Component({
     selector: 'fdp-button-icons-example',
     templateUrl: './platform-button-icons-example.component.html',
     styleUrls: ['./platform-button-examples.scss'],
@@ -29,14 +19,6 @@ export class PlatformButtonSizesExampleComponent {}
 })
 export class PlatformButtonIconsExampleComponent {}
 
-@Component({
-    selector: 'fdp-button-state-example',
-    templateUrl: './platform-button-state-example.component.html',
-    styleUrls: ['./platform-button-examples.scss'],
-    standalone: true,
-    imports: [PlatformButtonModule]
-})
-export class PlatformButtonStateExampleComponent {}
 @Component({
     selector: 'fdp-button-truncate-example',
     templateUrl: './platform-button-truncate-example.component.html',
