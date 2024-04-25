bfa2f7e7e79ddca1fb037e3a6a5f4330b95a78aa
droshev <mladen.droshev@sap.com>
2024-04-25 12:09:45 -0400

    docs(platform): fdp link stackblitz and docs

--- libs/docs/platform/link/platform-link-docs.component.ts
+++ libs/docs/platform/link/platform-link-docs.component.ts
@@ -9,23 +9,10 @@ import {
     SeparatorComponent,
     getAssetFromModuleAssets
 } from '@fundamental-ngx/docs/shared';
-import {
-    PlatformLinkDisabledEmphasizedExampleComponent,
-    PlatformLinkDisabledExampleComponent,
-    PlatformLinkEmphasizedExampleComponent,
-    PlatformLinkIconExampleComponent,
-    PlatformLinkInvertedExampleComponent,
-    PlatformLinkStandardExampleComponent,
-    PlatformLinkTruncatedExampleComponent
-} from './examples/platform-link-examples.component';
+import { PlatformLinkExampleComponent } from './examples/platform-link-example.component';
 
-const standardlinkSrc = 'platform-link-standard-example.component.html';
-const emphasizedlinkSrc = 'platform-link-emphasized-example.component.html';
-const disabledlinkSrc = 'platform-link-disabled-example.component.html';
-const disabledEmphasizedlinkSrc = 'platform-link-disabled-emphasized-example.component.html';
-const invertedlinkSrc = 'platform-link-inverted-example.component.html';
-const truncatedlinkSrc = 'platform-link-truncated-example.component.html';
-const iconlinkSrc = 'platform-link-icon-example.component.html';
+const iconlinkSrc = 'platform-link-example.component.html';
+const iconlinkTs = 'platform-link-example.component.ts';
 
 @Component({
     selector: 'app-link',
@@ -35,71 +22,23 @@ const iconlinkSrc = 'platform-link-icon-example.component.html';
         DocsSectionTitleComponent,
         DescriptionComponent,
         ComponentExampleComponent,
-        PlatformLinkIconExampleComponent,
+        PlatformLinkExampleComponent,
         CodeExampleComponent,
-        SeparatorComponent,
-        PlatformLinkStandardExampleComponent,
-        PlatformLinkEmphasizedExampleComponent,
-        PlatformLinkDisabledExampleComponent,
-        PlatformLinkDisabledEmphasizedExampleComponent,
-        PlatformLinkInvertedExampleComponent,
-        PlatformLinkTruncatedExampleComponent
+        SeparatorComponent
     ]
 })
 export class PlatformLinkDocsComponent {
-    standardLink: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(standardlinkSrc),
-            fileName: 'platform-link-standard-example'
-        }
-    ];
-
-    emphasizedLink: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(emphasizedlinkSrc),
-            fileName: 'platform-link-emphasized-example'
-        }
-    ];
-
-    disabledLink: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(disabledlinkSrc),
-            fileName: 'platform-link-disabled-example'
-        }
-    ];
-
-    disabledEmphasizedLink: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(disabledEmphasizedlinkSrc),
-            fileName: 'platform-link-disabled-emphasized-example'
-        }
-    ];
-
-    invertedLink: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(invertedlinkSrc),
-            fileName: 'platform-link-inverted-example'
-        }
-    ];
-
-    truncatedLink: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(truncatedlinkSrc),
-            fileName: 'platform-link-truncated-example'
-        }
-    ];
-
     iconLink: ExampleFile[] = [
         {
             language: 'html',
             code: getAssetFromModuleAssets(iconlinkSrc),
-            fileName: 'platform-link-icon-example'
+            fileName: 'platform-link-example'
+        },
+        {
+            language: 'typescript',
+            code: getAssetFromModuleAssets(iconlinkTs),
+            fileName: 'platform-link-example',
+            component: 'PlatformLinkExampleComponent'
         }
     ];
 }
