28e025f5906b103f9dd056678c3d84456e987a8d
deno <mladen.droshev@sap.com>
2024-04-28 11:08:53 -0400

    docs(platform): object status stackblitz (#11839)

---
+++ libs/docs/platform/object-status/examples/platform-object-status-generic-text-example.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ColorAccent } from '@fundamental-ngx/cdk';
+import { ObjectStatusComponent } from '@fundamental-ngx/platform/object-status';
+
+@Component({
+    selector: 'fdp-platform-object-status-generic-text-example',
+    templateUrl: './platform-object-status-generic-text-example.component.html',
+    styleUrls: ['./platform-object-status-example.component.scss'],
+    standalone: true,
+    imports: [ObjectStatusComponent]
+})
+export class PlatformObjectStatusGenericExampleComponent {
+    items: ColorAccent[] = new Array(8).fill(null).map((_, index) => (index + 1) as ColorAccent);
+}
--- libs/docs/platform/object-status/platform-object-status-docs.component.ts
+++ libs/docs/platform/object-status/platform-object-status-docs.component.ts
@@ -11,23 +11,26 @@ import {
 import { PlatformObjectStatusClickableAndIconExampleComponent } from './examples/platform-object-status-clickable-and-icon-example.component';
 import {
     PlatformObjectStatusExampleComponent,
-    PlatformObjectStatusGenericExampleComponent,
-    PlatformObjectStatusInvertedGenericTextExampleComponent,
     PlatformObjectStatusInvertedTextExampleComponent,
     PlatformObjectStatusTextExampleComponent,
     PlatformObjectStatusTextIconExampleComponent
 } from './examples/platform-object-status-example.component';
+import { PlatformObjectStatusGenericExampleComponent } from './examples/platform-object-status-generic-text-example.component';
+import { PlatformObjectStatusInvertedGenericTextExampleComponent } from './examples/platform-object-status-inverted-generic-text-example.component';
 import { PlatformObjectStatusLargeExampleComponent } from './examples/platform-object-status-large-example.component';
 
 const PlatformObjectStatusDefaultExampleScss = 'platform-object-status-example.component.scss';
 const PlatformObjectStatusDefaultExample = 'platform-object-status-example.component.html';
 const PlatformObjectStatusTextExample = 'platform-object-status-text-example.component.html';
 const PlatformObjectStatusGenericTextExample = 'platform-object-status-generic-text-example.component.html';
+const platformObjectStatusGenericTextExampleTs = 'platform-object-status-generic-text-example.component.ts';
 const PlatformObjectStatusTextIconExample = 'platform-object-status-icon-text-example.component.html';
 const PlatformObjectStatusClickableAndIConExample = 'platform-object-status-clickable-and-icon-example.component.html';
 const PlatformObjectStatusInvertedTextExample = 'platform-object-status-inverted-example.component.html';
 const PlatformObjectStatusInvertedGenericExample =
     'platform-object-status-inverted-generic-text-example.component.html';
+const platformObjectStatusInvertedGenericExampleTs =
+    'platform-object-status-inverted-generic-text-example.component.ts';
 const PlatformObjectStatusLargeExample = 'platform-object-status-large-example.component.html';
 const PlatformObjectStatusClickableAndIconExampleTs = 'platform-object-status-clickable-and-icon-example.component.ts';
 const PlatformObjectStatusLargeExampleTs = 'platform-object-status-large-example.component.ts';
@@ -59,8 +62,12 @@ export class PlatformObjectStatusDocsComponent {
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExample),
-            fileName: 'platform-object-status-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            fileName: 'platform-object-status-example'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
         }
     ];
 
@@ -68,17 +75,31 @@ export class PlatformObjectStatusDocsComponent {
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusTextExample),
-            fileName: 'platform-object-status-text-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            fileName: 'platform-object-status-text-example'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
         }
     ];
 
-    platformObjectStatusTextIconExample: ExampleFile[] = [
+    platformObjectStatusGenericTextExample: ExampleFile[] = [
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusGenericTextExample),
+            fileName: 'platform-object-status-generic-text-example'
+        },
+        {
+            language: 'typescript',
+            code: getAssetFromModuleAssets(platformObjectStatusGenericTextExampleTs),
             fileName: 'platform-object-status-generic-text-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            component: 'PlatformObjectStatusGenericExampleComponent'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
         }
     ];
 
@@ -86,8 +107,12 @@ export class PlatformObjectStatusDocsComponent {
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusTextIconExample),
-            fileName: 'platform-object-status-icon-text-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            fileName: 'platform-object-status-icon-text-example'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
         }
     ];
 
@@ -95,14 +120,18 @@ export class PlatformObjectStatusDocsComponent {
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusClickableAndIConExample),
-            fileName: 'platform-object-status-clickable-and-icon-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            fileName: 'platform-object-status-clickable-and-icon-example'
         },
         {
             language: 'typescript',
             code: getAssetFromModuleAssets(PlatformObjectStatusClickableAndIconExampleTs),
             fileName: 'platform-object-status-clickable-and-icon-example',
             component: 'PlatformObjectStatusClickableAndIconExampleComponent'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
         }
     ];
 
@@ -110,8 +139,12 @@ export class PlatformObjectStatusDocsComponent {
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusInvertedTextExample),
-            fileName: 'platform-object-status-inverted-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            fileName: 'platform-object-status-inverted-example'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
         }
     ];
 
@@ -119,8 +152,18 @@ export class PlatformObjectStatusDocsComponent {
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusInvertedGenericExample),
+            fileName: 'platform-object-status-inverted-generic-text-example'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
+        },
+        {
+            language: 'typescript',
+            code: getAssetFromModuleAssets(platformObjectStatusInvertedGenericExampleTs),
             fileName: 'platform-object-status-inverted-generic-text-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            component: 'PlatformObjectStatusInvertedGenericTextExampleComponent'
         }
     ];
 
@@ -128,14 +171,18 @@ export class PlatformObjectStatusDocsComponent {
         {
             language: 'html',
             code: getAssetFromModuleAssets(PlatformObjectStatusLargeExample),
-            fileName: 'platform-object-status-large-example',
-            scssFileCode: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss)
+            fileName: 'platform-object-status-large-example'
         },
         {
             language: 'typescript',
             code: getAssetFromModuleAssets(PlatformObjectStatusLargeExampleTs),
             fileName: 'platform-object-status-large-example',
             component: 'PlatformObjectStatusLargeExampleComponent'
+        },
+        {
+            language: 'scss',
+            code: getAssetFromModuleAssets(PlatformObjectStatusDefaultExampleScss),
+            fileName: 'platform-object-status-example'
         }
     ];
 }
