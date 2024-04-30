130490d90bbe002330096a353d6c05441917d40a
deno <mladen.droshev@sap.com>
2024-04-30 09:07:04 -0400

    docs(platform): smart filter bar stackblitz (#11856)

--- libs/docs/platform/smart-filter-bar/platform-smart-filter-bar-docs.component.ts
+++ libs/docs/platform/smart-filter-bar/platform-smart-filter-bar-docs.component.ts
@@ -6,7 +6,7 @@ import {
     DocsSectionTitleComponent,
     ExampleFile,
     SeparatorComponent,
-    getAssetFromModuleAssets
+    getExampleFile
 } from '@fundamental-ngx/docs/shared';
 import { SmartFilterBarCustomToolbarExampleComponent } from './examples/custom-toolbar/smart-filter-bar-custom-toolbar-example.component';
 import { PlatformSmartFilterBarLoadingExampleComponent } from './examples/loading/platform-smart-filter-bar-loading-example.component';
@@ -15,27 +15,6 @@ import { PlatformSmartFilterBarCustomFilterExampleComponent } from './examples/p
 import { PlatformSmartFilterBarCustomLabelsExampleComponent } from './examples/platform-smart-filter-bar-custom-labels-example.component';
 import { PlatformSmartFilterBarDynamicPageExampleComponent } from './examples/platform-smart-filter-bar-dynamic-page-example.component';
 import { PlatformSmartFilterBarObservableExampleComponent } from './examples/platform-smart-filter-bar-observable-example.component';
-
-const platformBasicSfbSrc = 'platform-smart-filter-bar-basic-example.component.html';
-const platformBasicSfbTsCode = 'platform-smart-filter-bar-basic-example.component.ts';
-
-const platformSfbCustomDataSourceSrc = 'platform-smart-filter-bar-observable-example.component.html';
-const platformSfbCustomDataSourceTsCode = 'platform-smart-filter-bar-observable-example.component.ts';
-
-const platformSfbCustomFiltersSrc = 'platform-smart-filter-bar-custom-filter-example.component.html';
-const platformSfbCustomFiltersTsCode = 'platform-smart-filter-bar-custom-filter-example.component.ts';
-
-const platformSfbCustomLabelsSrc = 'platform-smart-filter-bar-custom-labels-example.component.html';
-const platformSfbCustomLabelsTsCode = 'platform-smart-filter-bar-custom-labels-example.component.ts';
-
-const platformSfbDynamicPageSrc = 'platform-smart-filter-bar-dynamic-page-example.component.html';
-const platformSfbDynamicPageTsCode = 'platform-smart-filter-bar-dynamic-page-example.component.ts';
-
-const platformLoadingSfbSrc = 'loading/platform-smart-filter-bar-loading-example.component.html';
-const platformLoadingSfbTsCode = 'loading/platform-smart-filter-bar-loading-example.component.ts';
-
-const platformCustomToolbarSfbSrc = 'custom-toolbar/smart-filter-bar-custom-toolbar-example.component.html';
-const platformCustomToolbarSfbTsCode = 'custom-toolbar/smart-filter-bar-custom-toolbar-example.component.ts';
 
 @Component({
     selector: 'app-smart-filter-bar',
@@ -58,100 +37,58 @@ const platformCustomToolbarSfbTsCode = 'custom-toolbar/smart-filter-bar-custom-t
 })
 export class PlatformSmartFilterBarDocsComponent {
     sfbBasic: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformBasicSfbSrc),
-            fileName: 'platform-smart-filter-bar-basic-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformBasicSfbTsCode),
-            fileName: 'platform-smart-filter-bar-basic-example',
+        getExampleFile('platform-smart-filter-bar-basic-example.component.html'),
+        getExampleFile('platform-smart-filter-bar-basic-example.component.ts', {
+            selector: 'platform-smart-filter-bar-basic-example',
             component: 'PlatformSmartFilterBarBasicExampleComponent'
-        }
+        })
     ];
 
     sfbCustomDataSource: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformSfbCustomDataSourceSrc),
-            fileName: 'platform-smart-filter-bar-observable-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformSfbCustomDataSourceTsCode),
-            fileName: 'platform-smart-filter-bar-observable-example',
+        getExampleFile('platform-smart-filter-bar-observable-example.component.html'),
+        getExampleFile('platform-smart-filter-bar-observable-example.component.ts', {
+            selector: 'smart-filter-bar-observable-example',
             component: 'PlatformSmartFilterBarObservableExampleComponent'
-        }
+        })
     ];
 
     sfbCustomFilters: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformSfbCustomFiltersSrc),
-            fileName: 'platform-smart-filter-bar-custom-filter-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformSfbCustomFiltersTsCode),
-            fileName: 'platform-smart-filter-bar-custom-filter-example',
+        getExampleFile('platform-smart-filter-bar-custom-filter-example.component.html'),
+        getExampleFile('platform-smart-filter-bar-custom-filter-example.component.ts', {
+            selector: 'platform-smart-filter-bar-custom-filter-example',
             component: 'PlatformSmartFilterBarCustomFilterExampleComponent'
-        }
+        })
     ];
 
     sfbCustomLabels: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformSfbCustomLabelsSrc),
-            fileName: 'platform-smart-filter-bar-custom-labels-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformSfbCustomLabelsTsCode),
-            fileName: 'platform-smart-filter-bar-custom-labels-example',
+        getExampleFile('platform-smart-filter-bar-custom-labels-example.component.html'),
+        getExampleFile('platform-smart-filter-bar-custom-labels-example.component.ts', {
+            selector: 'platform-smart-filter-bar-custom-labels-example',
             component: 'PlatformSmartFilterBarCustomLabelsExampleComponent'
-        }
+        })
     ];
 
     sfbDynamicPage: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformSfbDynamicPageSrc),
-            fileName: 'platform-smart-filter-bar-dynamic-page-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformSfbDynamicPageTsCode),
-            fileName: 'platform-smart-filter-bar-dynamic-page-example',
+        getExampleFile('platform-smart-filter-bar-dynamic-page-example.component.html'),
+        getExampleFile('platform-smart-filter-bar-dynamic-page-example.component.ts', {
+            selector: 'platform-smart-filter-bar-dynamic-page-example',
             component: 'PlatformSmartFilterBarDynamicPageExampleComponent'
-        }
+        })
     ];
 
     sfbLoading: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformLoadingSfbSrc),
-            fileName: 'platform-smart-filter-bar-loading-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformLoadingSfbTsCode),
-            fileName: 'platform-smart-filter-bar-loading-example',
+        getExampleFile('loading/platform-smart-filter-bar-loading-example.component.html'),
+        getExampleFile('loading/platform-smart-filter-bar-loading-example.component.ts', {
+            selector: 'platform-smart-filter-bar-loading-example',
             component: 'PlatformSmartFilterBarLoadingExampleComponent'
-        }
+        })
     ];
 
     sfbCustomToolbar: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformCustomToolbarSfbSrc),
-            fileName: 'smart-filter-bar-custom-toolbar-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformCustomToolbarSfbTsCode),
-            fileName: 'smart-filter-bar-custom-toolbar-example',
+        getExampleFile('custom-toolbar/smart-filter-bar-custom-toolbar-example.component.html'),
+        getExampleFile('custom-toolbar/smart-filter-bar-custom-toolbar-example.component.ts', {
+            selector: 'smart-filter-bar-custom-toolbar-example',
             component: 'SmartFilterBarCustomToolbarExampleComponent'
-        }
+        })
     ];
 }
