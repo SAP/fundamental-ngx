307227f8c185f1b08d78dbb3632e1255055b93fd
deno <mladen.droshev@sap.com>
2024-05-01 14:21:34 -0400

    docs(platform): form generator stackblitz (#11865)

--- libs/docs/platform/form-container/platform-form-container-docs.component.ts
+++ libs/docs/platform/form-container/platform-form-container-docs.component.ts
@@ -6,13 +6,13 @@ import {
     DocsSectionTitleComponent,
     ExampleFile,
     SeparatorComponent,
-    getAssetFromModuleAssets
+    getAssetFromModuleAssets,
+    getExampleFile
 } from '@fundamental-ngx/docs/shared';
 import { PlatformFieldColumnChangeExampleComponent } from './examples/platform-field-layout/platform-field-column-change-example.component';
 import { PlatformFieldIsInlineChangeExampleComponent } from './examples/platform-field-layout/platform-field-isinline-change-example.component';
 import { PlatformFormBasicExampleComponent } from './examples/platform-form-basic/platform-form-basic-example.component';
 import { PlatformFormContainerComplexExampleComponent } from './examples/platform-form-container-complex-example.component';
-import { PlatformFormContainerNotRecommendedExampleComponent } from './examples/platform-form-container-not-recommended-example.component';
 import { PlatformFormContainerPossibleExampleComponent } from './examples/platform-form-container-possible-example.component';
 import { PlatformFormContainerRecommendedExampleComponent } from './examples/platform-form-container-recommended-example.component';
 import { PlatformFormCustomLayoutComponent } from './examples/platform-form-custom-layout.component';
@@ -26,12 +26,6 @@ const platformNotRecommendedFormContainerSrc = 'platform-form-container-not-reco
 const platformNotRecommendedFormContainerTsCode = 'platform-form-container-not-recommended-example.component.ts';
 const platformComplexFormContainerSrc = 'platform-form-container-complex-example.component.html';
 const platformComplexFormContainerTsCode = 'platform-form-container-complex-example.component.ts';
-const platformColFormContainerSrc = 'platform-form-basic/platform-form-basic-example.component.html';
-const platformGFormContainerSrc = 'platform-form-group/platform-form-group-example.component.html';
-const platformFormFieldLayoutSrc = 'platform-field-layout/platform-field-column-change-example.component.html';
-const platformFormFieldLayoutTsCode = 'platform-field-layout/platform-field-column-change-example.component.ts';
-const platformFormFieldInlineLayoutSrc = 'platform-field-layout/platform-field-isinline-change-example.component.html';
-const platformFormFieldInlineLayoutTsCode = 'platform-field-layout/platform-field-isinline-change-example.component.ts';
 
 @Component({
     selector: 'app-form-container',
@@ -45,7 +39,6 @@ const platformFormFieldInlineLayoutTsCode = 'platform-field-layout/platform-fiel
         CodeExampleComponent,
         SeparatorComponent,
         PlatformFormContainerPossibleExampleComponent,
-        PlatformFormContainerNotRecommendedExampleComponent,
         PlatformFormContainerComplexExampleComponent,
         PlatformFormBasicExampleComponent,
         PlatformFormGroupExampleComponent,
@@ -83,19 +76,6 @@ export class PlatformFormContainerDocsComponent {
         }
     ];
 
-    formContainerNotRecommended: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformNotRecommendedFormContainerSrc),
-            fileName: 'platform-form-container-not-recommended-example'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformNotRecommendedFormContainerTsCode),
-            fileName: 'platform-form-container-not-recommended-example',
-            component: 'PlatformFormContainerNotRecommendedExampleComponent'
-        }
-    ];
     formContainerComplex: ExampleFile[] = [
         {
             language: 'html',
@@ -111,50 +91,34 @@ export class PlatformFormContainerDocsComponent {
     ];
 
     formContainerColumn: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformColFormContainerSrc),
-            fileName: 'platform-form-container-columns-example',
-            component: 'PlatformFormContainerBasicExampleComponent'
-        }
+        getExampleFile('platform-form-basic/platform-form-basic-example.component.html'),
+        getExampleFile('platform-form-basic/platform-form-basic-example.component.ts', {
+            selector: 'platform-form-basic-example',
+            component: 'PlatformFormBasicExampleComponent'
+        })
     ];
 
     formContainerGroup: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformGFormContainerSrc),
-            fileName: 'platform-form-container-grouping-example',
-            component: 'PlatformFormContainerGroupExampleComponent'
-        }
+        getExampleFile('platform-form-group/platform-form-group-example.component.html'),
+        getExampleFile('platform-form-group/platform-form-group-example.component.ts', {
+            selector: 'platform-form-group-example',
+            component: 'PlatformFormGroupExampleComponent'
+        })
     ];
 
     formFieldColumnLayout: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformFormFieldLayoutSrc),
-            fileName: 'platform-field-column-change-example',
+        getExampleFile('platform-field-layout/platform-field-column-change-example.component.html'),
+        getExampleFile('platform-field-layout/platform-field-column-change-example.component.ts', {
+            selector: 'platform-form-column-change-example',
             component: 'PlatformFieldColumnChangeExampleComponent'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformFormFieldLayoutTsCode),
-            fileName: 'platform-field-column-change-example',
-            component: 'PlatformFieldColumnChangeExampleComponent'
-        }
+        })
     ];
 
     formFieldInlineLayout: ExampleFile[] = [
-        {
-            language: 'html',
-            code: getAssetFromModuleAssets(platformFormFieldInlineLayoutSrc),
-            fileName: 'platform-field-isinline-change-example',
+        getExampleFile('platform-field-layout/platform-field-isinline-change-example.component.html'),
+        getExampleFile('platform-field-layout/platform-field-isinline-change-example.component.ts', {
+            selector: 'platform-form-isinline-change-example',
             component: 'PlatformFieldIsInlineChangeExampleComponent'
-        },
-        {
-            language: 'typescript',
-            code: getAssetFromModuleAssets(platformFormFieldInlineLayoutTsCode),
-            fileName: 'platform-field-isinline-change-example',
-            component: 'PlatformFieldIsInlineChangeExampleComponent'
-        }
+        })
     ];
 }
