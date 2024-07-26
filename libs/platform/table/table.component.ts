8a2a5e363f53666ba102cc552e79156cff182579
mikerodonnell89@gmail.com <mikerodonnell89@gmail.com>
2024-07-26 14:27:45 -0400

    fix(platform): table freezing incorrect columns, and better explanation of how it works in the docs

--- libs/platform/table/table.component.ts
+++ libs/platform/table/table.component.ts
@@ -1089,7 +1089,6 @@ export class TableComponent<T = any>
 
         this._tableService.freezeTo(columnName, end);
         this.recalculateTableColumnWidth();
-        this._calculateLastFreezableColumn();
     }
 
     /** Unfreeze column */
