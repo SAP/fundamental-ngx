55890850f6e41fe55f1dd157d4616770ea8635a3
mikerodonnell89@gmail.com <mikerodonnell89@gmail.com>
2024-08-14 17:34:21 -0400

    fix(platform): add input for restricting table column min width

--- libs/docs/platform/table/examples/platform-table-default-example.component.ts
+++ libs/docs/platform/table/examples/platform-table-default-example.component.ts
@@ -121,8 +121,7 @@ const ITEMS: ExampleItem[] = [
     {
         id: 3,
         name: 'Astro Phone 6',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        description: 'penatibus et magnis',
         price: {
             value: 154.1,
             currency: 'IDR'
