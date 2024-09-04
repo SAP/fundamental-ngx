ff229b82a6d780567267fb6a4c399a57fbdfd165
Chandan Kumar Shaw <chandan.kumar.shaw@sap.com>
2024-09-05 01:34:37 +0530

    docs(core): update docs for year and month range date picker

--- libs/docs/core/date-picker/examples/date-picker-range-example.component.ts
+++ libs/docs/core/date-picker/examples/date-picker-range-example.component.ts
@@ -24,15 +24,30 @@ import {
         <br />
         <fd-date-picker
             [rangeHoverEffect]="true"
-            [showMonthYearDateRange]="true"
+            [dateRangeFormat]="'month'"
             [customDateTimeFormat]="monthYearDateFormat"
             type="range"
-            [(ngModel)]="monthYearFormatDateRange"
+            (selectedRangeDateChange)="handleMonthDateRangeChange($event)"
+            [(ngModel)]="customFormatSelectedRange"
         ></fd-date-picker>
         <br />
-        <div>Selected First Date: {{ monthYearFormatDates.start || 'null' }}</div>
+        <div>Selected First Date: {{ monthYearFormatDateRanges.start || 'null' }}</div>
         <br />
-        <div>Selected Last Date: {{ monthYearFormatDates.end || 'null' }}</div>`,
+        <div>Selected Last Date: {{ monthYearFormatDateRanges.end || 'null' }}</div>
+        <br />
+        <br />
+        <fd-date-picker
+            [rangeHoverEffect]="true"
+            [dateRangeFormat]="'year'"
+            [customDateTimeFormat]="yearDateFormat"
+            type="range"
+            (selectedRangeDateChange)="handleYearDateRangeChange($event)"
+            [(ngModel)]="customFormatSelectedRange"
+        ></fd-date-picker>
+        <br />
+        <div>Selected First Date: {{ yearFormatDateRanges.start || 'null' }}</div>
+        <br />
+        <div>Selected Last Date: {{ yearFormatDateRanges.end || 'null' }}</div>`,
     providers: [
         {
             provide: DatetimeAdapter,
@@ -48,17 +63,34 @@ import {
 })
 export class DatePickerRangeExampleComponent {
     selectedRange: Nullable<DateRange<FdDate>>;
-    monthYearFormatDateRange: Nullable<DateRange<FdDate>>;
+    customFormatSelectedRange: Nullable<DateRange<FdDate>>;
     monthYearDateFormat = { year: 'numeric', month: '2-digit' };
-    monthYearFormatDates = { start: '', end: '' };
+    yearDateFormat = { year: 'numeric' };
+    monthYearFormatDateRanges = { start: '', end: '' };
+    yearFormatDateRanges = { start: '', end: '' };
 
     constructor(private datetimeAdapter: DatetimeAdapter<FdDate>) {
         const today = this.datetimeAdapter.today();
         this.selectedRange = new DateRange(today, this.datetimeAdapter.addCalendarDays(today, 4));
-        this.monthYearFormatDateRange = new DateRange(today, this.datetimeAdapter.addCalendarMonths(today, 4));
-        this.monthYearFormatDates = {
-            start: datetimeAdapter.format(this.monthYearFormatDateRange.start, this.monthYearDateFormat),
-            end: datetimeAdapter.format(this.monthYearFormatDateRange.end, this.monthYearDateFormat)
+        this.customFormatSelectedRange = new DateRange(today, this.datetimeAdapter.addCalendarMonths(today, 16));
+        this.monthYearFormatDateRanges = {
+            start: datetimeAdapter.format(this.customFormatSelectedRange.start, this.monthYearDateFormat),
+            end: datetimeAdapter.format(this.customFormatSelectedRange.end, this.monthYearDateFormat)
         };
+
+        this.yearFormatDateRanges = {
+            start: datetimeAdapter.format(this.customFormatSelectedRange.start, this.yearDateFormat),
+            end: datetimeAdapter.format(this.customFormatSelectedRange.end, this.yearDateFormat)
+        };
+    }
+
+    handleMonthDateRangeChange(dates: DateRange<FdDate>) {
+        this.monthYearFormatDateRanges.start = this.datetimeAdapter.format(dates.start, this.monthYearDateFormat);
+        this.monthYearFormatDateRanges.end = this.datetimeAdapter.format(dates.end, this.monthYearDateFormat);
+    }
+
+    handleYearDateRangeChange(dates: DateRange<FdDate>) {
+        this.yearFormatDateRanges.start = this.datetimeAdapter.format(dates.start, this.yearDateFormat);
+        this.yearFormatDateRanges.end = this.datetimeAdapter.format(dates.end, this.yearDateFormat);
     }
 }
