215d350ee54212c9ce4611d8928847682cc11bd1
zsolt.deak <z.deak@sap.com>
2024-09-26 15:20:09 +0200

    fix(core): complete the subject when it's time

--- libs/docs/core/message-strip/examples/message-strip-auto-dismiss-example.component.ts
+++ libs/docs/core/message-strip/examples/message-strip-auto-dismiss-example.component.ts
@@ -1,10 +1,6 @@
-import { Component, inject, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonComponent } from '@fundamental-ngx/core/button';
-import {
-    AutoDismissMessageStripDirective,
-    MessageStripAlertService,
-    MessageStripComponent
-} from '@fundamental-ngx/core/message-strip';
+import { AutoDismissMessageStripDirective, MessageStripComponent } from '@fundamental-ngx/core/message-strip';
 
 @Component({
     selector: 'fd-message-strip-auto-dismiss-example',
@@ -19,37 +15,9 @@ import {
             Will be auto dismissed in 5 seconds, if mouse will not be over the message strip. If it's over it, then it
             will be dismissed in 5 seconds after mouse leaves.
         </fd-message-strip>
-        <button fd-button (click)="onClick()">Open message strip</button>
+        <button fd-button (click)="messageStripComponent.open()">Open message strip</button>
     `,
     standalone: true,
     imports: [MessageStripComponent, AutoDismissMessageStripDirective, ButtonComponent]
 })
-export class MessageStripAutoDismissExampleComponent implements OnDestroy {
-    private _msgStripService = inject(MessageStripAlertService);
-
-    ngOnDestroy(): void {
-        console.log('destroyed');
-    }
-
-    onClick() {
-        const ref = this._msgStripService.open({
-            position: 'bottom-middle',
-            content: 'Message from the future',
-            closeOnNavigation: true,
-            messageStrip: {
-                duration: 2000,
-                mousePersist: true,
-                type: 'error',
-                dismissible: true
-            }
-        });
-        ref.onDismiss$.subscribe({
-            next: () => {
-                console.log('dismissed alright ');
-            },
-            complete: () => {
-                console.log('completed dismiss');
-            }
-        });
-    }
-}
+export class MessageStripAutoDismissExampleComponent {}
