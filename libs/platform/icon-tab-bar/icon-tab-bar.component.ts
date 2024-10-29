6db8660dbf970c76b85a6246c1739e05b21b3928
droshev <mladen.droshev@sap.com>
2024-10-29 18:41:12 -0400

    fix(platform): icon-tab-bar _generateTabBarItems function improvement

--- libs/platform/icon-tab-bar/icon-tab-bar.component.ts
+++ libs/platform/icon-tab-bar/icon-tab-bar.component.ts
@@ -351,10 +351,11 @@ export class IconTabBarComponent implements OnInit, TabList {
         flatIndexRef = flatIndexRef || { value: 0 };
         return config.map((item, index) => {
             const uId = `${indexPrefix}${index}`;
+            item.color = item.color || 'default';
             const result: IconTabBarItem = {
                 ...item,
                 index,
-                cssClasses: [],
+                cssClasses: item.color ? [`fd-icon-tab-bar__item--${item.color}`] : [],
                 uId,
                 hidden: false,
                 parentUId,
@@ -366,9 +367,6 @@ export class IconTabBarComponent implements OnInit, TabList {
                     uId
                 )
             };
-            if (item.color) {
-                result.cssClasses = [`fd-icon-tab-bar__item--${item.color}`];
-            }
             return result;
         });
     }
