9853fd3b091e66601041da02ff2c79dede411fae
robertIsaac <dev.robert.isaac@gmail.com>
2024-12-07 21:39:54 +0200

    chore: wip

---
+++ apps/docs/eslint.config.js
@@ -0,0 +1,39 @@
+const nx = require('@nx/eslint-plugin');
+const baseConfig = require('../../eslint.config.js');
+const overrides = require('../../eslint.overrides');
+
+module.exports = [
+    ...baseConfig,
+    ...nx.configs['flat/angular'],
+    ...nx.configs['flat/angular-template'],
+    ...overrides,
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@angular-eslint/no-host-metadata-property': 'off',
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'fd',
+                    style: 'camelCase'
+                }
+            ],
+            '@angular-eslint/component-selector': [
+                'off',
+                {
+                    type: 'element',
+                    prefix: 'fd',
+                    style: 'kebab-case'
+                }
+            ],
+            'jsdoc/require-jsdoc': 'off',
+            'grouped-accessor-pairs': 'off'
+        }
+    },
+    {
+        files: ['**/*.html'],
+        // Override or add rules here
+        rules: {}
+    }
+];
