55bd5b1e719563b3aaf4c8583af946b78eefaef9
droshev <droshev@users.noreply.github.com>
2024-12-10 20:10:46 +0000

    Deploying to gh-pages from @ SAP/fundamental-ngx@295f59309def8a9ab70ba4e97464bcaf3d017d23 🚀

---
+++ docs/core/table/examples/no-data/no-data-example.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IconComponent } from '@fundamental-ngx/core/icon';
+import { IllustratedMessageModule } from '@fundamental-ngx/core/illustrated-message';
+import { LinkComponent } from '@fundamental-ngx/core/link';
+import { TableModule } from '@fundamental-ngx/core/table';
+
+@Component({
+    selector: 'fdp-platform-table-no-data-example',
+    templateUrl: './no-data-example.component.html',
+    standalone: true,
+    imports: [TableModule, LinkComponent, IconComponent, IllustratedMessageModule]
+})
+export class NoDataExampleComponent {
+    tableRows = [];
+
+    sceneConfig = {
+        scene: {
+            url: 'assets/images/sapIllus-Scene-NoMail.svg',
+            id: 'sapIllus-Scene-NoMail-1'
+        },
+        dialog: {
+            url: 'assets/images/sapIllus-Dialog-NoMail.svg',
+            id: 'sapIllus-Dialog-NoMail'
+        }
+    };
+}
