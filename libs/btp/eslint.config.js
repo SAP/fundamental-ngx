3c12c740090665edc7378c93f4d1471aa548c2ba
robertIsaac <dev.robert.isaac@gmail.com>
2024-12-19 21:35:24 +0200

    chore: fix lint error in btp

--- libs/btp/eslint.config.js
+++ libs/btp/eslint.config.js
@@ -2,33 +2,38 @@ const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
 
 module.exports = [
-  ...baseConfig,
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+    ...baseConfig,
+    ...nx.configs['flat/angular'],
+    ...nx.configs['flat/angular-template'],
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'fdb',
+                    style: 'camelCase'
+                }
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    type: 'element',
+                    prefix: 'fdb',
+                    style: 'kebab-case'
+                }
+            ]
+        }
     },
-  },
-  {
-    files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
-  },
+    {
+        files: ['**/*.html'],
+        // Override or add rules here
+        rules: {
+            '@angular-eslint/no-host-metadata-property': 'off',
+            '@angular-eslint/template/elements-content': 'off',
+            '@angular-eslint/template/interactive-supports-focus': 'off',
+            '@angular-eslint/template/click-events-have-key-events': 'off'
+        }
+    }
 ];
