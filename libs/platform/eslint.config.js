12295fd4fadc65ab14e9db062cc9194e54363e39
robertIsaac <dev.robert.isaac@gmail.com>
2024-12-19 23:08:42 +0200

    chore: fix lint error in platform

--- libs/platform/eslint.config.js
+++ libs/platform/eslint.config.js
@@ -1,34 +1,43 @@
 const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
+const overrides = require('../../eslint.overrides');
 
 module.exports = [
-  ...baseConfig,
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+    ...baseConfig,
+    ...nx.configs['flat/angular'],
+    ...nx.configs['flat/angular-template'],
+    ...overrides,
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'fdp',
+                    style: 'camelCase'
+                }
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    type: 'element',
+                    prefix: 'fdp',
+                    style: 'kebab-case'
+                }
+            ]
+        }
     },
-  },
-  {
-    files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
-  },
+    {
+        files: ['**/*.html'],
+        // Override or add rules here
+        rules: {
+            '@angular-eslint/template/label-has-associated-control': 'off',
+            '@angular-eslint/template/elements-content': 'off',
+            '@angular-eslint/template/interactive-supports-focus': 'off',
+            '@angular-eslint/template/click-events-have-key-events': 'off',
+            '@angular-eslint/template/valid-aria': 'off',
+            '@angular-eslint/template/role-has-required-aria': 'off'
+        }
+    }
 ];
