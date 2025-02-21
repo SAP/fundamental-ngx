5b2f2d2f258e9ebd3cf5d008d2ad83b3fbf6508a
mikerodonnell89@gmail.com <mikerodonnell89@gmail.com>
2025-02-21 12:04:55 -0500

    fix(cx): nested link component import bug

--- libs/cx/nested-list/nested-link/nested-link.component.ts
+++ libs/cx/nested-list/nested-link/nested-link.component.ts
@@ -1,3 +1,4 @@
+import { NgTemplateOutlet } from '@angular/common';
 import {
     ChangeDetectorRef,
     Component,
@@ -37,7 +38,7 @@ import {
             <ng-content select="[fdx-nested-list-expand-icon]"></ng-content>
         </ng-template>
     `,
-    imports: [],
+    imports: [NgTemplateOutlet],
     standalone: true,
     selector:
         // eslint-disable-next-line @angular-eslint/component-selector
