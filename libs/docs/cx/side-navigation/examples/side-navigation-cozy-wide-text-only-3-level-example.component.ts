ff298066cd96a26cc5be5d347531db7f3aad06bf
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2025-03-20 16:13:34 -0400

    fix(cx): update the CX Side nav with latest fund-styles changes (#13130)

--- libs/docs/cx/side-navigation/examples/side-navigation-cozy-wide-text-only-3-level-example.component.ts
+++ libs/docs/cx/side-navigation/examples/side-navigation-cozy-wide-text-only-3-level-example.component.ts
@@ -1,11 +1,44 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CxNestedListModule } from '@fundamental-ngx/cx/nested-list';
-import { CxSideNavigationModule } from '@fundamental-ngx/cx/side-navigation';
+import {
+    NestedItemComponent,
+    NestedLinkComponent,
+    NestedListButtonDirective,
+    NestedListComponent,
+    NestedListContentDirective,
+    NestedListExpandIconComponent,
+    NestedListHeaderDirective,
+    NestedListIconComponent,
+    NestedListPopoverComponent,
+    NestedListTitleDirective,
+    PreparedNestedListComponent
+} from '@fundamental-ngx/cx/nested-list';
+import {
+    SideNavigationButtonDirective,
+    SideNavigationComponent,
+    SideNavigationMainComponent,
+    SideNavigationUtilityDirective
+} from '@fundamental-ngx/cx/side-navigation';
 
 @Component({
     selector: 'fdx-side-navigation-cozy-wide-text-only-3-level-example',
     templateUrl: './side-navigation-cozy-wide-text-only-3-level-example.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CxSideNavigationModule, CxNestedListModule]
+    imports: [
+        NestedItemComponent,
+        NestedLinkComponent,
+        NestedListButtonDirective,
+        NestedListComponent,
+        NestedListContentDirective,
+        NestedListExpandIconComponent,
+        NestedListHeaderDirective,
+        NestedListIconComponent,
+        NestedListPopoverComponent,
+        NestedListTitleDirective,
+        PreparedNestedListComponent,
+        SideNavigationButtonDirective,
+        SideNavigationComponent,
+        SideNavigationMainComponent,
+        SideNavigationUtilityDirective
+    ]
 })
 export class SideNavigationCozyWideTextOnly3LevelExampleComponent {}
