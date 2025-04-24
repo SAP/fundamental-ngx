b38c55257b9f58037c6773fae891a662305b56e5
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2025-04-24 12:58:10 -0400

    fix(core,cx): vertical and side nav announcing selection incorrectly (#13212)

--- libs/cx/src/lib/nested-list/nested-link/nested-link.component.ts
+++ libs/cx/src/lib/nested-list/nested-link/nested-link.component.ts
@@ -44,6 +44,7 @@ export class NestedLinkComponent {
 
     /** Whether this element is selected*/
     @Input()
+    @HostBinding('attr.aria-selected')
     @HostBinding('class.is-selected')
     selected = false;
 
