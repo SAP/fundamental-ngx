0e673a3970a899ce90c3d159b6ea0919f8a8f5be
Inna Atanasova <i.atanasova@sap.com>
2025-05-12 16:25:04 -0400

    feat(core): add option to customize Facets content

--- libs/core/facets/index.ts
+++ libs/core/facets/index.ts
@@ -1,6 +1,8 @@
 export * from './constants';
 export * from './content/facet-content.component';
+export * from './facet-container.component';
 export * from './facet-group.component';
 export * from './facet.module';
 export * from './facet/facet.component';
+
 export * from './utils';
