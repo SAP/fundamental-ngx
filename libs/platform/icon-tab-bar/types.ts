9ec2a40e88cb0b37fc44cea0b07f7be8c60c2e3b
Anton Schuster <anton.schuster@sybit.de>
2025-05-22 08:52:53 +0200

    fix(platform): rename TabDestinyMode to TabDensityMode for consistency

--- libs/platform/icon-tab-bar/types.ts
+++ libs/platform/icon-tab-bar/types.ts
@@ -1,5 +1,5 @@
 export type TabType = 'text' | 'icon' | 'icon-only' | 'filter' | 'process';
-export type TabDestinyMode = 'cozy' | 'compact' | 'condensed' | 'inherit';
+export type TabDensityMode = 'cozy' | 'compact' | 'condensed' | 'inherit';
 export type SemanticColor = 'negative' | 'critical' | 'positive' | 'informative' | 'default';
 export type IconTabBarBackground = 'solid' | 'translucent' | 'transparent';
 export type IconTabBarSize = 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'responsive-paddings';
