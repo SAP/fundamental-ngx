be6cc7956ee67f111179815fc1061a39923d4f8e
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2025-06-17 16:45:27 -0400

    fix(core): update the Illustrated messages (#13353)

--- libs/docs/core/illustrated-message/examples/illustrated-message-dot-example.component.ts
+++ libs/docs/core/illustrated-message/examples/illustrated-message-dot-example.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '@fundamental-ngx/core/button';
 import { CardModule } from '@fundamental-ngx/core/card';
 import { IllustratedMessageModule, SvgConfig } from '@fundamental-ngx/core/illustrated-message';
 
 @Component({
     selector: 'fd-illustrated-message-dot-example',
     templateUrl: './illustrated-message-dot-example.component.html',
-    imports: [CardModule, IllustratedMessageModule, ButtonComponent]
+    imports: [CardModule, IllustratedMessageModule]
 })
 export class IllustratedMessageDotExampleComponent {
-    spotConfig: SvgConfig = {
-        dot: { url: 'assets/images/sapIllus-Spot-NoMail.svg', id: 'sapIllus-Spot-NoEmail' }
+    xsmallConfig: SvgConfig = {
+        xsmall: {
+            url: 'assets/images/sapIllus-Ice-Cream-Demo-ExtraSmall.svg',
+            id: 'sapIllus-Ice-Cream-Demo-ExtraSmall'
+        }
     };
 }
