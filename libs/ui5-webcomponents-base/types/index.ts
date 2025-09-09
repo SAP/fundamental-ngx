b2521d3252ef44c832e8bf8f07efb2dbd190f01c
droshev <mladen.droshev@sap.com>
2025-09-08 22:08:02 -0400

    fix: add types generation

--- libs/ui5-webcomponents-base/types/index.ts
+++ libs/ui5-webcomponents-base/types/index.ts
@@ -1,3 +1,7 @@
-export type CalendarType = 'Gregorian' | 'Islamic' | 'Japanese' | 'Buddhist' | 'Persian' | 'Indian' | undefined;
-export type ValueState = 'None' | 'Success' | 'Warning' | 'Error' | 'Information' | undefined;
-export type SortOrder = 'Ascending' | 'Descending' | undefined;
+export type AnimationMode = 'Full' | 'Basic' | 'Minimal' | 'None' | undefined;
+export type CalendarType = 'Gregorian' | 'Islamic' | 'Japanese' | 'Buddhist' | 'Persian' | undefined;
+export type ItemNavigationBehavior = 'Static' | 'Cyclic' | undefined;
+export type MovePlacement = 'On' | 'Before' | 'After' | undefined;
+export type NavigationMode = 'Auto' | 'Vertical' | 'Horizontal' | 'Paging' | undefined;
+export type SortOrder = 'None' | 'Ascending' | 'Descending' | undefined;
+export type ValueState = 'None' | 'Positive' | 'Critical' | 'Negative' | 'Information' | undefined;
