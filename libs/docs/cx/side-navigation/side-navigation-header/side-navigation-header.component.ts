121c9879a3aa06105171a0baef55d23e76afc32f
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2025-11-26 09:55:05 -0500

    docs(cx, core): add deprecation message for Side nav components in Core and CX documentations (#13610)

--- libs/docs/cx/side-navigation/side-navigation-header/side-navigation-header.component.ts
+++ libs/docs/cx/side-navigation/side-navigation-header/side-navigation-header.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LinkComponent } from '@fundamental-ngx/core/link';
+import { MessageStripComponent } from '@fundamental-ngx/core/message-strip';
 import {
     DescriptionComponent,
     DocPageComponent,
@@ -11,6 +13,14 @@ import {
     selector: 'app-side-navigation-header',
     templateUrl: './side-navigation-header.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [DocPageComponent, HeaderComponent, DescriptionComponent, ImportComponent, HeaderTabsComponent]
+    imports: [
+        DocPageComponent,
+        HeaderComponent,
+        DescriptionComponent,
+        ImportComponent,
+        HeaderTabsComponent,
+        MessageStripComponent,
+        LinkComponent
+    ]
 })
 export class SideNavigationHeaderComponent {}
