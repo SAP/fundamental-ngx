4c218bed52a0c4110bbdec97c8edc6a62113eb8c
droshev <mladen.droshev@sap.com>
2025-11-28 09:59:40 -0500

    chore: fix stackblitz versions

--- apps/docs/src/app.config.ts
+++ apps/docs/src/app.config.ts
@@ -5,12 +5,14 @@ import { PreloadAllModules, provideRouter, withHashLocation, withPreloading } fr
 import { provideContentDensity } from '@fundamental-ngx/core/content-density';
 import { provideDialogService } from '@fundamental-ngx/core/dialog';
 import { provideTheming, themingInitializer } from '@fundamental-ngx/core/theming';
-import { DocsService, PACKAGE_JSON, Translations } from '@fundamental-ngx/docs/shared';
+import { CORE_PACKAGE_JSON, DocsService, PACKAGE_JSON, Translations } from '@fundamental-ngx/docs/shared';
 import { FD_LANGUAGE, FD_LANGUAGE_ENGLISH } from '@fundamental-ngx/i18n';
 import { MarkdownModule } from 'ngx-markdown';
 import { BehaviorSubject } from 'rxjs';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import packageJson from '../../../package.json';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import corePackageJson from '../../../libs/core/package.json';
 import { ROUTES as applicationRoutes } from './environments/app.routes';
 import { translations } from './environments/translations';
 
@@ -27,6 +29,10 @@ export const appConfig: ApplicationConfig = {
         {
             provide: PACKAGE_JSON,
             useValue: packageJson
+        },
+        {
+            provide: CORE_PACKAGE_JSON,
+            useValue: corePackageJson
         },
         {
             provide: FD_LANGUAGE,
