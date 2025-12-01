981e13f4bd9683d353f11c876bcffdf2c8c2881a
Robert Isaac <robert.isaac@sap.com>
2025-12-01 17:46:45 +0200

    test: fix structuredClone in jest

--- libs/platform/test-setup.ts
+++ libs/platform/test-setup.ts
@@ -1,6 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
+globalThis.structuredClone = (val) => {
+    try {
+        return JSON.parse(JSON.stringify(val));
+    } catch (e) {
+        return val as any;
+    }
+};
+
+console.warn = () => undefined;
+
 Object.defineProperty(globalThis, 'ResizeObserver', {
     writable: true,
     value: class {
