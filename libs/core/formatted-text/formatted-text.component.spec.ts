b841de91a630669077161fdefabc0c36a37e7486
MariaIDineva <maria.dineva@sap.com>
2026-01-06 14:59:46 +0200

    refactor(core): use angular 21 signals for FormattedTextComponent

--- libs/core/formatted-text/formatted-text.component.spec.ts
+++ libs/core/formatted-text/formatted-text.component.spec.ts
@@ -23,16 +23,16 @@ describe('FormattedTextComponent', () => {
     });
 
     it('should add height class and style height', () => {
-        component.htmlText = 'Test embed code';
-        component.height = '100px';
+        fixture.componentRef.setInput('htmlText', 'Test embed code');
+        fixture.componentRef.setInput('height', '100px');
         fixture.detectChanges();
         expect(fixture.nativeElement.classList.contains('fd-formatted-text-with-height')).toBeTruthy();
         expect(fixture.nativeElement.style.height).toEqual('100px');
     });
 
     it('should add width class and style width', () => {
-        component.htmlText = 'Test embed code';
-        component.width = '100px';
+        fixture.componentRef.setInput('htmlText', 'Test embed code');
+        fixture.componentRef.setInput('width', '100px');
         fixture.detectChanges();
         expect(fixture.nativeElement.classList.contains('fd-formatted-text-with-width')).toBeTruthy();
         expect(fixture.nativeElement.style.width).toEqual('100px');
@@ -69,7 +69,7 @@ describe('FormattedTextComponent', () => {
         <li>Tag text</li>
         </ol>`.replace(/\n|\r/gi, '');
 
-        component.htmlText = embedCode;
+        fixture.componentRef.setInput('htmlText', embedCode);
         fixture.detectChanges();
         expect(fixture.nativeElement.innerHTML).toEqual(embedCode);
     });
@@ -77,14 +77,14 @@ describe('FormattedTextComponent', () => {
     it('should skip unsupported tags', () => {
         const embedCode = `<p>Sample text</p><script>alert(1);</script><form>Sample form</form>`;
 
-        component.htmlText = embedCode;
+        fixture.componentRef.setInput('htmlText', embedCode);
         fixture.detectChanges();
         expect(fixture.nativeElement.innerHTML).toEqual('<p>Sample text</p>');
     });
 
     it('should skip unsupported attributes on tag', () => {
         const embedCode = '<p class="test" style="color: red" href="http://www.sap.com">Sample text</p>';
-        component.htmlText = embedCode;
+        fixture.componentRef.setInput('htmlText', embedCode);
         fixture.detectChanges();
         expect(fixture.nativeElement.innerHTML).toEqual('<p class="test" style="color: red">Sample text</p>');
     });
