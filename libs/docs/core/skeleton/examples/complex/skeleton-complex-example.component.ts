55bdfbf5792f5ca441ca576b26beb9013eae6e4f
deno <mladen.droshev@sap.com>
2026-01-09 14:56:10 -0500

    fix(core): signalify skeleton and update jsdoc (#13699)

--- libs/docs/core/skeleton/examples/complex/skeleton-complex-example.component.ts
+++ libs/docs/core/skeleton/examples/complex/skeleton-complex-example.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { SkeletonModule } from '@fundamental-ngx/core/skeleton';
+import { SkeletonComponent } from '@fundamental-ngx/core/skeleton';
 
 @Component({
     selector: 'fd-skeleton-complex-example',
     templateUrl: './skeleton-complex-example.component.html',
-    imports: [SkeletonModule]
+    imports: [SkeletonComponent]
 })
 export class SkeletonComplexExampleComponent {}
