502c818145ef85dedb400179c9866c4bc7a8af23
MariaIDineva <maria.dineva@sap.com>
2026-01-14 16:13:47 +0200

    refactor(core): make BusyIndicatorExtendedDirective use signals, deprecate BusyIndicatorModule

--- libs/core/busy-indicator/busy-indicator.module.ts
+++ libs/core/busy-indicator/busy-indicator.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BusyIndicatorExtendedDirective } from './busy-indicator-extended/busy-indicator-extended.directive';
 import { BusyIndicatorComponent } from './busy-indicator.component';
 
+/**
+ * @deprecated
+ * Use direct import of `IconComponent`
+ */
 @NgModule({
     exports: [BusyIndicatorComponent, BusyIndicatorExtendedDirective],
     imports: [BusyIndicatorComponent, BusyIndicatorExtendedDirective]
