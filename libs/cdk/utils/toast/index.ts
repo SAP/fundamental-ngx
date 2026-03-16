79d7bbcdd721845dc392f847e6fcc1a7658bbb28
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2026-03-16 16:46:48 -0400

    fix(cdk, core): migrate Toast infrastructure to signals and Web   Animations API (#13965)
    BREAKING CHANGE: removed BaseToastActionDismissibleRef, BaseToastOverlayContainer, BaseAnimatedToastConfig, baseToastAnimations

--- libs/cdk/utils/toast/index.ts
+++ libs/cdk/utils/toast/index.ts
@@ -1,4 +1,3 @@
-export * from './base-toast-animations';
 export * from './base-toast-positions';
 export * from './base-toast.service';
 
@@ -6,10 +5,8 @@ export * from './interfaces/toast-container-component.interface';
 export * from './interfaces/toast-duration-dismissible-container-component.interface';
 export * from './interfaces/toast-text-component.interface';
 
-export * from './classes/action-dismissible/base-toast-action-dismissible-ref';
 export * from './classes/base-toast-config';
 export * from './classes/base-toast-container.component';
-export * from './classes/base-toast-overlay-container';
 export * from './classes/base-toast-ref';
 export * from './classes/duration-dismissible/base-toast-duration-dismissible-config';
 export * from './classes/duration-dismissible/base-toast-duration-dismissible-container.component';
