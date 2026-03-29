fd65213d9b74993e6755c8563cbe8568d9b5fd13
droshev <mladen.droshev@sap.com>
2026-03-29 16:00:20 -0400

    test(core): add tests for new InlineHelp popover inputs

--- libs/core/inline-help/inline-help.directive.spec.ts
+++ libs/core/inline-help/inline-help.directive.spec.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { PopoverService } from '@fundamental-ngx/core/popover';
+import { InlineHelpDirective } from './inline-help.directive';
 import { InlineHelpModule } from './inline-help.module';
 
 @Component({
@@ -9,27 +11,56 @@ import { InlineHelpModule } from './inline-help.module';
             <div #directiveElement fd-inline-help="123"></div>
         }
     `,
-    standalone: true,
     imports: [InlineHelpModule]
 })
-class TestComponent {
+class InlineHelpDefaultTestComponent {
     @ViewChild('directiveElement', { static: false, read: ElementRef })
     ref: ElementRef<HTMLDivElement>;
 
     visible = true;
 }
+
+@Component({
+    template: `
+        <div
+            #directiveElement
+            fd-inline-help="Test content"
+            [noArrow]="noArrow"
+            [closeOnEscapeKey]="closeOnEscapeKey"
+            [closeOnNavigation]="closeOnNavigation"
+            [restoreFocusOnClose]="restoreFocusOnClose"
+            [fixedPosition]="fixedPosition"
+            [maxWidth]="maxWidth"
+            [appendTo]="appendTo"
+        ></div>
+    `,
+    imports: [InlineHelpModule]
+})
+class InlineHelpInputsTestComponent {
+    @ViewChild('directiveElement', { static: false, read: ElementRef })
+    ref: ElementRef<HTMLDivElement>;
+
+    noArrow = false;
+    closeOnEscapeKey = false;
+    closeOnNavigation = true;
+    restoreFocusOnClose = true;
+    fixedPosition = false;
+    maxWidth: number | null = null;
+    appendTo: Element | null = null;
+}
+
 describe('InlineHelpDirective', () => {
-    let component: TestComponent;
-    let fixture: ComponentFixture<TestComponent>;
+    let component: InlineHelpDefaultTestComponent;
+    let fixture: ComponentFixture<InlineHelpDefaultTestComponent>;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            imports: [TestComponent]
+            imports: [InlineHelpDefaultTestComponent]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(TestComponent);
+        fixture = TestBed.createComponent(InlineHelpDefaultTestComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -48,24 +79,26 @@ describe('InlineHelpDirective', () => {
         expect(document.body.querySelector(selector)).toBeFalsy();
     }));
 
-    it('should hide the inline help if host element is destroyed', () => {
+    it('should hide the inline help if host element is destroyed', fakeAsync(() => {
         const selector = '.fd-popover__body.fd-inline-help__content';
         expect(document.body.querySelector(selector)).toBeFalsy();
         component.ref.nativeElement.dispatchEvent(new Event('mouseenter'));
         expect(document.body.querySelector(selector)).toBeTruthy();
         component.visible = false;
         fixture.detectChanges();
+        tick(50);
         expect(document.body.querySelector(selector)).toBeFalsy();
-    });
+    }));
 
-    it('should show the inline help on focus', () => {
+    it('should show the inline help on focus', fakeAsync(() => {
         const selector = '.fd-popover__body.fd-inline-help__content';
         expect(document.body.querySelector(selector)).toBeFalsy();
         component.ref.nativeElement.dispatchEvent(new Event('focusin'));
         expect(document.body.querySelector(selector)).toBeTruthy();
         component.ref.nativeElement.dispatchEvent(new Event('focusout'));
+        tick(50);
         expect(document.body.querySelector(selector)).toBeFalsy();
-    });
+    }));
 
     it('should have correct trigger class applied', () => {
         expect(component.ref.nativeElement.classList.contains('fd-inline-help__trigger')).toBe(true);
@@ -78,4 +111,77 @@ describe('InlineHelpDirective', () => {
         component.ref.nativeElement.dispatchEvent(new Event('mouseleave'));
         tick(50);
     }));
+});
+
+describe('InlineHelpDirective popover inputs', () => {
+    let fixture: ComponentFixture<InlineHelpInputsTestComponent>;
+    let component: InlineHelpInputsTestComponent;
+    let popoverService: PopoverService;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [InlineHelpInputsTestComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(InlineHelpInputsTestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        const directiveDebugEl = fixture.debugElement.query(By.directive(InlineHelpDirective));
+        popoverService = directiveDebugEl.injector.get(PopoverService);
+    });
+
+    it('should pass noArrow to popover service', () => {
+        expect(popoverService.noArrow()).toBe(false);
+        component.noArrow = true;
+        fixture.detectChanges();
+        expect(popoverService.noArrow()).toBe(true);
+    });
+
+    it('should pass closeOnEscapeKey to popover service', () => {
+        expect(popoverService.closeOnEscapeKey()).toBe(false);
+        component.closeOnEscapeKey = true;
+        fixture.detectChanges();
+        expect(popoverService.closeOnEscapeKey()).toBe(true);
+    });
+
+    it('should pass closeOnNavigation to popover service', () => {
+        expect(popoverService.closeOnNavigation()).toBe(true);
+        component.closeOnNavigation = false;
+        fixture.detectChanges();
+        expect(popoverService.closeOnNavigation()).toBe(false);
+    });
+
+    it('should pass restoreFocusOnClose to popover service', () => {
+        expect(popoverService.restoreFocusOnClose()).toBe(true);
+        component.restoreFocusOnClose = false;
+        fixture.detectChanges();
+        expect(popoverService.restoreFocusOnClose()).toBe(false);
+    });
+
+    it('should pass fixedPosition to popover service', () => {
+        expect(popoverService.fixedPosition()).toBe(false);
+        component.fixedPosition = true;
+        fixture.detectChanges();
+        expect(popoverService.fixedPosition()).toBe(true);
+    });
+
+    it('should pass maxWidth to popover service', () => {
+        expect(popoverService.maxWidth()).toBeNull();
+        component.maxWidth = 300;
+        fixture.detectChanges();
+        expect(popoverService.maxWidth()).toBe(300);
+    });
+
+    it('should pass appendTo to popover service', () => {
+        expect(popoverService.appendTo()).toBeNull();
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+        component.appendTo = container;
+        fixture.detectChanges();
+        expect(popoverService.appendTo()).toBe(container);
+        document.body.removeChild(container);
+    });
 });
