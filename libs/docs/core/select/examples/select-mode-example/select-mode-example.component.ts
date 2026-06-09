5b9bf57bce9ef6fecdf1afcfbae5d003528eeb8e
MariaIDineva <maria.dineva@sap.com>
2026-06-09 09:39:15 +0300

    docs: revert updated example

--- libs/docs/core/select/examples/select-mode-example/select-mode-example.component.ts
+++ libs/docs/core/select/examples/select-mode-example/select-mode-example.component.ts
@@ -1,19 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { ButtonComponent } from '@fundamental-ngx/core/button';
+import { Component } from '@angular/core';
 import { ContentDensityDirective } from '@fundamental-ngx/core/content-density';
-import { AutoDismissMessageStripDirective, MessageStripComponent } from '@fundamental-ngx/core/message-strip';
 import { SelectModule } from '@fundamental-ngx/core/select';
 
 @Component({
     selector: 'fd-select-mode-example',
     templateUrl: './select-mode-example.component.html',
-    imports: [
-        SelectModule,
-        ContentDensityDirective,
-        MessageStripComponent,
-        AutoDismissMessageStripDirective,
-        ButtonComponent
-    ]
+    imports: [SelectModule, ContentDensityDirective]
 })
 export class SelectModeExampleComponent {
     options: string[] = ['Apple', 'Pineapple', 'Tomato', 'Strawberry'];
@@ -22,11 +14,4 @@ export class SelectModeExampleComponent {
     selectedValue3: string;
     selectedValue4 = this.options[2];
     selectedValue5 = this.options[0];
-
-    readonly showBanner = signal(false);
-
-    addBanner(): void {
-        this.showBanner.set(true);
-        setTimeout(() => this.showBanner.set(false), 5000);
-    }
 }
