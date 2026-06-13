9f1aa301649406e0f4877f1ac22284c80abfa54d
droshev <mladen.droshev@sap.com>
2026-06-13 07:07:31 -0400

    docs(ui5): update timeline and view settings

--- libs/docs/ui5-webcomponents-fiori/view-settings-dialog/view-settings-dialog-docs.ts
+++ libs/docs/ui5-webcomponents-fiori/view-settings-dialog/view-settings-dialog-docs.ts
@@ -5,12 +5,16 @@ import {
     DescriptionComponent,
     DocsSectionTitleComponent,
     ExampleFile,
+    SeparatorComponent,
     getAssetFromModuleAssets
 } from '@fundamental-ngx/docs/shared';
 import { BasicSample } from './examples/basic-sample';
+import { CustomTabsSample } from './examples/custom-tabs-sample';
 
 const basicSampleTs = 'basic-sample.ts';
 const basicSampleHtml = 'basic-sample.html';
+const customTabsSampleTs = 'custom-tabs-sample.ts';
+const customTabsSampleHtml = 'custom-tabs-sample.html';
 
 @Component({
     selector: 'ui5-view-settings-dialog-docs',
@@ -21,7 +25,9 @@ const basicSampleHtml = 'basic-sample.html';
         ComponentExampleComponent,
         DescriptionComponent,
         DocsSectionTitleComponent,
-        BasicSample
+        SeparatorComponent,
+        BasicSample,
+        CustomTabsSample
     ]
 })
 export class ViewSettingsDialogDocs {
@@ -37,6 +43,21 @@ export class ViewSettingsDialogDocs {
             fileName: 'basic-sample',
             code: getAssetFromModuleAssets(basicSampleHtml),
             scssFileCode: getAssetFromModuleAssets(basicSampleHtml)
+        }
+    ];
+
+    customTabsExample: ExampleFile[] = [
+        {
+            language: 'typescript',
+            fileName: 'custom-tabs-sample',
+            code: getAssetFromModuleAssets(customTabsSampleTs),
+            component: 'CustomTabsSample'
+        },
+        {
+            language: 'html',
+            fileName: 'custom-tabs-sample',
+            code: getAssetFromModuleAssets(customTabsSampleHtml),
+            scssFileCode: getAssetFromModuleAssets(customTabsSampleHtml)
         }
     ];
 }
