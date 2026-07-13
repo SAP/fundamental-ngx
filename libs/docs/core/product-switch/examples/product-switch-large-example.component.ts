34c34fc30eb86c45287b3deb3b118efe1c6bf18c
MariaIDineva <maria.dineva@sap.com>
2026-07-13 14:08:43 +0300

    docs: update product switch docs, remove redundant styles

---
+++ libs/docs/core/product-switch/examples/product-switch-large-example.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { ProductSwitchItem, ProductSwitchModule } from '@fundamental-ngx/core/product-switch';
+
+@Component({
+    selector: 'fd-product-switch-large-example',
+    templateUrl: './product-switch-large-example.component.html',
+    imports: [ProductSwitchModule]
+})
+export class ProductSwitchLargeExampleComponent {
+    list: ProductSwitchItem[] = [
+        {
+            title: 'Home',
+            subtitle: 'Central Home',
+            icon: 'home',
+            stickToPosition: true,
+            disabledDragAndDrop: true
+        },
+        {
+            title: 'Analytics Cloud',
+            subtitle: 'Analytics Cloud',
+            icon: 'business-objects-experience'
+        },
+        {
+            title: 'Catalog',
+            subtitle: 'Ariba',
+            icon: 'contacts'
+        },
+        {
+            title: 'Guided Buying',
+            icon: 'credit-card'
+        },
+        {
+            title: 'Strategic Procurement',
+            icon: 'cart-3'
+        },
+        {
+            title: 'Travel & Expense',
+            subtitle: 'Concur',
+            icon: 'travel-expense'
+        },
+        {
+            title: 'Vendor Management',
+            subtitle: 'Fieldglass',
+            icon: 'shipping-status'
+        },
+        {
+            title: 'Human Capital Management',
+            icon: 'customer'
+        },
+        {
+            title: 'Sales Cloud',
+            subtitle: 'Sales Cloud',
+            icon: 'sales-notification'
+        },
+        {
+            title: 'Commerce Cloud',
+            subtitle: 'Commerce Cloud',
+            icon: 'retail-store'
+        },
+        {
+            title: 'Marketing Cloud',
+            subtitle: 'Marketing Cloud',
+            icon: 'marketing-campaign'
+        },
+        {
+            title: 'Service Cloud',
+            icon: 'family-care'
+        },
+        {
+            title: 'Customer Data Cloud',
+            icon: 'customer-and-contacts'
+        },
+        {
+            title: 'S/4HANA',
+            icon: 'batch-payments'
+        }
+    ];
+
+    productChangeHandle(products: ProductSwitchItem[]): void {
+        this.list = products;
+    }
+}
